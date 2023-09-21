a81eb0428f26b5d36e32285a7f68b8eb6bd59280
Marius Glittum <marius.glittum@nav.no>
2023-09-21 12:57:15 +0200

    Flytter vurderingene

---
+++ src/main/kotlin/no/nav/aap/avklaringsbehov/sykdom/SykdomsVurderinger.kt
@@ -0,0 +1,24 @@
+package no.nav.aap.avklaringsbehov.sykdom
+
+import no.nav.aap.domene.behandling.dokumenter.JournalpostId
+import java.time.LocalDate
+
+data class Sykdomsvurdering(
+    val begrunnelse: String,
+    val dokumenterBruktIVurdering: List<JournalpostId>,
+    val erSkadeSykdomEllerLyteVesentligdel: Boolean,
+    val erNedsettelseIArbeidsevneHøyereEnnNedreGrense: Boolean?,
+    val nedreGrense: NedreGrense?,
+    val skadetidspunkt: LocalDate?
+)
+
+enum class NedreGrense {
+    TRETTI, FEMTI
+}
+
+data class Yrkesskadevurdering(
+    val begrunnelse: String,
+    val dokumenterBruktIVurdering: List<JournalpostId>,
+    val erÅrsakssammenheng: Boolean,
+    val skadetidspunkt: LocalDate?
+)
--- src/main/kotlin/no/nav/aap/flate/behandling/SykdomsGrunnlagDto.kt
+++ src/main/kotlin/no/nav/aap/flate/behandling/SykdomsGrunnlagDto.kt
@@ -1,33 +1,13 @@
 package no.nav.aap.flate.behandling
 
+import no.nav.aap.avklaringsbehov.sykdom.Sykdomsvurdering
+import no.nav.aap.avklaringsbehov.sykdom.Yrkesskadevurdering
 import no.nav.aap.domene.Periode
-import no.nav.aap.domene.behandling.dokumenter.JournalpostId
-import java.time.LocalDate
 
 data class SykdomsGrunnlagDto(
     val opplysninger: InnhentetSykdomsOpplysninger,
     val yrkesskadevurdering: Yrkesskadevurdering?,
     val sykdomsvurdering: Sykdomsvurdering?
-)
-
-data class Sykdomsvurdering(
-    val begrunnelse: String,
-    val dokumenterBruktIVurdering: List<JournalpostId>,
-    val erSkadeSykdomEllerLyteVesentligdel: Boolean,
-    val erNedsettelseIArbeidsevneHøyereEnnNedreGrense: Boolean?,
-    val nedreGrense: NedreGrense?,
-    val skadetidspunkt: LocalDate?
-)
-
-enum class NedreGrense {
-    TRETTI, FEMTI
-}
-
-data class Yrkesskadevurdering(
-    val begrunnelse: String,
-    val dokumenterBruktIVurdering: List<JournalpostId>,
-    val erÅrsakssammenheng: Boolean,
-    val skadetidspunkt: LocalDate?
 )
 
 data class InnhentetSykdomsOpplysninger(
