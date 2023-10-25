56a0d416c076976b1fbb356835d2622133811779
Marius Glittum <marius.glittum@nav.no>
2023-10-25 18:58:18 +0200

    La til noen tabeller

--- src/main/resources/flyway/V1_1__Initial.sql
+++ src/main/resources/flyway/V1_1__Initial.sql
@@ -5,3 +5,36 @@ CREATE TABLE test
 (
     test TEXT
 );
+
+CREATE TABLE PERSON
+(
+    ID        SERIAL NOT NULL PRIMARY KEY,
+    REFERANSE uuid   NOT NULL UNIQUE
+);
+CREATE INDEX IDX_PERSON_REFERANSE ON PERSON (REFERANSE);
+
+CREATE TABLE PERSON_IDENT
+(
+    ID        SERIAL             NOT NULL PRIMARY KEY,
+    PERSON_ID BIGINT             NOT NULL REFERENCES PERSON (ID),
+    IDENT     varchar(19) UNIQUE NOT NULL
+);
+
+CREATE INDEX IDX_PERSON_IDENT_IDENT ON PERSON_IDENT (IDENT);
+
+CREATE TABLE SAK
+(
+    ID                    SERIAL                                 NOT NULL PRIMARY KEY,
+    SAKSNUMMER            VARCHAR(19)                            NOT NULL,
+    PERSON_ID             BIGINT                                 NOT NULL REFERENCES PERSON (ID),
+    RETTIGHETSPERIODE_FOM DATE                                   NOT NULL,
+    RETTIGHETSPERIODE_TOM DATE                                   NOT NULL,
+    STATUS                VARCHAR(100)                           NOT NULL,
+    VERSJON               BIGINT       DEFAULT 0                 NOT NULL,
+    OPPRETTET_TID         TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE INDEX IDX_SAK_SAKSNUMMER ON SAK (SAKSNUMMER);
+CREATE INDEX IDX_SAK_PERSON ON SAK (PERSON_ID);
+
+create sequence if not exists SEQ_SAKSNUMMER increment by 50 minvalue 10000000;
