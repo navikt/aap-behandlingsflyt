ce935e9ee67805a5be3226792cf5df1653d22b8f
Marius Glittum <marius.glittum@nav.no>
2023-10-30 13:18:37 +0100

    Legger til noe testing i testen

--- src/test/kotlin/no/nav/aap/behandlingsflyt/dbstuff/DaterangeParserTest.kt
+++ src/test/kotlin/no/nav/aap/behandlingsflyt/dbstuff/DaterangeParserTest.kt
@@ -1,11 +1,18 @@
 package no.nav.aap.behandlingsflyt.dbstuff
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.time.LocalDate
 
 class DaterangeParserTest {
 
     @Test
     fun `skal parse fra sql`() {
-        "[]"
+        val fom = LocalDate.now()
+        val tom = LocalDate.now()
+        val periode = DaterangeParser.fromSQL("[$fom,$tom]")
+
+        assertThat(periode.fom).isEqualTo(fom)
+        assertThat(periode.tom).isEqualTo(tom)
     }
 }
