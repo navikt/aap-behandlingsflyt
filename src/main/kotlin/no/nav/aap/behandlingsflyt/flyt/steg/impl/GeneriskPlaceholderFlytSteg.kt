ed64d6e7f1c5800f52c4c93a9001614b937f8b4c
Anders Østby <anders.ostby@nav.no>
2023-11-30 17:16:43 +0100

    Deprecated GeneriskPlaceholderFlytSteg og GeneriskPlaceholderSteg

--- src/main/kotlin/no/nav/aap/behandlingsflyt/flyt/steg/impl/GeneriskPlaceholderFlytSteg.kt
+++ src/main/kotlin/no/nav/aap/behandlingsflyt/flyt/steg/impl/GeneriskPlaceholderFlytSteg.kt
@@ -5,6 +5,7 @@ import no.nav.aap.behandlingsflyt.flyt.steg.BehandlingSteg
 import no.nav.aap.behandlingsflyt.flyt.steg.FlytSteg
 import no.nav.aap.behandlingsflyt.flyt.steg.StegType
 
+@Deprecated("Skal bort når alle steg er implementert")
 class GeneriskPlaceholderFlytSteg(private val stegType: StegType) : FlytSteg {
     override fun konstruer(connection: DBConnection): BehandlingSteg {
         return GeneriskPlaceholderSteg()
--- src/main/kotlin/no/nav/aap/behandlingsflyt/flyt/steg/impl/GeneriskPlaceholderSteg.kt
+++ src/main/kotlin/no/nav/aap/behandlingsflyt/flyt/steg/impl/GeneriskPlaceholderSteg.kt
@@ -4,6 +4,7 @@ import no.nav.aap.behandlingsflyt.flyt.FlytKontekst
 import no.nav.aap.behandlingsflyt.flyt.steg.BehandlingSteg
 import no.nav.aap.behandlingsflyt.flyt.steg.StegResultat
 
+@Deprecated("Skal bort når alle steg er implementert")
 class GeneriskPlaceholderSteg : BehandlingSteg {
     override fun utfør(kontekst: FlytKontekst): StegResultat {
         return StegResultat() // DO NOTHING
