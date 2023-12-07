6e7a7b2b8215f112a07d2e356d841b8e0b7e3714
Marius Glittum <marius.glittum@nav.no>
2023-12-07 10:24:09 +0100

    Woops

--- src/test/kotlin/no/nav/aap/behandlingsflyt/underveis/UnderveisServiceTest.kt
+++ src/test/kotlin/no/nav/aap/behandlingsflyt/underveis/UnderveisServiceTest.kt
@@ -1,9 +1,11 @@
 package no.nav.aap.behandlingsflyt.underveis
 
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class UnderveisServiceTest {
 
+    @Disabled
     @Test
     fun `skal vurdere alle reglene`() {
         TODO("Not yet implemented")
