3c997ddb5be18b72d38fefc65b2b314da447e98c
Anders Østby <anders.ostby@nav.no>
2024-01-05 11:26:08 +0100

    Bumper kotlin, gradle og assertj

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,7 +71,7 @@ dependencies {
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.1")
-    testImplementation("org.assertj:assertj-core:3.24.2")
+    testImplementation("org.assertj:assertj-core:3.25.1")
     testImplementation("org.testcontainers:postgresql:1.19.3")
     testImplementation(kotlin("test"))
 }
