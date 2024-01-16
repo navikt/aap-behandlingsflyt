c20b5b1773d9faf0518378620910a14da50c69ac
Anders Østby <anders.ostby@nav.no>
2024-01-16 21:56:07 +0100

    Bumper flyway til 10.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,8 +68,8 @@ dependencies {
     implementation(project(":tidslinje"))
     implementation(project(":dbconnect"))
     implementation("com.zaxxer:HikariCP:5.1.0")
-    implementation("org.flywaydb:flyway-core:10.4.1")
-    implementation("org.flywaydb:flyway-database-postgresql:10.4.1")
+    implementation("org.flywaydb:flyway-core:10.6.0")
+    implementation("org.flywaydb:flyway-database-postgresql:10.6.0")
     runtimeOnly("org.postgresql:postgresql:42.7.1")
 
     testImplementation(project(":dbtest"))
