2d73941b04108d549602bbea1f1a6b7046d0d0d1
Marius Glittum <marius.glittum@nav.no>
2024-02-21 13:24:42 +0100

    Bumper postgresdql driver

--- sakogbehandling/build.gradle.kts
+++ sakogbehandling/build.gradle.kts
@@ -10,7 +10,7 @@ dependencies {
     implementation("com.zaxxer:HikariCP:5.1.0")
     implementation("org.flywaydb:flyway-core:10.7.2")
     implementation("org.flywaydb:flyway-database-postgresql:10.7.2")
-    runtimeOnly("org.postgresql:postgresql:42.7.1")
+    runtimeOnly("org.postgresql:postgresql:42.7.2")
 
     testImplementation(project(":dbtestdata"))
     testImplementation(project(":dbtest"))
