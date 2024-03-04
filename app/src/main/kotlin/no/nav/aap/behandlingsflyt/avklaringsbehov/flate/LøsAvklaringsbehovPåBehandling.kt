d2d558ff69628ca663c114084c00b67c47a1dade
VHollund <vetlehoo@gmail.com>
2024-03-04 15:05:15 +0100

    trukket ut løsninger fra behov for å vise arv på riktig måte i swagger

--- app/src/main/kotlin/no/nav/aap/behandlingsflyt/avklaringsbehov/flate/LøsAvklaringsbehovPåBehandling.kt
+++ app/src/main/kotlin/no/nav/aap/behandlingsflyt/avklaringsbehov/flate/LøsAvklaringsbehovPåBehandling.kt
@@ -4,6 +4,13 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.papsign.ktor.openapigen.annotations.Response
 import no.nav.aap.behandlingsflyt.avklaringsbehov.løser.AvklaringsbehovLøsning
+import no.nav.aap.behandlingsflyt.avklaringsbehov.løser.arbeidsevne.FastsettArbeidsevneLøsning
+import no.nav.aap.behandlingsflyt.avklaringsbehov.løser.bistand.AvklarBistandsbehovLøsning
+import no.nav.aap.behandlingsflyt.avklaringsbehov.løser.meldeplikt.FritakMeldepliktLøsning
+import no.nav.aap.behandlingsflyt.avklaringsbehov.løser.student.AvklarStudentLøsning
+import no.nav.aap.behandlingsflyt.avklaringsbehov.løser.sykdom.AvklarSykepengerErstatningLøsning
+import no.nav.aap.behandlingsflyt.avklaringsbehov.løser.vedtak.FatteVedtakLøsning
+import no.nav.aap.behandlingsflyt.avklaringsbehov.løser.vedtak.ForeslåVedtakLøsning
 import java.util.*
 
 @Response(statusCode = 202)
@@ -12,5 +19,40 @@ data class LøsAvklaringsbehovPåBehandling(
     @JsonProperty(value = "referanse", required = true) val referanse: UUID,
     @JsonProperty(value = "behandlingVersjon", required = true, defaultValue = "0") val behandlingVersjon: Long,
     @JsonProperty(value = "behov", required = true) val behov: AvklaringsbehovLøsning,
+    @JsonProperty(value = "avklarStudentLøsning") val avklarStudentLøsning: AvklarStudentLøsning?,
+    @JsonProperty(value = "avklarSykepengerErstatningLøsning") val avklarSykepengerErstatningLøsning: AvklarSykepengerErstatningLøsning?,
+    @JsonProperty(value = "avklarBistandsbehovLøsning") val avklarBistandsbehovLøsning: AvklarBistandsbehovLøsning?,
+    @JsonProperty(value = "fritakMeldepliktLøsning") val fritakMeldepliktLøsning: FritakMeldepliktLøsning?,
+    @JsonProperty(value = "fastsettArbeidsevneLøsning") val fastsettArbeidsevneLøsning: FastsettArbeidsevneLøsning?,
+    @JsonProperty(value = "foreslåVedtakLøsning") val foreslåVedtakLøsning: ForeslåVedtakLøsning?,
+    @JsonProperty(value = "fatteVedtakLøsning") val fatteVedtakLøsning: FatteVedtakLøsning?,
     @JsonProperty(value = "ingenEndringIGruppe") val ingenEndringIGruppe: Boolean,
-)
+) {
+    init {
+        //kun en av løsningene kan og MÅ være satt
+        require(
+            listOfNotNull(
+                avklarStudentLøsning,
+                avklarSykepengerErstatningLøsning,
+                avklarBistandsbehovLøsning,
+                fritakMeldepliktLøsning,
+                fastsettArbeidsevneLøsning,
+                foreslåVedtakLøsning,
+                fatteVedtakLøsning
+            ).size == 1
+        ) { "Kun en av løsningene kan være satt" }
+    }
+    //hent den aktivt satte løsningen
+    fun behov(): AvklaringsbehovLøsning {
+        return listOf(
+            avklarStudentLøsning,
+            avklarSykepengerErstatningLøsning,
+            avklarBistandsbehovLøsning,
+            fritakMeldepliktLøsning,
+            fastsettArbeidsevneLøsning,
+            foreslåVedtakLøsning,
+            fatteVedtakLøsning
+        ).filterNotNull().first()
+    }
+
+}
