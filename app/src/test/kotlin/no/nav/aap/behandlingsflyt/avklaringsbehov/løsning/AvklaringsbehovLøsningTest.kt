dbd6916d6f9c6fb379517b178098d12398c27055
Marius Glittum <marius.glittum@nav.no>
2024-03-12 13:00:17 +0100

    Legger til sjekk om at alle løsninger har felt med behovstype

--- app/src/test/kotlin/no/nav/aap/behandlingsflyt/avklaringsbehov/løsning/AvklaringsbehovLøsningTest.kt
+++ app/src/test/kotlin/no/nav/aap/behandlingsflyt/avklaringsbehov/løsning/AvklaringsbehovLøsningTest.kt
@@ -1,11 +1,41 @@
 package no.nav.aap.behandlingsflyt.avklaringsbehov.løsning
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonTypeName
 import no.nav.aap.behandlingsflyt.avklaringsbehov.Definisjon
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class AvklaringsbehovLøsningTest {
+    @Test
+    fun `alle subtyper skal ha behovstype field `() {
+        val løsningSubtypes = utledSubtypes()
+
+        assertThat(løsningSubtypes).allMatch { validerAlleKlasserHarFelt(it) }
+    }
+
+    private fun validerAlleKlasserHarFelt(it: Class<*>): Boolean {
+        val felter = it.declaredFields
+
+        if (felter.size < 1) {
+            return false
+        }
+
+        val harFeltAvType = felter.any { felt ->
+            felt.name == "behovstype" && (!felt.isAnnotationPresent(JsonProperty::class.java) || felt.getAnnotation(
+                JsonProperty::class.java
+            ).value == "behovstype")
+        }
+        if (harFeltAvType) {
+            return true
+        }
+        return felter.any { felt ->
+            felt.isAnnotationPresent(JsonProperty::class.java) && felt.getAnnotation(
+                JsonProperty::class.java
+            ).value == "behovstype"
+        }
+    }
+
     @Test
     fun `alle subtyper skal ha annotasjon`() {
         val løsningSubtypes = utledSubtypes()
@@ -24,7 +54,8 @@ class AvklaringsbehovLøsningTest {
     @Test
     fun `alle subtyper skal ha tilhørende definisjon`() {
         val utledSubtypes = utledSubtypes()
-        val løsningSubtypes = utledSubtypes.map { Definisjon.forKode(it.getAnnotation(JsonTypeName::class.java).value) }.toSet()
+        val løsningSubtypes =
+            utledSubtypes.map { Definisjon.forKode(it.getAnnotation(JsonTypeName::class.java).value) }.toSet()
 
         assertThat(løsningSubtypes).hasSize(utledSubtypes.size)
     }
