0741cc111496ba9f75f8a6d55dc8daf42a29d97a
Marius Glittum <marius.glittum@nav.no>
2024-04-12 10:55:19 +0200

    Manglet en dto gitt
    Co-authored-by: Thomas Rognes <thomas.sebastian.rognes@nav.no>
    Co-authored-by: Vetle Hollund <vetle.hollund@nav.no>

--- app/src/main/kotlin/no/nav/aap/behandlingsflyt/avklaringsbehov/flate/FatteVedtakGrunnlagDto.kt
+++ app/src/main/kotlin/no/nav/aap/behandlingsflyt/avklaringsbehov/flate/FatteVedtakGrunnlagDto.kt
@@ -2,4 +2,4 @@ package no.nav.aap.behandlingsflyt.avklaringsbehov.flate
 
 import no.nav.aap.behandlingsflyt.avklaringsbehov.løser.vedtak.TotrinnsVurdering
 
-data class FatteVedtakGrunnlagDto(val vurderinger: List<TotrinnsVurdering>)
+data class FatteVedtakGrunnlagDto(val vurderinger: List<TotrinnsVurdering>, val historikk: List<Historikk>)
