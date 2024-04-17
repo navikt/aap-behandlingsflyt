d51e4e262d1c3f667cc26afc8139f46736f88927
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-04-17 14:03:01 +0200

    Legger til optional felt vor vurdering i sykepengerGrunnlag

--- faktagrunnlag/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/saksbehandler/sykdom/flate/SykepengerGrunnlagAPI.kt
+++ faktagrunnlag/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/saksbehandler/sykdom/flate/SykepengerGrunnlagAPI.kt
@@ -7,7 +7,6 @@ import com.papsign.ktor.openapigen.route.route
 import com.zaxxer.hikari.HikariDataSource
 import no.nav.aap.behandlingsflyt.dbconnect.transaction
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.sykdom.SykepengerErstatningRepository
-import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.sykdom.SykepengerVurdering
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.Behandling
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.flate.BehandlingReferanse
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.flate.BehandlingReferanseService
@@ -16,27 +15,13 @@ import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.flate.BehandlingRef
 fun NormalOpenAPIRoute.sykepengerGrunnlagApi(dataSource: HikariDataSource) {
     route("/api/behandling") {
         route("/{referanse}/grunnlag/sykdom/sykepengergrunnlag") {
-            get<BehandlingReferanse, SykepengerVurdering> { req ->
+            get<BehandlingReferanse, SykepengerGrunnlagDto> { req ->
                 val sykepengerErstatningGrunnlag = dataSource.transaction { connection ->
                     val behandling: Behandling = BehandlingReferanseService(connection).behandling(req)
-                    SykepengerErstatningRepository(connection).hentHvisEksisterer(behandlingId = behandling.id)
+                    SykepengerErstatningRepository(connection).hentHvisEksisterer(behandling.id)
                 }
 
-                //TODO: feilhåndetering
-                if (sykepengerErstatningGrunnlag?.vurdering != null) {
-                    respond(
-                        sykepengerErstatningGrunnlag.vurdering
-                    )
-                }else {
-                    //antar false om ikke funnet
-                    respond(
-                        SykepengerVurdering(
-                            begrunnelse = "Fant ikke sykepengerErstatningGrunnlag",
-                            harRettPå = false,
-                            dokumenterBruktIVurdering = emptyList()
-                        )
-                    )
-                }
+                respond(SykepengerGrunnlagDto(sykepengerErstatningGrunnlag?.vurdering))
             }
         }
     }
---
+++ faktagrunnlag/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/saksbehandler/sykdom/flate/SykepengerGrunnlagDto.kt
@@ -0,0 +1,5 @@
+package no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.sykdom.flate
+
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.sykdom.SykepengerVurdering
+
+data class SykepengerGrunnlagDto(val vurdering: SykepengerVurdering?)
