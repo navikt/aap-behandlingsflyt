a4abb443f5088ceedfff0a785ee20a5abf735e7e
Marius Glittum <marius.glittum@nav.no>
2024-04-24 09:22:08 +0200

    Legger inn 10 år i testperson default
    Co-authored-by: Anders Østby <anders.ostby@nav.no>
    Co-authored-by: Thomas Rognes <thomas.sebastian.rognes@nav.no>
    Co-authored-by: Tor Idland <tor.idland@nav.no>
    Co-authored-by: Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>

--- lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/test/modell/TestPerson.kt
+++ lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/test/modell/TestPerson.kt
@@ -4,8 +4,10 @@ import no.nav.aap.behandlingsflyt.faktagrunnlag.register.barn.Dødsdato
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.inntekt.InntektPerÅr
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.personopplysninger.Fødselsdato
 import no.nav.aap.behandlingsflyt.test.genererIdent
+import no.nav.aap.verdityper.Beløp
 import no.nav.aap.verdityper.sakogbehandling.Ident
 import java.time.LocalDate
+import java.time.Year
 
 class TestPerson (
     val fødselsdato: Fødselsdato = Fødselsdato(LocalDate.now().minusYears(19)),
@@ -13,5 +15,5 @@ class TestPerson (
     val dødsdato: Dødsdato? = null,
     val barn: List<TestPerson> = emptyList(),
     val yrkesskade:List<TestYrkesskade> = emptyList(),
-    var inntekter: List<InntektPerÅr> = emptyList()
+    var inntekter: List<InntektPerÅr> = (1..10).map { InntektPerÅr(Year.now().minusYears(it.toLong()), Beløp("1000000.0")) }
 )
