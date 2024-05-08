c98d93a28ba98e62e9bbd460a3401ef5ca422c57
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-05-08 09:21:57 +0200

    Gjør lovvalg før alder i Førstegangsbehandling

--- app/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/behandlingstyper/Førstegangsbehandling.kt
+++ app/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/behandlingstyper/Førstegangsbehandling.kt
@@ -33,8 +33,8 @@ object Førstegangsbehandling : BehandlingType {
     override fun flyt(): BehandlingFlyt {
         return BehandlingFlytBuilder()
             .medSteg(steg = StartBehandlingSteg, informasjonskrav = listOf(SøknadService))
-            .medSteg(steg = VurderAlderSteg, informasjonskrav = listOf(PersonopplysningService))
             .medSteg(steg = VurderLovvalgSteg)
+            .medSteg(steg = VurderAlderSteg, informasjonskrav = listOf(PersonopplysningService))
             .medSteg(steg = VurderStudentSteg)
             .medSteg(steg = VurderSykdomSteg, informasjonskrav = listOf(YrkesskadeService))
             .medSteg(steg = FritakMeldepliktSteg)
