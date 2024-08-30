bd337d979a4d9313b3facea9f30dac5e971217d1
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2024-08-30 10:48:44 +0200

    Tentativ mal for avklaringsbehov i samordning

--- app/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/samordning/SamordningRegelService.kt
+++ app/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/samordning/SamordningRegelService.kt
@@ -4,6 +4,7 @@ import no.nav.aap.tidslinje.Tidslinje
 import no.nav.aap.verdityper.sakogbehandling.BehandlingId
 
 class SamordningRegelService {
+    // Alt denne trenger å gjøre er å slå sammen alle ytelsesgraderinger til en tidslinje
     fun vurder(behandlingId: BehandlingId): Tidslinje<SamordningGradering> {
         return Tidslinje()
     }
