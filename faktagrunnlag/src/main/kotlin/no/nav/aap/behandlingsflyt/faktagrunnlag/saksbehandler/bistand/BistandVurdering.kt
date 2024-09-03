075978b9518c5e5f0409a64d078d201b923ef381
yongbinkwon <yongbinkwon97@gmail.com>
2024-09-03 12:30:18 +0200

    split grunnerTilBehovForBistand into 3 fields + remove erBehovForBistand

--- faktagrunnlag/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/saksbehandler/bistand/BistandVurdering.kt
+++ faktagrunnlag/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/saksbehandler/bistand/BistandVurdering.kt
@@ -1,10 +1,18 @@
 package no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.bistand
 
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.bistand.flate.BistandVurderingDto
+
 data class BistandVurdering(
     val begrunnelse: String,
-    val erBehovForBistand: Boolean,
     val erBehovForAktivBehandling: Boolean,
     val erBehovForArbeidsrettetTiltak: Boolean,
     val erBehovForAnnenOppfølging: Boolean
-)
+) {
+    fun toDto() = BistandVurderingDto(
+        begrunnelse = begrunnelse,
+        erBehovForAktivBehandling = erBehovForAktivBehandling,
+        erBehovForArbeidsrettetTiltak = erBehovForArbeidsrettetTiltak,
+        erBehovForAnnenOppfølging = erBehovForAnnenOppfølging
+    )
+}
 
