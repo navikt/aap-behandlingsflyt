9e8238f5cfd27fec93664cb5bbd3e7c41ecee536
Marius Glittum <marius.glittum@nav.no>
2024-09-03 14:58:39 +0200

    Lagrer vurderinger på barn

--- app/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løser/AvklarBarnetilleggLøser.kt
+++ app/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løser/AvklarBarnetilleggLøser.kt
@@ -3,15 +3,18 @@ package no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løser
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovKontekst
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.Definisjon
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løsning.AvklarBarnetilleggLøsning
+import no.nav.aap.behandlingsflyt.faktagrunnlag.register.barn.BarnRepository
 import no.nav.aap.komponenter.dbconnect.DBConnection
 
 class AvklarBarnetilleggLøser(val connection: DBConnection) : AvklaringsbehovsLøser<AvklarBarnetilleggLøsning> {
 
+    private val barnRepository = BarnRepository(connection)
+
     override fun løs(kontekst: AvklaringsbehovKontekst, løsning: AvklarBarnetilleggLøsning): LøsningsResultat {
 
-        // TODO:
+        barnRepository.lagreVurderinger(kontekst.kontekst.behandlingId, løsning.vurderingerForBarnetillegg.vurderteBarn)
 
-        return LøsningsResultat(begrunnelse = "TOO BE FIXED")
+        return LøsningsResultat(begrunnelse = "Vurdert barnetillegg")
     }
 
     override fun forBehov(): Definisjon {
