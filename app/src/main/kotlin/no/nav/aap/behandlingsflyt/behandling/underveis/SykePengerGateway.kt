5fe7572091f5adcfe094464b86b1e19c0a202e86
Henrik.Bugge.Halvorsen <henrik.bugge.halvorsen@nav.no>
2024-09-03 08:48:46 +0200

    Start av sykepenger gateway, mangler avklaringer

--- app/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/underveis/SykePengerGateway.kt
+++
@@ -1,5 +0,0 @@
-package no.nav.aap.behandlingsflyt.behandling.underveis
-
-class SykePengerGateway {
-    // Bruk denne?: https://github.com/navikt/helse-sporbar
-}
---
+++ app/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/underveis/sykepenger/SykepengerGateway.kt
@@ -0,0 +1,40 @@
+package no.nav.aap.behandlingsflyt.behandling.underveis.sykepenger
+
+import no.nav.aap.httpclient.ClientConfig
+import no.nav.aap.httpclient.Header
+import no.nav.aap.httpclient.RestClient
+import no.nav.aap.httpclient.post
+import no.nav.aap.httpclient.request.PostRequest
+import no.nav.aap.httpclient.tokenprovider.azurecc.ClientCredentialsTokenProvider
+import no.nav.aap.requiredConfigForKey
+import java.net.URI
+
+class SykepengerGateway {
+    private val url = URI.create(requiredConfigForKey("integrasjon.sykepenger.url") + "/somethingsomething")
+    val config = ClientConfig(scope = requiredConfigForKey("integrasjon.sykepenger.scope"))
+
+    private val client = RestClient.withDefaultResponseHandler(
+        config = config,
+        tokenProvider = ClientCredentialsTokenProvider,
+    )
+
+    private fun query(request: SykePengerRequest): List<SykePengerResponse> {
+        val httpRequest = PostRequest(
+            body = request,
+            additionalHeaders = listOf(
+                Header("Accept", "application/json")
+            )
+        )
+        return requireNotNull(client.post(uri = url, request = httpRequest))
+    }
+
+    fun hentYtelseSykepenger(request: SykePengerRequest): SykePengerResponse {
+        try {
+            val result = query(request)
+            return SykePengerResponse("")
+        } catch (e : Exception) {
+            throw RuntimeException("Feil ved henting av ytelser i sykepenger: ${e.message}")
+        }
+    }
+    // Bruk denne?: https://github.com/navikt/helse-sporbar
+}
