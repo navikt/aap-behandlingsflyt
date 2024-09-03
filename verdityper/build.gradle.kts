79584f3e1ec413493c676fbdd28a49035dcd5bfc
Marius Glittum <marius.glittum@nav.no>
2024-09-03 16:03:50 +0200

    Bump ktor

--- verdityper/build.gradle.kts
+++ verdityper/build.gradle.kts
@@ -2,6 +2,7 @@ val komponenterVersjon = "0.0.23"
 
 dependencies {
     implementation("no.nav.aap.kelvin:dbconnect:$komponenterVersjon")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.17.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.0")
     testImplementation("org.assertj:assertj-core:3.26.3")
