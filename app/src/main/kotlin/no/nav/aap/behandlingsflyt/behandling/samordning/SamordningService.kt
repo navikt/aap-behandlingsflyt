3321206a64e66ce9bbf5109c4508466918513985
Henrik.Bugge.Halvorsen <henrik.bugge.halvorsen@nav.no>
2024-09-05 13:10:00 +0200

    fjerning og oppklaring av kode

--- app/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/samordning/SamordningService.kt
+++ app/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/samordning/SamordningService.kt
@@ -6,13 +6,6 @@ import no.nav.aap.behandlingsflyt.faktagrunnlag.Informasjonskravkonstruktør
 import no.nav.aap.verdityper.flyt.FlytKontekst
 
 /*
-* Pleiepenger for sykt barn - team sykdom i familien
-* Pleiepenger for nærstående - team sykdom i familien
-* Omsorgspenger? - team sykdom i familien
-* Svangerskapspenger - team foreldrepenger
-* Foreldrepenger - team foreldrepenger
-* Sykepenger? - team sykdom (På vent fra Øyvind, har potensielt 2 apier, bør klareres)
-*
 * Informasjonselementer fra ytelsene:
 * Perioder med ytelse
 * utbetalingsgrad per periode
@@ -27,24 +20,14 @@ import no.nav.aap.verdityper.flyt.FlytKontekst
 *
 * YtelsesGradering
 * - id(pk) - samordningsperiodeId(fk) - ytelse(string/enum) - gradering (smallint)
- */
+*/
 
 class SamordningService: Informasjonskrav {
-    fun hentYtelsePleiePengerSyktBarn() {}
-
-    fun hentYtelsePleiePengerForNærFamilie() {}
-
-    fun hentYtelseOmsorgsPenger() {}
-
     /**
      * Anses som full ytelse, inklusivt 80% uttak
+     * Henter svangerskapspenger, foreldrepenger, omsorgspenger, pleiepenger sykt barn og pleiepenger sykdom i familie
      */
     fun hentYtelseForeldrePenger() {}
-
-    /**
-     * Fulle svangerskapspenger av deltidsstilling anses som en redusert ytelse
-     */
-    fun hentYtelseSvangerskapsPenger() {}
 
     /**
      * Fulle sykepenger av deltidsstilling anses som en redusert ytelse
