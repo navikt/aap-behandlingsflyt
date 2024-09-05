cc15d5fa1845b911f985ca1d7f2acb0be3493765
Henrik.Bugge.Halvorsen <henrik.bugge.halvorsen@nav.no>
2024-09-05 08:55:31 +0200

    henting av data fra spøkelse til rett url, for sykepenger

--- app/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/underveis/sykepenger/SykepengerModell.kt
+++ app/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/underveis/sykepenger/SykepengerModell.kt
@@ -1,11 +1,26 @@
 package no.nav.aap.behandlingsflyt.behandling.underveis.sykepenger
 
+import java.time.LocalDate
+
 data class SykePengerRequest (
-    //TODO: Finn og implementer modell
-    val t: String
+    val personidentifikatorer: Set<Personidentifikator>,
+    val fom: LocalDate,
+    val tom: LocalDate,
+    val oppløsning: Set<String> = emptySet()
 )
 
 data class SykePengerResponse(
-    //TODO: Finn og implementer modell
-    val t: String
+    val utbetaltePerioder: List<UtbetaltePerioder>
+)
+
+data class UtbetaltePerioder(
+    val personidentifikator: Personidentifikator,
+    val fom: LocalDate,
+    val tom: LocalDate,
+    val grad: Int,
+    val tags: Set<String>
+)
+
+data class Personidentifikator(
+    val id: String
 )
