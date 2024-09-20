7586ec91e024e60e5805d80a59acbaeeabcfea97
Henrik.Bugge.Halvorsen <henrik.bugge.halvorsen@nav.no>
2024-09-20 12:05:37 +0200

    refaktorer sykepenger og foreldrepenger

--- app/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/samordning/SamordningApi.kt
+++ app/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/samordning/SamordningApi.kt
@@ -4,9 +4,9 @@ import com.papsign.ktor.openapigen.route.path.normal.NormalOpenAPIRoute
 import com.papsign.ktor.openapigen.route.path.normal.post
 import com.papsign.ktor.openapigen.route.response.respond
 import com.papsign.ktor.openapigen.route.route
-import no.nav.aap.behandlingsflyt.behandling.underveis.foreldrepenger.ForeldrepengerGateway
-import no.nav.aap.behandlingsflyt.behandling.underveis.foreldrepenger.ForeldrepengerRequest
-import no.nav.aap.behandlingsflyt.behandling.underveis.foreldrepenger.ForeldrepengerResponse
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.foreldrepenger.ForeldrepengerGateway
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.foreldrepenger.ForeldrepengerRequest
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.foreldrepenger.ForeldrepengerResponse
 import no.nav.aap.komponenter.dbconnect.transaction
 import javax.sql.DataSource
 
