b1822eb61401f52f0057a8bc3de4b8d3517c11fb
Henrik.Bugge.Halvorsen <henrik.bugge.halvorsen@nav.no>
2024-09-24 15:25:45 +0200

    flytt gateways

--- faktagrunnlag/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/delvurdering/samordning/foreldrepenger/ForeldrepengerGrunnlag.kt
+++
@@ -1,6 +0,0 @@
-package no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.foreldrepenger
-
-data class ForeldrepengerGrunnlag (
-    val perioderId: Long,
-    val perioder: List<ForeldrepengerPeriode>
-)
--- faktagrunnlag/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/delvurdering/samordning/sykepenger/SykepengerGrunnlag.kt
+++
@@ -1,6 +0,0 @@
-package no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.sykepenger
-
-data class SykepengerGrunnlag (
-    val perioderId: Long,
-    val perioder: List<SykepengerPeriode>
-)
