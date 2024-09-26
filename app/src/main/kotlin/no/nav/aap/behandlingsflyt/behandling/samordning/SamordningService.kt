a41d9ef18831b63d4393eb147cfccf1e6424f43b
Henrik.Bugge.Halvorsen <henrik.bugge.halvorsen@nav.no>
2024-09-26 13:20:53 +0200

    videreføring avklaring

--- app/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/samordning/SamordningService.kt
+++ app/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/samordning/SamordningService.kt
@@ -1,36 +1,30 @@
 package no.nav.aap.behandlingsflyt.behandling.samordning
 
-import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.SamordningPeriode
-import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.SamordningRepository
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.ytelsevurdering.SamordningYtelseVurderingGrunnlag
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.ytelsevurdering.SamordningYtelseVurderingRepository
 import no.nav.aap.tidslinje.Tidslinje
 import no.nav.aap.verdityper.sakogbehandling.BehandlingId
 
 class SamordningService(
-    private val samordningRepository: SamordningRepository,
     private val samordningYtelseVurderingRepository: SamordningYtelseVurderingRepository
 ) {
-
     fun vurder(behandlingId: BehandlingId): Tidslinje<SamordningGradering> {
-        val samordningYtelseService = samordningYtelseVurderingRepository.hentHvisEksisterer(behandlingId)
-
-        // TODO: Implementer regler for overnevnte og produser tidslinje, finne ut om vi skal bruke SamordningGradering eller SamordningGraderingDto
-        val vurderRegler = vurderRegler()
-
-        samordningRepository.lagre(
-            behandlingId,
-            vurderRegler.segmenter()
-                .map {
-                    SamordningPeriode(
-                        it.periode,
-                        it.verdi.gradering
-                    )
-                })
+        val samordningYtelseVurderingGrunnlag = samordningYtelseVurderingRepository.hentHvisEksisterer(behandlingId)
+        val vurderRegler = vurderRegler(samordningYtelseVurderingGrunnlag)
 
         return vurderRegler
     }
 
-    fun vurderRegler() : Tidslinje<SamordningGradering> {
+    fun harGjortVurdering(behandlingId: BehandlingId): Boolean {
+        val samordningYtelseVurderingGrunnlag = samordningYtelseVurderingRepository.hentHvisEksisterer(behandlingId)
+
+        return samordningYtelseVurderingGrunnlag?.vurderingerId != null
+            && samordningYtelseVurderingGrunnlag.vurderinger != null
+            && samordningYtelseVurderingGrunnlag.vurderinger!!.isNotEmpty()
+    }
+
+    fun vurderRegler(samordning: SamordningYtelseVurderingGrunnlag?) : Tidslinje<SamordningGradering> {
+        //TODO: Kombiner til tidslinje her. Kaja skal utarbeide en oversikt over regler, avventer detteø
         return Tidslinje(listOf())
     }
 }
--- app/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/SamordningSteg.kt
+++ app/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/SamordningSteg.kt
@@ -5,61 +5,70 @@ import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovRepo
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.Definisjon
 import no.nav.aap.behandlingsflyt.behandling.samordning.AvklaringsType
 import no.nav.aap.behandlingsflyt.behandling.samordning.SamordningService
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.SamordningPeriode
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.SamordningRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.ytelsevurdering.SamordningYtelseVurderingRepository
 import no.nav.aap.behandlingsflyt.flyt.steg.BehandlingSteg
 import no.nav.aap.behandlingsflyt.flyt.steg.FlytSteg
 import no.nav.aap.behandlingsflyt.flyt.steg.StegResultat
 import no.nav.aap.komponenter.dbconnect.DBConnection
+import no.nav.aap.verdityper.flyt.FlytKontekst
 import no.nav.aap.verdityper.flyt.FlytKontekstMedPerioder
 import no.nav.aap.verdityper.flyt.StegType
 import org.slf4j.LoggerFactory
 
 class SamordningSteg(
     private val samordningService: SamordningService,
+    private val samordningRepository: SamordningRepository,
     private val avklaringsbehovRepository: AvklaringsbehovRepository
 
 ) : BehandlingSteg {
     private val log = LoggerFactory.getLogger(SamordningSteg::class.java)
 
-    private val erUvurdert = false //TODO: Finn ut når er uvurdert
-    // TODO: Finnes det noe som gjør at vi ikke trenger å vurdere manuell ytelsesgradering?
-    private val kanLukkes = false //TODO: Fiks
-
     override fun utfør(kontekst: FlytKontekstMedPerioder): StegResultat {
         val samordningTidslinje = samordningService.vurder(kontekst.behandlingId)
 
-        // Hvis perioden har ytelsesgradering som er manuell,
-        // så skal det opprettes et avklaringsbehov hvis ikke allerede vurdert
-        if (samordningTidslinje.segmenter().any {
-                it.verdi.ytelsesGraderinger.any {
+        if (samordningTidslinje.segmenter().any { segment ->
+                segment.verdi.ytelsesGraderinger.any {
                     it.ytelse.type == AvklaringsType.MANUELL
                 }
             }) {
-            // Sjekk om det finnes perioder som ikke er vurdert
-            if (erUvurdert) {
+            if (!samordningService.harGjortVurdering(kontekst.behandlingId)) {
                 return StegResultat(listOf(Definisjon.AVKLAR_SAMORDNING_GRADERING))
-            } else {
-                // Sjekk om det finnes avklaringsbehov
-                val avklaringsbehovene = avklaringsbehovRepository.hentAvklaringsbehovene(kontekst.behandlingId)
-                val avklaringsbehov = avklaringsbehovene.hentBehovForDefinisjon(Definisjon.AVKLAR_SAMORDNING_GRADERING)
-                // Sjekk om de kan lukkes
-                if (avklaringsbehov != null && avklaringsbehov.erÅpent() && kanLukkes) {
-                    avklaringsbehovene.avbryt(Definisjon.AVKLAR_SAMORDNING_GRADERING)
-                }
             }
         }
+
+        // Før eller etter? Hva gir mening?
+        samordningRepository.lagre(
+            kontekst.behandlingId,
+            samordningTidslinje.segmenter()
+                .map {
+                    SamordningPeriode(
+                        it.periode,
+                        it.verdi.gradering
+                    )
+                }
+        )
+
         log.info("Samordning tidslinje $samordningTidslinje")
         return StegResultat()
+    }
+
+    override fun vedTilbakeføring(kontekst: FlytKontekst) {
+        val avklaringsbehovene = avklaringsbehovRepository.hentAvklaringsbehovene(kontekst.behandlingId)
+        val avklaringsbehov = avklaringsbehovene.hentBehovForDefinisjon(Definisjon.AVKLAR_SAMORDNING_GRADERING)
+        if (avklaringsbehov != null && avklaringsbehov.erÅpent()) {
+            avklaringsbehovene.avbryt(Definisjon.AVKLAR_SAMORDNING_GRADERING)
+        }
     }
 
     companion object : FlytSteg {
         override fun konstruer(connection: DBConnection): BehandlingSteg {
             return SamordningSteg(
                 SamordningService(
-                    SamordningRepository(connection),
                     SamordningYtelseVurderingRepository(connection),
                 ),
+                SamordningRepository(connection),
                 AvklaringsbehovRepositoryImpl(connection)
             )
         }
