fb4269e7e0e831e1d42be2cd53c97799c260560c
Henrik.Bugge.Halvorsen <henrik.bugge.halvorsen@nav.no>
2024-09-26 10:24:08 +0200

    alle ytelser skal være manuelle enn så lenge

--- app/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/samordning/Ytelse.kt
+++ app/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/samordning/Ytelse.kt
@@ -2,13 +2,12 @@ package no.nav.aap.behandlingsflyt.behandling.samordning
 
 enum class Ytelse(val type: AvklaringsType) {
     SYKEPENGER(AvklaringsType.MANUELL),
-    FORELDREPENGER(AvklaringsType.AUTOMATISK),
-    PLEIEPENGER_BARN(AvklaringsType.AUTOMATISK),
-    PLEIEPENGER_NÆR_FAMILIE(AvklaringsType.AUTOMATISK),
-    SVANGERSKAPSPENGER(AvklaringsType.AUTOMATISK),
-    OMSORGSPENGER(AvklaringsType.AUTOMATISK),
-    OPPLÆRINGSPENGER(AvklaringsType.AUTOMATISK),
-    ENGANGSTØNAD(AvklaringsType.AUTOMATISK)
+    FORELDREPENGER(AvklaringsType.MANUELL),
+    PLEIEPENGER_BARN(AvklaringsType.MANUELL),
+    PLEIEPENGER_NÆR_FAMILIE(AvklaringsType.MANUELL),
+    SVANGERSKAPSPENGER(AvklaringsType.MANUELL),
+    OMSORGSPENGER(AvklaringsType.MANUELL),
+    OPPLÆRINGSPENGER(AvklaringsType.MANUELL)
 }
 
 enum class AvklaringsType {
