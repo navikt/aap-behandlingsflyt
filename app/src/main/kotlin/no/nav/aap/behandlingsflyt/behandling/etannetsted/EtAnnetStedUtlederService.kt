4757fd956f32b9aaa70b4847269ceb7e04ae4a58
VHollund <vetlehoo@gmail.com>
2024-10-03 12:57:17 +0200

    skrevet om utled behov ved bruk av tidslinjer, slik at vi lettere kan legge til regler for tid mellom utskrivning og nytt opphold

--- app/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/etannetsted/EtAnnetStedUtlederService.kt
+++ app/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/etannetsted/EtAnnetStedUtlederService.kt
@@ -4,6 +4,9 @@ import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.barnetillegg.Barnet
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.institusjonsopphold.InstitusjonsoppholdRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.institusjonsopphold.Institusjonstype
 import no.nav.aap.komponenter.dbconnect.DBConnection
+import no.nav.aap.tidslinje.JoinStyle
+import no.nav.aap.tidslinje.Segment
+import no.nav.aap.tidslinje.Tidslinje
 import no.nav.aap.verdityper.sakogbehandling.BehandlingId
 import java.time.LocalDate
 
@@ -23,29 +26,45 @@ class EtAnnetStedUtlederService(connection: DBConnection) {
         val helseopphold = opphold.filter { segment -> segment.verdi.type == Institusjonstype.HS }
         val barnetillegg = input.barnetillegg
 
-        // Oppholdet må være lengre enn 3 måneder for å være aktuelt for avklaring
-        val lengreOpphold = helseopphold.filter { segment ->
-            val startTelling = LocalDate.of(segment.fom().year, segment.fom().month.plus(1).value,1)
-            val kretivPeriode = startTelling.plusMonths(3) < segment.periode.fom
-
-            return@filter kretivPeriode
+        if(soningsOppgold.isNotEmpty()){
+            return BehovForAvklaringer(true, false)
         }
 
-        // Oppholdet må være lengre enn 2 måneder for å være aktuelt for avklaring
-        val klarForAvklaringer = lengreOpphold.filter { segment ->
-            val startTelling = LocalDate.of(segment.fom().year, segment.fom().month.plus(1).value,1)
-            val toMånederInnIOppholdet = startTelling.plusMonths(2) >= LocalDate.now()
+        val helseOppholdTidslinje = Tidslinje(helseopphold.map { segment ->
+            Segment(
+                segment.periode,
+                true
+            )
+        })
 
-            return@filter toMånederInnIOppholdet
+        val barnetilleggTidslinje = Tidslinje(barnetillegg.map { segment ->
+            Segment(
+                segment.periode,
+                true
+            )
+        })
+
+        //fjern perioder hvor bruker har barnetillegg gjennom hele helseinstitusjonsoppholdet
+        val oppholdUtenBarnetillegg = Tidslinje(helseOppholdTidslinje.filter { segment ->
+            Tidslinje(segment.periode, false).kombiner(barnetilleggTidslinje,
+                JoinStyle.LEFT_JOIN { periode, helseSegment, barnetilleggSegment ->
+                    Segment(periode, barnetilleggSegment?.verdi == true)
+                }
+            ).map { seg -> seg.verdi.not() }.any()
+        })
+
+        // Oppholdet må være lengre enn 3 måneder for å være aktuelt for avklaring og må ha vart i minimum 2 måneder for å være klar for avklaring
+        if (oppholdUtenBarnetillegg.segmenter().filter { segment -> segment.verdi }.any(
+                { segment ->
+                    val startTelling = LocalDate.of(segment.fom().year, segment.fom().month.plus(1).value,1)
+                    startTelling.plusMonths(3) < segment.periode.tom && startTelling.plusMonths(2) >= LocalDate.now()
+                }
+            )
+        ) {
+            return BehovForAvklaringer(false, true)
         }
 
-        // Dersom bruker har barnetillegg i perioden for innleggelse, er det ikke behov for avklaring
-        val harIkkeBarnetillegg = klarForAvklaringer.filter { segment ->
-            return@filter barnetillegg.map { it.periode }.none { it.overlapper(segment.periode) }
-        }
-
-
-        return BehovForAvklaringer(input.harUavklartSoningsforhold(), harIkkeBarnetillegg.isNotEmpty())
+        return BehovForAvklaringer(false, false)
     }
 
     private fun konstruerInput(behandlingId: BehandlingId): EtAnnetStedInput {
