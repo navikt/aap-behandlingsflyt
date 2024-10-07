7a3821c8f59a5df111ff5ba5c6cedf19ed627802
yongbinkwon <yongbinkwon97@gmail.com>
2024-10-07 15:55:13 +0200

    støtt mulighet for å legge til flere fritak på en gang + refaktorer tidslinjeberegning i egen klasse

--- app/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løser/FritakFraMeldepliktLøser.kt
+++ app/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løser/FritakFraMeldepliktLøser.kt
@@ -4,6 +4,7 @@ import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovKont
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løsning.FritakMeldepliktLøsning
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.meldeplikt.Fritaksvurdering
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.meldeplikt.MeldepliktRepository
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.meldeplikt.flate.FritaksvurderingDto
 import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingRepositoryImpl
 import no.nav.aap.komponenter.dbconnect.DBConnection
@@ -20,18 +21,18 @@ class FritakFraMeldepliktLøser(val connection: DBConnection) : Avklaringsbehovs
 
     override fun løs(kontekst: AvklaringsbehovKontekst, løsning: FritakMeldepliktLøsning): LøsningsResultat {
         val behandling = behandlingRepository.hent(kontekst.kontekst.behandlingId)
-        val fritaksvurdering = løsning.fritaksvurdering.toFritaksvurdering()
+        val fritaksvurderinger = løsning.fritaksvurderinger.map(FritaksvurderingDto::toFritaksvurdering)
 
         val eksisterendeFritaksvurderinger = meldepliktRepository.hentHvisEksisterer(behandling.id)?.vurderinger.orEmpty()
 
         meldepliktRepository.lagre(
             behandlingId = behandling.id,
-            vurderinger =  (eksisterendeFritaksvurderinger plasser fritaksvurdering).komprimer().map { it.verdi }
+            vurderinger =  (eksisterendeFritaksvurderinger plasser fritaksvurderinger).komprimer().map { it.verdi.copy(fraDato = it.periode.fom) }
         )
 
         return LøsningsResultat(
-            begrunnelse = fritaksvurdering.begrunnelse,
-            kreverToTrinn = fritaksvurdering.harFritak
+            begrunnelse = "Vurdert fritak meldeplikt",
+            kreverToTrinn = fritaksvurderinger.minstEttFritak()
         )
     }
 
@@ -39,9 +40,13 @@ class FritakFraMeldepliktLøser(val connection: DBConnection) : Avklaringsbehovs
         return Definisjon.FRITAK_MELDEPLIKT
     }
 
-    private infix fun List<Fritaksvurdering>.plasser(nyVurdering: Fritaksvurdering): Tidslinje<Fritaksvurdering> {
-        val sortedFritaksvurdering = this.sortedBy { it.opprettetTid } + nyVurdering
-        return sortedFritaksvurdering.drop(1).fold(sortedFritaksvurdering.first().tidslinje()) { acc, fritaksvurdering ->
+    //eller this.sorted.tidslinje.kombiner(nye.sorted.tidslinje)
+    private infix fun List<Fritaksvurdering>.plasser(nyeVurderinger: List<Fritaksvurdering>): Tidslinje<Fritaksvurdering> {
+        return (this.sortedBy { it.fraDato } + nyeVurderinger.sortedBy { it.fraDato }).tidslinje()
+    }
+
+    private fun List<Fritaksvurdering>.tidslinje(): Tidslinje<Fritaksvurdering> {
+        return this.drop(1).fold(this.first().tidslinje()) { acc, fritaksvurdering ->
             acc.kombiner(fritaksvurdering.tidslinje(), StandardSammenslåere.prioriterHøyreSideCrossJoin())
         }
     }
@@ -50,5 +55,9 @@ class FritakFraMeldepliktLøser(val connection: DBConnection) : Avklaringsbehovs
         return Tidslinje(
             listOf(Segment(Periode(fraDato, Tid.MAKS), this))
         )
+    }
+
+    private fun List<Fritaksvurdering>.minstEttFritak(): Boolean {
+        return this.any { it.harFritak }
     }
 }
--- app/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løsning/FritakMeldepliktLøsning.kt
+++ app/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løsning/FritakMeldepliktLøsning.kt
@@ -1,6 +1,5 @@
 package no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løsning
 
-import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonTypeName
@@ -8,14 +7,13 @@ import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovKont
 import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.FRITAK_MELDEPLIKT_KODE
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løser.FritakFraMeldepliktLøser
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løser.LøsningsResultat
-import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.meldeplikt.Fritaksvurdering
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.meldeplikt.flate.FritaksvurderingDto
 import no.nav.aap.komponenter.dbconnect.DBConnection
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonTypeName(value = FRITAK_MELDEPLIKT_KODE)
 class FritakMeldepliktLøsning(
-    @JsonProperty("fritaksvurdering", required = true) val fritaksvurdering: FritaksvurderingDto,
+    @JsonProperty("fritaksvurderinger", required = true) val fritaksvurderinger: List<FritaksvurderingDto>,
     @JsonProperty("behovstype", required = true, defaultValue = FRITAK_MELDEPLIKT_KODE) val behovstype: String = FRITAK_MELDEPLIKT_KODE
 ) : AvklaringsbehovLøsning {
 
