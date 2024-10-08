18bacf21966628560a90ddca6e828ed27929c451
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2024-10-08 13:35:21 +0200

    Implementer AktivtBidragRegel § 11-7

--- app/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/underveis/regler/AktivtBidragRegel.kt
+++ app/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/underveis/regler/AktivtBidragRegel.kt
@@ -1,5 +1,10 @@
 package no.nav.aap.behandlingsflyt.behandling.underveis.regler
 
+import no.nav.aap.behandlingsflyt.faktagrunnlag.dokument.arbeid.BruddAktivitetsplikt
+import no.nav.aap.behandlingsflyt.faktagrunnlag.dokument.arbeid.BruddAktivitetsplikt.Paragraf.PARAGRAF_11_7
+import no.nav.aap.behandlingsflyt.faktagrunnlag.dokument.arbeid.BruddAktivitetsplikt.Type.IKKE_AKTIVT_BIDRAG
+import no.nav.aap.tidslinje.JoinStyle
+import no.nav.aap.tidslinje.Segment
 import no.nav.aap.tidslinje.Tidslinje
 
 /**
@@ -8,6 +13,34 @@ import no.nav.aap.tidslinje.Tidslinje
  */
 class AktivtBidragRegel : UnderveisRegel {
     override fun vurder(input: UnderveisInput, resultat: Tidslinje<Vurdering>): Tidslinje<Vurdering> {
-        return resultat
+        val vurderinger = input.bruddAktivitetsplikt
+            .mapValue { brudd ->
+                when {
+                    brudd.type == IKKE_AKTIVT_BIDRAG -> {
+                        assert(brudd.paragraf == PARAGRAF_11_7)
+                        return@mapValue AktivtBidragVurdering(
+                            brudd = brudd,
+                            vilkårsvurdering = AktivtBidragVurdering.Vilkårsvurdering.VILKÅR_OPPFYLT,
+                        )
+                    }
+                    else -> {
+                        return@mapValue AktivtBidragVurdering(
+                            brudd = brudd,
+                            vilkårsvurdering = AktivtBidragVurdering.Vilkårsvurdering.IKKE_RELEVANT_BRUDD
+                        )
+                    }
+                }
+            }
+
+        return vurderinger.kombiner(
+            resultat,
+            JoinStyle.OUTER_JOIN
+            { periode, bruddSegment, vurderingSegment ->
+                if (bruddSegment == null) return@OUTER_JOIN vurderingSegment
+                val vurdering = (vurderingSegment?.verdi ?: Vurdering())
+                    .leggTilAktivtBidragVurdering(bruddSegment.verdi)
+                Segment(periode, vurdering)
+            },
+        )
     }
 }
