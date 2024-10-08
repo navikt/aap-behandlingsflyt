a7ec285d62e0d9941f569279b3c251eed9c2b9a6
yongbinkwon <yongbinkwon97@gmail.com>
2024-10-08 08:15:07 +0200

    use Fritaksperioder in FritakFraMeldepliktLøser

--- app/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løser/FritakFraMeldepliktLøser.kt
+++ app/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løser/FritakFraMeldepliktLøser.kt
@@ -2,17 +2,13 @@ package no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løser
 
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovKontekst
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løsning.FritakMeldepliktLøsning
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.meldeplikt.Fritaksperioder
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.meldeplikt.Fritaksvurdering
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.meldeplikt.MeldepliktRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.meldeplikt.flate.FritaksvurderingDto
 import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingRepositoryImpl
 import no.nav.aap.komponenter.dbconnect.DBConnection
-import no.nav.aap.komponenter.type.Periode
-import no.nav.aap.tidslinje.Segment
-import no.nav.aap.tidslinje.StandardSammenslåere
-import no.nav.aap.tidslinje.Tidslinje
-import no.nav.aap.verdityper.Tid
 
 class FritakFraMeldepliktLøser(val connection: DBConnection) : AvklaringsbehovsLøser<FritakMeldepliktLøsning> {
 
@@ -22,12 +18,11 @@ class FritakFraMeldepliktLøser(val connection: DBConnection) : Avklaringsbehovs
     override fun løs(kontekst: AvklaringsbehovKontekst, løsning: FritakMeldepliktLøsning): LøsningsResultat {
         val behandling = behandlingRepository.hent(kontekst.kontekst.behandlingId)
         val fritaksvurderinger = løsning.fritaksvurderinger.map(FritaksvurderingDto::toFritaksvurdering)
-
-        val eksisterendeFritaksvurderinger = meldepliktRepository.hentHvisEksisterer(behandling.id)?.vurderinger.orEmpty()
+        val eksisterendeFritaksperioder = Fritaksperioder(meldepliktRepository.hentHvisEksisterer(behandling.id)?.vurderinger.orEmpty())
 
         meldepliktRepository.lagre(
             behandlingId = behandling.id,
-            vurderinger =  (eksisterendeFritaksvurderinger plasser fritaksvurderinger).komprimer().map { it.verdi.copy(fraDato = it.periode.fom) }
+            vurderinger =  (eksisterendeFritaksperioder leggTil fritaksvurderinger).gjeldendeFritaksvurderinger()
         )
 
         return LøsningsResultat(
@@ -38,23 +33,6 @@ class FritakFraMeldepliktLøser(val connection: DBConnection) : Avklaringsbehovs
 
     override fun forBehov(): Definisjon {
         return Definisjon.FRITAK_MELDEPLIKT
-    }
-
-    //eller this.sorted.tidslinje.kombiner(nye.sorted.tidslinje)
-    private infix fun List<Fritaksvurdering>.plasser(nyeVurderinger: List<Fritaksvurdering>): Tidslinje<Fritaksvurdering> {
-        return (this.sortedBy { it.fraDato } + nyeVurderinger.sortedBy { it.fraDato }).tidslinje()
-    }
-
-    private fun List<Fritaksvurdering>.tidslinje(): Tidslinje<Fritaksvurdering> {
-        return this.drop(1).fold(this.first().tidslinje()) { acc, fritaksvurdering ->
-            acc.kombiner(fritaksvurdering.tidslinje(), StandardSammenslåere.prioriterHøyreSideCrossJoin())
-        }
-    }
-
-    private fun Fritaksvurdering.tidslinje(): Tidslinje<Fritaksvurdering> {
-        return Tidslinje(
-            listOf(Segment(Periode(fraDato, Tid.MAKS), this))
-        )
     }
 
     private fun List<Fritaksvurdering>.minstEttFritak(): Boolean {
