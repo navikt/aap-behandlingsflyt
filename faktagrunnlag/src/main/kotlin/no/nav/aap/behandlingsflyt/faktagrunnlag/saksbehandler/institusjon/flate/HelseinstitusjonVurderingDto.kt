063068ed2ef3db8a2759a7cbb8581f9e18408aea
Marius Glittum <marius.glittum@nav.no>
2024-10-28 14:01:52 +0100

    Fjerner ubrukt metode

--- faktagrunnlag/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/saksbehandler/institusjon/flate/HelseinstitusjonVurderingDto.kt
+++ faktagrunnlag/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/saksbehandler/institusjon/flate/HelseinstitusjonVurderingDto.kt
@@ -1,6 +1,5 @@
 package no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.institusjon.flate
 
-import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.institusjon.HelseinstitusjonVurdering
 import no.nav.aap.komponenter.type.Periode
 
 data class HelseinstitusjonVurderingDto(
@@ -9,13 +8,4 @@ data class HelseinstitusjonVurderingDto(
     val faarFriKostOgLosji: Boolean,
     val forsoergerEktefelle: Boolean? = null,
     val harFasteUtgifter: Boolean? = null,
-) {
-
-    fun tilDomeneobjekt() = HelseinstitusjonVurdering(
-        begrunnelse = begrunnelse,
-        faarFriKostOgLosji = faarFriKostOgLosji,
-        forsoergerEktefelle = forsoergerEktefelle,
-        harFasteUtgifter = harFasteUtgifter,
-        periode = periode
-    )
-}
+)
