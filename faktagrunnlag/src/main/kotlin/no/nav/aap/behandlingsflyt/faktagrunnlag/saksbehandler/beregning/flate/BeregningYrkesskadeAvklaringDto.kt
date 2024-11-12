3b58117f827e351074f7a08bb17b1a135c7e8f6c
Marius Glittum <marius.glittum@nav.no>
2024-11-12 14:21:24 +0100

    Endepunkt for yrkesskade grunnlag
    Co-authored-by: Anders Østby <anders.ostby@nav.no>
    Co-authored-by: Thomas Rognes <thomas.sebastian.rognes@nav.no>

---
+++ faktagrunnlag/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/saksbehandler/beregning/flate/BeregningYrkesskadeAvklaringDto.kt
@@ -0,0 +1,12 @@
+package no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.beregning.flate
+
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.beregning.YrkesskadeBeløpVurdering
+import no.nav.aap.verdityper.Beløp
+import java.time.LocalDate
+
+data class BeregningYrkesskadeAvklaringDto(
+    val skalVurderes: List<YrkesskadeTilVurdering>,
+    val vurderinger: List<YrkesskadeBeløpVurdering>
+)
+
+data class YrkesskadeTilVurdering(val referanse: String, val skadeDato: LocalDate, val grunnbeløp: Beløp)
