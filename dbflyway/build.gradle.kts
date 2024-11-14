6d51affd624bd6212f690e9a3491aa6e572c8edd
Fredrik Meyer <fredrik.meyer@nav.no>
2024-11-14 08:49:25 +0100

    Test på æøå i migreringsskript

--- dbflyway/build.gradle.kts
+++ dbflyway/build.gradle.kts
@@ -8,4 +8,8 @@ dependencies {
     implementation("no.nav.aap.kelvin:dbmigrering:$komponenterVersjon")
     implementation("org.flywaydb:flyway-database-postgresql:10.18.2")
     runtimeOnly("org.postgresql:postgresql:42.7.4")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.3")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.3")
+    testImplementation("org.assertj:assertj-core:3.26.3")
 }
