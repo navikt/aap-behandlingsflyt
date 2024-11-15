edc7c9a6fa4466303fc4fcd2da749c52abdc3647
Henrik.Bugge.Halvorsen <henrik.bugge.halvorsen@nav.no>
2024-11-15 10:41:21 +0100

    * fjerne sjekk mot venteårsak grunn * benytte behandlingsId i oppslag av dokumenter * tester for behovløsing på avslåtte legeerklæringer

--- app/src/main/kotlin/no/nav/aap/behandlingsflyt/flyt/ventebehov/LegeerklæringVentebehovEvaluerer.kt
+++ app/src/main/kotlin/no/nav/aap/behandlingsflyt/flyt/ventebehov/LegeerklæringVentebehovEvaluerer.kt
@@ -1,10 +1,8 @@
 package no.nav.aap.behandlingsflyt.flyt.ventebehov
 
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.Avklaringsbehov
-import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løser.ÅrsakTilSettPåVent
 import no.nav.aap.behandlingsflyt.faktagrunnlag.dokument.MottattDokumentRepository
 import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon
-import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingRepositoryImpl
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.dokumenter.Brevkode
 import no.nav.aap.komponenter.dbconnect.DBConnection
 import no.nav.aap.verdityper.sakogbehandling.BehandlingId
@@ -16,13 +14,9 @@ class LegeerklæringVentebehovEvaluerer(private val connection: DBConnection): S
 
     override fun ansesSomLøst(behandlingId: BehandlingId, avklaringsbehov: Avklaringsbehov): Boolean {
         val mottattDokumentRepository = MottattDokumentRepository(connection)
-        val behandlingRepository = BehandlingRepositoryImpl(connection)
-
-        val behandling = behandlingRepository.hent(behandlingId)
-        val avslåtteDokumenter = mottattDokumentRepository.hentDokumenterAvType(behandling.sakId, Brevkode.LEGEERKLÆRING_AVSLÅTT)
-
+        val avslåtteDokumenter = mottattDokumentRepository.hentDokumenterAvType(behandlingId, Brevkode.LEGEERKLÆRING_AVSLÅTT)
         val relevanteAvslåtteDokumenter = avslåtteDokumenter.filter { it.behandlingId == behandlingId }
 
-        return relevanteAvslåtteDokumenter.any() && avklaringsbehov.erÅpent() && avklaringsbehov.grunn() == ÅrsakTilSettPåVent.VENTER_PÅ_MEDISINSKE_OPPLYSNINGER
+        return relevanteAvslåtteDokumenter.any() && avklaringsbehov.erÅpent()
     }
 }
