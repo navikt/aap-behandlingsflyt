ac3e37a1fcf7358c32e3265d60d4360235cc4f99
HenrikBuggeHalvorsen <henrik.bugge.halvorsen@nav.no>
2024-11-18 14:17:40 +0100

    ett stk brev-støtte får frontenden.

--- lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/test/FakeServers.kt
+++ lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/test/FakeServers.kt
@@ -307,7 +307,25 @@ object FakeServers : AutoCloseable {
         routing {
             post("/api/dokumentinnhenting/syfo/brevpreview") {
                 val req = call.receive<ForhåndsvisBrevRequest>()
-                call.respond(req.fritekst)
+                val fnr = 12341234123
+                val navn = "Ronny Råkjører"
+
+                val brev =  """
+                    Forespørsel om legeerklæring ved arbeidsuførhet\n
+                    Gjelder pasient: $navn., $fnr.\n
+                    Nav trenger opplysninger fra deg vedrørende din pasient. Du kan utelate opplysninger som etter din vurdering faller utenfor formålet.\n
+                    «Legeerklæring ved arbeidsuførhet» leveres på blankett Nav 08-07.08, og honoreres med takst L40.\n
+                    ${req.fritekst}\n
+                    Lovhjemmel\n
+                    Folketrygdloven § 21-4 andre ledd gir Nav rett til å innhente nødvendige opplysninger. Dette gjelder selv om opplysningene er taushetsbelagte, jf. § 21-4 sjette ledd.\n
+                    Pålegget om utlevering av opplysninger kan påklages etter forvaltningsloven § 14.\n
+                    Klageadgangen gjelder kun lovligheten i pålegget. Fristen for å klage er tre dager etter at pålegget er mottatt. Klagen kan fremsettes muntlig eller skriftlig.\n
+                    Med vennlig hilsen\n
+                    ${req.veilederNavn}\n
+                    Nav
+                """.trimIndent()
+
+                call.respond(brev)
             }
         }
     }
