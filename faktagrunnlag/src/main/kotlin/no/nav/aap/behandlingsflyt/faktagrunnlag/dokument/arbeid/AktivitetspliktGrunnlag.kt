1f1a568f026ab3384306ae6404e7fd07ab494f8e
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2024-12-02 16:37:42 +0100

    Aktivitetsplikt: beregn tidslinje uten antagelse om overlapp ved endring

--- faktagrunnlag/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/dokument/arbeid/AktivitetspliktGrunnlag.kt
+++ faktagrunnlag/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/dokument/arbeid/AktivitetspliktGrunnlag.kt
@@ -1,42 +1,26 @@
 package no.nav.aap.behandlingsflyt.faktagrunnlag.dokument.arbeid
 
-import no.nav.aap.komponenter.tidslinje.JoinStyle
 import no.nav.aap.komponenter.tidslinje.Segment
+import no.nav.aap.komponenter.tidslinje.StandardSammenslåere
 import no.nav.aap.komponenter.tidslinje.Tidslinje
 
 data class AktivitetspliktGrunnlag(
     val bruddene: Set<AktivitetspliktDokument>,
 ) {
-    fun tidslinje(paragraf: Brudd.Paragraf): Tidslinje<AktivitetspliktRegistrering> {
+    fun dokumentTidslinje(paragraf: Brudd.Paragraf): Tidslinje<AktivitetspliktDokument> {
         return bruddene
             .asSequence()
             .filter { it.brudd.paragraf == paragraf }
+            .sortedBy { it.metadata.opprettetTid }
             .map { Tidslinje(it.brudd.periode, it) }
             .fold(Tidslinje()) { bruddtidslinje1, bruddtidslinje2 ->
-                bruddtidslinje1.kombiner(bruddtidslinje2, JoinStyle.OUTER_JOIN { periode, brudd1, brudd2 ->
-                    merge(brudd1?.verdi, brudd2?.verdi)?.let {
-                        Segment(periode, it)
-                    }
-                })
+                bruddtidslinje1.kombiner(bruddtidslinje2, StandardSammenslåere.prioriterHøyreSideCrossJoin())
             }
     }
 
-    companion object {
-        private fun merge(
-            dokument1: AktivitetspliktDokument?,
-            dokument2: AktivitetspliktDokument?
-        ): AktivitetspliktRegistrering? {
-            val nyesteDokument = when {
-                dokument1 == null || dokument2 == null -> dokument1 ?: dokument2
-                ?: error("outer join hvor begge sider er null")
-
-                dokument1.metadata.opprettetTid < dokument2.metadata.opprettetTid -> dokument2
-                else -> dokument1
-            }
-            return when (nyesteDokument) {
-                is AktivitetspliktFeilregistrering -> null
-                is AktivitetspliktRegistrering -> nyesteDokument
-            }
-        }
+    fun tidslinje(paragraf: Brudd.Paragraf): Tidslinje<AktivitetspliktRegistrering> {
+        return dokumentTidslinje(paragraf)
+            .filterIsInstance<Segment<AktivitetspliktRegistrering>>()
+            .let(::Tidslinje)
     }
 }
