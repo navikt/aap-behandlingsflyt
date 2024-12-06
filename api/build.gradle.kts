a4eab5212506ae8bc55097d247103cd4f628563b
Fredrik Meyer <fredrik.meyer@nav.no>
2024-12-06 14:04:21 +0100

    Manglende test-avhengighet

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -18,6 +18,7 @@ dependencies {
     api("no.nav.aap.tilgang:api-kontrakt:$tilgangVersjon")
     compileOnly("io.ktor:ktor-http-jvm:$ktorVersion")
 
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.3")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.3")
     testImplementation("org.assertj:assertj-core:3.26.3")
 }
