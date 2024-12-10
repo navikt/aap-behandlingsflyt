406d61a5c4f910811d3c5c595c0056454fb95cd7
Fredrik Meyer <fredrik.meyer@nav.no>
2024-12-10 16:38:01 +0100

    Enda en iterasjon

--- kontrakt/src/main/kotlin/no/nav/aap/behandlingsflyt/kontrakt/hendelse/MottattHendelseDto.kt
+++ kontrakt/src/main/kotlin/no/nav/aap/behandlingsflyt/kontrakt/hendelse/MottattHendelseDto.kt
@@ -1,11 +1,13 @@
 package no.nav.aap.behandlingsflyt.kontrakt.hendelse
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+import no.nav.aap.behandlingsflyt.kontrakt.hendelse.Melding.Søknad
 import no.nav.aap.behandlingsflyt.kontrakt.sak.Saksnummer
+import no.nav.aap.verdityper.dokument.JournalpostId
 import no.nav.aap.verdityper.dokument.Kanal
 import java.time.LocalDate
 
 public data class MottattHendelseDto(
-    // ha denne i url i stedet?
     val saksnummer: Saksnummer,
     val type: InnsendingType,
     val kanal: Kanal,
@@ -15,23 +17,42 @@ public data class MottattHendelseDto(
 
 public class Innsending(
     public val saksnummer: Saksnummer,
-    public val type: InnsendingType,
     public val referanse: InnsendingReferanse,
-    public val melding: Melding
+    public val type: InnsendingType,
+    public val melding: Melding,
 )
 
-public sealed interface Melding
 
-public sealed interface Pliktkort : Melding
+///**
+// * @sample no.nav.aap.behandlingsflyt.kontrakt.hendelse.example
+// */
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "meldingType", visible = true)
+public interface Melding {
+    public sealed interface Søknad : Melding
+    public sealed interface Pliktkort : Melding
+    public sealed interface Dialogmelding : Melding
+    public sealed interface Legeerklærling : Melding
+    public sealed interface LegeerklærlingAvvist : Melding
+    public sealed interface DialogMelding : Melding
+}
 
-public class PliktkortV0(public val jfnr: Double) : Pliktkort
 
-public sealed interface Søknad : Melding
+// SØKNAD
 
-public class SøknadV0(
+/**
+ * Dagens søknad-objekt. Ved inkompatibel endring, lag ny versjon.
+ */
+public data class SøknadV0(
     public val student: SøknadStudentDto,
     public val yrkesskade: String,
     public val oppgitteBarn: OppgitteBarn?
+) : Søknad
+
+public data class SøknadV1(
+    public val student: SøknadStudentDto,
+    public val yrkesskade: String,
+    public val oppgitteBarn: OppgitteBarn?,
+    public val nyttFelt: Number
 ) : Søknad
 
 public class SøknadStudentDto(
@@ -39,68 +60,48 @@ public class SøknadStudentDto(
     public val kommeTilbake: String? = null
 )
 
-public class SøknadV1(
-    public val student: SøknadStudentDto,
-    public val yrkesskade: String,
-    public val oppgitteBarn: OppgitteBarn?,
-    public val nyttFelt: Number
-) : Søknad
-
 public class OppgitteBarn(public val identer: Set<String>)
 
-public sealed interface Legeerklærling : Melding
+// PLIKTKORT
 
-public class LegeerklæringV0(public val fnr: Number) : Legeerklærling
+public class PliktkortV0(public val jfnr: Double, type: InnsendingType) : Melding.Pliktkort
+
+// LEGEERKLÆRING
+
+public class LegeerklæringV0(
+    public val journalpostId: String
+) : Melding.Legeerklærling
+
+// DIALOGMELDING
+
+public class DialogmeldingV0(public val fnr: Number) : Melding.Dialogmelding
 
 
-fun example(innsending: Innsending) {
+/**
+ * Eksempel på hvordan håndtere meldingtyper.
+ */
+private fun example(innsending: Innsending) {
     when (innsending.melding) {
-        is Søknad -> when (innsending.melding) {
+        is Melding.Søknad -> when (innsending.melding) {
             is SøknadV0 -> TODO()
             is SøknadV1 -> TODO()
         }
 
-        is Pliktkort -> when (innsending.melding) {
+        is Melding.Pliktkort -> when (innsending.melding) {
             is PliktkortV0 -> TODO()
         }
 
-        is Legeerklærling -> when (innsending.melding) {
+        is Melding.Legeerklærling -> when (innsending.melding) {
             is LegeerklæringV0 -> TODO()
         }
+
+        is Melding.Dialogmelding -> TODO()
     }
 }
 
-
-//@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "behovstype", visible = true)
-//public sealed interface Innsending {
-//    public val saksnummer: Saksnummer
-//    public val type: InnsendingType
-//    public val referanse: InnsendingReferanse
-//}
-
-//public class Pliktkort(
-//    override val referanse: InnsendingReferanse,
-//    override val saksnummer: Saksnummer,
-//    override val type: InnsendingType,
-//    public val timerArbeidet: List<PeriodeMedArbeid>
-//) : Innsending
 
 public data class PeriodeMedArbeid(
     val fom: LocalDate,
     val tom: LocalDate?,
     public val timerIArbeid: Double
 )
-
-//public class Søknad(
-//    override val referanse: InnsendingReferanse,
-//    override val saksnummer: Saksnummer,
-//    override val type: InnsendingType,
-//    public val søknad: Any // Søknad-felter
-//) : Innsending
-//
-//public class Legeærklaring(
-//    override val referanse: InnsendingReferanse,
-//    override val saksnummer: Saksnummer,
-//    override val type: InnsendingType,
-//    public val ff: String
-//) : Innsending
