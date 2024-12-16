f19b9aa7b1df2dfa319416ef3ad8c4e9a22e92ed
Fredrik Meyer <fredrik.meyer@nav.no>
2024-12-16 11:00:29 +0100

    Etter kommentarer

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/delvurdering/beregning/BeregningsgrunnlagRepository.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/delvurdering/beregning/BeregningsgrunnlagRepository.kt
@@ -3,7 +3,7 @@ package no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.beregning
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingId
 import no.nav.aap.lookup.repository.Repository
 
-interface BeregningsgrunnlagRepository : Repository{
+interface BeregningsgrunnlagRepository : Repository {
     fun hentHvisEksisterer(behandlingId: BehandlingId): Beregningsgrunnlag?
     fun lagre(behandlingId: BehandlingId, beregningsgrunnlag: Beregningsgrunnlag)
     override fun kopier(fraBehandling: BehandlingId, tilBehandling: BehandlingId)
