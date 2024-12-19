eb46aa12c74af0991bfcc5630280d43fc06ee516
Marius Glittum <marius.glittum@nav.no>
2024-12-19 12:04:22 +0100

    gjør connection private og legger på en metode med kommentar som beskriver hvorfor denne er public og at man ikke skal bruke den

--- behandlingsflyt/src/main/kotlin/no/nav/aap/lookup/repository/RepositoryProvider.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/lookup/repository/RepositoryProvider.kt
@@ -7,7 +7,7 @@ import kotlin.reflect.full.companionObjectInstance
 import kotlin.reflect.full.isSubclassOf
 import kotlin.reflect.full.starProjectedType
 
-class RepositoryProvider(val connection: DBConnection) {
+class RepositoryProvider(private val connection: DBConnection) {
 
     inline fun <reified T : Repository> provide(type: KClass<T>): T {
         val repositoryKlass = RepositoryRegistry.fetch(type.starProjectedType)
@@ -28,12 +28,21 @@ class RepositoryProvider(val connection: DBConnection) {
             "Repository må ha companion object"
         }
         if (companionObject is Factory<*>) {
-            return companionObject.konstruer(connection) as T
+            return companionObject.konstruer(connection()) as T
         }
         throw IllegalStateException("Repository må ha et companion object som implementerer Factory<T> interfacet.")
     }
 
     fun provideAlle(): List<Repository> {
         return RepositoryRegistry.alle().map { klass -> internalCreate(klass) }
+    }
+
+    /**
+     * NB! Ikke til bruk utenfor denne klassen
+     *
+     * Er kun synlig for inline reified functions ovenfor
+     */
+    fun connection(): DBConnection {
+        return connection
     }
 }
