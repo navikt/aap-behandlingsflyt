ac926d532ec5631c2c005a7939a0f0c192bb5f76
HenrikBuggeHalvorsen <henrik.bugge.halvorsen@nav.no>
2025-01-06 14:41:24 +0100

    Flytt distinct ut i større scope slik at den faktisk fjerner på unike dokumentInfoId

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/sakogbehandling/sak/adapters/SafListDokumentGateway.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/sakogbehandling/sak/adapters/SafListDokumentGateway.kt
@@ -58,8 +58,7 @@ object SafListDokumentGateway {
                             }!!
                         )
                     }
-                    .distinctBy { it.dokumentInfoId }
-            }
+            }.distinctBy { it.dokumentInfoId }
         }
     }
 }
