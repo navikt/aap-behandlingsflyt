6d1586401ecd22cb38d81a51fb3f625196ddca5d
HenrikBuggeHalvorsen <henrik.bugge.halvorsen@nav.no>
2025-01-06 14:27:05 +0100

    fjern like dokumenter fra oppslag som begge peker til samme original-dokument

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/sakogbehandling/sak/adapters/SafListDokumentGateway.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/sakogbehandling/sak/adapters/SafListDokumentGateway.kt
@@ -58,6 +58,7 @@ object SafListDokumentGateway {
                             }!!
                         )
                     }
+                    .distinctBy { it.dokumentInfoId }
             }
         }
     }
