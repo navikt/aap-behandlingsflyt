d20507de57cfda33bb7a70dde6f6c7f3a3782a4a
HenrikBuggeHalvorsen <henrik.bugge.halvorsen@nav.no>
2025-01-31 15:45:32 +0100

    et par tester til medlemskap vurdering service

---
+++ behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/behandling/vilkår/medlemskap/MedlemskapLovvalgVurderingServiceTest.kt
@@ -0,0 +1,72 @@
+package no.nav.aap.behandlingsflyt.behandling.vilkår.medlemskap
+
+import no.nav.aap.behandlingsflyt.behandling.lovvalg.MedlemskapArbeidInntektGrunnlag
+import no.nav.aap.behandlingsflyt.behandling.lovvalg.MedlemskapLovvalgGrunnlag
+import no.nav.aap.behandlingsflyt.faktagrunnlag.register.medlemskap.MedlemskapUnntakGrunnlag
+import no.nav.aap.behandlingsflyt.faktagrunnlag.register.medlemskap.Unntak
+import no.nav.aap.behandlingsflyt.faktagrunnlag.register.personopplysninger.Fødselsdato
+import no.nav.aap.behandlingsflyt.faktagrunnlag.register.personopplysninger.Personopplysning
+import no.nav.aap.behandlingsflyt.faktagrunnlag.register.personopplysninger.PersonopplysningGrunnlag
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.utenlandsopphold.UtenlandsOppholdData
+import no.nav.aap.behandlingsflyt.sakogbehandling.sak.adapters.PersonStatus
+import no.nav.aap.komponenter.tidslinje.Segment
+import no.nav.aap.komponenter.type.Periode
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+
+class MedlemskapLovvalgVurderingServiceTest {
+    private val service = MedlemskapLovvalgVurderingService()
+
+    @Test
+    fun automatiskOmAlleKravErOppfylt() {
+        val grunnlag = MedlemskapLovvalgGrunnlag(
+            medlemskapArbeidInntektGrunnlag = MedlemskapArbeidInntektGrunnlag(
+                medlemskapGrunnlag = MedlemskapUnntakGrunnlag(
+                    unntak = listOf(
+                        Segment(
+                            periode = Periode(LocalDate.now().minusMonths(1), LocalDate.now()),
+                            verdi = Unntak("unntak", "statusaarsak", true, "grunnlag", "lovvalg", false, EØSLand.NOR.toString())
+                        )
+                    )
+                ),
+                inntekterINorgeGrunnlag = emptyList(),
+                arbeiderINorgeGrunnlag = emptyList()
+            ),
+            personopplysningGrunnlag = PersonopplysningGrunnlag(
+                brukerPersonopplysning = Personopplysning(Fødselsdato(LocalDate.now().minusYears(18)), 1, null, EØSLand.NOR.toString(), LocalDate.now().minusMonths(1), null, PersonStatus.bosatt),
+                relatertePersonopplysninger = null
+            ),
+            nyeSoknadGrunnlag = UtenlandsOppholdData(true, true, false, false, null)
+        )
+
+        val resultat = service.vurderTilhørighet(grunnlag, Periode(LocalDate.now().minusYears(1), LocalDate.now()))
+        assertEquals(true, resultat.kanBehandlesAutomatisk)
+    }
+
+    @Test
+    fun manuellOmLovvalgslandIkkeErNorge() {
+        val grunnlag = MedlemskapLovvalgGrunnlag(
+            medlemskapArbeidInntektGrunnlag = MedlemskapArbeidInntektGrunnlag(
+                medlemskapGrunnlag = MedlemskapUnntakGrunnlag(
+                    unntak = listOf(
+                        Segment(
+                            periode = Periode(LocalDate.now().minusMonths(1), LocalDate.now()),
+                            verdi = Unntak("unntak", "statusaarsak", true, "grunnlag", "lovvalg", false, EØSLand.SWE.toString())
+                        )
+                    )
+                ),
+                inntekterINorgeGrunnlag = emptyList(),
+                arbeiderINorgeGrunnlag = emptyList()
+            ),
+            personopplysningGrunnlag = PersonopplysningGrunnlag(
+                brukerPersonopplysning = Personopplysning(Fødselsdato(LocalDate.now().minusYears(18)), 1, null, EØSLand.NOR.toString(), LocalDate.now().minusMonths(1), null, PersonStatus.bosatt),
+                relatertePersonopplysninger = null
+            ),
+            nyeSoknadGrunnlag = UtenlandsOppholdData(true, true, false, false, null)
+        )
+
+        val resultat = service.vurderTilhørighet(grunnlag, Periode(LocalDate.now().minusYears(1), LocalDate.now()))
+        assertEquals(false, resultat.kanBehandlesAutomatisk)
+    }
+}
