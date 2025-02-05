173461384918c439e983e7417ef3da3fdfc9f74a
Øivind Stensrud <oivind.stensrud@nav.no>
2025-02-05 14:41:12 +0100

    Eksponerer behandlingstype for frontend via flyt

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/flyt/flate/visning/Visning.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/flyt/flate/visning/Visning.kt
@@ -1,10 +1,13 @@
 package no.nav.aap.behandlingsflyt.flyt.flate.visning
 
+import no.nav.aap.behandlingsflyt.kontrakt.behandling.TypeBehandling
+
 data class Visning(
     val saksbehandlerReadOnly: Boolean,
     val beslutterReadOnly: Boolean,
     val visBeslutterKort: Boolean,
     val visVentekort: Boolean,
     val kvalitetssikringReadOnly: Boolean,
-    val visKvalitetssikringKort: Boolean
+    val visKvalitetssikringKort: Boolean,
+    val typeBehandling: TypeBehandling
 )
