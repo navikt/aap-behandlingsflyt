0c81009eb34494a960c1a7f8c32231e75e19e182
Fredrik Meyer <fredrik.meyer@nav.no>
2025-02-10 16:28:53 +0100

    Docstring på Informasjonskrav

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/Informasjonskrav.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/Informasjonskrav.kt
@@ -1,12 +1,21 @@
 package no.nav.aap.behandlingsflyt.faktagrunnlag
 
+import no.nav.aap.behandlingsflyt.flyt.steg.BehandlingSteg
 import no.nav.aap.behandlingsflyt.sakogbehandling.flyt.FlytKontekstMedPerioder
 
+/**
+ * Et _Informasjonskrav_ har ansvar for å hente inn nødvendig informasjon for et gitt [BehandlingSteg].
+ */
 interface Informasjonskrav {
     enum class Endret {
         ENDRET,
         IKKE_ENDRET,
     }
 
+    /**
+     * Skal sjekke om det har kommet ny informasjon på dette kravet, og persistere ny informasjon.
+     *
+     * Om det ikke er noen endringer, returneres [Endret.IKKE_ENDRET].
+     */
     fun oppdater(kontekst: FlytKontekstMedPerioder): Endret
 }
