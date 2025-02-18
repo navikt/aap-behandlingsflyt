6def0d5c0f195b9cb05a72d01304a7a350a5da60
Fredrik Meyer <fredrik.meyer@nav.no>
2025-02-18 16:20:42 +0100

    Litt ryddigere med noen klasser i samme fil

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/samordning/SamordningGradering.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/samordning/SamordningGradering.kt
@@ -3,3 +3,5 @@ package no.nav.aap.behandlingsflyt.behandling.samordning
 import no.nav.aap.komponenter.verdityper.Prosent
 
 data class SamordningGradering(val gradering: Prosent, val ytelsesGraderinger: List<YtelseGradering>)
+
+data class YtelseGradering(val ytelse: Ytelse, val gradering: Prosent)
--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/samordning/YtelseGradering.kt
+++
@@ -1,5 +0,0 @@
-package no.nav.aap.behandlingsflyt.behandling.samordning
-
-import no.nav.aap.komponenter.verdityper.Prosent
-
-data class YtelseGradering(val ytelse: Ytelse, val gradering: Prosent)
