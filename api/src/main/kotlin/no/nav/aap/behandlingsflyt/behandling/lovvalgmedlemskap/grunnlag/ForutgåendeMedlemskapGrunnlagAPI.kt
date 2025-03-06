b9e079057a4f26e9b874b5644e16ef503a005ca4
HenrikBuggeHalvorsen <henrik.bugge.halvorsen@nav.no>
2025-03-06 10:19:22 +0100

    benytte tilgangplugin i grunnlagsapier

--- api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/lovvalgmedlemskap/grunnlag/ForutgåendeMedlemskapGrunnlagAPI.kt
+++ api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/lovvalgmedlemskap/grunnlag/ForutgåendeMedlemskapGrunnlagAPI.kt
@@ -1,7 +1,6 @@
 package no.nav.aap.behandlingsflyt.behandling.lovvalgmedlemskap.grunnlag
 
 import com.papsign.ktor.openapigen.route.path.normal.NormalOpenAPIRoute
-import com.papsign.ktor.openapigen.route.path.normal.get
 import com.papsign.ktor.openapigen.route.response.respond
 import com.papsign.ktor.openapigen.route.route
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.medlemskap.MedlemskapArbeidInntektForutgåendeRepository
@@ -10,12 +9,17 @@ import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingRepositor
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.flate.BehandlingReferanseService
 import no.nav.aap.komponenter.dbconnect.transaction
 import no.nav.aap.lookup.repository.RepositoryProvider
+import no.nav.aap.tilgang.AuthorizationParamPathConfig
+import no.nav.aap.tilgang.BehandlingPathParam
+import no.nav.aap.tilgang.authorizedGet
 import javax.sql.DataSource
 
 fun NormalOpenAPIRoute.forutgåendeMedlemskapAPI(dataSource: DataSource) {
     route("/api/behandling") {
         route("/{referanse}/grunnlag/forutgaaendemedlemskap") {
-            get<BehandlingReferanse, ForutgåendeMedlemskapGrunnlagDto> { req ->
+            authorizedGet<BehandlingReferanse, ForutgåendeMedlemskapGrunnlagDto>(
+                pathConfig = AuthorizationParamPathConfig(behandlingPathParam = BehandlingPathParam("referanse"))
+            ) { req ->
                 val grunnlag = dataSource.transaction { connection ->
                     val repositoryProvider = RepositoryProvider(connection)
                     val behandlingRepository = repositoryProvider.provide<BehandlingRepository>()
