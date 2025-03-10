dfdb6470863bbf125963a7e32c231413c187af5c
HenrikBuggeHalvorsen <henrik.bugge.halvorsen@nav.no>
2025-03-10 10:34:23 +0100

    minimalistisk løser for utenlandsk videreføring

---
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løser/UtenlandskVidereføringLøser.kt
@@ -0,0 +1,18 @@
+package no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løser
+
+import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovKontekst
+import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løsning.UtenlandskVidereføringLøsning
+import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon
+import no.nav.aap.komponenter.dbconnect.DBConnection
+
+class UtenlandskVidereføringLøser(val connection: DBConnection) :
+    AvklaringsbehovsLøser<UtenlandskVidereføringLøsning> {
+    override fun løs(kontekst: AvklaringsbehovKontekst, løsning: UtenlandskVidereføringLøsning): LøsningsResultat {
+        // TODO: Kreve at de legger inn ny data her, denne vil låse seg selv igjen til ny manuell vurdering kommer inn
+        return LøsningsResultat("Tatt av vent (ventet på utenlandsk videreføring)")
+    }
+
+    override fun forBehov(): Definisjon {
+        return Definisjon.VENTE_PÅ_UTENLANDSK_VIDEREFØRING_AVKLARING
+    }
+}
