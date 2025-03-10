e87f3077e62b1d77bd37d32ec62e9906e176136f
HenrikBuggeHalvorsen <henrik.bugge.halvorsen@nav.no>
2025-03-10 10:34:23 +0100

    minimalistisk løser for utenlandsk videreføring

---
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løsning/UtenlandskVidereføringLøsning.kt
@@ -0,0 +1,23 @@
+package no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løsning
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonTypeName
+import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovKontekst
+import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løser.LøsningsResultat
+import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løser.UtenlandskVidereføringLøser
+import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.AVKLAR_UTENLANDSK_MEDLEMSKAP_KODE
+import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.AvklaringsbehovKode
+import no.nav.aap.komponenter.dbconnect.DBConnection
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonTypeName(value = AVKLAR_UTENLANDSK_MEDLEMSKAP_KODE)
+class UtenlandskVidereføringLøsning(
+    @JsonProperty(
+        "behovstype", required = true, defaultValue = AVKLAR_UTENLANDSK_MEDLEMSKAP_KODE
+    ) val behovstype: AvklaringsbehovKode = AvklaringsbehovKode.`5019`
+) : AvklaringsbehovLøsning {
+    override fun løs(connection: DBConnection, kontekst: AvklaringsbehovKontekst): LøsningsResultat {
+        return UtenlandskVidereføringLøser(connection).løs(kontekst, this)
+    }
+}
