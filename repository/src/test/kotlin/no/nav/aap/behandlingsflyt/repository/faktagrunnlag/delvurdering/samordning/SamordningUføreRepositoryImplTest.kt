80982448898a0c942401cb7af642fb3790a9e5c6
charliemidtlyng <charlie.midtlyng@bekk.no>
2025-03-12 14:02:11 +0100

    Lagt til test for samordningUføreRepositoryImpl

---
+++ repository/src/test/kotlin/no/nav/aap/behandlingsflyt/repository/faktagrunnlag/delvurdering/samordning/SamordningUføreRepositoryImplTest.kt
@@ -0,0 +1,76 @@
+package no.nav.aap.behandlingsflyt.repository.faktagrunnlag.delvurdering.samordning
+
+import no.nav.aap.behandlingsflyt.faktagrunnlag.GrunnlagKopierer
+import no.nav.aap.behandlingsflyt.faktagrunnlag.SakOgBehandlingService
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.uførevurdering.SamordningUføreVurdering
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.uførevurdering.SamordningUføreVurderingPeriode
+import no.nav.aap.behandlingsflyt.repository.avklaringsbehov.FakePdlGateway
+import no.nav.aap.behandlingsflyt.repository.behandling.BehandlingRepositoryImpl
+import no.nav.aap.behandlingsflyt.repository.faktagrunnlag.delvurdering.samordning.ytelsesvurdering.SamordningYtelseVurderingRepositoryImpl
+import no.nav.aap.behandlingsflyt.repository.sak.PersonRepositoryImpl
+import no.nav.aap.behandlingsflyt.repository.sak.SakRepositoryImpl
+import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.Behandling
+import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.Årsak
+import no.nav.aap.behandlingsflyt.sakogbehandling.flyt.ÅrsakTilBehandling
+import no.nav.aap.behandlingsflyt.sakogbehandling.sak.PersonOgSakService
+import no.nav.aap.behandlingsflyt.sakogbehandling.sak.Sak
+import no.nav.aap.behandlingsflyt.test.ident
+import no.nav.aap.komponenter.dbconnect.DBConnection
+import no.nav.aap.komponenter.dbconnect.transaction
+import no.nav.aap.komponenter.dbtest.InitTestDatabase
+import no.nav.aap.komponenter.type.Periode
+import no.nav.aap.komponenter.verdityper.Prosent
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+
+class SamordningUføreRepositoryImplTest {
+    private val periode = Periode(LocalDate.now(), LocalDate.now().plusYears(3))
+
+    @Test
+    fun `skal lagre ned en helt ny vurdering`() {
+        val dataSource = InitTestDatabase.dataSource
+        val behandling = dataSource.transaction {
+            behandling(it, sak(it))
+        }
+
+        // Lagre ytelse
+        val vurdering = SamordningUføreVurdering(begrunnelse = "En fin begrunnelse", vurderingPerioder = listOf(
+            SamordningUføreVurderingPeriode(periode = Periode(periode.fom, LocalDate.MAX), uføregradTilSamordning = Prosent.`50_PROSENT`)
+        ))
+        dataSource.transaction {
+            SamordningUføreRepositoryImpl(it).lagre(behandling.id, vurdering)
+        }
+
+        val uthentet = dataSource.transaction {
+            SamordningUføreRepositoryImpl(it).hentHvisEksisterer(behandling.id)
+        }
+
+        assertThat(uthentet).isNotNull
+        assertThat(uthentet).isEqualTo(vurdering)
+
+    }
+
+    private fun sak(connection: DBConnection): Sak {
+        return PersonOgSakService(
+            FakePdlGateway,
+            PersonRepositoryImpl(connection),
+            SakRepositoryImpl(connection)
+        ).finnEllerOpprett(
+            ident(),
+            periode
+        )
+    }
+
+    private fun behandling(connection: DBConnection, sak: Sak): Behandling {
+        return SakOgBehandlingService(
+            GrunnlagKopierer(connection), SakRepositoryImpl(connection),
+            BehandlingRepositoryImpl(connection)
+        ).finnEllerOpprettBehandling(
+            sak.saksnummer,
+            listOf(Årsak(ÅrsakTilBehandling.MOTTATT_SØKNAD))
+        ).behandling
+    }
+}
