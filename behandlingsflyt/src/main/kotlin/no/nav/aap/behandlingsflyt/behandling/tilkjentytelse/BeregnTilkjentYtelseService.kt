4f32d2a5af2b5f01490c6ea9dcc543e0d88764fc
Fredrik Meyer <fredrik.meyer@nav.no>
2025-03-13 14:18:54 +0100

    Barnetilleggsatsen er 37 kr fra 1. januar 2025

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/tilkjentytelse/BeregnTilkjentYtelseService.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/tilkjentytelse/BeregnTilkjentYtelseService.kt
@@ -15,8 +15,6 @@ import no.nav.aap.komponenter.type.Periode
 import no.nav.aap.komponenter.verdityper.Beløp
 import no.nav.aap.komponenter.verdityper.GUnit
 import no.nav.aap.komponenter.verdityper.Prosent
-import java.math.BigDecimal
-import java.math.RoundingMode
 import java.time.LocalDate
 
 
