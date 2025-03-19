9ff31251815ed4873f40fab200a2e7ac34fe153c
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2025-03-19 07:43:55 +0100

    Legg til posttresdriver igjen

--- app/src/main/kotlin/no/nav/aap/behandlingsflyt/App.kt
+++ app/src/main/kotlin/no/nav/aap/behandlingsflyt/App.kt
@@ -355,6 +355,7 @@ fun initDatasource(dbConfig: DbConfig): DataSource = HikariDataSource(HikariConf
     password = dbConfig.password
     maximumPoolSize = 10 + (ANTALL_WORKERS * 2)
     minimumIdle = 1
+    driverClassName = "org.postgresql.Driver"
     connectionTestQuery = "SELECT 1"
     metricRegistry = prometheus
 })
