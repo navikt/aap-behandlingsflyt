2f52d3e7a67568cd9b372fb9bae8a7ad1e37084f
Fredrik Meyer <fredrik.meyer@nav.no>
2025-03-19 15:35:09 +0100

    Hardkode sykepenger for en viss person

--- repository/src/main/kotlin/no/nav/aap/behandlingsflyt/integrasjon/samordning/AbakusSykepengerGateway.kt
+++ repository/src/main/kotlin/no/nav/aap/behandlingsflyt/integrasjon/samordning/AbakusSykepengerGateway.kt
@@ -3,6 +3,7 @@ package no.nav.aap.behandlingsflyt.integrasjon.samordning
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.ytelsevurdering.gateway.SykepengerGateway
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.ytelsevurdering.gateway.SykepengerRequest
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.ytelsevurdering.gateway.SykepengerResponse
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.ytelsevurdering.gateway.UtbetaltePerioder
 import no.nav.aap.behandlingsflyt.prometheus
 import no.nav.aap.komponenter.config.requiredConfigForKey
 import no.nav.aap.komponenter.httpklient.httpclient.ClientConfig
@@ -11,8 +12,11 @@ import no.nav.aap.komponenter.httpklient.httpclient.RestClient
 import no.nav.aap.komponenter.httpklient.httpclient.post
 import no.nav.aap.komponenter.httpklient.httpclient.request.PostRequest
 import no.nav.aap.komponenter.httpklient.httpclient.tokenprovider.azurecc.ClientCredentialsTokenProvider
+import no.nav.aap.komponenter.miljo.Miljø
+import no.nav.aap.komponenter.miljo.MiljøKode
 import no.nav.aap.lookup.gateway.Factory
 import java.net.URI
+import java.time.LocalDate
 
 class AbakusSykepengerGateway : SykepengerGateway {
     private val url = URI.create(requiredConfigForKey("integrasjon.sykepenger.url") + "/utbetalte-perioder-aap")
@@ -25,15 +29,21 @@ class AbakusSykepengerGateway : SykepengerGateway {
     }
 
     private val client = RestClient.withDefaultResponseHandler(
-        config = config,
-        tokenProvider = ClientCredentialsTokenProvider,
-        prometheus = prometheus
+        config = config, tokenProvider = ClientCredentialsTokenProvider, prometheus = prometheus
     )
 
     private fun query(request: SykepengerRequest): SykepengerResponse {
+        if ("10437709470" in request.personidentifikatorer && Miljø.er() != MiljøKode.PROD) {
+            return SykepengerResponse(
+                listOf(
+                    UtbetaltePerioder(
+                        LocalDate.now().minusMonths(3), LocalDate.now().minusWeeks(1), 100
+                    )
+                )
+            )
+        }
         val httpRequest = PostRequest(
-            body = request,
-            additionalHeaders = listOf(
+            body = request, additionalHeaders = listOf(
                 Header("Accept", "application/json")
             )
         )
