f1a378fbaaf7b3d8a7d924f6d03057a36ee76bdd
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2025-03-24 15:01:17 +0100

    Sjekker om bruker kan saksbehandle refusjonkrav
    Co-authored-by: Tor Idland <tor.idland@nav.no>

--- api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/beregning/grunnlag/refusjon/RefusjonkravGrunnlagDto.kt
+++ api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/beregning/grunnlag/refusjon/RefusjonkravGrunnlagDto.kt
@@ -3,7 +3,8 @@ package no.nav.aap.behandlingsflyt.behandling.beregning.grunnlag.refusjon
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.refusjonkrav.RefusjonkravVurdering
 
 
-data class RefusjonkravGrunnlagDto (
+data class RefusjonkravGrunnlagDto(
+    val harTilgangTilÅSaksbehandle: Boolean,
     val gjeldendeVurdering: RefusjonkravVurdering?,
     val historiskeVurderinger: List<RefusjonkravVurdering>?
 )
