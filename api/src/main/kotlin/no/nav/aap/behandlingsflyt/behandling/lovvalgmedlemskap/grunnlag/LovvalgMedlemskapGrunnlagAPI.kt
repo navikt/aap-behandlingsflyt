bc2902eab22be3bab50ce9a62b8d5ec3be710bf4
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2025-03-24 14:37:44 +0100

    Sjekker om bruker kan saksbehandle lovvalg og medlemskap
    Co-authored-by: Tor Idland <tor.idland@nav.no>

--- api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/lovvalgmedlemskap/grunnlag/LovvalgMedlemskapGrunnlagAPI.kt
+++ api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/lovvalgmedlemskap/grunnlag/LovvalgMedlemskapGrunnlagAPI.kt
@@ -4,10 +4,13 @@ import com.papsign.ktor.openapigen.route.path.normal.NormalOpenAPIRoute
 import com.papsign.ktor.openapigen.route.response.respond
 import com.papsign.ktor.openapigen.route.route
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.medlemskap.MedlemskapArbeidInntektRepository
+import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon
 import no.nav.aap.behandlingsflyt.kontrakt.behandling.BehandlingReferanse
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingRepository
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.flate.BehandlingReferanseService
+import no.nav.aap.behandlingsflyt.tilgang.TilgangGatewayImpl
 import no.nav.aap.komponenter.dbconnect.transaction
+import no.nav.aap.komponenter.httpklient.auth.token
 import no.nav.aap.lookup.repository.RepositoryProvider
 import no.nav.aap.tilgang.AuthorizationParamPathConfig
 import no.nav.aap.tilgang.BehandlingPathParam
@@ -26,9 +29,22 @@ fun NormalOpenAPIRoute.lovvalgMedlemskapGrunnlagAPI(dataSource: DataSource) {
                     val lovvalgMedlemskapRepository = repositoryProvider.provide<MedlemskapArbeidInntektRepository>()
                     val behandling = BehandlingReferanseService(behandlingRepository).behandling(req)
 
-                    val gjeldendeManuellVurdering = lovvalgMedlemskapRepository.hentHvisEksisterer(behandling.id)?.manuellVurdering
-                    val historiskeManuelleVurderinger = lovvalgMedlemskapRepository.hentHistoriskeVurderinger(behandling.sakId)
-                    LovvalgMedlemskapGrunnlagDto(gjeldendeManuellVurdering, historiskeManuelleVurderinger)
+                    val gjeldendeManuellVurdering =
+                        lovvalgMedlemskapRepository.hentHvisEksisterer(behandling.id)?.manuellVurdering
+                    val historiskeManuelleVurderinger =
+                        lovvalgMedlemskapRepository.hentHistoriskeVurderinger(behandling.sakId)
+
+                    val harTilgangTilÅSaksbehandle = TilgangGatewayImpl.sjekkTilgang(
+                        req.referanse,
+                        Definisjon.AVKLAR_LOVVALG_MEDLEMSKAP.kode.toString(),
+                        token()
+                    )
+
+                    LovvalgMedlemskapGrunnlagDto(
+                        harTilgangTilÅSaksbehandle,
+                        gjeldendeManuellVurdering,
+                        historiskeManuelleVurderinger
+                    )
                 }
                 respond(grunnlag)
             }
