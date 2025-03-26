b6259601282d55081b8ed09083a46b8b30330538
VHollund <vetle.hollund@nav.no>
2025-03-26 10:09:57 +0100

    fikset api-internGateway fake

--- lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/test/FakeApiInternGateway.kt
+++ lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/test/FakeApiInternGateway.kt
@@ -3,9 +3,11 @@ package no.nav.aap.behandlingsflyt.test
 import no.nav.aap.behandlingsflyt.behandling.tilkjentytelse.TilkjentYtelsePeriode
 import no.nav.aap.behandlingsflyt.datadeling.SakStatus
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.underveis.Underveisperiode
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.RettighetsType
 import no.nav.aap.behandlingsflyt.hendelse.datadeling.ApiInternGateway
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.Behandling
 import no.nav.aap.behandlingsflyt.sakogbehandling.sak.Sak
+import no.nav.aap.komponenter.tidslinje.Tidslinje
 import no.nav.aap.komponenter.type.Periode
 import no.nav.aap.lookup.gateway.Factory
 import java.time.LocalDate
@@ -29,7 +31,8 @@ class FakeApiInternGateway : ApiInternGateway {
         behandling: Behandling,
         tilkjent: List<TilkjentYtelsePeriode>?,
         underveis: List<Underveisperiode>,
-        vedtaksDato: LocalDate
+        vedtaksDato: LocalDate,
+        rettighetsTypeTidslinje: Tidslinje<RettighetsType>
     ) {
     }
 }
