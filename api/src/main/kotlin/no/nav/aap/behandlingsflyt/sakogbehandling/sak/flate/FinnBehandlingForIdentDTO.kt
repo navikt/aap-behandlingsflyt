e7e3fa23a26a4ad1be885391973c9e93dccc4273
Hein Haraldsen <hein.haraldsen@bekk.no>
2025-03-27 16:52:43 +0100

    Satt opp finnBehandingForIdent og SakForIdent til å arve fra Behandlingsreferanse for å håndtere OBO-token

--- api/src/main/kotlin/no/nav/aap/behandlingsflyt/sakogbehandling/sak/flate/FinnBehandlingForIdentDTO.kt
+++ api/src/main/kotlin/no/nav/aap/behandlingsflyt/sakogbehandling/sak/flate/FinnBehandlingForIdentDTO.kt
@@ -1,10 +1,22 @@
 package no.nav.aap.behandlingsflyt.sakogbehandling.sak.flate
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon
+import no.nav.aap.tilgang.plugin.kontrakt.Behandlingsreferanse
 import java.time.LocalDate
 import java.time.LocalDateTime
+import java.util.*
 
 data class FinnBehandlingForIdentDTO(
     @JsonProperty(value = "ident", required = true) val ident: String,
-    @JsonProperty(value = "mottattTidspunkt", required = true) val mottattTidspunkt: LocalDate
-)
+    @JsonProperty(value = "mottattTidspunkt", required = true) val mottattTidspunkt: LocalDate,
+    val behandlingsReferanse: UUID
+) : Behandlingsreferanse {
+    override fun hentAvklaringsbehovKode(): String? {
+        return Definisjon.BESTILL_LEGEERKLÆRING.kode.toString()
+    }
+
+    override fun behandlingsreferanseResolverInput(): String {
+        return behandlingsReferanse.toString()
+    }
+}
--- api/src/main/kotlin/no/nav/aap/behandlingsflyt/sakogbehandling/sak/flate/FinnSakForIdentDTO.kt
+++ api/src/main/kotlin/no/nav/aap/behandlingsflyt/sakogbehandling/sak/flate/FinnSakForIdentDTO.kt
@@ -1,5 +1,19 @@
 package no.nav.aap.behandlingsflyt.sakogbehandling.sak.flate
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon
+import no.nav.aap.tilgang.plugin.kontrakt.Behandlingsreferanse
+import java.util.*
 
-data class FinnSakForIdentDTO(@JsonProperty(value = "ident", required = true) val ident: String)
+data class FinnSakForIdentDTO(
+    @JsonProperty(value = "ident", required = true) val ident: String,
+    val behandlingsReferanse: UUID
+) : Behandlingsreferanse {
+    override fun hentAvklaringsbehovKode(): String? {
+        return Definisjon.BESTILL_LEGEERKLÆRING.kode.toString()
+    }
+
+    override fun behandlingsreferanseResolverInput(): String {
+        return behandlingsReferanse.toString()
+    }
+}
