1bea6f738d680c313034ba7d910403bdadf3c816
Hein Haraldsen <hein.haraldsen@bekk.no>
2025-03-28 06:58:46 +0100

    Legger til vilkårstype

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/tilkjentytelse/VirkningstidspunktUtleder.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/tilkjentytelse/VirkningstidspunktUtleder.kt
@@ -1,6 +1,7 @@
 package no.nav.aap.behandlingsflyt.behandling.tilkjentytelse
 
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.VilkårsresultatRepository
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Vilkårtype
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingId
 import java.time.LocalDate
 
@@ -9,6 +10,10 @@ class VirkningstidspunktUtleder(
 
 ) {
     fun utledVirkningsTidspunkt(behandlingId: BehandlingId): LocalDate? {
+        val vilkårsResultat = vilkårsresultatRepository.hent(behandlingId)
+        val bistandsvilkåret = vilkårsResultat.finnVilkår(Vilkårtype.BISTANDSVILKÅRET)
+        if (!bistandsvilkåret.harPerioderSomErOppfylt()) return null
+        else
         return vilkårsresultatRepository.hent(behandlingId).rettighetstypeTidslinje().firstOrNull()?.periode?.fom
     }
 }
