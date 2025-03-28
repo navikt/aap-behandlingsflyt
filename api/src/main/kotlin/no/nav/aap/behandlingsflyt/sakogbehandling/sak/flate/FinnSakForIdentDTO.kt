1270a4fd367f0ec6a321be1fd19ad07c9ee169af
Hein Haraldsen <hein.haraldsen@bekk.no>
2025-03-28 08:48:33 +0100

    Fjerner importer

--- api/src/main/kotlin/no/nav/aap/behandlingsflyt/sakogbehandling/sak/flate/FinnSakForIdentDTO.kt
+++ api/src/main/kotlin/no/nav/aap/behandlingsflyt/sakogbehandling/sak/flate/FinnSakForIdentDTO.kt
@@ -1,7 +1,5 @@
 package no.nav.aap.behandlingsflyt.sakogbehandling.sak.flate
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import no.nav.aap.tilgang.plugin.kontrakt.Behandlingsreferanse
-import java.util.*
 
 data class FinnSakForIdentDTO(@JsonProperty(value = "ident", required = true) val ident: String)
