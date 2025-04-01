9d8c2031b6931b89ec0c2432fa72ae6906b455e3
Nicolas N <nicolas.nordhagen@nav.no>
2025-04-01 13:42:48 +0200

    støtte saksnummer på innsending av hendelse

--- api/src/main/kotlin/no/nav/aap/behandlingsflyt/hendelse/MottattHendelseAPI.kt
+++ api/src/main/kotlin/no/nav/aap/behandlingsflyt/hendelse/MottattHendelseAPI.kt
@@ -8,6 +8,7 @@ import com.papsign.ktor.openapigen.route.route
 import io.ktor.http.*
 import no.nav.aap.behandlingsflyt.EMPTY_JSON_RESPONSE
 import no.nav.aap.behandlingsflyt.Tags
+import no.nav.aap.behandlingsflyt.behandling.bruddaktivitetsplikt.SaksnummerParameter
 import no.nav.aap.behandlingsflyt.faktagrunnlag.dokument.MottattDokumentRepository
 import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.Innsending
 import no.nav.aap.behandlingsflyt.prosessering.HendelseMottattHåndteringJobbUtfører
@@ -19,7 +20,9 @@ import no.nav.aap.komponenter.dbconnect.transaction
 import no.nav.aap.lookup.repository.RepositoryProvider
 import no.nav.aap.motor.FlytJobbRepository
 import no.nav.aap.tilgang.AuthorizationBodyPathConfig
+import no.nav.aap.tilgang.AuthorizationParamPathConfig
 import no.nav.aap.tilgang.Operasjon
+import no.nav.aap.tilgang.SakPathParam
 import no.nav.aap.tilgang.authorizedPost
 import org.slf4j.LoggerFactory
 import org.slf4j.MDC
@@ -29,8 +32,7 @@ private val log = LoggerFactory.getLogger("hendelse.MottattHendelseAPI")
 
 fun NormalOpenAPIRoute.mottattHendelseApi(dataSource: DataSource) {
     route("/api/hendelse") {
-        route("/send")
-        {
+        route("/send") {
             authorizedPost<Unit, String, Innsending>(
                 modules = arrayOf(TagModule(listOf(Tags.Sak))),
                 routeConfig = AuthorizationBodyPathConfig(
@@ -38,34 +40,55 @@ fun NormalOpenAPIRoute.mottattHendelseApi(dataSource: DataSource) {
                     applicationsOnly = false,
                     applicationRole = "opprett-sak",
                 )
-            )
-            { _, dto ->
-                MDC.putCloseable("saksnummer", dto.saksnummer.toString()).use {
-                    dataSource.transaction { connection ->
-                        val repositoryProvider = RepositoryProvider(connection)
-                        val sak = repositoryProvider.provide<SakRepository>().hent(dto.saksnummer)
-                        val mottattDokumentRepository = repositoryProvider.provide<MottattDokumentRepository>()
-
-                        log.info("Mottok dokumenthendelse. Brevkategori: ${dto.type} Mottattdato: ${dto.mottattTidspunkt}")
-
-                        if (kjennerTilDokumentFraFør(dto, sak, mottattDokumentRepository)) {
-                            log.warn("Allerede håndtert dokument med referanse {}", dto.referanse)
-                        } else {
-                            val flytJobbRepository = FlytJobbRepository(connection)
-                            flytJobbRepository.leggTil(
-                                HendelseMottattHåndteringJobbUtfører.nyJobb(
-                                    sakId = sak.id,
-                                    dokumentReferanse = dto.referanse,
-                                    brevkategori = dto.type,
-                                    kanal = dto.kanal,
-                                    melding = dto.melding,
-                                    mottattTidspunkt = dto.mottattTidspunkt
-                                ),
-                            )
-                        }
-                    }
-                }
+            ) { _, dto ->
+                registrerMottattHendelse(dto, dataSource)
                 respond(EMPTY_JSON_RESPONSE, HttpStatusCode.Accepted)
+            }
+        }
+
+        route("/sak/{saksnummer}/send") {
+            authorizedPost<SaksnummerParameter, String, Innsending>(
+                modules = arrayOf(TagModule(listOf(Tags.Sak))),
+                routeConfig = AuthorizationParamPathConfig(
+                    sakPathParam = SakPathParam("saksnummer"),
+                    operasjon = Operasjon.SAKSBEHANDLE,
+                    applicationsOnly = false,
+                    applicationRole = "opprett-sak",
+                )
+            ) { _, dto ->
+                registrerMottattHendelse(dto, dataSource)
+                respond(EMPTY_JSON_RESPONSE, HttpStatusCode.Accepted)
+            }
+        }
+    }
+}
+
+private fun registrerMottattHendelse(
+    dto: Innsending,
+    dataSource: DataSource
+) {
+    MDC.putCloseable("saksnummer", dto.saksnummer.toString()).use {
+        dataSource.transaction { connection ->
+            val repositoryProvider = RepositoryProvider(connection)
+            val sak = repositoryProvider.provide<SakRepository>().hent(dto.saksnummer)
+            val mottattDokumentRepository = repositoryProvider.provide<MottattDokumentRepository>()
+
+            log.info("Mottok dokumenthendelse. Brevkategori: ${dto.type} Mottattdato: ${dto.mottattTidspunkt}")
+
+            if (kjennerTilDokumentFraFør(dto, sak, mottattDokumentRepository)) {
+                log.warn("Allerede håndtert dokument med referanse {}", dto.referanse)
+            } else {
+                val flytJobbRepository = FlytJobbRepository(connection)
+                flytJobbRepository.leggTil(
+                    HendelseMottattHåndteringJobbUtfører.nyJobb(
+                        sakId = sak.id,
+                        dokumentReferanse = dto.referanse,
+                        brevkategori = dto.type,
+                        kanal = dto.kanal,
+                        melding = dto.melding,
+                        mottattTidspunkt = dto.mottattTidspunkt
+                    ),
+                )
             }
         }
     }
