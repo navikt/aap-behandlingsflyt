da34439400750dc1c5d01db1fa260866f75b6f28
Hein Haraldsen <hein.haraldsen@bekk.no>
2025-04-02 10:56:15 +0200

    Sender inn SE-operasjon til harTilgangTilSak

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/tilgang/TilgangGateway.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/tilgang/TilgangGateway.kt
@@ -3,9 +3,10 @@ package no.nav.aap.behandlingsflyt.tilgang
 import no.nav.aap.behandlingsflyt.kontrakt.sak.Saksnummer
 import no.nav.aap.komponenter.httpklient.httpclient.tokenprovider.OidcToken
 import no.nav.aap.lookup.gateway.Gateway
+import no.nav.aap.tilgang.Operasjon
 import java.util.*
 
 interface TilgangGateway : Gateway {
     fun sjekkTilgangTilBehandling(behandlingsreferanse: UUID, avklaringsbehovKode: String, token: OidcToken): Boolean
-    fun sjekkTilgangTilSak(saksnummer: Saksnummer, token: OidcToken): Boolean
+    fun sjekkTilgangTilSak(saksnummer: Saksnummer, token: OidcToken, Operasjon: Operasjon): Boolean
 }
--- lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/test/inmemorygateway/FakeTilgangGateway.kt
+++ lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/test/inmemorygateway/FakeTilgangGateway.kt
@@ -3,13 +3,14 @@ package no.nav.aap.behandlingsflyt.test.inmemorygateway
 import no.nav.aap.behandlingsflyt.kontrakt.sak.Saksnummer
 import no.nav.aap.behandlingsflyt.tilgang.TilgangGateway
 import no.nav.aap.komponenter.httpklient.httpclient.tokenprovider.OidcToken
+import no.nav.aap.tilgang.Operasjon
 import java.util.*
 
 object FakeTilgangGateway : TilgangGateway {
     override fun sjekkTilgangTilBehandling(behandlingsreferanse: UUID, avklaringsbehovKode: String, token: OidcToken): Boolean {
         return true
     }
-    override fun sjekkTilgangTilSak(saksnummer: Saksnummer, token: OidcToken): Boolean {
+    override fun sjekkTilgangTilSak(saksnummer: Saksnummer, token: OidcToken, operasjon: Operasjon): Boolean {
         return true
     }
 }
