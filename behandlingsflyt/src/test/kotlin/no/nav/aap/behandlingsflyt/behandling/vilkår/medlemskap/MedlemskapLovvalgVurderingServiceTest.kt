a904d322593be322ca44a4c3d34888a1e2715059
HenrikBuggeHalvorsen <henrik.bugge.halvorsen@nav.no>
2025-04-09 15:59:48 +0200

    par tester for å dekke at gammelt og nytt format støttes i lovvalg statsborgeskap

--- behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/behandling/vilkår/medlemskap/MedlemskapLovvalgVurderingServiceTest.kt
+++ behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/behandling/vilkår/medlemskap/MedlemskapLovvalgVurderingServiceTest.kt
@@ -8,6 +8,7 @@ import no.nav.aap.behandlingsflyt.faktagrunnlag.register.personopplysninger.Fød
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.personopplysninger.Personopplysning
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.personopplysninger.PersonopplysningGrunnlag
 import no.nav.aap.behandlingsflyt.faktagrunnlag.lovvalgmedlemskap.utenlandsopphold.UtenlandsOppholdData
+import no.nav.aap.behandlingsflyt.faktagrunnlag.register.personopplysninger.Statsborgerskap
 import no.nav.aap.behandlingsflyt.sakogbehandling.sak.adapters.PersonStatus
 import no.nav.aap.komponenter.tidslinje.Segment
 import no.nav.aap.komponenter.type.Periode
@@ -36,6 +37,66 @@ class MedlemskapLovvalgVurderingServiceTest {
             ),
             personopplysningGrunnlag = PersonopplysningGrunnlag(
                 brukerPersonopplysning = Personopplysning(Fødselsdato(LocalDate.now().minusYears(18)), 1, null, EØSLand.NOR.toString(), LocalDate.now().minusMonths(1), null, PersonStatus.bosatt),
+                relatertePersonopplysninger = null
+            ),
+            nyeSoknadGrunnlag = UtenlandsOppholdData(true, true, false, false, null)
+        )
+
+        val resultat = service.vurderTilhørighet(grunnlag, Periode(LocalDate.now().minusYears(1), LocalDate.now()))
+        assertEquals(true, resultat.kanBehandlesAutomatisk)
+    }
+
+    @Test
+    fun kanHåndtereGammeltStatsborgerskapFormat() {
+        val grunnlag = MedlemskapLovvalgGrunnlag(
+            medlemskapArbeidInntektGrunnlag = MedlemskapArbeidInntektGrunnlag(
+                medlemskapGrunnlag = MedlemskapUnntakGrunnlag(
+                    unntak = listOf(
+                        Segment(
+                            periode = Periode(LocalDate.now().minusMonths(1), LocalDate.now()),
+                            verdi = Unntak("unntak", "statusaarsak", true, "grunnlag", "lovvalg", false, EØSLand.NOR.toString())
+                        )
+                    )
+                ),
+                inntekterINorgeGrunnlag = emptyList(),
+                arbeiderINorgeGrunnlag = emptyList(),
+                manuellVurdering = null
+            ),
+            personopplysningGrunnlag = PersonopplysningGrunnlag(
+                brukerPersonopplysning = Personopplysning(Fødselsdato(LocalDate.now().minusYears(18)), 1, null, "NOR", LocalDate.now().minusMonths(1), null, PersonStatus.bosatt,
+                    listOf(
+                    )),
+                relatertePersonopplysninger = null
+            ),
+            nyeSoknadGrunnlag = UtenlandsOppholdData(true, true, false, false, null)
+        )
+
+        val resultat = service.vurderTilhørighet(grunnlag, Periode(LocalDate.now().minusYears(1), LocalDate.now()))
+        assertEquals(true, resultat.kanBehandlesAutomatisk)
+    }
+
+    @Test
+    fun kanHåndtereFlereStatsborgerskap() {
+        val grunnlag = MedlemskapLovvalgGrunnlag(
+            medlemskapArbeidInntektGrunnlag = MedlemskapArbeidInntektGrunnlag(
+                medlemskapGrunnlag = MedlemskapUnntakGrunnlag(
+                    unntak = listOf(
+                        Segment(
+                            periode = Periode(LocalDate.now().minusMonths(1), LocalDate.now()),
+                            verdi = Unntak("unntak", "statusaarsak", true, "grunnlag", "lovvalg", false, EØSLand.NOR.toString())
+                        )
+                    )
+                ),
+                inntekterINorgeGrunnlag = emptyList(),
+                arbeiderINorgeGrunnlag = emptyList(),
+                manuellVurdering = null
+            ),
+            personopplysningGrunnlag = PersonopplysningGrunnlag(
+                brukerPersonopplysning = Personopplysning(Fødselsdato(LocalDate.now().minusYears(18)), 1, null, "XUK", LocalDate.now().minusMonths(1), null, PersonStatus.bosatt,
+                    listOf(
+                        Statsborgerskap("XUK"),
+                        Statsborgerskap("NOR"),
+                    )),
                 relatertePersonopplysninger = null
             ),
             nyeSoknadGrunnlag = UtenlandsOppholdData(true, true, false, false, null)
