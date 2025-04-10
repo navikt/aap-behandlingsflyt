cadda4b00030d3efd0de27aa8683f68060970d4a
VHollund <vetle.hollund@nav.no>
2025-04-10 11:55:48 +0200

    in memory tjeneste pensjon repo oppdatert til å lagre riktig data

--- lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/test/inmemoryrepo/InMemoryTjenestePensjonRepository.kt
+++ lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/test/inmemoryrepo/InMemoryTjenestePensjonRepository.kt
@@ -5,30 +5,30 @@ import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.tjeneste
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingId
 
 object InMemoryTjenestePensjonRepository : TjenestePensjonRepository {
-    private val tjenestePensjon = mutableMapOf<BehandlingId, List<TjenestePensjon>>()
+    private val tjenestePensjon = mutableMapOf<BehandlingId, TjenestePensjon>()
     private val lock = Object()
     override fun hentHvisEksisterer(behandlingId: BehandlingId): TjenestePensjon? {
         synchronized(lock) {
-            return tjenestePensjon[behandlingId]?.firstOrNull()
+            return tjenestePensjon[behandlingId]
         }
     }
 
     override fun hent(behandlingId: BehandlingId): TjenestePensjon {
         synchronized(lock) {
-            return tjenestePensjon[behandlingId]?.firstOrNull()
+            return tjenestePensjon[behandlingId]
                 ?: throw IllegalStateException("Fant ikke tjeneste pensjon for behandlingId $behandlingId")
         }
     }
 
     override fun lagre(behandlingId: BehandlingId, tjenestePensjon: TjenestePensjon) {
         synchronized(lock) {
-            this.tjenestePensjon[behandlingId] = listOf(tjenestePensjon)
+            this.tjenestePensjon[behandlingId] = tjenestePensjon
         }
     }
 
     override fun kopier(fraBehandling: BehandlingId, tilBehandling: BehandlingId) {
         synchronized(lock) {
-            tjenestePensjon[tilBehandling] = tjenestePensjon[fraBehandling] ?: emptyList()
+            tjenestePensjon[tilBehandling] = tjenestePensjon[fraBehandling]?: TjenestePensjon(emptyList())
         }
     }
 
