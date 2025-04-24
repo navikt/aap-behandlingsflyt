47482107388ac7d82c3e47ee4091b16d15522c27
charliemidtlyng <charlie.midtlyng@bekk.no>
2025-04-24 15:23:17 +0200

    Skal returnere søknadsdato fra rettighetsperiodeApi for å vise i saksbehandlingsløsningen

--- api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/rettighetsperiode/RettighetsperiodeApi.kt
+++ api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/rettighetsperiode/RettighetsperiodeApi.kt
@@ -3,8 +3,8 @@ package no.nav.aap.behandlingsflyt.behandling.rettighetsperiode
 import com.papsign.ktor.openapigen.route.path.normal.NormalOpenAPIRoute
 import com.papsign.ktor.openapigen.route.response.respond
 import com.papsign.ktor.openapigen.route.route
-import java.time.LocalDate
-import javax.sql.DataSource
+import no.nav.aap.behandlingsflyt.behandling.søknad.SøknadsdatoUtleder
+import no.nav.aap.behandlingsflyt.faktagrunnlag.dokument.MottattDokumentRepository
 import no.nav.aap.behandlingsflyt.kontrakt.behandling.BehandlingReferanse
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingRepository
 import no.nav.aap.komponenter.dbconnect.transaction
@@ -13,11 +13,13 @@ import no.nav.aap.tilgang.AuthorizationParamPathConfig
 import no.nav.aap.tilgang.BehandlingPathParam
 import no.nav.aap.tilgang.Operasjon
 import no.nav.aap.tilgang.authorizedGet
+import java.time.LocalDate
+import javax.sql.DataSource
 
 
 class RettighetsperiodeGrunnlagDto(
-    val vurdering: RettighetsperiodeVurderingDto?
-
+    val vurdering: RettighetsperiodeVurderingDto?,
+    val søknadsdato: LocalDate?,
 )
 
 class RettighetsperiodeVurderingDto(
@@ -39,17 +41,20 @@ fun NormalOpenAPIRoute.rettighetsperiodeGrunnlagAPI(dataSource: DataSource) {
             val repositoryProvider = RepositoryProvider(connection)
             val behandlingRepository = repositoryProvider.provide<BehandlingRepository>()
             val rettighetsperiodeRepository = repositoryProvider.provide<VurderRettighetsperiodeRepository>()
+            val mottattDokumentRepository = repositoryProvider.provide<MottattDokumentRepository>()
+            val søknadsdatoUtleder = SøknadsdatoUtleder(mottattDokumentRepository)
 
-            val behandlingId = behandlingRepository.hent(BehandlingReferanse(req.referanse)).id
+            val behandling = behandlingRepository.hent(BehandlingReferanse(req.referanse))
             RettighetsperiodeGrunnlagDto(
-                vurdering = rettighetsperiodeRepository.hentVurdering(behandlingId)?.let {
+                vurdering = rettighetsperiodeRepository.hentVurdering(behandling.id)?.let {
                     RettighetsperiodeVurderingDto(
                         begrunnelse = it.begrunnelse,
                         startDato = it.startDato,
                         harRettUtoverSøknadsdato = it.harRettUtoverSøknadsdato,
                         harKravPåRenter = it.harKravPåRenter,
                     )
-                }
+                },
+                søknadsdato = søknadsdatoUtleder.utledSøknadsdatoForSak(behandling.sakId)?.toLocalDate()
             )
         }
         respond(trukketSøknadVurderingDto)
---
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/søknad/SøknadsdatoUtleder.kt
@@ -0,0 +1,19 @@
+package no.nav.aap.behandlingsflyt.behandling.søknad
+
+import no.nav.aap.behandlingsflyt.faktagrunnlag.dokument.MottattDokumentRepository
+import no.nav.aap.behandlingsflyt.kontrakt.hendelse.InnsendingType
+import no.nav.aap.behandlingsflyt.sakogbehandling.sak.SakId
+import java.time.LocalDateTime
+
+class SøknadsdatoUtleder(private val mottattDokumentRepository: MottattDokumentRepository) {
+
+    fun utledSøknadsdatoForSak(sakId: SakId): LocalDateTime? {
+        val søknader = mottattDokumentRepository.hentDokumenterAvType(
+            sakId,
+            InnsendingType.SØKNAD
+        )
+        return søknader
+            .minOfOrNull { it.mottattTidspunkt }
+
+    }
+}
