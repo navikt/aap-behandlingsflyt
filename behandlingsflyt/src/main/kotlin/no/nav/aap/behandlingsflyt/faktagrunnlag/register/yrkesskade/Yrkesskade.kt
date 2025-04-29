e42e57c21854966cfb81459bfb63199152a97bf5
Fredrik Meyer <fredrik.meyer@nav.no>
2025-04-29 08:31:40 +0200

    Legg til data der jeg glemte

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/register/yrkesskade/Yrkesskade.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/register/yrkesskade/Yrkesskade.kt
@@ -2,4 +2,4 @@ package no.nav.aap.behandlingsflyt.faktagrunnlag.register.yrkesskade
 
 import java.time.LocalDate
 
-class Yrkesskade(val ref: String, val skadedato: LocalDate)
+data class Yrkesskade(val ref: String, val skadedato: LocalDate)
--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/register/yrkesskade/YrkesskadeGrunnlag.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/register/yrkesskade/YrkesskadeGrunnlag.kt
@@ -2,22 +2,4 @@ package no.nav.aap.behandlingsflyt.faktagrunnlag.register.yrkesskade
 
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingId
 
-class YrkesskadeGrunnlag(val id: Long, val behandlingId: BehandlingId, val yrkesskader: Yrkesskader) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as YrkesskadeGrunnlag
-
-        if (behandlingId != other.behandlingId) return false
-        if (yrkesskader != other.yrkesskader) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = behandlingId.hashCode()
-        result = 31 * result + yrkesskader.hashCode()
-        return result
-    }
-}
+data class YrkesskadeGrunnlag(val id: Long, val behandlingId: BehandlingId, val yrkesskader: Yrkesskader)
