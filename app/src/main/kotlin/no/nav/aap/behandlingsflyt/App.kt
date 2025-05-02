70b3759b67d6681725ca0a5fa4afcf32ca282c0d
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2025-05-02 17:28:34 +0200

    Fjern test av feature toggles

--- app/src/main/kotlin/no/nav/aap/behandlingsflyt/App.kt
+++ app/src/main/kotlin/no/nav/aap/behandlingsflyt/App.kt
@@ -191,14 +191,6 @@ internal fun Application.server(dbConfig: DbConfig) {
     registerGateways()
     registerRepositories()
 
-    if (!Miljø.erLokal()) {
-        /* sjekk at feature toggles funker i dev og prod før det tas i bruk .. */
-        LoggerFactory.getLogger("main").info(
-            "test feature toggle: {}",
-            GatewayProvider.provide<UnleashGateway>().isEnabled(BehandlingsflytFeature.FasttrackMeldekort)
-        )
-    }
-
     routing {
         authenticate(AZURE) {
             install(NavIdentInterceptor)
