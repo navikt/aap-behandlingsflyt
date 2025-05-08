9a7da8528ebd0789d74a18c966ade446e7479d0d
Hein Haraldsen <hein.haraldsen@bekk.no>
2025-05-08 13:18:32 +0200

    Fikser feil i meldekortsletting

--- repository/src/main/kotlin/no/nav/aap/behandlingsflyt/repository/faktagrunnlag/dokument/arbeid/MeldekortRepositoryImpl.kt
+++ repository/src/main/kotlin/no/nav/aap/behandlingsflyt/repository/faktagrunnlag/dokument/arbeid/MeldekortRepositoryImpl.kt
@@ -182,8 +182,8 @@ class MeldekortRepositoryImpl(private val connection: DBConnection) : MeldekortR
         val meldekorteneIds = getMeldekorteneIds(behandlingId)
         val meldekortIds = getMeldekortIds(meldekorteneIds)
         connection.execute("""
-            delete from meldekort_periode where id = ANY(?::bigint[]);
-            delete from meldekort where id = ANY(?::bigint[]);
+            delete from meldekort_periode where meldekort_id = ANY(?::bigint[]);
+            delete from meldekort where meldekortene_id = ANY(?::bigint[]);
             delete from meldekortene where id = ANY(?::bigint[]);
             delete from meldekort_grunnlag where behandling_id = ? 
         """.trimIndent()) {
