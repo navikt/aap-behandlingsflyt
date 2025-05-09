dc402e23ac44c57d21e7746f6435663a2c2bd133
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2025-05-09 11:59:00 +0200

    Bump komponenterVersjon til 1.0.242

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 
 val ktorVersion = "3.1.3"
-val komponenterVersjon = "1.0.238"
+val komponenterVersjon = "1.0.242"
 val junitVersjon = "5.12.2"
 
 plugins {
--- behandlingsflyt/build.gradle.kts
+++ behandlingsflyt/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("behandlingsflyt.conventions")
 }
 
-val komponenterVersjon = "1.0.238"
+val komponenterVersjon = "1.0.242"
 val ktorVersion = "3.1.3"
 val tilgangVersjon = "1.0.64"
 val utbetalVersjon = "0.0.43"
