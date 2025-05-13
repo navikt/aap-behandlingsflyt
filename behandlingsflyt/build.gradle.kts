a953cf26c872490e55686846b6ea63918e5ec0d2
Fredrik Meyer <fredrik.meyer@nav.no>
2025-05-13 08:41:42 +0200

    Bump tilgang

--- behandlingsflyt/build.gradle.kts
+++ behandlingsflyt/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 val komponenterVersjon = "1.0.242"
 val ktorVersion = "3.1.3"
-val tilgangVersjon = "1.0.64"
+val tilgangVersjon = "1.0.67"
 val utbetalVersjon = "0.0.43"
 val junitVersjon = "5.12.2"
 
--- lib-test/build.gradle.kts
+++ lib-test/build.gradle.kts
@@ -1,6 +1,6 @@
 val ktorVersion = "3.1.3"
 val komponenterVersjon = "1.0.242"
-val tilgangVersjon = "1.0.64"
+val tilgangVersjon = "1.0.67"
 val junitVersjon = "5.12.2"
 
 plugins {
