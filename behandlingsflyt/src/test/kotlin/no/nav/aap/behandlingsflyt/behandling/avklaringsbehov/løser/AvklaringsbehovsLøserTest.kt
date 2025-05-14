065f6a4e2d171fe45110b3a1f8f572ca3e5a6b38
Fredrik Meyer <fredrik.meyer@nav.no>
2025-05-14 15:50:22 +0200

    Ikke bruk ekte database i test som ikke trenger det

--- behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løser/AvklaringsbehovsLøserTest.kt
+++ behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løser/AvklaringsbehovsLøserTest.kt
@@ -2,8 +2,7 @@ package no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løser
 
 import no.nav.aap.behandlingsflyt.repository.postgresRepositoryRegistry
 import no.nav.aap.behandlingsflyt.test.FakeUnleash
-import no.nav.aap.komponenter.dbconnect.transaction
-import no.nav.aap.komponenter.dbtest.InitTestDatabase
+import no.nav.aap.behandlingsflyt.test.MockConnection
 import no.nav.aap.komponenter.gateway.GatewayRegistry
 import no.nav.aap.lookup.repository.RepositoryProvider
 import org.assertj.core.api.Assertions
@@ -20,16 +19,15 @@ internal class AvklaringsbehovsLøserTest {
     @Test
     fun `alle subtyper skal ha unik verdi`() {
         val utledSubtypes = AvklaringsbehovsLøser::class.sealedSubclasses
-        InitTestDatabase.freshDatabase().transaction { dbConnection ->
-            val repositoryProvider = postgresRepositoryRegistry.provider(dbConnection)
+        val repositoryProvider = postgresRepositoryRegistry.provider(MockConnection().toDBConnection())
 
-            val løsningSubtypes = utledSubtypes.map {
-                it.constructors
-                    .find { it.parameters.singleOrNull()?.type?.classifier == RepositoryProvider::class }!!
-                    .call(repositoryProvider).forBehov()
-            }.toSet()
+        val løsningSubtypes = utledSubtypes.map {
+            it.constructors
+                .find { it.parameters.singleOrNull()?.type?.classifier == RepositoryProvider::class }!!
+                .call(repositoryProvider).forBehov()
+        }.toSet()
 
-            Assertions.assertThat(løsningSubtypes).hasSize(utledSubtypes.size)
-        }
+        Assertions.assertThat(løsningSubtypes).hasSize(utledSubtypes.size)
+
     }
 }
