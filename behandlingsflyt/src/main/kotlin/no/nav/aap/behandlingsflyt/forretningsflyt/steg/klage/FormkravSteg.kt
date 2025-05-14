cc357b1c3ac2283659473674e9424204e9d221b4
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2025-05-14 09:30:54 +0200

    Fullfør formkravsteg dersom behovet har blitt løst

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/klage/FormkravSteg.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/klage/FormkravSteg.kt
@@ -1,26 +1,42 @@
 package no.nav.aap.behandlingsflyt.forretningsflyt.steg.klage
 
+import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovRepository
+import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.Avklaringsbehovene
 import no.nav.aap.behandlingsflyt.flyt.steg.BehandlingSteg
 import no.nav.aap.behandlingsflyt.flyt.steg.FantAvklaringsbehov
 import no.nav.aap.behandlingsflyt.flyt.steg.FlytSteg
+import no.nav.aap.behandlingsflyt.flyt.steg.Fullført
 import no.nav.aap.behandlingsflyt.flyt.steg.StegResultat
 import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon
+import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Status
 import no.nav.aap.behandlingsflyt.kontrakt.steg.StegType
 import no.nav.aap.behandlingsflyt.sakogbehandling.flyt.FlytKontekstMedPerioder
 import no.nav.aap.lookup.repository.RepositoryProvider
 
-class FormkravSteg private constructor(): BehandlingSteg {
+class FormkravSteg private constructor(
+    private val avklaringsbehovRepository: AvklaringsbehovRepository,
+): BehandlingSteg {
     override fun utfør(kontekst: FlytKontekstMedPerioder): StegResultat {
-        return FantAvklaringsbehov(Definisjon.VURDER_FORMKRAV)
+        val avklaringsbehov = avklaringsbehovRepository.hentAvklaringsbehovene(kontekst.behandlingId)
+
+        return if (avklaringsbehov.harIkkeBlittLøst(Definisjon.VURDER_FORMKRAV)) {
+            FantAvklaringsbehov(Definisjon.VURDER_FORMKRAV)
+        } else Fullført
     }
     
     companion object : FlytSteg {
         override fun konstruer(repositoryProvider: RepositoryProvider): BehandlingSteg {
-            return FormkravSteg()
+            return FormkravSteg(repositoryProvider.provide()    )
         }
 
         override fun type(): StegType {
             return StegType.FORMKRAV
         }
+    }
+
+    private fun Avklaringsbehovene.harIkkeBlittLøst(definisjon: Definisjon): Boolean {
+        return this.alle()
+            .filter { it.definisjon == definisjon }
+            .none { it.status() == Status.AVSLUTTET }
     }
 }
