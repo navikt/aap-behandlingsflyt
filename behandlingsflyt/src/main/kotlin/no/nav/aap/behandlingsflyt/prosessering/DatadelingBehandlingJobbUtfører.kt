7b63bf87d2b6766e25de14133b6f9a6acb2ddfac
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2025-05-19 09:35:35 +0200

    Ikke oversend klagebehandling til api-intern (må avgjøres om vi skal gjøre dette)

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/prosessering/DatadelingBehandlingJobbUtfører.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/prosessering/DatadelingBehandlingJobbUtfører.kt
@@ -4,6 +4,7 @@ import no.nav.aap.behandlingsflyt.behandling.tilkjentytelse.TilkjentYtelseReposi
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.underveis.UnderveisRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.VilkårsresultatRepository
 import no.nav.aap.behandlingsflyt.hendelse.datadeling.ApiInternGateway
+import no.nav.aap.behandlingsflyt.kontrakt.behandling.TypeBehandling
 import no.nav.aap.behandlingsflyt.kontrakt.hendelse.BehandlingFlytStoppetHendelse
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingRepository
 import no.nav.aap.behandlingsflyt.sakogbehandling.sak.SakRepository
@@ -12,6 +13,7 @@ import no.nav.aap.lookup.repository.RepositoryProvider
 import no.nav.aap.motor.JobbInput
 import no.nav.aap.motor.JobbUtfører
 import no.nav.aap.motor.ProviderJobbSpesifikasjon
+import org.slf4j.LoggerFactory
 
 class DatadelingBehandlingJobbUtfører(
     private val apiInternGateway: ApiInternGateway,
@@ -21,9 +23,22 @@ class DatadelingBehandlingJobbUtfører(
     private val underveisRepository: UnderveisRepository,
     private val vilkårsresultatRepository: VilkårsresultatRepository
 ) : JobbUtfører {
+    private val log = LoggerFactory.getLogger(javaClass)
+
     override fun utfør(input: JobbInput) {
         val hendelse = input.payload<BehandlingFlytStoppetHendelse>()
         val behandling = behandlingRepository.hent(hendelse.referanse)
+        
+        if (behandling.typeBehandling() !in listOf(
+                TypeBehandling.Førstegangsbehandling,
+                TypeBehandling.Revurdering
+            )
+        ) {
+            // TODO: Avgjør om vi skal sende klagebehandlinger til api-intern
+            log.info("Fant behandling av type ${behandling.typeBehandling()} - oversender ikke til api-intern")
+            return
+        }
+        
         val sak = sakRepository.hent(behandling.sakId)
         val tilkjentYtelse = tilkjentRepository.hentHvisEksisterer(behandling.id)
         val underveis = underveisRepository.hentHvisEksisterer(behandling.id)
