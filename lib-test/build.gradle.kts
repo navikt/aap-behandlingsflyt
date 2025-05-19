f385d09b3f1e3c253c490998250ca464e448d360
Greger Siem Knudsen <greger.siem.knudsen@nav.no>
2025-05-19 11:10:09 +0200

    ASP-952 Bumpet versjon av aap-brev

--- lib-test/build.gradle.kts
+++ lib-test/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     implementation(project(":behandlingsflyt"))
     implementation(project(":repository"))
     implementation(project(":kontrakt"))
-    implementation("no.nav.aap.brev:kontrakt:0.0.113")
+    implementation("no.nav.aap.brev:kontrakt:0.0.114")
     implementation("no.nav.aap.tilgang:api-kontrakt:$tilgangVersjon")
     implementation("no.nav.aap.kelvin:httpklient:$komponenterVersjon")
     implementation("no.nav.aap.kelvin:verdityper:$komponenterVersjon")
