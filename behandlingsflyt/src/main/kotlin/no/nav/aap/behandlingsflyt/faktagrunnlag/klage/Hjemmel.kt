da3bfc2932dcb5730a453783d87c2816bdf30ba6
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2025-05-21 13:35:40 +0200

    Legg til noen flere hjemler

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/klage/Hjemmel.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/klage/Hjemmel.kt
@@ -2,12 +2,18 @@ package no.nav.aap.behandlingsflyt.faktagrunnlag.klage
 
 import no.nav.aap.behandlingsflyt.kontrakt.statistikk.ÅrsakTilBehandling
 
-
 // TODO: Fyll på, evt. se om vi skal bruke Vilkårtype
 enum class Hjemmel(val hjemmel: String) {
+    FOLKETRYGDLOVEN_11_2("§ 11-2"),
     FOLKETRYGDLOVEN_11_5("§ 11-5"),
-    FOLKETRYGDLOVEN_11_6("§ 11-6");
-    
+    FOLKETRYGDLOVEN_11_6("§ 11-6"),
+    FOLKETRYGDLOVEN_11_13("§ 11-13"),
+    FOLKETRYGDLOVEN_11_17("§ 11-17"),
+    FOLKETRYGDLOVEN_11_18("§ 11-18"),
+    FOLKETRYGDLOVEN_11_20("§ 11-20"),
+    FOLKETRYGDLOVEN_11_22("§ 11-22"),
+    FOLKETRYGDLOVEN_11_24("§ 11-24");
+
     companion object {
         fun fraHjemmel(hjemmel: String): Hjemmel? {
             return entries.firstOrNull { it.hjemmel == hjemmel }
@@ -16,8 +22,18 @@ enum class Hjemmel(val hjemmel: String) {
 
     fun tilÅrsak(): ÅrsakTilBehandling {
         return when (this) {
-            FOLKETRYGDLOVEN_11_5 -> ÅrsakTilBehandling.SYKDOM_ARBEVNE_BEHOV_FOR_BISTAND
-            FOLKETRYGDLOVEN_11_6 -> ÅrsakTilBehandling.SYKDOM_ARBEVNE_BEHOV_FOR_BISTAND
+            FOLKETRYGDLOVEN_11_2 -> ÅrsakTilBehandling.FORUTGAENDE_MEDLEMSKAP
+
+            FOLKETRYGDLOVEN_11_5,
+            FOLKETRYGDLOVEN_11_6,
+            FOLKETRYGDLOVEN_11_13,
+            FOLKETRYGDLOVEN_11_17,
+            FOLKETRYGDLOVEN_11_18
+                -> ÅrsakTilBehandling.SYKDOM_ARBEVNE_BEHOV_FOR_BISTAND
+
+            FOLKETRYGDLOVEN_11_20 -> ÅrsakTilBehandling.BARNETILLEGG
+            FOLKETRYGDLOVEN_11_22 -> ÅrsakTilBehandling.REVURDER_YRKESSKADE
+            FOLKETRYGDLOVEN_11_24 -> ÅrsakTilBehandling.SAMORDNING_OG_AVREGNING
         }
     }
 }
