f3e782c46f16ee2efe773415997aee44cdbdaab7
Hein Haraldsen <hein.haraldsen@bekk.no>
2025-05-28 13:45:23 +0200

    Fikser på PersonSakService

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/sakogbehandling/sak/PersonOgSakService.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/sakogbehandling/sak/PersonOgSakService.kt
@@ -2,22 +2,13 @@ package no.nav.aap.behandlingsflyt.sakogbehandling.sak
 
 import no.nav.aap.behandlingsflyt.sakogbehandling.Ident
 import no.nav.aap.behandlingsflyt.sakogbehandling.sak.db.PersonRepository
-import no.nav.aap.komponenter.gateway.GatewayProvider
 import no.nav.aap.komponenter.type.Periode
-import no.nav.aap.lookup.repository.RepositoryProvider
 
 class PersonOgSakService(
     private val pdlGateway: IdentGateway,
     private val personRepository: PersonRepository,
     private val sakRepository: SakRepository,
 ) {
-
-
-    constructor(repositoryProvider: RepositoryProvider): this (
-        pdlGateway =  GatewayProvider.provide(IdentGateway::class),
-        personRepository = repositoryProvider.provide(),
-        sakRepository = repositoryProvider.provide(),
-    )
 
     fun finnEllerOpprett(ident: Ident, periode: Periode): Sak {
         val identliste = pdlGateway.hentAlleIdenterForPerson(ident)
