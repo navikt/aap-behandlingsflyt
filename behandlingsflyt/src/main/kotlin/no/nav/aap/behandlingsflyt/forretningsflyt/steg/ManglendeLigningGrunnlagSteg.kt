6efe5376abed9837ad51f5b480f42558c8e15680
HenrikBuggeHalvorsen <henrik.bugge.halvorsen@nav.no>
2025-05-28 16:20:56 +0200

    default til førstegangbehandling oppførsel i revurdering for nå, i manglende inntekt

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/ManglendeLigningGrunnlagSteg.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/ManglendeLigningGrunnlagSteg.kt
@@ -64,7 +64,7 @@ class ManglendeLigningGrunnlagSteg private constructor(
             }
 
             VurderingType.REVURDERING -> {
-                return FantAvklaringsbehov(Definisjon.FASTSETT_MANUELL_INNTEKT)
+                vurderInntekter(kontekst)
             }
 
             VurderingType.MELDEKORT,
