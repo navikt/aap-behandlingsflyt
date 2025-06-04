eaad760e00390ee3f1c183cd0be77f0dfe215c87
Henrik Gundersen <henrik.gundersen@nav.no>
2025-06-04 16:14:57 +0200

    Endrer StoppetHendelseJobbUtfører til å kjøre i rekkefølge
    Vi ser at det ofte feiler ifm med kall til oppdater-oppgave. Dette kan sannsynligvis skyldes at
    oppgaver ikke oppdateres i rekkefølge for en gitt behandling.

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/hendelse/avløp/BehandlingHendelseServiceImpl.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/hendelse/avløp/BehandlingHendelseServiceImpl.kt
@@ -113,16 +113,20 @@ class BehandlingHendelseServiceImpl(
         log.info("Legger til flytjobber til statistikk og stoppethendelse for behandling: ${behandling.id}")
         flytJobbRepository.leggTil(
             JobbInput(jobb = StoppetHendelseJobbUtfører).medPayload(hendelse)
+                .forBehandling(sak.id.id, behandling.id.id)
         )
         flytJobbRepository.leggTil(
             JobbInput(jobb = StatistikkJobbUtfører).medPayload(hendelse)
+                .forBehandling(sak.id.id, behandling.id.id)
         )
         flytJobbRepository.leggTil(
             JobbInput(jobb = DatadelingMeldePerioderJobbUtfører).medPayload(hendelse)
+                .forBehandling(sak.id.id, behandling.id.id)
         )
 
         flytJobbRepository.leggTil(
             JobbInput(jobb = DatadelingSakStatusJobbUtfører).medPayload(hendelse)
+                .forBehandling(sak.id.id, behandling.id.id)
         )
 
         if (behandling.typeBehandling() in listOf(TypeBehandling.Førstegangsbehandling, TypeBehandling.Revurdering)) {
@@ -132,6 +136,7 @@ class BehandlingHendelseServiceImpl(
         if (behandling.status().erAvsluttet()) {
             flytJobbRepository.leggTil(
                 JobbInput(jobb = DatadelingBehandlingJobbUtfører).medPayload(hendelse)
+                    .forBehandling(sak.id.id, behandling.id.id)
             )
         }
     }
