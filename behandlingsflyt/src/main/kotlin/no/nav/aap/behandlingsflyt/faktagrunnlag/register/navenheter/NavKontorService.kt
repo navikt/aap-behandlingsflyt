6e625d0e53e8b23ae8297b9f3a36a173a2bca15e
Hein Haraldsen <hein.haraldsen@bekk.no>
2025-06-17 12:15:57 +0200

    Legger til endepunkt for å hente alle enheter fra Norg (#662)

---
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/register/navenheter/NavKontorService.kt
@@ -0,0 +1,23 @@
+package no.nav.aap.behandlingsflyt.faktagrunnlag.register.navenheter
+
+import no.nav.aap.behandlingsflyt.behandling.ansattinfo.Enhet
+import no.nav.aap.behandlingsflyt.behandling.ansattinfo.EnhetGateway
+import no.nav.aap.komponenter.gateway.GatewayProvider
+import org.slf4j.LoggerFactory
+
+class NavKontorService {
+    val logger = LoggerFactory.getLogger(javaClass)
+
+    private val enhetGateway = GatewayProvider.provide<EnhetGateway>()
+
+    fun hentNavEnheter(): List<Enhet>? {
+        try {
+            val enheter = enhetGateway.hentAlleEnheter()
+            return enheter.map { it }
+        } catch (e: Exception) {
+            logger.error("Kunne ikke hente enheter.", e)
+            return emptyList()
+        }
+    }
+
+}
