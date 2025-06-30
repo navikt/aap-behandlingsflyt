9f1e696428282db3dafb0dec62350a1d76ee582d
Fredrik Meyer <fredrik.meyer@nav.no>
2025-06-30 15:57:53 +0200

    Fjern noen compiler-warnings etter ny ktor-versjon

--- app/src/test/kotlin/no/nav/aap/behandlingsflyt/TestDTO.kt
+++ app/src/test/kotlin/no/nav/aap/behandlingsflyt/TestDTO.kt
@@ -2,10 +2,8 @@ package no.nav.aap.behandlingsflyt
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.papsign.ktor.openapigen.annotations.Response
-import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.tjenestepensjon.gateway.TjenestePensjonRespons
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.inntekt.InntektPerÅr
 import no.nav.aap.behandlingsflyt.test.modell.TestPerson
-import no.nav.aap.komponenter.type.Periode
 import no.nav.aap.komponenter.verdityper.Beløp
 import org.jetbrains.annotations.NotNull
 import java.time.LocalDate
@@ -18,26 +16,26 @@ data class Institusjoner(
 
 @Response(statusCode = 202)
 data class OpprettTestcaseDTO(
-    @JsonProperty(value = "fødselsdato", required = true) val fødselsdato: LocalDate,
-    @NotNull @JsonProperty(value = "yrkesskade", defaultValue = "false") val yrkesskade: Boolean,
-    @JsonProperty(value = "uføre") val uføre: Int?,
-    @NotNull @JsonProperty(value = "student", defaultValue = "false") val student: Boolean,
-    @NotNull @JsonProperty(value = "barn") val barn: List<TestBarn> = emptyList(),
-    @NotNull @JsonProperty(value = "medlemskap", defaultValue = "true") val medlemskap: Boolean,
-    @JsonProperty(value = "inntekterPerAr") val inntekterPerAr: List<InntektPerÅrDto>? = null,
-    @JsonProperty(value = "tjenestePensjon") val tjenestePensjon: Boolean? = null,
+    @param:JsonProperty(value = "fødselsdato", required = true) val fødselsdato: LocalDate,
+    @param:NotNull @param:JsonProperty(value = "yrkesskade", defaultValue = "false") val yrkesskade: Boolean,
+    @param:JsonProperty(value = "uføre") val uføre: Int?,
+    @param:NotNull @param:JsonProperty(value = "student", defaultValue = "false") val student: Boolean,
+    @param:NotNull @param:JsonProperty(value = "barn") val barn: List<TestBarn> = emptyList(),
+    @param:NotNull @param:JsonProperty(value = "medlemskap", defaultValue = "true") val medlemskap: Boolean,
+    @param:JsonProperty(value = "inntekterPerAr") val inntekterPerAr: List<InntektPerÅrDto>? = null,
+    @param:JsonProperty(value = "tjenestePensjon") val tjenestePensjon: Boolean? = null,
     val institusjoner: Institusjoner = Institusjoner(),
     val sykepenger: List<TestPerson.Sykepenger> = emptyList(),
     val søknadsdato: LocalDate? = null,
 )
 
 data class TestBarn(
-    @JsonProperty(value = "fodselsdato", required = true) val fodselsdato: LocalDate,
+    @param:JsonProperty(value = "fodselsdato", required = true) val fodselsdato: LocalDate,
     val harRelasjon: Boolean = true
 )
 
 data class InntektPerÅrDto(val år: Int, val beløp: Beløp) {
-    fun to() : InntektPerÅr {
+    fun to(): InntektPerÅr {
         return InntektPerÅr(Year.of(år), beløp)
     }
 }
