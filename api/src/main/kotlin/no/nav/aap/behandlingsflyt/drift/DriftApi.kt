006b067e6f149b2265819f20fdf606a9fcdb25a5
Matias Vinjevoll <matias.vinjevoll@nav.no>
2025-07-04 12:26:18 +0200

    Drift-endepunkt for å kunne resende meldeperioder til meldekort-backend

--- api/src/main/kotlin/no/nav/aap/behandlingsflyt/drift/DriftApi.kt
+++ api/src/main/kotlin/no/nav/aap/behandlingsflyt/drift/DriftApi.kt
@@ -4,13 +4,16 @@ import com.papsign.ktor.openapigen.route.path.normal.NormalOpenAPIRoute
 import com.papsign.ktor.openapigen.route.response.respondWithStatus
 import com.papsign.ktor.openapigen.route.route
 import io.ktor.http.*
+import no.nav.aap.behandlingsflyt.behandling.bruddaktivitetsplikt.SaksnummerParameter
 import no.nav.aap.behandlingsflyt.kontrakt.behandling.BehandlingReferanse
+import no.nav.aap.behandlingsflyt.kontrakt.sak.Saksnummer
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingRepository
 import no.nav.aap.komponenter.dbconnect.transaction
 import no.nav.aap.komponenter.repository.RepositoryRegistry
 import no.nav.aap.tilgang.AuthorizationParamPathConfig
 import no.nav.aap.tilgang.BehandlingPathParam
 import no.nav.aap.tilgang.Operasjon
+import no.nav.aap.tilgang.SakPathParam
 import no.nav.aap.tilgang.authorizedPost
 import javax.sql.DataSource
 
@@ -29,13 +32,27 @@ fun NormalOpenAPIRoute.driftAPI(dataSource: DataSource, repositoryRegistry: Repo
                     behandlingPathParam = BehandlingPathParam("referanse"),
                     operasjon = Operasjon.DRIFTE
                 )
-            ){ req, _ ->
+            ) { req, _ ->
                 dataSource.transaction { connection ->
                     val repositoryProvider = repositoryRegistry.provider(connection)
                     val behandlingRepository = repositoryProvider.provide<BehandlingRepository>()
                     val behandling = behandlingRepository.hent(BehandlingReferanse(req.referanse))
 
                     Driftfunksjoner(repositoryProvider).flyttBehandlingTilStart(behandling.id, connection)
+                }
+                respondWithStatus(HttpStatusCode.Accepted)
+            }
+        }
+        route("/sak/{saksnummer}/resend-meldeperioder-til-meldekort-backend") {
+            authorizedPost<SaksnummerParameter, Unit, Unit>(
+                AuthorizationParamPathConfig(
+                    sakPathParam = SakPathParam("saksnummer"),
+                    operasjon = Operasjon.DRIFTE
+                )
+            ) { req, _ ->
+                dataSource.transaction { connection ->
+                    val repositoryProvider = repositoryRegistry.provider(connection)
+                    Driftfunksjoner(repositoryProvider).resendMeldeperioderTilMeldekortBackend(Saksnummer(req.saksnummer))
                 }
                 respondWithStatus(HttpStatusCode.Accepted)
             }
--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/drift/Driftfunksjoner.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/drift/Driftfunksjoner.kt
@@ -1,10 +1,17 @@
 package no.nav.aap.behandlingsflyt.drift
 
 import no.nav.aap.behandlingsflyt.faktagrunnlag.SakOgBehandlingService
+import no.nav.aap.behandlingsflyt.kontrakt.behandling.Status
+import no.nav.aap.behandlingsflyt.kontrakt.behandling.TypeBehandling
+import no.nav.aap.behandlingsflyt.kontrakt.sak.Saksnummer
+import no.nav.aap.behandlingsflyt.prosessering.MeldeperiodeTilMeldekortBackendJobbUtfører
 import no.nav.aap.behandlingsflyt.prosessering.ProsesserBehandlingService
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingId
+import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingRepository
+import no.nav.aap.behandlingsflyt.sakogbehandling.sak.SakRepository
 import no.nav.aap.komponenter.dbconnect.DBConnection
 import no.nav.aap.komponenter.repository.RepositoryProvider
+import org.slf4j.LoggerFactory
 
 /**
  * Klasse for alle driftsfunksjoner. Skal kún brukes av DriftApi.
@@ -12,11 +19,17 @@ import no.nav.aap.komponenter.repository.RepositoryProvider
 class Driftfunksjoner(
     private val prosesserBehandlingService: ProsesserBehandlingService,
     private val sakOgBehandlingService: SakOgBehandlingService,
+    private val sakRepository: SakRepository,
+    private val behandlingRepository: BehandlingRepository,
 ) {
-    constructor(repositoryProvider: RepositoryProvider): this(
+    constructor(repositoryProvider: RepositoryProvider) : this(
         prosesserBehandlingService = ProsesserBehandlingService(repositoryProvider),
         sakOgBehandlingService = SakOgBehandlingService(repositoryProvider),
+        sakRepository = repositoryProvider.provide(),
+        behandlingRepository = repositoryProvider.provide()
     )
+
+    private val log = LoggerFactory.getLogger(javaClass)
 
     fun flyttBehandlingTilStart(behandlingId: BehandlingId, connection: DBConnection) {
         val query = """
@@ -49,6 +62,39 @@ class Driftfunksjoner(
                 setLong(1, behandlingId.toLong())
                 setLong(2, behandlingId.toLong())
                 setLong(3, behandlingId.toLong())
+            }
+        }
+    }
+
+    fun resendMeldeperioderTilMeldekortBackend(saksnummer: Saksnummer) {
+        val sak = sakRepository.hent(saksnummer)
+        val behandling =
+            behandlingRepository.finnSisteBehandlingFor(
+                sakId = sak.id,
+                behandlingstypeFilter = TypeBehandling.entries
+            )
+        val aktuelleBehandlingstyper = listOf(TypeBehandling.Førstegangsbehandling, TypeBehandling.Revurdering)
+
+        when {
+            behandling == null -> {
+                log.info("Finner ikke behandling for sak ${sak.saksnummer}. Resender ikke meldeperioder til meldekort-backend.")
+            }
+
+            !aktuelleBehandlingstyper.contains(behandling.typeBehandling()) -> {
+                log.info("Siste behandling ${behandling.referanse} for sak ${sak.saksnummer} er ikke av type " +
+                        "${aktuelleBehandlingstyper.joinToString("eller")}. Resender ikke meldeperioder til meldekort-backend.")
+            }
+
+            behandling.status() != Status.AVSLUTTET -> {
+                log.info(
+                    "Siste behandling ${behandling.referanse} for sak ${sak.saksnummer} er ikke avsluttet. " +
+                            "Resender ikke meldeperioder til meldekort-backend."
+                )
+            }
+
+            else -> {
+                log.info("Resender meldeperioder til meldekort-backend for sak ${sak.saksnummer} og behandling ${behandling.referanse}")
+                MeldeperiodeTilMeldekortBackendJobbUtfører.nyJobb(behandling.sakId, behandling.id)
             }
         }
     }
