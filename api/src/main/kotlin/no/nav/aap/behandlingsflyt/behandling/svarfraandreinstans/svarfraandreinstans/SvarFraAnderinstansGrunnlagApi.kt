10064eb89c5feb7ec280547b98b20956d94eb5f6
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2025-07-15 12:43:43 +0200

    getGrunnlag svarfraandreinstans

--- api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/svarfraandreinstans/svarfraandreinstans/SvarFraAnderinstansGrunnlagApi.kt
+++ api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/svarfraandreinstans/svarfraandreinstans/SvarFraAnderinstansGrunnlagApi.kt
@@ -5,15 +5,16 @@ import com.papsign.ktor.openapigen.route.response.respond
 import com.papsign.ktor.openapigen.route.route
 import no.nav.aap.behandlingsflyt.faktagrunnlag.dokument.MottattDokumentRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.svarfraandreinstans.SvarFraAndreinstansRepository
+import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.HÅNDTER_SVAR_FRA_ANDREINSTANS_KODE
 import no.nav.aap.behandlingsflyt.kontrakt.behandling.BehandlingReferanse
 import no.nav.aap.behandlingsflyt.kontrakt.hendelse.InnsendingType
 import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.KabalHendelseV0
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingRepository
+import no.nav.aap.behandlingsflyt.tilgang.kanSaksbehandle
 import no.nav.aap.komponenter.dbconnect.transaction
 import no.nav.aap.komponenter.repository.RepositoryRegistry
-import no.nav.aap.tilgang.AuthorizationParamPathConfig
 import no.nav.aap.tilgang.BehandlingPathParam
-import no.nav.aap.tilgang.authorizedGet
+import no.nav.aap.tilgang.getGrunnlag
 import javax.sql.DataSource
 
 fun NormalOpenAPIRoute.svarFraAndreinstansGrunnlagApi(
@@ -21,12 +22,9 @@ fun NormalOpenAPIRoute.svarFraAndreinstansGrunnlagApi(
     repositoryRegistry: RepositoryRegistry
 ) {
     route("api/svar-fra-andreinstans/{referanse}/grunnlag/svar-fra-andreinstans") {
-        authorizedGet<BehandlingReferanse, SvarFraAndreinstansGrunnlagDto>(
-            AuthorizationParamPathConfig(
-                behandlingPathParam = BehandlingPathParam(
-                    "referanse"
-                )
-            )
+        getGrunnlag<BehandlingReferanse, SvarFraAndreinstansGrunnlagDto>(
+                behandlingPathParam = BehandlingPathParam("referanse"),
+                avklaringsbehovKode = HÅNDTER_SVAR_FRA_ANDREINSTANS_KODE
         ) { req ->
             val respons = dataSource.transaction(readOnly = true) {
                 val repositoryProvider = repositoryRegistry.provider(it)
@@ -45,6 +43,7 @@ fun NormalOpenAPIRoute.svarFraAndreinstansGrunnlagApi(
                 SvarFraAndreinstansGrunnlagDto(
                     svarFraAndreinstans = hendelse.tilDto(),
                     gjeldendeVurdering = grunnlag?.vurdering?.tilDto(),
+                    harTilgangTilÅSaksbehandle = kanSaksbehandle()
                 )
             }
 
