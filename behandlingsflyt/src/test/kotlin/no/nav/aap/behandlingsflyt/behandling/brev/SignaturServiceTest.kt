28f0ee6e423141764ef1580c34f4aea0a8ad747a
Steffen Hageland <steffen.hageland@nav.no>
2025-07-17 14:52:04 +0200

    Fjerne duplikater når man henter ut saksbehandlere for signaturer

--- behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/behandling/brev/SignaturServiceTest.kt
+++ behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/behandling/brev/SignaturServiceTest.kt
@@ -87,6 +87,63 @@ class SignaturServiceTest {
         )
     }
 
+    @Test
+    fun `skal fjerne duplikater om samme person har løst avklaringsbehov som er knytte til ulike roller – eksempel samme person på lokalkontor og NAY`() {
+
+        val avklaringsbehovRepository = mockk<AvklaringsbehovRepository>()
+        val avklaringsbehovOperasjonerRepository = mockk<AvklaringsbehovOperasjonerRepository>()
+
+        val signaturService = SignaturService(avklaringsbehovRepository)
+
+        val behandlingId = BehandlingId(1)
+        val brevbestilling = Brevbestilling(
+            id = 0,
+            behandlingId = behandlingId,
+            typeBrev = TypeBrev.VEDTAK_INNVILGELSE,
+            referanse = BrevbestillingReferanse(UUID.randomUUID()),
+            status = Status.FORHÅNDSVISNING_KLAR,
+            opprettet = LocalDateTime.now()
+        )
+        val veilederIdent = "v000000"
+        val kvalitetssikrerIdent = "k000000"
+        val beslutterIdent = "b000000"
+
+        // SAKSBEHANDLER_OPPFOLGING
+        leggTilEndring(Definisjon.FRITAK_MELDEPLIKT, endretAv = veilederIdent, AvklaringsbehovStatus.AVSLUTTET)
+        leggTilEndring(Definisjon.AVKLAR_SYKDOM, endretAv = SYSTEMBRUKER.ident, AvklaringsbehovStatus.OPPRETTET)
+        leggTilEndring(Definisjon.AVKLAR_SYKDOM, endretAv = veilederIdent, AvklaringsbehovStatus.AVSLUTTET)
+        leggTilEndring(Definisjon.AVKLAR_SYKDOM, endretAv = kvalitetssikrerIdent, AvklaringsbehovStatus.KVALITETSSIKRET)
+        leggTilEndring(Definisjon.AVKLAR_SYKDOM, endretAv = beslutterIdent, AvklaringsbehovStatus.TOTRINNS_VURDERT)
+
+        // SAKSBEHANDLER_NASJONAL
+        leggTilEndring(Definisjon.AVKLAR_STUDENT, endretAv = veilederIdent, AvklaringsbehovStatus.AVSLUTTET)
+        leggTilEndring(
+            Definisjon.FASTSETT_BEREGNINGSTIDSPUNKT, endretAv = veilederIdent, AvklaringsbehovStatus.AVSLUTTET
+        )
+
+        // KVALITETSSIKRER
+        leggTilEndring(Definisjon.KVALITETSSIKRING, endretAv = kvalitetssikrerIdent, AvklaringsbehovStatus.AVSLUTTET)
+
+        // BESLUTTER
+        leggTilEndring(Definisjon.FATTE_VEDTAK, endretAv = beslutterIdent, AvklaringsbehovStatus.AVSLUTTET)
+
+
+        every { avklaringsbehovOperasjonerRepository.hent(behandlingId) } returns avklaringsbehovene
+        every { avklaringsbehovRepository.hentAvklaringsbehovene(behandlingId) } returns Avklaringsbehovene(
+            avklaringsbehovOperasjonerRepository,
+            behandlingId
+        )
+
+        val signaturer = signaturService.finnSignaturGrunnlag(brevbestilling, Bruker(""))
+
+        assertThat(signaturer).hasSize(3)
+        assertThat(signaturer).containsExactlyInAnyOrder(
+            SignaturGrunnlag(navIdent = beslutterIdent, rolle = Rolle.BESLUTTER),
+            SignaturGrunnlag(navIdent = kvalitetssikrerIdent, rolle = Rolle.KVALITETSSIKRER),
+            SignaturGrunnlag(navIdent = veilederIdent, rolle = Rolle.SAKSBEHANDLER_NASJONAL)
+        )
+    }
+
     private val avklaringsbehovene = mutableListOf<Avklaringsbehov>()
 
     private fun leggTilEndring(
