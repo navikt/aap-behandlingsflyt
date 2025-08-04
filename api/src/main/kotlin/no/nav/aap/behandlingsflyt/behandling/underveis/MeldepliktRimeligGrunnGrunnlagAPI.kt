0e87dfab8541c45b1490aac5a078ee5b7684d120
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2025-08-04 16:12:28 +0200

    Grunnlag api for meldeplikt rimelig grunn

---
+++ api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/underveis/MeldepliktRimeligGrunnGrunnlagAPI.kt
@@ -0,0 +1,91 @@
+package no.nav.aap.behandlingsflyt.behandling.underveis
+
+import com.papsign.ktor.openapigen.route.path.normal.NormalOpenAPIRoute
+import com.papsign.ktor.openapigen.route.response.respond
+import com.papsign.ktor.openapigen.route.route
+import no.nav.aap.behandlingsflyt.behandling.ansattinfo.AnsattInfoService
+import no.nav.aap.behandlingsflyt.behandling.vurdering.VurdertAvResponse
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.meldeplikt.MeldepliktRimeligGrunnRepository
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.meldeplikt.RimeligGrunnVurdering
+import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon
+import no.nav.aap.behandlingsflyt.kontrakt.behandling.BehandlingReferanse
+import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.Behandling
+import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingRepository
+import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.flate.BehandlingReferanseService
+import no.nav.aap.behandlingsflyt.tilgang.kanSaksbehandle
+import no.nav.aap.komponenter.dbconnect.transaction
+import no.nav.aap.komponenter.repository.RepositoryRegistry
+import no.nav.aap.tilgang.BehandlingPathParam
+import no.nav.aap.tilgang.getGrunnlag
+import java.time.LocalDateTime
+import javax.sql.DataSource
+
+fun NormalOpenAPIRoute.meldepliktRimeligGrunnGrunnlagApi(
+    dataSource: DataSource,
+    repositoryRegistry: RepositoryRegistry
+) {
+    route("/api/behandling/{referanse}/grunnlag/meldeplikt-rimelig-grunn") {
+        getGrunnlag<BehandlingReferanse, MeldepliktRimeligGrunnGrunnlagResponse>(
+            behandlingPathParam = BehandlingPathParam("referanse"),
+            avklaringsbehovKode = Definisjon.OVERSTYR_IKKE_OPPFYLT_MELDEPLIKT.kode.toString(),
+        ) { req ->
+            val meldepliktGrunnlag =
+                dataSource.transaction(readOnly = true) { connection ->
+                    val repositoryProvider = repositoryRegistry.provider(connection)
+                    val behandlingRepository = repositoryProvider.provide<BehandlingRepository>()
+                    val meldepliktRimeligGrunnRepository = repositoryProvider.provide<MeldepliktRimeligGrunnRepository>()
+
+                    val behandling: Behandling =
+                        BehandlingReferanseService(behandlingRepository).behandling(req)
+
+                    val nåTilstand = meldepliktRimeligGrunnRepository.hentHvisEksisterer(behandling.id)?.vurderinger
+
+                    val vedtatteVerdier =
+                        behandling.forrigeBehandlingId?.let { meldepliktRimeligGrunnRepository.hentHvisEksisterer(it) }?.vurderinger
+                            ?: emptyList()
+                    val historikk =
+                        meldepliktRimeligGrunnRepository.hentAlleVurderinger(behandling.sakId, behandling.id)
+
+                    // TODO: Legg til meldeperioder som ikke er oppfylt i grunnlaget
+                    MeldepliktRimeligGrunnGrunnlagResponse(
+                        harTilgangTilÅSaksbehandle = kanSaksbehandle(),
+                        historikk =
+                            historikk
+                                .map { tilResponse(it) }
+                                .sortedBy { it.vurderingsTidspunkt }
+                                .toSet(),
+                        gjeldendeVedtatteVurderinger =
+                            vedtatteVerdier
+                                .map { tilResponse(it) }
+                                .sortedBy { it.fraDato },
+                        vurderinger =
+                            nåTilstand
+                                ?.filterNot { vedtatteVerdier.contains(it) }
+                                ?.map { tilResponse(it) }
+                                ?.sortedBy { it.fraDato } ?: emptyList()
+                    )
+                }
+
+            respond(meldepliktGrunnlag)
+        }
+    }
+}
+
+private fun tilResponse(rimeligGrunnVurdering: RimeligGrunnVurdering): MeldepliktRimeligGrunnVurderingResponse {
+    val ansattNavnOgEnhet = AnsattInfoService().hentAnsattNavnOgEnhet(rimeligGrunnVurdering.vurdertAv)
+
+    return MeldepliktRimeligGrunnVurderingResponse(
+        begrunnelse = rimeligGrunnVurdering.begrunnelse,
+        vurderingsTidspunkt = rimeligGrunnVurdering.opprettetTid ?: LocalDateTime.now(),
+        harRimeligGrunn = rimeligGrunnVurdering.harRimeligGrunn,
+        fraDato = rimeligGrunnVurdering.fraDato,
+        vurdertAv = VurdertAvResponse(
+            ident = rimeligGrunnVurdering.vurdertAv,
+            dato =
+                rimeligGrunnVurdering.opprettetTid?.toLocalDate()
+                    ?: error("Fant ikke opprettet tidspunkt for rimeligGrunnVurdering"),
+            ansattnavn = ansattNavnOgEnhet?.navn,
+            enhetsnavn = ansattNavnOgEnhet?.enhet
+        )
+    )
+}
