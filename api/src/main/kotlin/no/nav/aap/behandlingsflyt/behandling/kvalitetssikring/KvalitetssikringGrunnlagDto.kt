6473697126591eedb348c3c6a39f6c3661f5b7ea
Greger Siem Knudsen <greger.siem.knudsen@nav.no>
2025-08-07 14:49:56 +0200

    La til KvalitetssikringTilgangDto

--- api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/kvalitetssikring/KvalitetssikringGrunnlagDto.kt
+++ api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/kvalitetssikring/KvalitetssikringGrunnlagDto.kt
@@ -4,5 +4,3 @@ import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.flate.Historikk
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løser.vedtak.TotrinnsVurdering
 
 data class KvalitetssikringGrunnlagDto(val harTilgangTilÅSaksbehandle: Boolean, val vurderinger: List<TotrinnsVurdering>, val historikk: List<Historikk>)
-
-data class KvalitetssikringTilgangDto(val harTilgangTilÅKvalitetssikre: Boolean)
