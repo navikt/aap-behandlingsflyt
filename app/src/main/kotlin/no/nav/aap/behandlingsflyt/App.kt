601150edc8e194a27d398cf47324f7f3ab1bb5ed
Hein Haraldsen <hein.haraldsen@bekk.no>
2025-08-11 16:18:54 +0200

    Legger til løsning og løsere for overgang uføre

--- app/src/main/kotlin/no/nav/aap/behandlingsflyt/App.kt
+++ app/src/main/kotlin/no/nav/aap/behandlingsflyt/App.kt
@@ -170,6 +170,7 @@ internal fun Application.server(
                 meldepliktsgrunnlagApi(dataSource, repositoryRegistry, gatewayProvider)
                 meldepliktOverstyringGrunnlagApi(dataSource, repositoryRegistry, gatewayProvider)
                 arbeidsevneGrunnlagApi(dataSource, repositoryRegistry, gatewayProvider)
+                overgangUforeGrunnlagApi(dataSource, repositoryRegistry)
                 medlemskapsgrunnlagApi(dataSource, repositoryRegistry)
                 studentgrunnlagApi(dataSource, repositoryRegistry, gatewayProvider)
                 sykdomsgrunnlagApi(dataSource, repositoryRegistry, gatewayProvider)
