da4e3b6ce7272482fd1b7eceb8d5371778c88ca2
Nicolas N <nicolas.nordhagen@nav.no>
2025-08-19 14:18:16 +0200

    AAP-1168 Liten fiks på logikken

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løser/FastsettFullmektigLøser.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løser/FastsettFullmektigLøser.kt
@@ -2,8 +2,8 @@ package no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løser
 
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovKontekst
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løsning.FastsettFullmektigLøsning
+import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løsning.FullmektigLøsningDto
 import no.nav.aap.behandlingsflyt.faktagrunnlag.klage.fullmektig.FullmektigRepository
-import no.nav.aap.behandlingsflyt.faktagrunnlag.klage.fullmektig.IdentMedType
 import no.nav.aap.behandlingsflyt.faktagrunnlag.klage.fullmektig.IdentType
 import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon
 import no.nav.aap.komponenter.verdityper.validering.FolkeregisterIdentValidering
@@ -19,7 +19,7 @@ class FastsettFullmektigLøser(
     )
 
     override fun løs(kontekst: AvklaringsbehovKontekst, løsning: FastsettFullmektigLøsning): LøsningsResultat {
-        validerFullmektig(løsning.fullmektigVurdering.fullmektigIdentMedType)
+        validerFullmektig(løsning.fullmektigVurdering)
 
         fullmektigRepository.lagre(
             kontekst.kontekst.behandlingId,
@@ -30,7 +30,11 @@ class FastsettFullmektigLøser(
         )
     }
 
-    private fun validerFullmektig(fullmektig: IdentMedType?) {
+    private fun validerFullmektig(løsning: FullmektigLøsningDto) {
+        if (!løsning.harFullmektig) return
+
+        val fullmektig = løsning.fullmektigIdentMedType
+
         if (fullmektig?.type == IdentType.ORGNR) {
             check(OrganisasjonsnummerValidering.erGyldig(fullmektig.ident)) {
                 "Fullmektig ident er av type ORGNR, men organisasjonsnummeret er ikke gyldig"
