efe6b83302df5bdefd3115f9e38a0617189eab74
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2025-08-20 16:30:46 +0200

    Hent siste ytelsesbehandling basert på forrigeBehandlingId
    co-authored-by: Ine Lofthus Arnesen <Ine.Lofthus.Arnesen@nav.no>

---
+++ behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/SakOgBehandlingServiceTest.kt
@@ -0,0 +1,72 @@
+package no.nav.aap.behandlingsflyt.faktagrunnlag
+
+import no.nav.aap.behandlingsflyt.help.opprettSak
+import no.nav.aap.behandlingsflyt.integrasjon.createGatewayProvider
+import no.nav.aap.behandlingsflyt.kontrakt.behandling.Status
+import no.nav.aap.behandlingsflyt.repository.behandling.BehandlingRepositoryImpl
+import no.nav.aap.behandlingsflyt.repository.postgresRepositoryRegistry
+import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.VurderingsbehovMedPeriode
+import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.ÅrsakTilOpprettelse
+import no.nav.aap.behandlingsflyt.sakogbehandling.flyt.Vurderingsbehov
+import no.nav.aap.behandlingsflyt.test.FakeUnleashFasttrackMeldekort
+import no.nav.aap.behandlingsflyt.test.januar
+import no.nav.aap.komponenter.dbconnect.transaction
+import no.nav.aap.komponenter.dbtest.TestDatabase
+import no.nav.aap.komponenter.type.Periode
+import org.assertj.core.api.Assertions.assertThat
+import javax.sql.DataSource
+import kotlin.test.Test
+
+class SakOgBehandlingServiceTest {
+    @TestDatabase
+    lateinit var dataSource: DataSource
+
+    private val gatewayProvider = createGatewayProvider {
+        register<FakeUnleashFasttrackMeldekort>()
+    }
+
+    @Test
+    fun `gjenbruker åpen behandling hvis vi prøver å opprette enda en ny behandling etter et meldekort`() {
+        dataSource.transaction {  connection ->
+            val sakOgBehandlingService = SakOgBehandlingService(postgresRepositoryRegistry.provider(connection), gatewayProvider)
+            val behandlingRepository = BehandlingRepositoryImpl(connection)
+            val sak = opprettSak(connection, Periode(1 januar 2020, 1 januar 2021))
+
+            /* Førstegangsbehandling */
+            sakOgBehandlingService.finnEllerOpprettBehandling(
+                sak.id,
+                listOf(VurderingsbehovMedPeriode(Vurderingsbehov.MOTTATT_SØKNAD)),
+                ÅrsakTilOpprettelse.SØKNAD,
+            ).åpenBehandling!!.also {
+                behandlingRepository.oppdaterBehandlingStatus(it.id, Status.AVSLUTTET)
+            }
+
+            /* Åpen revurdering */
+            sakOgBehandlingService.finnEllerOpprettBehandling(
+                sak.id,
+                listOf(VurderingsbehovMedPeriode(Vurderingsbehov.REVURDER_SAMORDNING)),
+                ÅrsakTilOpprettelse.MANUELL_OPPRETTELSE,
+            )
+
+            /* Send inn meldekort */
+            val meldekortbehandling = (sakOgBehandlingService.finnEllerOpprettBehandling(
+                sak.id,
+                listOf(VurderingsbehovMedPeriode(Vurderingsbehov.MOTTATT_MELDEKORT)),
+                ÅrsakTilOpprettelse.MELDEKORT,
+            ) as SakOgBehandlingService.MåBehandlesAtomært).nyBehandling
+            behandlingRepository.oppdaterBehandlingStatus(meldekortbehandling.id, Status.AVSLUTTET)
+
+            /* Forsøk å opprett en til revurdering (f.eks. nye opplysninger fra register). */
+            sakOgBehandlingService.finnEllerOpprettBehandling(
+                sak.id,
+                listOf(VurderingsbehovMedPeriode(Vurderingsbehov.INSTITUSJONSOPPHOLD)),
+                ÅrsakTilOpprettelse.ENDRING_I_REGISTERDATA,
+            )
+
+
+            val behandlinger = behandlingRepository.hentAlleFor(sak.id)
+            assertThat(behandlinger)
+                .hasSize(3)
+        }
+    }
+}
