8a8fa1c7ada5a2cf1f02506f4343003946f62c53
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2025-08-22 07:31:07 +0200

    wip uføre-informasjonskrav

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/register/uføre/UføreService.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/register/uføre/UføreService.kt
@@ -92,21 +92,18 @@ class UføreService(
         return samordningUføreRepository.hentHvisEksisterer(behandlingId)
     }
 
-    private fun harEndringerUføre(
-        eksisterende: UføreGrunnlag?,
-        uføregrader: List<Uføre>
-    ): Boolean {
-        if (eksisterende == null && uføregrader.isEmpty()) {
-            return false
-        }
-        return uføregrader != eksisterende?.vurderinger
-    }
-
     companion object : Informasjonskravkonstruktør {
         override val navn = InformasjonskravNavn.UFØRE
 
         override fun konstruer(repositoryProvider: RepositoryProvider, gatewayProvider: GatewayProvider): UføreService {
             return UføreService(repositoryProvider, gatewayProvider)
+        }
+
+        fun harEndringerUføre(
+            eksisterende: UføreGrunnlag?,
+            uføregrader: List<Uføre>
+        ): Boolean {
+            return eksisterende == null || uføregrader.toSet() != eksisterende.vurderinger.toSet()
         }
     }
 }
