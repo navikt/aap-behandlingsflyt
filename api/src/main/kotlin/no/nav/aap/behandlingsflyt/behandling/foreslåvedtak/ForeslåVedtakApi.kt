e21c46cd1c07b8669e131f56d804920e271c1af2
SelmaBergstrand <78426277+SelmaBergstrand@users.noreply.github.com>
2025-08-25 09:40:46 +0200

    AAP-766 Nytt grunnlagApi for foreslå vedtak (#968)
    * AAP-766 Nytt grunnlagApi for foreslå vedtak
    
    * AAP-766 Håndter at underveisGrunnlag kan være null ved tidlig avslag
    
    * AAP-766 Fjern at periode er nullable, svar med tom liste hvis avslag

---
+++ api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/foreslåvedtak/ForeslåVedtakApi.kt
@@ -0,0 +1,68 @@
+package no.nav.aap.behandlingsflyt.behandling.foreslåvedtak
+
+import com.papsign.ktor.openapigen.route.path.normal.NormalOpenAPIRoute
+import com.papsign.ktor.openapigen.route.response.respond
+import com.papsign.ktor.openapigen.route.route
+import no.nav.aap.behandlingsflyt.behandling.foreslåvedtak.ForeslåVedtakDto.Companion.tilForeslåVedtakData
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.underveis.UnderveisRepository
+import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.FORESLÅ_VEDTAK_KODE
+import no.nav.aap.behandlingsflyt.kontrakt.behandling.BehandlingReferanse
+import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingRepository
+import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.flate.BehandlingReferanseService
+import no.nav.aap.komponenter.dbconnect.transaction
+import no.nav.aap.komponenter.repository.RepositoryRegistry
+import no.nav.aap.komponenter.tidslinje.Segment
+import no.nav.aap.komponenter.tidslinje.Tidslinje
+import no.nav.aap.tilgang.BehandlingPathParam
+import no.nav.aap.tilgang.getGrunnlag
+import javax.sql.DataSource
+
+fun NormalOpenAPIRoute.foreslaaVedtakAPI(dataSource: DataSource, repositoryRegistry: RepositoryRegistry) {
+    route("/api/behandling") {
+        route("/{referanse}/grunnlag/foreslaa-vedtak").getGrunnlag<BehandlingReferanse, ForeslåVedtakResponse>(
+            behandlingPathParam = BehandlingPathParam("referanse"),
+            avklaringsbehovKode = FORESLÅ_VEDTAK_KODE
+        ) { behandlingReferanse ->
+            val underveisGrunnlag = dataSource.transaction(readOnly = true) { conn ->
+                val repositoryProvider = repositoryRegistry.provider(conn)
+                val behandlingRepository = repositoryProvider.provide<BehandlingRepository>()
+                val behandling =
+                    BehandlingReferanseService(behandlingRepository).behandling(behandlingReferanse)
+                val underveisRepository = repositoryProvider.provide<UnderveisRepository>()
+                underveisRepository.hentHvisEksisterer(behandling.id)
+            }
+
+            // Hvis avslag tidlig i behandlingen finnes ikke underveisgrunnlag
+            if (underveisGrunnlag == null) {
+                respond(ForeslåVedtakResponse(emptyList()))
+            } else {
+                val foreslåVedtakPerioder = underveisGrunnlag.perioder.map {
+                    ForeslåVedtakDto(
+                        it.periode,
+                        it.utfall,
+                        it.rettighetsType,
+                    )
+                }
+
+                val perioderMedUtfallOgRettighetstype = foreslåVedtakPerioder
+                    .map {
+                        Segment(it.periode, it.tilForeslåVedtakData())
+                    }
+                    .let(::Tidslinje).komprimer()
+                    .map {
+                        ForeslåVedtakDto(
+                            periode = it.periode,
+                            utfall = it.verdi.utfall,
+                            rettighetsType = it.verdi.rettighetsType,
+                        )
+                    }
+
+                respond(
+                    ForeslåVedtakResponse(
+                        perioderMedUtfallOgRettighetstype
+                    )
+                )
+            }
+        }
+    }
+}
---
+++ api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/foreslåvedtak/ForeslåVedtakDto.kt
@@ -0,0 +1,25 @@
+package no.nav.aap.behandlingsflyt.behandling.foreslåvedtak
+
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.RettighetsType
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Utfall
+import no.nav.aap.komponenter.type.Periode
+
+data class ForeslåVedtakDto(
+    val periode: Periode,
+    val utfall: Utfall,
+    val rettighetsType: RettighetsType?
+) {
+    companion object {
+        fun ForeslåVedtakDto.tilForeslåVedtakData(): ForeslåVedtakData {
+            return ForeslåVedtakData(
+                this.utfall,
+                this.rettighetsType
+            )
+        }
+    }
+}
+
+data class ForeslåVedtakData(
+    val utfall: Utfall,
+    val rettighetsType: RettighetsType?
+)
