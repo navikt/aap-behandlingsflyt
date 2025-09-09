8e5263325f14c2427511e595d59bbb5317e66554
Steffen Hageland <steffen.hageland@nav.no>
2025-09-09 16:32:59 +0200

    Fikser en liten visningsbug i overstyringsperioder

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/saksbehandler/meldeplikt/OverstyringMeldepliktGrunnlag.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/saksbehandler/meldeplikt/OverstyringMeldepliktGrunnlag.kt
@@ -11,44 +11,46 @@ data class OverstyringMeldepliktGrunnlag(
     val vurderinger: List<OverstyringMeldepliktVurdering>
 ) {
     fun tilTidslinje(): Tidslinje<OverstyringMeldepliktData> {
-        return vurderinger
-            .sortedBy { it.opprettetTid }
-            .map { it.tilTidslinje() }
-            .fold(Tidslinje()) { acc, tidslinje ->
-                acc.outerJoin(tidslinje) { left, right ->
-                    check(left != null || right != null) {
-                        "Både left og right kan ikke være NULL når man lager tidslinje for overstyring av meldeplikt. Det tyder på kodefeil."
-                    }
+        return vurderinger.tilTidslinje()
+    }
+}
 
-                    OverstyringMeldepliktData(
-                        vurdertAv = right?.vurdertAv ?: left?.vurdertAv!!,
-                        vurdertIBehandling = right?.vurdertIBehandling ?: left?.vurdertIBehandling!!,
-                        opprettetTid = right?.opprettetTid ?: left?.opprettetTid!!,
-                        begrunnelse = right?.begrunnelse ?: left?.begrunnelse!!,
-                        meldepliktOverstyringStatus = right?.meldepliktOverstyringStatus ?: left?.meldepliktOverstyringStatus!!
-                    )
-                }
+fun List<OverstyringMeldepliktVurdering>.tilTidslinje(): Tidslinje<OverstyringMeldepliktData> =
+    this.sortedBy { it.opprettetTid }
+        .map { it.tilTidslinje() }
+        .fold(Tidslinje()) { acc, tidslinje ->
+        acc.outerJoin(tidslinje) { left, right ->
+            check(left != null || right != null) {
+                "Både left og right kan ikke være NULL når man lager tidslinje for overstyring av meldeplikt. Det tyder på kodefeil."
             }
-    }
 
-    private fun OverstyringMeldepliktVurdering.tilTidslinje(): Tidslinje<OverstyringMeldepliktData> {
-        return Tidslinje(
-            perioder.map { it.tilTidslinjeSegment(vurdertAv, vurdertIBehandling, opprettetTid) }
-        )
-    }
-
-    private fun OverstyringMeldepliktVurderingPeriode.tilTidslinjeSegment(vurdertAv: String, vurdertIBehandling: BehandlingReferanse, opprettetTid: LocalDateTime?): Segment<OverstyringMeldepliktData> {
-        return Segment(
-            periode = Periode(fom = fom, tom = tom),
-            verdi = OverstyringMeldepliktData(
-                begrunnelse = begrunnelse,
-                meldepliktOverstyringStatus = meldepliktOverstyringStatus,
-                vurdertAv = vurdertAv,
-                vurdertIBehandling = vurdertIBehandling,
-                opprettetTid = opprettetTid ?: LocalDateTime.now(),
+            OverstyringMeldepliktData(
+                vurdertAv = right?.vurdertAv ?: left?.vurdertAv!!,
+                vurdertIBehandling = right?.vurdertIBehandling ?: left?.vurdertIBehandling!!,
+                opprettetTid = right?.opprettetTid ?: left?.opprettetTid!!,
+                begrunnelse = right?.begrunnelse ?: left?.begrunnelse!!,
+                meldepliktOverstyringStatus = right?.meldepliktOverstyringStatus ?: left?.meldepliktOverstyringStatus!!
             )
-        )
+        }
     }
+
+fun OverstyringMeldepliktVurdering.tilTidslinje(): Tidslinje<OverstyringMeldepliktData> {
+    return Tidslinje(
+        perioder.map { it.tilTidslinjeSegment(vurdertAv, vurdertIBehandling, opprettetTid) }
+    )
+}
+
+fun OverstyringMeldepliktVurderingPeriode.tilTidslinjeSegment(vurdertAv: String, vurdertIBehandling: BehandlingReferanse, opprettetTid: LocalDateTime?): Segment<OverstyringMeldepliktData> {
+    return Segment(
+        periode = Periode(fom = fom, tom = tom),
+        verdi = OverstyringMeldepliktData(
+            begrunnelse = begrunnelse,
+            meldepliktOverstyringStatus = meldepliktOverstyringStatus,
+            vurdertAv = vurdertAv,
+            vurdertIBehandling = vurdertIBehandling,
+            opprettetTid = opprettetTid ?: LocalDateTime.now(),
+        )
+    )
 }
 
 
