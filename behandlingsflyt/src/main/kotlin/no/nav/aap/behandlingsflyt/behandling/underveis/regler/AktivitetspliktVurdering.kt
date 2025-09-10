323a23b6b29eea352d44e91c6c57fc528915f57f
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2025-09-10 07:23:10 +0200

    Fjern utdatert kommentar

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/underveis/regler/AktivitetspliktVurdering.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/underveis/regler/AktivitetspliktVurdering.kt
@@ -3,16 +3,6 @@ package no.nav.aap.behandlingsflyt.behandling.underveis.regler
 import no.nav.aap.behandlingsflyt.faktagrunnlag.aktivitetsplikt.Aktivitetsplikt11_7Vurdering
 import no.nav.aap.behandlingsflyt.faktagrunnlag.aktivitetsplikt.Utfall
 
-/** Den ferdige vurderingen av § 11-8 "Medlemmets aktivitetsplikt".
- *
- * Vurderingen kan være at det blir opphør av aap.
- *
- * Datoen/perioden som vurderingen gjelder for er implisitt, da typen brukes
- * inne i konteksten av en [Tidslinje][no.nav.aap.komponenter.tidslinje.Tidslinje].
- *
- * - [Folketrygdloven § 11-7](https://lovdata.no/lov/1997-02-28-19/§11-7)
- */
-
 class AktivitetspliktVurdering(
     val vurdering: Aktivitetsplikt11_7Vurdering,
 ) {
