38edb8230c14f56b914998789a8cca6199b42b7e
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2025-09-14 01:30:21 +0200

    Støtte for å sende assertions som tidslinje til assertTidslinje

--- lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/help/AssertTidslinje.kt
+++ lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/help/AssertTidslinje.kt
@@ -7,9 +7,8 @@ import no.nav.aap.komponenter.type.Periode
 import org.junit.jupiter.api.Assertions
 import org.opentest4j.AssertionFailedError
 
-inline fun <reified T> Tidslinje<T>.assertTidslinje(vararg assertions: Segment<(T) -> Unit>) {
-    this.kombiner(
-        Tidslinje(assertions.toList()), JoinStyle.OUTER_JOIN<_, _, Nothing?> { periode, tsegment, assertion ->
+inline fun <reified T> Tidslinje<T>.assertTidslinje(assertions: Tidslinje<(T) -> Unit>) {
+    this.kombiner(assertions, JoinStyle.OUTER_JOIN<_, _, Nothing?> { periode, tsegment, assertion ->
             Assertions.assertNotNull(tsegment, "Verdi av type ${T::class.simpleName} mangler for periode $periode")
             Assertions.assertNotNull(assertion, "Assert mangler for periode $periode")
 
@@ -22,6 +21,10 @@ inline fun <reified T> Tidslinje<T>.assertTidslinje(vararg assertions: Segment<(
             null
         }
     )
+}
+
+inline fun <reified T> Tidslinje<T>.assertTidslinje(vararg assertions: Segment<(T) -> Unit>) {
+    this.assertTidslinje(Tidslinje(assertions.toList()))
 }
 
 inline fun <reified T> assertTidslinje(tidslinje: Tidslinje<T>, vararg assertions: Pair<Periode, (T) -> Unit>) {
