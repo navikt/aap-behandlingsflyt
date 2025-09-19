59645318598d1c5ca96a64be0bde2e29681e4e8e
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2025-09-19 18:19:19 +0200

    Lagr ned rettighetsperioden som lå til grunn for oppdatering av informasjonskrav

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/InformasjonskravOppdatert.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/InformasjonskravOppdatert.kt
@@ -1,6 +1,7 @@
 package no.nav.aap.behandlingsflyt.faktagrunnlag
 
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingId
+import no.nav.aap.komponenter.type.Periode
 import java.time.Instant
 import java.time.LocalDate
 import java.time.ZoneId
@@ -9,6 +10,15 @@ class InformasjonskravOppdatert(
     val behandlingId: BehandlingId,
     val navn: InformasjonskravNavn,
     val oppdatert: Instant,
+
+    /** Rettighetsperioden på tidspunktet informasjonskravet kjørte sist.
+     *
+     * Siden rettighetsperioden kan endre seg flere ganger i en behandling, så
+     * kan man ikke utlede rettighetsperioden på annet vis. Og det er heller
+     * ikke garantert at rettighetsperioden her matcher nåverende rettighetsperiode,
+     * selv om oppdateringen skjedde i samem behandling.
+     */
+    val rettighetsperiode: Periode?,
 ) {
     val datoOppdatert: LocalDate
         get() = oppdatert.atZone(ZoneId.of("Europe/Oslo")).toLocalDate()
