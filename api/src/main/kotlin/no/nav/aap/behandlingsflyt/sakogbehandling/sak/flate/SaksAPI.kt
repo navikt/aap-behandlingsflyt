c1f446ef36e8cd318e08640a79207821a7068784
Simen Andre Dahl Jensen <51543274+Toxi2209@users.noreply.github.com>
2025-09-22 14:23:06 +0200

    Aap 1446 legg årsak (#1145)
    * Legger til avklaringsDefinisjon på BehandlinginfoDTO, dette for å finne hvilket steg oppfølgningsoppgaven befinner seg i
    
    * reformat
    
    * linker årsak til opprinselse av oppfølgnings oppgave
    
    * refactor

--- api/src/main/kotlin/no/nav/aap/behandlingsflyt/sakogbehandling/sak/flate/SaksAPI.kt
+++ api/src/main/kotlin/no/nav/aap/behandlingsflyt/sakogbehandling/sak/flate/SaksAPI.kt
@@ -12,6 +12,9 @@ import no.nav.aap.behandlingsflyt.Tags
 import no.nav.aap.behandlingsflyt.behandling.Resultat
 import no.nav.aap.behandlingsflyt.behandling.ResultatUtleder
 import no.nav.aap.behandlingsflyt.faktagrunnlag.SakOgBehandlingService
+import no.nav.aap.behandlingsflyt.faktagrunnlag.dokument.MottaDokumentService
+import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.AvklaringsbehovKode
+import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon
 import no.nav.aap.behandlingsflyt.kontrakt.behandling.TypeBehandling
 import no.nav.aap.behandlingsflyt.kontrakt.sak.Saksnummer
 import no.nav.aap.behandlingsflyt.kontrakt.sak.Status
