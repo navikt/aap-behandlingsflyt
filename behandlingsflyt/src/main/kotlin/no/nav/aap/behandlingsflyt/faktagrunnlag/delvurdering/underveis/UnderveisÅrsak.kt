ef1326b58dbd8969a37035b0aa1f013ae73ac979
Per Henden <per.christian.henden@nav.no>
2025-09-23 14:38:33 +0200

    Dropp enum-utvidelse

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/delvurdering/underveis/UnderveisÅrsak.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/delvurdering/underveis/UnderveisÅrsak.kt
@@ -1,7 +1,5 @@
 package no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.underveis
 
-import kotlin.DeprecationLevel.*
-
 enum class UnderveisÅrsak {
     IKKE_GRUNNLEGGENDE_RETT,
     MELDEPLIKT_FRIST_IKKE_PASSERT,
@@ -11,8 +9,4 @@ enum class UnderveisÅrsak {
     VARIGHETSKVOTE_BRUKT_OPP,
     BRUDD_PÅ_AKTIVITETSPLIKT_11_7_STANS,
     BRUDD_PÅ_AKTIVITETSPLIKT_11_7_OPPHØR,
-
-    @Suppress("EnumEntryName")
-    @Deprecated("Ble delt i to. Brukes fremdeles i dev-db", level = ERROR)
-    BRUDD_PÅ_AKTIVITETSPLIKT
 }
