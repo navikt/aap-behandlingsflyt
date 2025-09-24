76e59db278561383667d38aa43c83156b01c8692
Fredrik Meyer <hrmeyer@gmail.com>
2025-09-24 10:10:03 +0200

    Revert "Bump org.postgresql:postgresql from 42.7.7 to 42.7.8 (#1172)"
    This reverts commit 2d7a16915854f9e172b0361e991a0811c671e874.

--- repository/build.gradle.kts
+++ repository/build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     implementation(libs.tidslinje)
     implementation("org.flywaydb:flyway-database-postgresql:11.13.1")
     implementation("io.getunleash:unleash-client-java:11.1.0")
-    runtimeOnly("org.postgresql:postgresql:42.7.8")
+    runtimeOnly("org.postgresql:postgresql:42.7.7")
     implementation("no.bekk.bekkopen:nocommons:0.16.0")
 
     testImplementation(project(":lib-test"))
