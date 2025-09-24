cff398235ebc1532fc55cd8532b0cb27d8f307f4
Per Henden <per.christian.henden@nav.no>
2025-09-24 11:24:50 +0200

    Forbedre logging

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/hendelse/datadeling/ApiInternGatewayImpl.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/hendelse/datadeling/ApiInternGatewayImpl.kt
@@ -6,7 +6,9 @@ import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.underveis.Underveis
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.RettighetsType
 import no.nav.aap.behandlingsflyt.kontrakt.datadeling.DetaljertMeldekortDTO
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.Behandling
+import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingId
 import no.nav.aap.behandlingsflyt.sakogbehandling.sak.Sak
+import no.nav.aap.behandlingsflyt.sakogbehandling.sak.SakId
 import no.nav.aap.komponenter.gateway.Gateway
 import no.nav.aap.komponenter.tidslinje.Tidslinje
 import no.nav.aap.komponenter.type.Periode
@@ -33,6 +35,10 @@ interface ApiInternGateway : Gateway {
         rettighetsTypeTidslinje: Tidslinje<RettighetsType>
     )
 
-    fun sendDetaljertMeldekortListe(detaljertMeldekortListe: List<DetaljertMeldekortDTO>)
+    fun sendDetaljertMeldekortListe(
+        detaljertMeldekortListe: List<DetaljertMeldekortDTO>,
+        sakId: SakId,
+        behandlingId: BehandlingId
+    )
 }
 
