aaf1cbf895434f7c1aafd911ae47c89d4a8d2db4
Fredrik Meyer <fredrik.meyer@nav.no>
2025-10-02 10:39:40 +0200

    Bruk nytt pattern i refusjonskravsteg

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/RefusjonkravSteg.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/RefusjonkravSteg.kt
@@ -1,16 +1,11 @@
 package no.nav.aap.behandlingsflyt.forretningsflyt.steg
 
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovRepository
+import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovService
 import no.nav.aap.behandlingsflyt.behandling.vilkår.TidligereVurderinger
 import no.nav.aap.behandlingsflyt.behandling.vilkår.TidligereVurderingerImpl
-import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Vilkårsresultat
-import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.VilkårsresultatRepository
-import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Vilkårtype
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.refusjonkrav.RefusjonkravRepository
-import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.sykdom.SykdomRepository
-import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.sykdom.Sykdomsvurdering
 import no.nav.aap.behandlingsflyt.flyt.steg.BehandlingSteg
-import no.nav.aap.behandlingsflyt.flyt.steg.FantAvklaringsbehov
 import no.nav.aap.behandlingsflyt.flyt.steg.FlytSteg
 import no.nav.aap.behandlingsflyt.flyt.steg.Fullført
 import no.nav.aap.behandlingsflyt.flyt.steg.StegResultat
@@ -23,76 +18,48 @@ import no.nav.aap.lookup.repository.RepositoryProvider
 
 class RefusjonkravSteg private constructor(
     private val refusjonkravRepository: RefusjonkravRepository,
-    private val vilkårsresultatRepository: VilkårsresultatRepository,
     private val avklaringsbehovRepository: AvklaringsbehovRepository,
-    private val sykdomRepository: SykdomRepository,
     private val tidligereVurderinger: TidligereVurderinger,
+    private val avklaringsbehovService: AvklaringsbehovService
 ) : BehandlingSteg {
     constructor(repositoryProvider: RepositoryProvider) : this(
         refusjonkravRepository = repositoryProvider.provide(),
-        vilkårsresultatRepository = repositoryProvider.provide(),
         avklaringsbehovRepository = repositoryProvider.provide(),
-        sykdomRepository = repositoryProvider.provide(),
         tidligereVurderinger = TidligereVurderingerImpl(repositoryProvider),
+        avklaringsbehovService = AvklaringsbehovService(repositoryProvider)
     )
 
     override fun utfør(kontekst: FlytKontekstMedPerioder): StegResultat {
-        val vilkårsresultat = vilkårsresultatRepository.hent(kontekst.behandlingId)
-        val sykdomsvurderinger = sykdomRepository.hentHvisEksisterer(kontekst.behandlingId)?.sykdomsvurderinger.orEmpty()
+        val grunnlag = lazy { refusjonkravRepository.hentHvisEksisterer(kontekst.behandlingId) }
 
-        if (!erIkkeAvslagPåVilkårTidligere(vilkårsresultat, sykdomsvurderinger)) {
-            val avklaringsbehovene = avklaringsbehovRepository.hentAvklaringsbehovene(kontekst.behandlingId)
-            val refusjonBehov = avklaringsbehovene.hentBehovForDefinisjon(Definisjon.REFUSJON_KRAV)
-            if (refusjonBehov != null && refusjonBehov.erÅpent()) {
-                avklaringsbehovene.avbryt(Definisjon.REFUSJON_KRAV)
-            }
-            return Fullført
-        }
-
-        when (kontekst.vurderingType) {
-            VurderingType.FØRSTEGANGSBEHANDLING -> {
-                if (tidligereVurderinger.girIngenBehandlingsgrunnlag(kontekst, type())) {
-                    avklaringsbehovRepository.hentAvklaringsbehovene(kontekst.behandlingId)
-                        .avbrytForSteg(type())
-                    return Fullført
+        avklaringsbehovService.oppdaterAvklaringsbehov(
+            avklaringsbehovene = avklaringsbehovRepository.hentAvklaringsbehovene(kontekst.behandlingId),
+            definisjon = Definisjon.REFUSJON_KRAV,
+            vedtakBehøverVurdering = {
+                when (kontekst.vurderingType) {
+                    VurderingType.FØRSTEGANGSBEHANDLING -> {
+                        !tidligereVurderinger.girIngenBehandlingsgrunnlag(kontekst, type())
+                    }
+                    VurderingType.REVURDERING,
+                    VurderingType.MELDEKORT,
+                    VurderingType.EFFEKTUER_AKTIVITETSPLIKT,
+                    VurderingType.IKKE_RELEVANT -> false
                 }
-
-                refusjonkravRepository.hentHvisEksisterer(kontekst.behandlingId) ?: return FantAvklaringsbehov(
-                    Definisjon.REFUSJON_KRAV
-                )
-            }
-
-            VurderingType.REVURDERING -> {
-                //refusjonkravRepository.hentHvisEksisterer(kontekst.behandlingId) ?: return FantAvklaringsbehov(Definisjon.REFUSJON_KRAV)
-                if (tidligereVurderinger.girIngenBehandlingsgrunnlag(kontekst, type())) {
-                    avklaringsbehovRepository.hentAvklaringsbehovene(kontekst.behandlingId).avbrytForSteg(type())
-                }
-            }
-
-            VurderingType.MELDEKORT, VurderingType.EFFEKTUER_AKTIVITETSPLIKT, VurderingType.IKKE_RELEVANT -> {
-                // Do nothing
-            }
-        }
+            },
+            erTilstrekkeligVurdert = {
+                grunnlag.value != null
+            },
+            tilbakestillGrunnlag = {
+                kontekst.forrigeBehandlingId
+                    ?.let { grunnlag.value }
+                    ?.let {
+                        refusjonkravRepository.lagre(kontekst.sakId, kontekst.behandlingId, it)
+                    }
+            },
+            kontekst = kontekst
+        )
 
         return Fullført
-    }
-
-    private fun erIkkeAvslagPåVilkårTidligere(
-        vilkårsresultat: Vilkårsresultat, sykdomsvurderinger: List<Sykdomsvurdering>
-    ): Boolean {
-        val bistandsvilkåret = vilkårsresultat.finnVilkår(Vilkårtype.BISTANDSVILKÅRET)
-        val nedsattVissVarighet = sykdomsvurderinger.any { it.erNedsettelseIArbeidsevneAvEnVissVarighet == true }
-
-        val bistandsvilkåretErOppfyltEllerIkkeVissVarighet = if (!nedsattVissVarighet) {
-            true
-        } else {
-            bistandsvilkåret.harPerioderSomErOppfylt()
-        }
-
-        return vilkårsresultat.finnVilkår(Vilkårtype.ALDERSVILKÅRET).harPerioderSomErOppfylt()
-                && vilkårsresultat.finnVilkår(Vilkårtype.LOVVALG).harPerioderSomErOppfylt()
-                && sykdomsvurderinger.any { it.erOppfyltSettBortIfraVissVarighet() }
-                && bistandsvilkåretErOppfyltEllerIkkeVissVarighet
     }
 
     companion object : FlytSteg {
