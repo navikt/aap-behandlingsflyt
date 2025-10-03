8b083082eb5512ec22f1a17237824c7e173f7a76
thao1 <thao.than.nguyen@nav.no>
2025-10-03 12:50:34 +0200

    AAP-1151: Legger til tester for å sikre at historikk kun viser vurderinger fra tidligere behandlinger og ikke inkluderer vurderinger fra avbrutte revurderinger.

--- repository/src/test/kotlin/no/nav/aap/behandlingsflyt/repository/faktagrunnlag/saksbehandler/overgangufore/OvergangUføreRepositoryImplTest.kt
+++ repository/src/test/kotlin/no/nav/aap/behandlingsflyt/repository/faktagrunnlag/saksbehandler/overgangufore/OvergangUføreRepositoryImplTest.kt
@@ -1,11 +1,21 @@
 package no.nav.aap.behandlingsflyt.repository.faktagrunnlag.saksbehandler.overgangufore
 
+import no.nav.aap.behandlingsflyt.behandling.avbrytrevurdering.AvbrytRevurderingVurdering
+import no.nav.aap.behandlingsflyt.behandling.avbrytrevurdering.AvbrytRevurderingÅrsak
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.overgangufore.OvergangUføreVurdering
 import no.nav.aap.behandlingsflyt.help.FakePdlGateway
 import no.nav.aap.behandlingsflyt.help.finnEllerOpprettBehandling
+import no.nav.aap.behandlingsflyt.kontrakt.behandling.TypeBehandling
+import no.nav.aap.behandlingsflyt.repository.behandling.BehandlingRepositoryImpl
+import no.nav.aap.behandlingsflyt.repository.faktagrunnlag.saksbehandler.avbrytrevurdering.AvbrytRevurderingRepositoryImpl
 import no.nav.aap.behandlingsflyt.repository.faktagrunnlag.saksbehandler.overganguføre.OvergangUføreRepositoryImpl
 import no.nav.aap.behandlingsflyt.repository.sak.PersonRepositoryImpl
 import no.nav.aap.behandlingsflyt.repository.sak.SakRepositoryImpl
+import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.Behandling
+import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.VurderingsbehovMedPeriode
+import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.VurderingsbehovOgÅrsak
+import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.ÅrsakTilOpprettelse
+import no.nav.aap.behandlingsflyt.sakogbehandling.flyt.Vurderingsbehov
 import no.nav.aap.behandlingsflyt.sakogbehandling.sak.PersonOgSakService
 import no.nav.aap.behandlingsflyt.sakogbehandling.sak.Sak
 import no.nav.aap.behandlingsflyt.test.ident
@@ -13,7 +23,9 @@ import no.nav.aap.komponenter.dbconnect.DBConnection
 import no.nav.aap.komponenter.dbconnect.transaction
 import no.nav.aap.komponenter.dbtest.InitTestDatabase
 import no.nav.aap.komponenter.type.Periode
+import no.nav.aap.komponenter.verdityper.Bruker
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import java.time.LocalDate
@@ -97,9 +109,75 @@ internal class OvergangUføreRepositoryImplTest {
         }
     }
 
+    @Test
+    fun `historikk viser kun vurderinger fra tidligere behandlinger og ikke inkluderer vurdering fra avbrutt revurdering`() {
+        val overgangUføreVurdering1 = lagOvergangUføreVurdering("B1")
+        val overgangUføreVurdering2 = lagOvergangUføreVurdering("B2")
+        val overgangUføreVurdering3 = lagOvergangUføreVurdering("B3")
+
+        val førstegangsbehandling = dataSource.transaction { connection ->
+            val overgangUføreRepo = OvergangUføreRepositoryImpl(connection)
+            val sak = sak(connection)
+            val førstegangsbehandling = finnEllerOpprettBehandling(connection, sak)
+
+            overgangUføreRepo.lagre(førstegangsbehandling.id, listOf(overgangUføreVurdering1))
+            førstegangsbehandling
+        }
+
+        dataSource.transaction { connection ->
+            val overgangUføreRepo = OvergangUføreRepositoryImpl(connection)
+            val avbrytRevurderingRepo = AvbrytRevurderingRepositoryImpl(connection)
+            val revurderingAvbrutt = revurderingOvergangUføre(connection, førstegangsbehandling)
+
+            // Marker revurderingen som avbrutt
+            avbrytRevurderingRepo.lagre(
+                revurderingAvbrutt.id, AvbrytRevurderingVurdering(
+                    AvbrytRevurderingÅrsak.REVURDERINGEN_BLE_OPPRETTET_VED_EN_FEIL, "avbryte pga. feil",
+                    Bruker("Z00000")
+                )
+            )
+            overgangUføreRepo.lagre(revurderingAvbrutt.id, listOf(overgangUføreVurdering2))
+        }
+
+        dataSource.transaction { connection ->
+            val overgangUføreRepo = OvergangUføreRepositoryImpl(connection)
+            val revurdering = revurderingOvergangUføre(connection, førstegangsbehandling)
+
+            overgangUføreRepo.lagre(revurdering.id, listOf(overgangUføreVurdering3))
+
+            val historikk = overgangUføreRepo.hentHistoriskeOvergangUforeVurderinger(revurdering.sakId, revurdering.id)
+            assertEquals(listOf(overgangUføreVurdering1), historikk)
+        }
+    }
 
     private companion object {
         private val periode = Periode(LocalDate.now(), LocalDate.now().plusYears(3))
+
+        fun assertEquals(expected: List<OvergangUføreVurdering>, actual: List<OvergangUføreVurdering>) {
+            Assertions.assertEquals(expected.size, actual.size)
+            for ((expected, actual) in expected.zip(actual)) {
+                assertEquals(expected, actual)
+            }
+        }
+
+        fun assertEquals(expected: OvergangUføreVurdering, actual: OvergangUføreVurdering) {
+            Assertions.assertEquals(expected.begrunnelse, actual.begrunnelse)
+            Assertions.assertEquals(expected.brukerHarSøktOmUføretrygd, actual.brukerHarSøktOmUføretrygd)
+
+            if (expected.brukerHarFåttVedtakOmUføretrygd != null && actual.brukerHarFåttVedtakOmUføretrygd != null) {
+                Assertions.assertEquals(expected.brukerHarFåttVedtakOmUføretrygd, actual.brukerHarFåttVedtakOmUføretrygd)
+            }
+
+            if (expected.brukerRettPåAAP != null && actual.brukerRettPåAAP != null) {
+                Assertions.assertEquals(expected.brukerRettPåAAP, actual.brukerRettPåAAP)
+            }
+
+            if (expected.virkningsdato != null && actual.virkningsdato != null) {
+                Assertions.assertEquals(expected.virkningsdato, actual.virkningsdato)
+            }
+
+            Assertions.assertEquals(expected.vurdertAv, actual.vurdertAv)
+        }
     }
 
     private fun sak(connection: DBConnection): Sak {
@@ -108,6 +186,29 @@ internal class OvergangUføreRepositoryImplTest {
             PersonRepositoryImpl(connection),
             SakRepositoryImpl(connection)
         ).finnEllerOpprett(ident(), periode)
+    }
+
+    private fun lagOvergangUføreVurdering(begrunnelse: String): OvergangUføreVurdering {
+        return OvergangUføreVurdering(
+            begrunnelse = begrunnelse,
+            brukerHarSøktOmUføretrygd = true,
+            brukerHarFåttVedtakOmUføretrygd = "NEI",
+            brukerRettPåAAP = true,
+            virkningsdato = LocalDate.now(),
+            vurdertAv = "Saksbehandler",
+        )
+    }
+
+    private fun revurderingOvergangUføre(connection: DBConnection, behandling: Behandling): Behandling {
+        return BehandlingRepositoryImpl(connection).opprettBehandling(
+            behandling.sakId,
+            typeBehandling = TypeBehandling.Revurdering,
+            forrigeBehandlingId = behandling.id,
+            vurderingsbehovOgÅrsak = VurderingsbehovOgÅrsak(
+                vurderingsbehov = listOf(VurderingsbehovMedPeriode(Vurderingsbehov.OVERGANG_UFORE)),
+                årsak = ÅrsakTilOpprettelse.MANUELL_OPPRETTELSE
+            )
+        )
     }
 
 }
