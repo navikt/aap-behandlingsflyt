4eea2300267cba2c511f4d11929848506b7f89f5
Fredrik Meyer <fredrik.meyer@nav.no>
2025-10-09 09:50:11 +0200

    Skisse til struktur infokrav

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/register/barn/BarnInformasjonskrav.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/register/barn/BarnInformasjonskrav.kt
@@ -14,11 +14,13 @@ import no.nav.aap.behandlingsflyt.faktagrunnlag.ikkeKjørtSisteKalenderdag
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.barn.adapter.BarnInnhentingRespons
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.barn.BarnIdentifikator
 import no.nav.aap.behandlingsflyt.kontrakt.steg.StegType
+import no.nav.aap.behandlingsflyt.sakogbehandling.Ident
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingId
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.VurderingsbehovMedPeriode
 import no.nav.aap.behandlingsflyt.sakogbehandling.flyt.FlytKontekstMedPerioder
 import no.nav.aap.behandlingsflyt.sakogbehandling.flyt.Vurderingsbehov.BARNETILLEGG
 import no.nav.aap.behandlingsflyt.sakogbehandling.sak.IdentGateway
+import no.nav.aap.behandlingsflyt.sakogbehandling.sak.Person
 import no.nav.aap.behandlingsflyt.sakogbehandling.sak.PersonId
 import no.nav.aap.behandlingsflyt.sakogbehandling.sak.db.PersonRepository
 import no.nav.aap.komponenter.gateway.GatewayProvider
@@ -51,68 +53,108 @@ class BarnInformasjonskrav private constructor(
                 !tidligereVurderinger.girAvslagEllerIngenBehandlingsgrunnlag(kontekst, steg)
     }
 
-    override fun oppdater(kontekst: FlytKontekstMedPerioder): Informasjonskrav.Endret {
-        log.info("Oppdaterer barnegrunnlag for behandling ${kontekst.behandlingId} av type ${kontekst.behandlingType} med årsak(er) ${kontekst.vurderingsbehovRelevanteForSteg}")
+    data class BarnInput(
+        val barnGrunnlag: BarnGrunnlag?,
+        val person: Person,
+    )
 
+    data class Registerdata(
+        val barnInnhentingRespons: BarnInnhentingRespons,
+        val identlisteForBarn: Map<BarnIdentifikator.BarnIdent, List<Ident>>
+    )
+
+    fun hentInput(kontekst: FlytKontekstMedPerioder): BarnInput {
         val behandlingId = kontekst.behandlingId
         val barnGrunnlag = barnRepository.hentHvisEksisterer(behandlingId)
-        val barn = hentRegisterBarn(behandlingId, barnGrunnlag)
-        val registerBarn = barn.registerBarn
+        val sak = sakOgBehandlingService.hentSakFor(behandlingId)
+        val person = sak.person
 
-        if (harEndringer(behandlingId)) {
-            val barnMedPersonId = oppdaterPersonIdenter(barn.alleBarn())
+        return BarnInput(
+            barnGrunnlag = barnGrunnlag,
+            person = person
+        )
+    }
+
+    fun hentData(input: BarnInput): Registerdata {
+        val (barnGrunnlag, person) = input
+        val oppgitteBarnIdenter = barnGrunnlag?.oppgitteBarn?.oppgitteBarn?.mapNotNull { it.ident }.orEmpty()
+        val barn = barnGateway.hentBarn(person, oppgitteBarnIdenter)
+
+        val identListeForBarn = barn.alleBarn()
+            .map { it.ident }
+            .filterIsInstance<BarnIdentifikator.BarnIdent>()
+            .associateWith {
+                identGateway.hentAlleIdenterForPerson(it.ident).also {
+                    if (it.isEmpty()) {
+                        throw IllegalStateException("Fikk ingen treff på ident i PDL.")
+                    }
+                }
+            }
+
+        return Registerdata(barnInnhentingRespons = barn, identlisteForBarn = identListeForBarn)
+    }
+
+    fun sammenlign(
+        kontekst: FlytKontekstMedPerioder,
+        input: BarnInput,
+        registerdata: Registerdata
+    ): Informasjonskrav.Endret {
+        val behandlingId = kontekst.behandlingId
+        val barnGrunnlag = input.barnGrunnlag
+        val registerbarn = registerdata.barnInnhentingRespons.registerBarn
+        val barn = registerdata.barnInnhentingRespons
+        if (harEndringer(barnGrunnlag, registerbarn)) {
+            val barnMedPersonId = oppdaterPersonIdenter(barn.alleBarn(), registerdata.identlisteForBarn)
 
             val registerBarnMedFolkeregisterRelasjon =
-                barnMedPersonId.filterKeys { barn -> barn.ident in registerBarn.map { it.ident } }
+                barnMedPersonId.filterKeys { barn -> barn.ident in registerbarn.map { it.ident } }
             barnRepository.lagreRegisterBarn(behandlingId, registerBarnMedFolkeregisterRelasjon)
             return ENDRET
         }
         return IKKE_ENDRET
     }
 
-    private fun oppdaterPersonIdenter(barn: List<Barn>): Map<Barn, PersonId?> {
+    override fun oppdater(kontekst: FlytKontekstMedPerioder): Informasjonskrav.Endret {
+        log.info("Oppdaterer barnegrunnlag for behandling ${kontekst.behandlingId} av type ${kontekst.behandlingType} med årsak(er) ${kontekst.vurderingsbehovRelevanteForSteg}")
+        val input = hentInput(kontekst)
+
+        val data = hentData(input)
+
+        return sammenlign(kontekst, input, data)
+    }
+
+    private fun oppdaterPersonIdenter(
+        barn: List<Barn>,
+        identlisteForBarn: Map<BarnIdentifikator.BarnIdent, List<Ident>>
+    ): Map<Barn, PersonId?> {
         return barn.associateWith { barn ->
-            if (barn.ident is BarnIdentifikator.BarnIdent) {
-                val identliste = identGateway.hentAlleIdenterForPerson(barn.ident.ident)
-                if (identliste.isEmpty()) {
-                    throw IllegalStateException("Fikk ingen treff på ident i PDL.")
+            when (barn.ident) {
+                is BarnIdentifikator.BarnIdent -> {
+                    val identliste = identlisteForBarn[barn.ident]!!
+                    personRepository.finnEllerOpprett(identliste).id
                 }
 
-                personRepository.finnEllerOpprett(identliste).id
-            } else {
-                null
+                is BarnIdentifikator.NavnOgFødselsdato -> null
             }
-
         }
     }
 
-    private fun harEndringer(behandlingId: BehandlingId): Boolean {
-        val barnGrunnlag = barnRepository.hentHvisEksisterer(behandlingId)
-        val registerBarn = hentRegisterBarn(behandlingId, barnGrunnlag).registerBarn
+    private fun hentRegisterBarn(
+        barnGrunnlag: BarnGrunnlag?,
+        person: Person,
+    ): BarnInnhentingRespons {
+        val oppgitteBarnIdenter = barnGrunnlag?.oppgitteBarn?.oppgitteBarn?.mapNotNull { it.ident }.orEmpty()
+        val barn = barnGateway.hentBarn(person, oppgitteBarnIdenter)
+        return barn
+    }
 
+    private fun harEndringer(barnGrunnlag: BarnGrunnlag?, registerBarn: List<Barn>): Boolean {
         return manglerBarnGrunnlagEllerFantNyeBarnFraRegister(
             registerBarn,
             barnGrunnlag?.registerbarn?.barn
         )
     }
 
-    override fun behovForRevurdering(behandlingId: BehandlingId): List<VurderingsbehovMedPeriode> {
-        return if (!harEndringer(behandlingId)) {
-            emptyList()
-        } else {
-            listOf(VurderingsbehovMedPeriode(BARNETILLEGG, null))
-        }
-    }
-
-    private fun hentRegisterBarn(
-        behandlingId: BehandlingId,
-        barnGrunnlag: BarnGrunnlag?
-    ): BarnInnhentingRespons {
-        val sak = sakOgBehandlingService.hentSakFor(behandlingId)
-        val oppgitteBarnIdenter = barnGrunnlag?.oppgitteBarn?.oppgitteBarn?.mapNotNull { it.ident }.orEmpty()
-        val barn = barnGateway.hentBarn(sak.person, oppgitteBarnIdenter)
-        return barn
-    }
 
     private fun manglerBarnGrunnlagEllerFantNyeBarnFraRegister(
         barnIdenter: List<Barn>,
@@ -121,10 +163,24 @@ class BarnInformasjonskrav private constructor(
         return barnIdenter.map { it.ident }.toSet() != eksisterendeRegisterBarn?.map { it.ident }?.toSet()
     }
 
+    override fun behovForRevurdering(behandlingId: BehandlingId): List<VurderingsbehovMedPeriode> {
+        val barnGrunnlag = barnRepository.hentHvisEksisterer(behandlingId)
+        val sak = sakOgBehandlingService.hentSakFor(behandlingId)
+        val registerBarn = hentRegisterBarn(barnGrunnlag, sak.person).registerBarn
+        return if (!harEndringer(barnGrunnlag, registerBarn)) {
+            emptyList()
+        } else {
+            listOf(VurderingsbehovMedPeriode(BARNETILLEGG, null))
+        }
+    }
+
     companion object : Informasjonskravkonstruktør {
         override val navn = InformasjonskravNavn.BARN
 
-        override fun konstruer(repositoryProvider: RepositoryProvider, gatewayProvider: GatewayProvider): BarnInformasjonskrav {
+        override fun konstruer(
+            repositoryProvider: RepositoryProvider,
+            gatewayProvider: GatewayProvider
+        ): BarnInformasjonskrav {
             return BarnInformasjonskrav(
                 repositoryProvider.provide(),
                 repositoryProvider.provide(),
