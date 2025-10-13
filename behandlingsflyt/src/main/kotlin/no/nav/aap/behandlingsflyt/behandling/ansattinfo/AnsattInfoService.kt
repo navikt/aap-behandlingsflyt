c88b1715d3607a8582e4db12964df47a46f66d40
Nicolas N. <nicolas.nordhagen@nav.no>
2025-10-13 15:05:11 +0200

    bedre logging mot NOM

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/ansattinfo/AnsattInfoService.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/ansattinfo/AnsattInfoService.kt
@@ -17,7 +17,7 @@ class AnsattInfoService(private val ansattInfoGateway: AnsattInfoGateway, privat
             val enhet = enhetGateway.hentEnhet(ansattInfo.enhetsnummer)
             return AnsattNavnOgEnhet(navn = ansattInfo.navn, enhet = enhet.navn)
         } catch (e: Exception) {
-            logger.info("Kunne ikke hente ansattnavn og enhet. Fortsetter uten disse verdiene.", e)
+            logger.warn("Kunne ikke hente ansattnavn og enhet for ident $navIdent.", e)
             return null
         }
     }
