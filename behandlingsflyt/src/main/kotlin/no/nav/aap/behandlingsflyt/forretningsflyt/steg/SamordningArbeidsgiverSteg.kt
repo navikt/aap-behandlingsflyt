71dd8b87a558d338e288d175f5a11c7a00f80a83
Nicolas N. <nicolas.nordhagen@nav.no>
2025-10-21 16:48:46 +0200

    AAP-1482 Bruke AvklaringsbehovService i stegene

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/SamordningArbeidsgiverSteg.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/SamordningArbeidsgiverSteg.kt
@@ -1,50 +1,35 @@
 package no.nav.aap.behandlingsflyt.forretningsflyt.steg
 
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovRepository
-import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.Avklaringsbehovene
+import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovService
 import no.nav.aap.behandlingsflyt.flyt.steg.BehandlingSteg
-import no.nav.aap.behandlingsflyt.flyt.steg.FantAvklaringsbehov
 import no.nav.aap.behandlingsflyt.flyt.steg.FlytSteg
 import no.nav.aap.behandlingsflyt.flyt.steg.Fullført
 import no.nav.aap.behandlingsflyt.flyt.steg.StegResultat
 import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon
 import no.nav.aap.behandlingsflyt.kontrakt.steg.StegType
 import no.nav.aap.behandlingsflyt.sakogbehandling.flyt.FlytKontekstMedPerioder
-import no.nav.aap.behandlingsflyt.sakogbehandling.flyt.VurderingType
 import no.nav.aap.behandlingsflyt.sakogbehandling.flyt.Vurderingsbehov
 import no.nav.aap.komponenter.gateway.GatewayProvider
 import no.nav.aap.lookup.repository.RepositoryProvider
 
 class SamordningArbeidsgiverSteg(
     private val avklaringsbehovRepository: AvklaringsbehovRepository,
+    private val avklaringsbehovService: AvklaringsbehovService
 ) : BehandlingSteg {
     override fun utfør(kontekst: FlytKontekstMedPerioder): StegResultat {
-        when (kontekst.vurderingType) {
-            VurderingType.FØRSTEGANGSBEHANDLING -> {}
-            VurderingType.REVURDERING -> {
-                val avklaringsbehovene = avklaringsbehovRepository.hentAvklaringsbehovene(kontekst.behandlingId)
-
-                if (erManueltTriggetRevurdering(kontekst, avklaringsbehovene)) {
-                    return FantAvklaringsbehov(Definisjon.SAMORDNING_ARBEIDSGIVER)
-                }
-            }
-
-            VurderingType.MELDEKORT,
-            VurderingType.EFFEKTUER_AKTIVITETSPLIKT,
-            VurderingType.EFFEKTUER_AKTIVITETSPLIKT_11_9,
-            VurderingType.IKKE_RELEVANT -> {
-            }
-        }
+        avklaringsbehovService.oppdaterAvklaringsbehov(
+            avklaringsbehovene = avklaringsbehovRepository.hentAvklaringsbehovene(kontekst.behandlingId),
+            definisjon = Definisjon.SAMORDNING_ARBEIDSGIVER,
+            vedtakBehøverVurdering = {
+                Vurderingsbehov.REVURDER_SAMORDNING_ARBEIDSGIVER in kontekst.vurderingsbehovRelevanteForSteg
+            },
+            erTilstrekkeligVurdert = { true },
+            tilbakestillGrunnlag = { },
+            kontekst = kontekst
+        )
 
         return Fullført
-    }
-
-    private fun erManueltTriggetRevurdering(
-        kontekst: FlytKontekstMedPerioder,
-        avklaringsbehovene: Avklaringsbehovene
-    ): Boolean {
-        return !avklaringsbehovene.erVurdertTidligereIBehandlingen(Definisjon.SAMORDNING_ARBEIDSGIVER) &&
-                kontekst.erRevurderingMedVurderingsbehov(Vurderingsbehov.REVURDER_SAMORDNING_ARBEIDSGIVER)
     }
 
     companion object : FlytSteg {
@@ -52,7 +37,10 @@ class SamordningArbeidsgiverSteg(
             repositoryProvider: RepositoryProvider,
             gatewayProvider: GatewayProvider
         ): BehandlingSteg {
-            return SamordningArbeidsgiverSteg(repositoryProvider.provide())
+            return SamordningArbeidsgiverSteg(
+                avklaringsbehovRepository = repositoryProvider.provide(),
+                avklaringsbehovService = AvklaringsbehovService(repositoryProvider)
+            )
         }
 
         override fun type(): StegType {
