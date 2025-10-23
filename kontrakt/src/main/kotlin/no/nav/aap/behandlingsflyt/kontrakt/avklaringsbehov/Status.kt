36a0ea3826d7116bef2ab6da5c20e1133a30e26a
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2025-10-23 08:29:41 +0200

    Dokumenter betydning av status på avklaringsbehov

--- kontrakt/src/main/kotlin/no/nav/aap/behandlingsflyt/kontrakt/avklaringsbehov/Status.kt
+++ kontrakt/src/main/kotlin/no/nav/aap/behandlingsflyt/kontrakt/avklaringsbehov/Status.kt
@@ -1,13 +1,34 @@
 package no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov
 
 public enum class Status {
+     /**  Hvis Definisjon.type er
+      * - MANUELT_PÅKREVD:  behandlingsflyt venter på en løsning
+      * - MANUELT_FRIVILLIG:  behandlingsflyt kan godta en løsning
+      * - OVERSTYR: ???
+      * */
     OPPRETTET,
+
+    /** Behandlingsflyt har mottatt en løsning, og avklaringsbehovet
+     * er en kandidat for kvalitetssikring og totrinnskontroll. */
     AVSLUTTET,
+
+    /** Beslutter har godkjent løsningen. */
     TOTRINNS_VURDERT,
+
+    /** Beslutter har avvist løsningen. */
     SENDT_TILBAKE_FRA_BESLUTTER,
+
+    /** Kvalitetssikrer har godkjent løsningen. */
     KVALITETSSIKRET,
+
+    /** Kvalitetssikrer har avvist løsningen. */
     SENDT_TILBAKE_FRA_KVALITETSSIKRER,
-    AVBRUTT;
+
+    /** Behovet er avbrutt. Eventuelle løsninger er ikke
+     * kandidater for kvalitetssikring eller totrinnskontroll. Kan ses på som en
+     * form for soft-delete. */
+    AVBRUTT,
+    ;
 
     public fun erÅpent(): Boolean {
         return this in setOf(
