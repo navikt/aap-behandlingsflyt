b746137f33789dd649d36f185f5749e061ff04c7
Nicolas N. <nicolas.nordhagen@nav.no>
2025-10-23 09:56:50 +0200

    AAP-1580 Bruke avklaringsbehovService i TjenestepensjonRefusjonskravSteg

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/TjenestepensjonRefusjonskravSteg.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/TjenestepensjonRefusjonskravSteg.kt
@@ -1,13 +1,12 @@
 package no.nav.aap.behandlingsflyt.forretningsflyt.steg
 
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovRepository
-import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.Avklaringsbehovene
+import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovService
 import no.nav.aap.behandlingsflyt.behandling.vilkår.TidligereVurderinger
 import no.nav.aap.behandlingsflyt.behandling.vilkår.TidligereVurderingerImpl
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.tjenestepensjon.TjenestePensjonRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.samordning.refusjonskrav.TjenestepensjonRefusjonsKravVurderingRepository
 import no.nav.aap.behandlingsflyt.flyt.steg.BehandlingSteg
-import no.nav.aap.behandlingsflyt.flyt.steg.FantAvklaringsbehov
 import no.nav.aap.behandlingsflyt.flyt.steg.FlytSteg
 import no.nav.aap.behandlingsflyt.flyt.steg.Fullført
 import no.nav.aap.behandlingsflyt.flyt.steg.StegResultat
@@ -22,55 +21,55 @@ import no.nav.aap.lookup.repository.RepositoryProvider
 class TjenestepensjonRefusjonskravSteg private constructor(
     private val tjenestepensjonRefusjonsKravVurderingRepository: TjenestepensjonRefusjonsKravVurderingRepository,
     private val avklaringsbehovRepository: AvklaringsbehovRepository,
+    private val avklaringsbehovService: AvklaringsbehovService,
     private val tjenestePensjonRepository: TjenestePensjonRepository,
     private val tidligereVurderinger: TidligereVurderinger,
 ) : BehandlingSteg {
     constructor(repositoryProvider: RepositoryProvider) : this(
         tjenestepensjonRefusjonsKravVurderingRepository = repositoryProvider.provide(),
         avklaringsbehovRepository = repositoryProvider.provide(),
+        avklaringsbehovService = AvklaringsbehovService(repositoryProvider),
         tjenestePensjonRepository = repositoryProvider.provide(),
         tidligereVurderinger = TidligereVurderingerImpl(repositoryProvider)
     )
 
-
     override fun utfør(kontekst: FlytKontekstMedPerioder): StegResultat {
         val avklaringsbehovene = avklaringsbehovRepository.hentAvklaringsbehovene(kontekst.behandlingId)
 
-        when (kontekst.vurderingType) {
-            VurderingType.FØRSTEGANGSBEHANDLING -> {
-                if (tidligereVurderinger.girAvslagEllerIngenBehandlingsgrunnlag(kontekst, type())) {
-                    avklaringsbehovene.avbrytForSteg(type())
-                    return Fullført
+        avklaringsbehovService.oppdaterAvklaringsbehov(
+            avklaringsbehovene = avklaringsbehovene,
+            definisjon = Definisjon.SAMORDNING_REFUSJONS_KRAV,
+            vedtakBehøverVurdering = {
+                when (kontekst.vurderingType) {
+                    VurderingType.FØRSTEGANGSBEHANDLING -> {
+                        when {
+                            tidligereVurderinger.girAvslagEllerIngenBehandlingsgrunnlag(kontekst, type()) -> true
+
+                            else -> {
+                                val tpResultat = tjenestePensjonRepository.hent(kontekst.behandlingId)
+                                val tidligereTpVurderinger =
+                                    tjenestepensjonRefusjonsKravVurderingRepository.hentHvisEksisterer(kontekst.behandlingId)
+
+                                tidligereTpVurderinger == null && tpResultat.isNotEmpty()
+                            }
+                        }
+                    }
+
+                    VurderingType.REVURDERING ->
+                        Vurderingsbehov.REVURDER_SAMORDNING_TJENESTEPENSJON in kontekst.vurderingsbehovRelevanteForSteg
+
+                    VurderingType.MELDEKORT,
+                    VurderingType.EFFEKTUER_AKTIVITETSPLIKT,
+                    VurderingType.EFFEKTUER_AKTIVITETSPLIKT_11_9,
+                    VurderingType.IKKE_RELEVANT -> false
                 }
-                val tpResultat = tjenestePensjonRepository.hent(kontekst.behandlingId)
-                val tidligereTpVurderinger =
-                    tjenestepensjonRefusjonsKravVurderingRepository.hentHvisEksisterer(kontekst.behandlingId)
+            },
+            erTilstrekkeligVurdert = { true },
+            tilbakestillGrunnlag = { },
+            kontekst = kontekst,
+        )
 
-                if (tidligereTpVurderinger == null && tpResultat.isNotEmpty())
-                    return FantAvklaringsbehov(Definisjon.SAMORDNING_REFUSJONS_KRAV)
-            }
-
-            VurderingType.REVURDERING -> {
-                if (erManueltTriggetRevurdering(kontekst, avklaringsbehovene))
-                    return FantAvklaringsbehov(Definisjon.SAMORDNING_REFUSJONS_KRAV)
-            }
-
-            VurderingType.MELDEKORT,
-            VurderingType.EFFEKTUER_AKTIVITETSPLIKT,
-            VurderingType.EFFEKTUER_AKTIVITETSPLIKT_11_9,
-            VurderingType.IKKE_RELEVANT -> {
-                //Do nothing
-            }
-        }
         return Fullført
-    }
-
-    private fun erManueltTriggetRevurdering(
-        kontekst: FlytKontekstMedPerioder,
-        avklaringsbehovene: Avklaringsbehovene
-    ): Boolean {
-        return !avklaringsbehovene.erVurdertTidligereIBehandlingen(Definisjon.SAMORDNING_REFUSJONS_KRAV) &&
-                kontekst.erRevurderingMedVurderingsbehov(Vurderingsbehov.REVURDER_SAMORDNING_TJENESTEPENSJON)
     }
 
     companion object : FlytSteg {
