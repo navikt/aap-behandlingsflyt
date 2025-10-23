2ab0033f65f34b565a3d34c69898c6b75592a8b8
Øivind Stensrud <oivind.stensrud@nav.no>
2025-10-23 12:21:40 +0200

    AAP-1568 Bruker oppdaterAvklaringsbehov

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/klage/KlagebehandlingKontorSteg.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/klage/KlagebehandlingKontorSteg.kt
@@ -1,13 +1,13 @@
 package no.nav.aap.behandlingsflyt.forretningsflyt.steg.klage
 
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovRepository
+import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovService
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.Avklaringsbehovene
 import no.nav.aap.behandlingsflyt.behandling.trekkklage.TrekkKlageService
 import no.nav.aap.behandlingsflyt.faktagrunnlag.klage.resultat.Avslått
 import no.nav.aap.behandlingsflyt.faktagrunnlag.klage.resultat.KlageresultatUtleder
 import no.nav.aap.behandlingsflyt.faktagrunnlag.klage.behandlendeenhet.BehandlendeEnhetRepository
 import no.nav.aap.behandlingsflyt.flyt.steg.BehandlingSteg
-import no.nav.aap.behandlingsflyt.flyt.steg.FantAvklaringsbehov
 import no.nav.aap.behandlingsflyt.flyt.steg.FlytSteg
 import no.nav.aap.behandlingsflyt.flyt.steg.Fullført
 import no.nav.aap.behandlingsflyt.flyt.steg.StegResultat
@@ -22,43 +22,49 @@ class KlagebehandlingKontorSteg private constructor(
     private val avklaringsbehovRepository: AvklaringsbehovRepository,
     private val behandlendeEnhetRepository: BehandlendeEnhetRepository,
     private val klageresultatUtleder: KlageresultatUtleder,
-    private val trekkKlageService: TrekkKlageService
+    private val trekkKlageService: TrekkKlageService,
+    private val avklaringsbehovService: AvklaringsbehovService,
 ) : BehandlingSteg {
     override fun utfør(kontekst: FlytKontekstMedPerioder): StegResultat {
         val resultat = klageresultatUtleder.utledKlagebehandlingResultat(kontekst.behandlingId)
-        if (resultat is Avslått) {
-            return Fullført
-        }
-
         val avklaringsbehov = avklaringsbehovRepository.hentAvklaringsbehovene(kontekst.behandlingId)
-
-        if(trekkKlageService.klageErTrukket(kontekst.behandlingId)) {
-            avklaringsbehov.avbrytForSteg(type())
-            return Fullført
-        }
 
         val behandlendeEnhetVurdering = behandlendeEnhetRepository.hentHvisEksisterer(kontekst.behandlingId)?.vurdering
         requireNotNull(behandlendeEnhetVurdering) {
             "Behandlende kontor skal være satt"
         }
-        return if (behandlendeEnhetVurdering.skalBehandlesAvKontor && avklaringsbehov.harIkkeBlittLøst(Definisjon.VURDER_KLAGE_KONTOR)) {
-            FantAvklaringsbehov(Definisjon.VURDER_KLAGE_KONTOR)
-        } else {
-            avklaringsbehov.avbrytForSteg(type())
-            /** TODO: Eventuell utnulling av vurdering kan skje i senere steg.
-             *Vil kanskje ta vare på vurderingen "så lenge som mulig" i tilfelle man ombestemmer seg
-             * **/
-            Fullført
-        }
+
+        val klageErAvslått = resultat is Avslått
+        val klageErTrukket = trekkKlageService.klageErTrukket(kontekst.behandlingId)
+        val avklaringsbehovetErIkkeLøst = avklaringsbehov.harIkkeBlittLøst(Definisjon.VURDER_KLAGE_KONTOR)
+
+        avklaringsbehovService.oppdaterAvklaringsbehov(
+            avklaringsbehovene = avklaringsbehov,
+            definisjon = Definisjon.VURDER_KLAGE_KONTOR,
+            vedtakBehøverVurdering = {
+                !klageErAvslått
+                        || !klageErTrukket
+                        || (behandlendeEnhetVurdering.skalBehandlesAvKontor && avklaringsbehovetErIkkeLøst)
+            },
+            erTilstrekkeligVurdert = { true },
+            tilbakestillGrunnlag = { /* TODO: Eventuell utnulling av vurdering kan skje i senere steg. Vil kanskje ta vare på vurderingen "så lenge som mulig" i tilfelle man ombestemmer seg */ },
+            kontekst = kontekst,
+        )
+
+        return Fullført
     }
 
     companion object : FlytSteg {
-        override fun konstruer(repositoryProvider: RepositoryProvider, gatewayProvider: GatewayProvider): BehandlingSteg {
+        override fun konstruer(
+            repositoryProvider: RepositoryProvider,
+            gatewayProvider: GatewayProvider
+        ): BehandlingSteg {
             return KlagebehandlingKontorSteg(
                 repositoryProvider.provide(),
                 repositoryProvider.provide(),
                 KlageresultatUtleder(repositoryProvider),
-                TrekkKlageService(repositoryProvider)
+                TrekkKlageService(repositoryProvider),
+                AvklaringsbehovService(repositoryProvider),
             )
         }
 
