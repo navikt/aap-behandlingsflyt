917b5f6fd7060d65aa8c9590a011437ffc319d72
Hein Haraldsen <hein.haraldsen@bekk.no>
2025-10-23 10:39:05 +0200

    Fjernet kall til deprecated metoder

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/FatteVedtakSteg.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/FatteVedtakSteg.kt
@@ -1,6 +1,7 @@
 package no.nav.aap.behandlingsflyt.forretningsflyt.steg
 
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovRepository
+import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovService
 import no.nav.aap.behandlingsflyt.behandling.trekkklage.TrekkKlageService
 import no.nav.aap.behandlingsflyt.behandling.vilkår.TidligereVurderinger
 import no.nav.aap.behandlingsflyt.behandling.vilkår.TidligereVurderingerImpl
@@ -22,50 +23,35 @@ import org.slf4j.LoggerFactory
 
 class FatteVedtakSteg(
     private val avklaringsbehovRepository: AvklaringsbehovRepository,
-    private val tidligereVurderinger: TidligereVurderinger,
-    private val klageresultatUtleder: KlageresultatUtleder,
     private val trekkKlageService: TrekkKlageService,
+    private val avklaringsbehovService: AvklaringsbehovService
+
 ) : BehandlingSteg {
-
-    constructor(repositoryProvider: RepositoryProvider) : this(
-        avklaringsbehovRepository = repositoryProvider.provide(),
-        tidligereVurderinger = TidligereVurderingerImpl(repositoryProvider),
-        klageresultatUtleder = KlageresultatUtleder(repositoryProvider),
-        trekkKlageService = TrekkKlageService(repositoryProvider),
-    )
-
+    
     override fun utfør(kontekst: FlytKontekstMedPerioder): StegResultat {
-        val avklaringsbehov = avklaringsbehovRepository.hentAvklaringsbehovene(kontekst.behandlingId)
+        val avklaringsbehovene = avklaringsbehovRepository.hentAvklaringsbehovene(kontekst.behandlingId)
 
-        if (tidligereVurderinger.girIngenBehandlingsgrunnlag(kontekst, type()) || trekkKlageService.klageErTrukket(
-                kontekst.behandlingId
-            )
-        ) {
-            avklaringsbehov.avbrytForSteg(type())
-            return Fullført
-        }
-
-        if (kontekst.behandlingType == TypeBehandling.Klage) {
-            val klageresultat = klageresultatUtleder.utledKlagebehandlingResultat(kontekst.behandlingId)
-            if (klageresultat is Opprettholdes) {
-                avklaringsbehov.avbrytForSteg(type())
-                return Fullført
-            }
-        }
-
-        if (avklaringsbehov.skalTilbakeføresEtterTotrinnsVurdering()) {
-            return TilbakeføresFraBeslutter
-        }
-        if (avklaringsbehov.harHattAvklaringsbehovSomHarKrevdToTrinn()) {
-            return FantAvklaringsbehov(Definisjon.FATTE_VEDTAK)
-        }
+        avklaringsbehovService.oppdaterAvklaringsbehov(
+            avklaringsbehovene = avklaringsbehovene,
+            definisjon = Definisjon.FATTE_VEDTAK,
+            vedtakBehøverVurdering = { !trekkKlageService.klageErTrukket(kontekst.behandlingId) },
+            erTilstrekkeligVurdert = { true },
+            tilbakestillGrunnlag = {},
+            kontekst
+        )
 
         return Fullført
     }
 
     companion object : FlytSteg {
-        override fun konstruer(repositoryProvider: RepositoryProvider, gatewayProvider: GatewayProvider): BehandlingSteg {
-            return FatteVedtakSteg(repositoryProvider)
+        override fun konstruer(
+            repositoryProvider: RepositoryProvider,
+            gatewayProvider: GatewayProvider
+        ): BehandlingSteg {
+            return FatteVedtakSteg(
+                repositoryProvider.provide(), TrekkKlageService(repositoryProvider),
+                AvklaringsbehovService(repositoryProvider)
+            )
         }
 
         override fun type(): StegType {
