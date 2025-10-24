331479b081cccfe22f6afdf37d958527d1e0abcc
Per Henden <per.christian.henden@nav.no>
2025-10-24 12:30:22 +0200

    Gjør datasource synlig igjen i AbstraktFlytOrkestratorTest.kt

--- behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/flyt/AbstraktFlytOrkestratorTest.kt
+++ behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/flyt/AbstraktFlytOrkestratorTest.kt
@@ -150,8 +150,6 @@ import no.nav.aap.behandlingsflyt.test.modell.defaultInntekt
 import no.nav.aap.behandlingsflyt.unleash.UnleashGateway
 import no.nav.aap.komponenter.dbconnect.transaction
 import no.nav.aap.komponenter.dbtest.TestDataSource
-import no.nav.aap.komponenter.dbtest.TestDatabase
-import no.nav.aap.komponenter.dbtest.TestDatabaseExtension
 import no.nav.aap.komponenter.tidslinje.tidslinjeOf
 import no.nav.aap.komponenter.type.Periode
 import no.nav.aap.komponenter.verdityper.Bruker
@@ -162,19 +160,18 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AutoClose
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.extension.ExtendWith
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.*
-import javax.sql.DataSource
 import kotlin.reflect.KClass
 
 
 @Fakes
 open class AbstraktFlytOrkestratorTest(unleashGateway: KClass<out UnleashGateway>) {
+
     @AutoClose
-    private val dataSource = TestDataSource()
+    val dataSource = TestDataSource()
 
     protected val motor by lazy {
         ManuellMotorImpl(
