17c87941a9f3ae101dc71f56dbf4e2f141a28173
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2025-10-24 13:26:19 +0200

    Fjern feature toggle for aktivitetsplikt 11-7

--- lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/test/FakeUnleash.kt
+++ lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/test/FakeUnleash.kt
@@ -21,7 +21,6 @@ object FakeUnleash : FakeUnleashBase(
         BehandlingsflytFeature.SendForvaltningsmelding to true,
         BehandlingsflytFeature.NyBrevtype11_17 to true,
         BehandlingsflytFeature.NyBrevtype11_18 to true,
-        BehandlingsflytFeature.Aktivitetsplikt11_7 to true,
         BehandlingsflytFeature.Aktivitetsplikt11_9 to true,
         BehandlingsflytFeature.OverforingsdatoNullForAvregning to true,
         BehandlingsflytFeature.IverksettUtbetalingSomSelvstendigJobb to true,
