39d9f5b12785954f2f7a7062efbe23efdc95e6ff
Martin Aasen Fromreide <martin.aasen.fromreide@nav.no>
2025-10-24 16:02:52 +0200

    Legg til erTilstrekkeligVurdert

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/KvalitetssikringsSteg.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/KvalitetssikringsSteg.kt
@@ -2,14 +2,15 @@ package no.nav.aap.behandlingsflyt.forretningsflyt.steg
 
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovRepository
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovService
+import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.Avklaringsbehovene
 import no.nav.aap.behandlingsflyt.behandling.trekkklage.TrekkKlageService
 import no.nav.aap.behandlingsflyt.flyt.steg.BehandlingSteg
-import no.nav.aap.behandlingsflyt.flyt.steg.FantAvklaringsbehov
 import no.nav.aap.behandlingsflyt.flyt.steg.FlytSteg
 import no.nav.aap.behandlingsflyt.flyt.steg.Fullført
 import no.nav.aap.behandlingsflyt.flyt.steg.StegResultat
 import no.nav.aap.behandlingsflyt.flyt.steg.TilbakeføresFraKvalitetsikrer
 import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon
+import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Status
 import no.nav.aap.behandlingsflyt.kontrakt.behandling.TypeBehandling
 import no.nav.aap.behandlingsflyt.kontrakt.steg.StegType
 import no.nav.aap.behandlingsflyt.sakogbehandling.flyt.FlytKontekstMedPerioder
@@ -34,18 +35,13 @@ class KvalitetssikringsSteg private constructor(
             avklaringsbehovene = avklaringsbehovene,
             definisjon = Definisjon.KVALITETSSIKRING,
             vedtakBehøverVurdering = { vedtakBehøverVurdering(kontekst) },
-            erTilstrekkeligVurdert = { true },
-            tilbakestillGrunnlag = { tilbakestillGrunnlag(kontekst) },
+            erTilstrekkeligVurdert = { erTilstrekkeligVurdert(avklaringsbehovene) },
+            tilbakestillGrunnlag = {},
             kontekst
         )
 
         if (avklaringsbehovene.skalTilbakeføresEtterKvalitetssikring()) {
             return TilbakeføresFraKvalitetsikrer
-        }
-
-        // TODO Hva gjør man med denne?
-        if (avklaringsbehovene.harHattAvklaringsbehovSomKreverKvalitetssikring()) {
-            return FantAvklaringsbehov(Definisjon.KVALITETSSIKRING)
         }
 
         return Fullført
@@ -63,8 +59,8 @@ class KvalitetssikringsSteg private constructor(
         }
     }
 
-    private fun tilbakestillGrunnlag(kontekst: FlytKontekstMedPerioder) {
-        // TODO Hva trengs å tilbakestilles her?
+    private fun erTilstrekkeligVurdert(avklaringsbehovene: Avklaringsbehovene): Boolean {
+        return avklaringsbehovene.alle().all { it.status() == Status.KVALITETSSIKRET }
     }
 
     companion object : FlytSteg {
