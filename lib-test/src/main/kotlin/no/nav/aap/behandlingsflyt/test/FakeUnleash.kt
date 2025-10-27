eb03a294be4f74153c9b44f1bfbc35f91c8774ef
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2025-10-27 09:25:45 +0100

    Lagre ned vedtak-info i FatteVedtakSteg

--- lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/test/FakeUnleash.kt
+++ lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/test/FakeUnleash.kt
@@ -31,6 +31,7 @@ object FakeUnleash : FakeUnleashBase(
         BehandlingsflytFeature.Arbeidsopptrapping to true,
         BehandlingsflytFeature.IkkeAntaNullTimerArbeidet to true,
         BehandlingsflytFeature.BedreUttrekkAvSakerMedFritakMeldeplikt to false,
+        BehandlingsflytFeature.LagreVedtakIFatteVedtak to true,
     )
 )
 
@@ -50,5 +51,6 @@ object LokalUnleash : FakeUnleashBase(
         BehandlingsflytFeature.Arbeidsopptrapping to true,
         BehandlingsflytFeature.IkkeAntaNullTimerArbeidet to true,
         BehandlingsflytFeature.BedreUttrekkAvSakerMedFritakMeldeplikt to true,
+        BehandlingsflytFeature.LagreVedtakIFatteVedtak to true,
     )
 )
