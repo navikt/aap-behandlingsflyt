2834d694b00c2287ebd3b4e8955c5268a6d2f0d5
Martin Aasen Fromreide <martin.aasen.fromreide@nav.no>
2025-10-29 13:24:09 +0100

    Legg til bryter

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/KvalitetssikringsSteg.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/KvalitetssikringsSteg.kt
@@ -7,6 +7,7 @@ import no.nav.aap.behandlingsflyt.behandling.trekkklage.TrekkKlageService
 import no.nav.aap.behandlingsflyt.behandling.vilkår.TidligereVurderinger
 import no.nav.aap.behandlingsflyt.behandling.vilkår.TidligereVurderingerImpl
 import no.nav.aap.behandlingsflyt.flyt.steg.BehandlingSteg
+import no.nav.aap.behandlingsflyt.flyt.steg.FantAvklaringsbehov
 import no.nav.aap.behandlingsflyt.flyt.steg.FlytSteg
 import no.nav.aap.behandlingsflyt.flyt.steg.Fullført
 import no.nav.aap.behandlingsflyt.flyt.steg.StegResultat
@@ -16,6 +17,8 @@ import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Status
 import no.nav.aap.behandlingsflyt.kontrakt.behandling.TypeBehandling
 import no.nav.aap.behandlingsflyt.kontrakt.steg.StegType
 import no.nav.aap.behandlingsflyt.sakogbehandling.flyt.FlytKontekstMedPerioder
+import no.nav.aap.behandlingsflyt.unleash.BehandlingsflytFeature
+import no.nav.aap.behandlingsflyt.unleash.UnleashGateway
 import no.nav.aap.komponenter.gateway.GatewayProvider
 import no.nav.aap.lookup.repository.RepositoryProvider
 
@@ -24,15 +27,24 @@ class KvalitetssikringsSteg private constructor(
     private val avklaringsbehovService: AvklaringsbehovService,
     private val tidligereVurderinger: TidligereVurderinger,
     private val trekkKlageService: TrekkKlageService,
+    private val unleashGateway: UnleashGateway,
 ) : BehandlingSteg {
-    constructor(repositoryProvider: RepositoryProvider) : this(
+    constructor(repositoryProvider: RepositoryProvider, gatewayProvider: GatewayProvider) : this(
         avklaringsbehovRepository = repositoryProvider.provide(),
         avklaringsbehovService = AvklaringsbehovService(repositoryProvider),
         tidligereVurderinger = TidligereVurderingerImpl(repositoryProvider),
         trekkKlageService = TrekkKlageService(repositoryProvider),
+        unleashGateway = gatewayProvider.provide(),
     )
 
     override fun utfør(kontekst: FlytKontekstMedPerioder): StegResultat {
+        if (unleashGateway.isEnabled(BehandlingsflytFeature.KvalitetssikringsSteg)) {
+            return utførNy(kontekst)
+        }
+        return utførGammel(kontekst)
+    }
+
+    fun utførNy(kontekst: FlytKontekstMedPerioder): StegResultat {
         val avklaringsbehovene = avklaringsbehovRepository.hentAvklaringsbehovene(kontekst.behandlingId)
 
         avklaringsbehovService.oppdaterAvklaringsbehov(
@@ -46,6 +58,29 @@ class KvalitetssikringsSteg private constructor(
 
         if (avklaringsbehovene.skalTilbakeføresEtterKvalitetssikring()) {
             return TilbakeføresFraKvalitetsikrer
+        }
+
+        return Fullført
+    }
+
+    fun utførGammel(kontekst: FlytKontekstMedPerioder): StegResultat {
+        if (kontekst.behandlingType !in listOf(TypeBehandling.Førstegangsbehandling, TypeBehandling.Klage)  ) {
+            return Fullført
+        }
+
+        val avklaringsbehov = avklaringsbehovRepository.hentAvklaringsbehovene(kontekst.behandlingId)
+        if (tidligereVurderinger.girIngenBehandlingsgrunnlag(kontekst, type()) || trekkKlageService.klageErTrukket(kontekst.behandlingId)) {
+            avklaringsbehov.avbrytForSteg(type())
+            return Fullført
+        }
+
+
+
+        if (avklaringsbehov.skalTilbakeføresEtterKvalitetssikring()) {
+            return TilbakeføresFraKvalitetsikrer
+        }
+        if (avklaringsbehov.harHattAvklaringsbehovSomKreverKvalitetssikring()) {
+            return FantAvklaringsbehov(Definisjon.KVALITETSSIKRING)
         }
 
         return Fullført
@@ -87,7 +122,7 @@ class KvalitetssikringsSteg private constructor(
         override fun konstruer(
             repositoryProvider: RepositoryProvider, gatewayProvider: GatewayProvider
         ): BehandlingSteg {
-            return KvalitetssikringsSteg(repositoryProvider)
+            return KvalitetssikringsSteg(repositoryProvider, gatewayProvider)
         }
 
         override fun type(): StegType {
