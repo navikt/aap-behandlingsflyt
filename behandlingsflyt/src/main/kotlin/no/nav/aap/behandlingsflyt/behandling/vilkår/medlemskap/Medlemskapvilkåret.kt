0da1e965701042ef60cc9ccceb43f666cb304cf7
Henrik Gundersen <henrik.gundersen@nav.no>
2025-11-03 10:40:38 +0100

    AAP-1390 Kun periodisert vv for lovvalg og medlemskap og fjerner gammel steglogikk

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/vilkår/medlemskap/Medlemskapvilkåret.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/vilkår/medlemskap/Medlemskapvilkåret.kt
@@ -14,19 +14,10 @@ import no.nav.aap.komponenter.type.Periode
 class Medlemskapvilkåret(
     vilkårsresultat: Vilkårsresultat,
     private val rettighetsPeriode: Periode,
-    val brukPeriodisertManuellVurdering: Boolean = false
 ) : Vilkårsvurderer<MedlemskapLovvalgGrunnlag> {
     private val vilkår = vilkårsresultat.leggTilHvisIkkeEksisterer(Vilkårtype.LOVVALG)
 
     override fun vurder(grunnlag: MedlemskapLovvalgGrunnlag) {
-        if (brukPeriodisertManuellVurdering) {
-            vurderNy(grunnlag)
-        } else {
-            vurderGammel(grunnlag)
-        }
-    }
-
-    private fun vurderNy(grunnlag: MedlemskapLovvalgGrunnlag) {
         val brukManuellVurderingForLovvalgMedlemskap = grunnlag.medlemskapArbeidInntektGrunnlag?.vurderinger?.isNotEmpty() ?: false
 
         if (brukManuellVurderingForLovvalgMedlemskap) {
@@ -72,36 +63,6 @@ class Medlemskapvilkåret(
             val vurderingsResultat = VurderingsResultat(utfall, null, null)
             leggTilVurdering(rettighetsPeriode, grunnlag, vurderingsResultat, false)
         }
-    }
-
-    private fun vurderGammel(grunnlag: MedlemskapLovvalgGrunnlag) {
-        var vurdertManuelt = false
-        val manuellVurderingForLovvalgMedlemskap = grunnlag.medlemskapArbeidInntektGrunnlag?.vurderinger?.firstOrNull()
-
-        val vurderingsResultat = if (manuellVurderingForLovvalgMedlemskap != null) {
-            vurdertManuelt = true
-            val lovvalgsLand = manuellVurderingForLovvalgMedlemskap.lovvalgVedSøknadsTidspunkt.lovvalgsEØSLandEllerLandMedAvtale
-            val varMedlemIFolketrygd = manuellVurderingForLovvalgMedlemskap.medlemskapVedSøknadsTidspunkt?.varMedlemIFolketrygd
-
-            val annetLandMedAvtaleIEØS = lovvalgsLand != null && lovvalgsLand != EØSLandEllerLandMedAvtale.NOR && lovvalgsLand in enumValues<EØSLandEllerLandMedAvtale>().map { it }
-
-            if (annetLandMedAvtaleIEØS) {
-                VurderingsResultat(Utfall.IKKE_OPPFYLT, Avslagsårsak.NORGE_IKKE_KOMPETENT_STAT, null)
-            }
-            else if (varMedlemIFolketrygd != true) {
-                VurderingsResultat(Utfall.IKKE_OPPFYLT, Avslagsårsak.IKKE_MEDLEM, null)
-            } else {
-                VurderingsResultat(Utfall.OPPFYLT, null, null)
-            }
-        } else if (grunnlag.nyeSoknadGrunnlag == null)  {
-            VurderingsResultat(Utfall.IKKE_RELEVANT, null, null)
-        } else {
-            val kanBehandlesAutomatisk = MedlemskapLovvalgVurderingService().vurderTilhørighet(grunnlag, rettighetsPeriode).kanBehandlesAutomatisk
-            val utfall = if (kanBehandlesAutomatisk) Utfall.OPPFYLT else Utfall.IKKE_VURDERT
-            VurderingsResultat(utfall, null, null)
-        }
-
-        leggTilVurdering(rettighetsPeriode, grunnlag, vurderingsResultat, vurdertManuelt)
     }
 
     private fun leggTilVurdering(
