74f0c387e5e1c75ac627897c52eefad4d246f2c7
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2025-11-10 07:40:40 +0100

    Enkel minimum-stegtest for overgang arbeid

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/OvergangArbeidSteg.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/OvergangArbeidSteg.kt
@@ -33,7 +33,7 @@ import no.nav.aap.komponenter.type.Periode
 import no.nav.aap.komponenter.verdityper.Tid
 import no.nav.aap.lookup.repository.RepositoryProvider
 
-class OvergangArbeidSteg private constructor(
+class OvergangArbeidSteg internal constructor(
     private val vilkårsresultatRepository: VilkårsresultatRepository,
     private val avklaringsbehovRepository: AvklaringsbehovRepository,
     private val overgangArbeidRepository: OvergangArbeidRepository,
