594de035e27f0a5a38e7c99488f9338d6c03614f
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2025-11-12 10:24:30 +0100

    Bruk egen unleash-fake for lokal kjøring (#1514)

--- lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/test/FakeUnleash.kt
+++ lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/test/FakeUnleash.kt
@@ -29,3 +29,18 @@ object FakeUnleash : FakeUnleashBase(
         BehandlingsflytFeature.NyBeregningAvklarFaktaSteg to true,
     )
 )
+
+object LokalUnleash : FakeUnleashBase(
+    mapOf(
+        BehandlingsflytFeature.IngenValidering to true,
+        BehandlingsflytFeature.NyBrevtype11_17 to true,
+        BehandlingsflytFeature.NyBrevtype11_18 to true,
+        BehandlingsflytFeature.Aktivitetsplikt11_9 to true,
+        BehandlingsflytFeature.OverforingsdatoNullForAvregning to true,
+        BehandlingsflytFeature.OvergangArbeid to true,
+        BehandlingsflytFeature.AvklaringsbehovServiceFormkrav to true,
+        BehandlingsflytFeature.KvalitetssikringsSteg to true,
+        BehandlingsflytFeature.FatteVedtakAvklaringsbehovService to true,
+        BehandlingsflytFeature.NyBeregningAvklarFaktaSteg to true,
+    )
+)
