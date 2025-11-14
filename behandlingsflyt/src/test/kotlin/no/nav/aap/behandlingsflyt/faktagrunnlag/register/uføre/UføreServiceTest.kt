ef646ce7ffad8b6a94e1939f704c2b4ec84310ef
Fredrik Meyer <fredrik.meyer@nav.no>
2025-11-14 13:34:08 +0100

    Listen over uføregrader har ingen rekkefølge, så gjør den til set

--- behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/register/uføre/UføreServiceTest.kt
+++ behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/register/uføre/UføreServiceTest.kt
@@ -2,29 +2,28 @@ package no.nav.aap.behandlingsflyt.faktagrunnlag.register.uføre
 
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingId
 import no.nav.aap.komponenter.verdityper.Prosent
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.Assertions.*
 import java.time.LocalDate
 
 class UføreServiceTest {
-    
+
     @Test
     fun `Ulik rekkefølge i lister skal ikke gi endring`() {
         val nå = LocalDate.now()
         val eksisterendeGrunnlag = UføreGrunnlag(
-            1, BehandlingId(1), listOf(
+            BehandlingId(1), setOf(
                 Uføre(nå, Prosent.`50_PROSENT`),
                 Uføre(nå.plusDays(2), Prosent.`30_PROSENT`),
             )
         )
 
-        val ny = listOf(
+        val ny = setOf(
             Uføre(nå.plusDays(2), Prosent.`30_PROSENT`),
             Uføre(nå, Prosent.`50_PROSENT`),
         )
-        
-        assertFalse(UføreInformasjonskrav.harEndringerUføre(eksisterendeGrunnlag, ny))
 
+        assertThat(UføreInformasjonskrav.harEndringerUføre(eksisterendeGrunnlag, ny)).isFalse
     }
 
 }
