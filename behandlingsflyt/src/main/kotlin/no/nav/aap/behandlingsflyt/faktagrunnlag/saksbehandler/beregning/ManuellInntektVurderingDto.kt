326aa6ef870f7e7fc4f2aba01565812b73c999c2
Martin Aasen Fromreide <martin.aasen.fromreide@nav.no>
2025-11-24 08:49:44 +0100

    Fjern årsak fra ManuellInntektVurderingDto

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/saksbehandler/beregning/ManuellInntektVurderingDto.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/saksbehandler/beregning/ManuellInntektVurderingDto.kt
@@ -6,7 +6,6 @@ data class ManuellInntektVurderingDto(
     val begrunnelse: String,
     @Deprecated("Ikke bruk, skal over til flere vurderinger") val belop: BigDecimal,
     val vurderinger: List<AarsVurdering>? = emptyList(),
-    val aarsak: ÅrsakTilManuellInntektVurdering? = null,
 )
 
 data class AarsVurdering(
@@ -14,8 +13,3 @@ data class AarsVurdering(
     val eosBelop: BigDecimal? = null,
     val ar: Int
 )
-
-enum class ÅrsakTilManuellInntektVurdering {
-    EOS_BEREGNING,
-    IKKE_FERDIG_LIGNET
-}
