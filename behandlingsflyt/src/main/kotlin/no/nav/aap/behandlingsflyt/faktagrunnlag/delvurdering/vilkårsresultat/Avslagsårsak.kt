61b1a46a1f7e91f45b2278d256672d9368bf0fb2
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2025-11-25 11:23:39 +0100

    Riktig avslagsårsak på sykepengeerstatning (#1624)

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/delvurdering/vilkårsresultat/Avslagsårsak.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/delvurdering/vilkårsresultat/Avslagsårsak.kt
@@ -7,6 +7,7 @@ enum class Avslagsårsak(val kode: String, val hjemmel: String) {
     BRUKER_UNDER_18(kode = "11-4-1-1", hjemmel = "§ 11-4 1. ledd"),
     BRUKER_OVER_67(kode = "11-4-1-2", hjemmel = "§ 11-4 1. ledd"),
     MANGLENDE_DOKUMENTASJON(kode = "21-3", hjemmel = "§ 21-3, § 11-1"),// FIXME: Dette er neppe rett med 11-1,
+    IKKE_RETT_PA_SYKEPENGEERSTATNING(kode = "11-13", hjemmel = "§ 11-13"),
     IKKE_SYKDOM_AV_VISS_VARIGHET(kode = "11-5-1", "§ 11-5"),
     IKKE_SYKDOM_SKADE_LYTE_VESENTLIGDEL(kode = "11-5-1", "§ 11-5"),
     IKKE_NOK_REDUSERT_ARBEIDSEVNE(kode = "11-5-1", "§ 11-5"),
