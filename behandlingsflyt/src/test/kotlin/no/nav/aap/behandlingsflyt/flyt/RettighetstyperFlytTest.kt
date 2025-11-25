86c947725e75a9cd1d60bf03b79f1a76dc9c8d50
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2025-11-25 10:28:34 +0100

    Oppdater test til å reflektere ny rettighetsperiode

--- behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/flyt/RettighetstyperFlytTest.kt
+++ behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/flyt/RettighetstyperFlytTest.kt
@@ -4,6 +4,9 @@ import no.nav.aap.behandlingsflyt.behandling.Resultat
 import no.nav.aap.behandlingsflyt.behandling.ResultatUtleder
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løsning.AvklarSykepengerErstatningLøsning
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løsning.ForeslåVedtakLøsning
+import no.nav.aap.behandlingsflyt.behandling.underveis.regler.Hverdager.Companion.plusHverdager
+import no.nav.aap.behandlingsflyt.behandling.underveis.regler.Hverdager.Companion.plussEtÅrMedHverdager
+import no.nav.aap.behandlingsflyt.behandling.underveis.regler.ÅrMedHverdager
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.RettighetsType
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.sykdom.SykepengerGrunn
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.sykdom.flate.SykepengerVurderingDto
@@ -77,7 +80,10 @@ class RettighetstyperFlytTest : AbstraktFlytOrkestratorTest(FakeUnleash::class) 
             .løsAvklaringsBehov(ForeslåVedtakLøsning())
             .fattVedtak()
             .assertRettighetstype(
-                Periode(periode.fom, periode.tom) to RettighetsType.SYKEPENGEERSTATNING,
+                Periode(
+                    sak.rettighetsperiode.fom,
+                    sak.rettighetsperiode.fom.plussEtÅrMedHverdager(ÅrMedHverdager.FØRSTE_ÅR) // TODO: Rettighetstypen skal vare et halvt år
+                ) to RettighetsType.SYKEPENGEERSTATNING,
             )
             .medKontekst {
                 assertThat(this.behandling.status()).isEqualTo(Status.IVERKSETTES)
