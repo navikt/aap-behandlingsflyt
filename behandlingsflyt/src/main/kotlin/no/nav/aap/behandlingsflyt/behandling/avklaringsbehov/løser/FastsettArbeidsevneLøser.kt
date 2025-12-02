21b452e4e546d8e61df498305a0b6dbdfdde2939
SelmaBergstrand <selma.kristine@gmail.com>
2025-12-02 12:21:32 +0100

    Gjør det mulig å fjerne vurdering for arbeidsevne som ikke er utnyttet

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løser/FastsettArbeidsevneLøser.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løser/FastsettArbeidsevneLøser.kt
@@ -5,27 +5,31 @@ import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løsning.FastsettAr
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.arbeidsevne.ArbeidsevnePerioder
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.arbeidsevne.ArbeidsevneRepository
 import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon
+import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingRepository
 import no.nav.aap.lookup.repository.RepositoryProvider
+import kotlin.collections.orEmpty
 
 class FastsettArbeidsevneLøser(
     private val arbeidsevneRepository: ArbeidsevneRepository,
+    private val behandlingRepository: BehandlingRepository,
 ) : AvklaringsbehovsLøser<FastsettArbeidsevneLøsning> {
 
     constructor(repositoryProvider: RepositoryProvider) : this(
         arbeidsevneRepository = repositoryProvider.provide(),
+        behandlingRepository = repositoryProvider.provide(),
     )
 
     override fun løs(
         kontekst: AvklaringsbehovKontekst,
         løsning: FastsettArbeidsevneLøsning
     ): LøsningsResultat {
+        val behandling = behandlingRepository.hent(kontekst.kontekst.behandlingId)
         val arbeidsevneVurderinger =
             løsning.arbeidsevneVurderinger.map { it.toArbeidsevnevurdering(kontekst.bruker.ident) }
-        val eksisterendeArbeidsevnePerioder = ArbeidsevnePerioder(
-            arbeidsevneRepository.hentHvisEksisterer(kontekst.behandlingId())?.vurderinger.orEmpty()
-        )
+        val gamleVurderinger =
+            ArbeidsevnePerioder(behandling.forrigeBehandlingId?.let { arbeidsevneRepository.hentHvisEksisterer(it) }?.vurderinger.orEmpty())
         val nyeArbeidsevnePerioder =
-            eksisterendeArbeidsevnePerioder.leggTil(ArbeidsevnePerioder(arbeidsevneVurderinger))
+            gamleVurderinger.leggTil(ArbeidsevnePerioder(arbeidsevneVurderinger))
 
         arbeidsevneRepository.lagre(
             kontekst.behandlingId(),
