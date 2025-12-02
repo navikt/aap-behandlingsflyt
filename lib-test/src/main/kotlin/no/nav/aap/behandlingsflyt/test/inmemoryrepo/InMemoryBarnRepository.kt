f77dc6c9a5aea9618d09d2a0b1d610f5f5c65bda
Hein Haraldsen <hein.haraldsen@bekk.no>
2025-12-02 17:32:47 +0100

    Legger på sjekk av oppgitte barn

--- lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/test/inmemoryrepo/InMemoryBarnRepository.kt
+++ lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/test/inmemoryrepo/InMemoryBarnRepository.kt
@@ -121,7 +121,15 @@ object InMemoryBarnRepository : BarnRepository {
         TODO("Not yet implemented")
     }
 
-    override fun hentBehandlingIdForSakSomFårBarnetilleggForBarn(ident: Ident): List<BehandlingId> {
+    override fun hentBehandlingIdForSakSomFårBarnetilleggForRegisterBarn(ident: Ident): List<BehandlingId> {
+        TODO("Not yet implemented")
+    }
+
+    override fun hentBehandlingIdForSakSomFårBarnetilleggForOppgitteBarn(ident: Ident): List<BehandlingId> {
+        TODO("Not yet implemented")
+    }
+
+    override fun hentSaksbehandlerOppgitteBarn(ident: String): SaksbehandlerOppgitteBarn? {
         TODO("Not yet implemented")
     }
 
--- repository/src/main/kotlin/no/nav/aap/behandlingsflyt/repository/faktagrunnlag/register/barn/BarnRepositoryImpl.kt
+++ repository/src/main/kotlin/no/nav/aap/behandlingsflyt/repository/faktagrunnlag/register/barn/BarnRepositoryImpl.kt
@@ -80,20 +80,55 @@ class BarnRepositoryImpl(private val connection: DBConnection) : BarnRepository 
         return requireNotNull(hentHvisEksisterer(behandlingId))
     }
 
-    override fun hentBehandlingIdForSakSomFårBarnetilleggForBarn(ident: Ident): List<BehandlingId> {
+    override fun hentBehandlingIdForSakSomFårBarnetilleggForRegisterBarn(ident: Ident): List<BehandlingId> {
         log.info("Henter info for ident {}", ident)
         val registerBarnId = getRegisterBarnId(ident)
         log.info("Henter registerbarnid for registerBarnId {}", registerBarnId)
         if (registerBarnId != null) {
-            val behandlingId = hentBehandlingIdForBarneId(registerBarnId)
+            val behandlingId = hentBehandlingIdForRegisterBarnId(registerBarnId)
             log.info("Henter behandling for behandlingId {}", behandlingId)
             return behandlingId
         }
         return emptyList()
     }
 
+    override fun hentBehandlingIdForSakSomFårBarnetilleggForOppgitteBarn(ident: Ident): List<BehandlingId> {
+        log.info("Henter info for ident {}", ident)
+        val oppgittBarnId = getOppgitteBarnId(ident)
+        log.info("Henter oppgitte for registerBarnId {}", oppgittBarnId)
+        if (oppgittBarnId != null) {
+            val behandlingId = hentBehandlingIdForOppgitteBarneId(oppgittBarnId)
+            log.info("Henter behandling for behandlingId {}", behandlingId)
+            return behandlingId
+        }
+        return emptyList()
+    }
 
-    private fun hentBehandlingIdForBarneId(id: Long): List<BehandlingId> {
+    override fun hentSaksbehandlerOppgitteBarn(ident: String): SaksbehandlerOppgitteBarn {
+        return SaksbehandlerOppgitteBarn(
+            null, connection.queryList(
+                """
+                SELECT p.ident, p.navn, p.fodselsdato, p.relasjon
+                FROM BARN_SAKSBEHANDLER_OPPGITT p
+                WHERE p.ident = ?
+            """.trimIndent()
+            ) {
+                setParams {
+                    setString(1, ident)
+                }
+                setRowMapper { row ->
+                    SaksbehandlerOppgitteBarn.SaksbehandlerOppgitteBarn(
+                        ident = row.getStringOrNull("ident")?.let(::Ident),
+                        navn = row.getString("navn"),
+                        fødselsdato = Fødselsdato(row.getLocalDate("fodselsdato")),
+                        relasjon = row.getString("relasjon").let(Relasjon::valueOf),
+                    )
+                }
+            }
+        )
+    }
+
+    private fun hentBehandlingIdForRegisterBarnId(id: Long): List<BehandlingId> {
 
 
         val behandlingIds = connection.queryList(
@@ -101,6 +136,29 @@ class BarnRepositoryImpl(private val connection: DBConnection) : BarnRepository 
             SELECT BEHANDLING_ID 
             FROM BARNOPPLYSNING_GRUNNLAG g 
             WHERE g.AKTIV AND g.REGISTER_BARN_ID = ?
+        """.trimIndent()
+        ) {
+            setParams {
+                setLong(1, id)
+            }
+            setRowMapper {
+                BehandlingId(
+                    id = it.getLong("behandling_id"),
+                )
+            }
+        }
+
+        return behandlingIds
+    }
+
+    private fun hentBehandlingIdForOppgitteBarneId(id: Long): List<BehandlingId> {
+
+
+        val behandlingIds = connection.queryList(
+            """
+            SELECT BEHANDLING_ID 
+            FROM BARNOPPLYSNING_GRUNNLAG g 
+            WHERE g.AKTIV AND g.saksbehandler_oppgitt_barn_id = ?
         """.trimIndent()
         ) {
             setParams {
@@ -131,6 +189,20 @@ class BarnRepositoryImpl(private val connection: DBConnection) : BarnRepository 
         }
     }
 
+    private fun getOppgitteBarnId(ident: Ident): Long? = connection.queryFirstOrNull(
+        """
+                    SELECT saksbehandler_oppgitt_barn_id
+                    FROM barn_saksbehandler_oppgitt
+                    WHERE ident = ? AND ident is not null
+                 
+                """.trimIndent()
+    ) {
+        setParams { setString(1, ident.identifikator) }
+        setRowMapper { row ->
+            row.getLong("saksbehandler_oppgitt_barn_id")
+        }
+    }
+
     private fun hentSaksbehandlerOppgitteBarn(id: Long): SaksbehandlerOppgitteBarn {
         return SaksbehandlerOppgitteBarn(
             id, connection.queryList(
@@ -153,6 +225,7 @@ class BarnRepositoryImpl(private val connection: DBConnection) : BarnRepository 
                 }
             }
         )
+
     }
 
     private fun hentOppgittBarn(id: Long): OppgitteBarn {
