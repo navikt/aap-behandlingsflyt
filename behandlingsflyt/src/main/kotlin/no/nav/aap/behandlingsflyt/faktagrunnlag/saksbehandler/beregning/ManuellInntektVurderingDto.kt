62d5f6eb9e4014d79da20514ec6c8c977e2b648c
HenrikBuggeHalvorsen <henrik.bugge.halvorsen@nav.no>
2025-12-02 15:16:13 +0100

    revert nullable belop

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/saksbehandler/beregning/ManuellInntektVurderingDto.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/saksbehandler/beregning/ManuellInntektVurderingDto.kt
@@ -4,7 +4,7 @@ import java.math.BigDecimal
 
 data class ManuellInntektVurderingDto(
     val begrunnelse: String,
-    @Deprecated("Ikke bruk, skal over til flere vurderinger") val belop: BigDecimal?,
+    @Deprecated("Ikke bruk, skal over til flere vurderinger") val belop: BigDecimal,
     val vurderinger: List<ÅrsVurdering>? = emptyList(),
 )
 
