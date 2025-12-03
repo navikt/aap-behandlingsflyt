a8bdc7cc6f6f74672e7e3221b12838d80817f891
SelmaBergstrand <selma.kristine@gmail.com>
2025-12-03 13:02:47 +0100

    Trenger ikke hente behandling for å få tak i forrigebehandlingId

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løser/FastsettArbeidsevneLøser.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løser/FastsettArbeidsevneLøser.kt
@@ -5,29 +5,25 @@ import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løsning.FastsettAr
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.arbeidsevne.ArbeidsevnePerioder
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.arbeidsevne.ArbeidsevneRepository
 import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon
-import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingRepository
 import no.nav.aap.lookup.repository.RepositoryProvider
 import kotlin.collections.orEmpty
 
 class FastsettArbeidsevneLøser(
     private val arbeidsevneRepository: ArbeidsevneRepository,
-    private val behandlingRepository: BehandlingRepository,
 ) : AvklaringsbehovsLøser<FastsettArbeidsevneLøsning> {
 
     constructor(repositoryProvider: RepositoryProvider) : this(
         arbeidsevneRepository = repositoryProvider.provide(),
-        behandlingRepository = repositoryProvider.provide(),
     )
 
     override fun løs(
         kontekst: AvklaringsbehovKontekst,
         løsning: FastsettArbeidsevneLøsning
     ): LøsningsResultat {
-        val behandling = behandlingRepository.hent(kontekst.kontekst.behandlingId)
         val arbeidsevneVurderinger =
             løsning.arbeidsevneVurderinger.map { it.toArbeidsevnevurdering(kontekst.bruker.ident) }
         val gamleVurderinger =
-            ArbeidsevnePerioder(behandling.forrigeBehandlingId?.let { arbeidsevneRepository.hentHvisEksisterer(it) }?.vurderinger.orEmpty())
+            ArbeidsevnePerioder(kontekst.kontekst.forrigeBehandlingId?.let { arbeidsevneRepository.hentHvisEksisterer(it) }?.vurderinger.orEmpty())
         val nyeArbeidsevnePerioder =
             gamleVurderinger.leggTil(ArbeidsevnePerioder(arbeidsevneVurderinger))
 
