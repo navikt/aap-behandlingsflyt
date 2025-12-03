f92f9ea5a7ed1b224f907ebb962269624abd7801
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2025-12-03 09:27:25 +0100

    Begynn å ta i bruk bistand.fom

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løser/AvklarBistandLøser.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løser/AvklarBistandLøser.kt
@@ -35,7 +35,7 @@ class AvklarBistandLøser(
             .sykdomsvurderinger.maxBy { it.opprettet }
         val bistandsVurdering = løsning.bistandsVurdering.tilBistandVurdering(
             kontekst.bruker,
-            defaultVurderingenGjelderFra = nyesteSykdomsvurdering.vurderingenGjelderFra, // TODO: Gjør uavhengig fra sykdom
+            defaultVurderingenGjelderFra = løsning.bistandsVurdering.fom ?: nyesteSykdomsvurdering.vurderingenGjelderFra, // TODO: Gjør uavhengig fra sykdom
             kontekst.behandlingId()
         )
 
