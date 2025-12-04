e83a6256780621ab95e4c9422c0a888011aaa6e2
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2025-12-04 07:10:17 +0100

    Merge

--- behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/flyt/SamordningFlyttest.kt
+++ behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/flyt/SamordningFlyttest.kt
@@ -472,7 +472,7 @@ class SamordningFlyttest : AbstraktFlytOrkestratorTest(FakeUnleash::class) {
 
         behandling
             .løsSykdom(periode.fom)
-            .løsBistand()
+            .løsBistand(periode.fom)
             .løsRefusjonskrav()
             .løsSykdomsvurderingBrev()
             .kvalitetssikreOk()
