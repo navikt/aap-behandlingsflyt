b5b2bc929d218d1a182306a7f1fe6001befcf8e5
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2025-12-08 16:10:59 +0100

    Grunnlagrespons på nytt format for overgang uføre

--- api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/beregning/grunnlag/sykdom/overgangufore/OvergangUføreGrunnlagResponse.kt
+++ api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/beregning/grunnlag/sykdom/overgangufore/OvergangUføreGrunnlagResponse.kt
@@ -1,11 +1,20 @@
 package no.nav.aap.behandlingsflyt.behandling.beregning.grunnlag.sykdom.overgangufore
 
+import no.nav.aap.behandlingsflyt.PeriodiserteVurderingerDto
 import no.nav.aap.behandlingsflyt.behandling.beregning.grunnlag.sykdom.sykdom.SykdomsvurderingResponse
+import no.nav.aap.komponenter.type.Periode
 
 data class OvergangUføreGrunnlagResponse(
-    val harTilgangTilÅSaksbehandle: Boolean,
+    override val harTilgangTilÅSaksbehandle: Boolean,
+    @Deprecated("Erstattes av nyeVurderinger")
     val vurdering: OvergangUføreVurderingResponse?,
+    override val nyeVurderinger: List<OvergangUføreVurderingResponse>,
+    @Deprecated("Erstattes av sisteVedtatteVurderinger")
     val gjeldendeVedtatteVurderinger: List<OvergangUføreVurderingResponse>,
+    override val sisteVedtatteVurderinger: List<OvergangUføreVurderingResponse>,
     val historiskeVurderinger: List<OvergangUføreVurderingResponse>,
     val gjeldendeSykdsomsvurderinger: List<SykdomsvurderingResponse>,
-)
+    override val kanVurderes: List<Periode>,
+    override val behøverVurderinger: List<Periode>,
+    val perioderSomIkkeErTilstrekkeligVurdert: List<Periode>
+): PeriodiserteVurderingerDto<OvergangUføreVurderingResponse>
