3d78b96b21cbd1dba5e3686a1e60dbed19757c34
Steffen Hageland <steffen.hageland@nav.no>
2025-12-10 09:14:11 +0100

    Skru på OvergangArbeid lokalt

--- lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/test/FakeUnleash.kt
+++ lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/test/FakeUnleash.kt
@@ -23,7 +23,7 @@ object FakeUnleash : FakeUnleashBase(
         BehandlingsflytFeature.NyBrevtype11_18 to true,
         BehandlingsflytFeature.Aktivitetsplikt11_9 to true,
         BehandlingsflytFeature.OverforingsdatoNullForAvregning to true,
-        BehandlingsflytFeature.OvergangArbeid to false,
+        BehandlingsflytFeature.OvergangArbeid to true,
         BehandlingsflytFeature.KvalitetssikringsSteg to true,
         BehandlingsflytFeature.FatteVedtakAvklaringsbehovService to true,
         BehandlingsflytFeature.EOSBeregning to true,
