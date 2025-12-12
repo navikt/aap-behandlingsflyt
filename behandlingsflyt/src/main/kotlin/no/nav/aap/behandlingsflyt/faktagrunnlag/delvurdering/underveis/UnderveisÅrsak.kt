d5bf8354d17f76f8858472833a1681e9d9eb16f7
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2025-12-12 17:05:28 +0100

    Fjern SoningRegel fra UnderveisSteg
    Vilkåret vurderes nå av StraffegjennomføringVilkår som
    kjøres fra InstitusjonsoppholdSteg.
    
    Siden det ikke er noen saker i prod som har vurdering
    av straffegjennomføring, så trenger vi ikke bakoverkompabilitet
    for de åpne behandlingene.

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/delvurdering/underveis/UnderveisÅrsak.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/delvurdering/underveis/UnderveisÅrsak.kt
@@ -3,12 +3,14 @@ package no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.underveis
 enum class UnderveisÅrsak(val konsekvens: Konsekvens) {
     IKKE_GRUNNLEGGENDE_RETT(Konsekvens.KONSEKVENS_FØLGER_AVSLAGSÅRSAK_TIL_RETTIGHETSTYPEN),
     MELDEPLIKT_FRIST_IKKE_PASSERT(Konsekvens.REDUKSJON),
-    SONER_STRAFF(Konsekvens.STANS),
     VARIGHETSKVOTE_BRUKT_OPP(Konsekvens.OPPHØR),
     BRUDD_PÅ_AKTIVITETSPLIKT_11_7_STANS(Konsekvens.STANS),
     BRUDD_PÅ_AKTIVITETSPLIKT_11_7_OPPHØR(Konsekvens.OPPHØR),
     BRUDD_PÅ_OPPHOLDSKRAV_11_3_STANS(Konsekvens.STANS),
     BRUDD_PÅ_OPPHOLDSKRAV_11_3_OPPHØR(Konsekvens.OPPHØR),
+
+    @Deprecated("Sjekk om verdien finnes i prod-databasen før den evnt. slettes. Og dev?")
+    SONER_STRAFF(Konsekvens.STANS),
 
     @Deprecated("""
         Ble brukt da ikke overholdt meldeplikt førte til stans, og ikke til reduksjon.
