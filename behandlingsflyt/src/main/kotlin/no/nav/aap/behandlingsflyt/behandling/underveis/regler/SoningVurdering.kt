cbd4f36e7a04d492c8f0fcfcc8e7c3471a8259cc
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2025-12-17 14:29:57 +0100

    Fjern SoningRegel fra UnderveisSteg (#1768)
    Vilkåret vurderes nå av StraffegjennomføringVilkår som
    kjøres fra InstitusjonsoppholdSteg.
    
    Siden det ikke er noen saker i prod som har vurdering
    av straffegjennomføring, så trenger vi ikke bakoverkompabilitet
    for de åpne behandlingene.

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/underveis/regler/SoningVurdering.kt
+++
@@ -1,5 +0,0 @@
-package no.nav.aap.behandlingsflyt.behandling.underveis.regler
-
-data class SoningVurdering(
-    val girOpphør: Boolean = true
-)
--- behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/behandling/underveis/regler/SoningRegelTest.kt
+++ behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/behandling/underveis/regler/SoningRegelTest.kt
@@ -2,26 +2,27 @@ package no.nav.aap.behandlingsflyt.behandling.underveis.regler
 
 import no.nav.aap.behandlingsflyt.behandling.institusjonsopphold.InstitusjonsoppholdInput
 import no.nav.aap.behandlingsflyt.behandling.institusjonsopphold.InstitusjonsoppholdUtlederService
-import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.underveis.ArbeidsGradering
-import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.underveis.UnderveisÅrsak
-import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.RettighetsType
+import no.nav.aap.behandlingsflyt.behandling.vilkår.straffegjennomføring.StraffegjennomføringGrunnlag
+import no.nav.aap.behandlingsflyt.behandling.vilkår.straffegjennomføring.StraffegjennomføringVilkår
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Avslagsårsak
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Utfall
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Vilkårsresultat
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Vilkårtype
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.institusjonsopphold.Institusjon
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.institusjonsopphold.Institusjonstype
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.institusjonsopphold.Oppholdstype
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.institusjon.Soningsvurdering
+import no.nav.aap.behandlingsflyt.help.assertTidslinje
 import no.nav.aap.behandlingsflyt.repository.behandling.BehandlingRepositoryImpl
 import no.nav.aap.behandlingsflyt.repository.faktagrunnlag.barnetillegg.BarnetilleggRepositoryImpl
 import no.nav.aap.behandlingsflyt.repository.faktagrunnlag.register.institusjonsopphold.InstitusjonsoppholdRepositoryImpl
 import no.nav.aap.behandlingsflyt.repository.sak.SakRepositoryImpl
 import no.nav.aap.behandlingsflyt.test.MockConnection
 import no.nav.aap.komponenter.tidslinje.Segment
-import no.nav.aap.komponenter.tidslinje.Tidslinje
 import no.nav.aap.komponenter.type.Periode
-import no.nav.aap.komponenter.verdityper.Prosent
-import no.nav.aap.komponenter.verdityper.TimerArbeid
-import org.junit.jupiter.api.Assertions.assertEquals
+import no.nav.aap.komponenter.verdityper.Tid
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import java.math.BigDecimal
 import java.time.LocalDate
 
 class SoningRegelTest {
@@ -34,26 +35,9 @@ class SoningRegelTest {
         BehandlingRepositoryImpl(mockConnection)
     )
 
-    val regel = SoningRegel()
-
-
     @Test
     fun vurder() {
         val periode = Periode(LocalDate.of(2024, 1, 1), LocalDate.of(2024, 12, 1))
-        val vurderingFraTidligereResultat = Vurdering(
-            RettighetsType.BISTANDSBEHOV, MeldepliktVurdering.Fritak,
-            null,
-            null,
-            ArbeidsGradering(
-                totaltAntallTimer = TimerArbeid(BigDecimal.ZERO),
-                andelArbeid = Prosent(0),
-                fastsattArbeidsevne = Prosent.`0_PROSENT`,
-                gradering = Prosent.`100_PROSENT`,
-                opplysningerMottatt = null,
-            ), grenseverdi = Prosent(60)
-        )
-        val tidligereResultatTidslinje = Tidslinje(listOf(Segment(periode, vurderingFraTidligereResultat)))
-
         val utlederInput = InstitusjonsoppholdInput(
             institusjonsOpphold = listOf(
                 Segment(
@@ -90,42 +74,47 @@ class SoningRegelTest {
             ),
             barnetillegg = emptyList(),
             helsevurderinger = emptyList(),
-            rettighetsperiode = Periode(periode.fom, periode.fom.plusYears(3))
+            rettighetsperiode = periode,
         )
 
         val delresultat = utlederService.utledBehov(utlederInput)
 
-        val soningOppholdet = MapInstitusjonoppholdTilRegel.map(delresultat)
-
-        val input = tomUnderveisInput.copy(
-            institusjonsopphold = soningOppholdet,
+        val vilkårsresultat = Vilkårsresultat()
+        StraffegjennomføringVilkår(vilkårsresultat).vurder(
+            StraffegjennomføringGrunnlag(
+                institusjonsopphold = MapInstitusjonoppholdTilRegel.map(delresultat),
+                vurderFra = periode.fom,
+            )
         )
+        val straffegjennomføringVilkårsvurderinger =
+            vilkårsresultat.finnVilkår(Vilkårtype.STRAFFEGJENNOMFØRING).tidslinje()
+        assertTidslinje(
+            straffegjennomføringVilkårsvurderinger,
 
-        val resultat = regel.vurder(input, tidligereResultatTidslinje)
+            //Soner ikke
+            Periode(LocalDate.of(2024, 1, 1), (LocalDate.of(2024, 1, 5))) to {
+                assertThat(it.utfall).isEqualTo(Utfall.OPPFYLT)
+                assertThat(it.manuellVurdering).isEqualTo(false)
+            },
 
-        assertEquals(3, resultat.segmenter().count())
+            //Formue under forvaltning og soner i fengsel
+            Periode(LocalDate.of(2024, 1, 6), (LocalDate.of(2024, 1, 15))) to {
+                assertThat(it.utfall).isEqualTo(Utfall.IKKE_OPPFYLT)
+                assertThat(it.avslagsårsak).isEqualTo(Avslagsårsak.IKKE_RETT_UNDER_STRAFFEGJENNOMFØRING)
+                assertThat(it.manuellVurdering).isEqualTo(true)
+            },
 
-        //Soner ikke
-        assertEquals(
-            Periode(LocalDate.of(2024, 1, 1), (LocalDate.of(2024, 1, 5))),
-            resultat.segmenter().elementAt(0).periode
+            // Arbeider utenfor anstalten og soner i ved frigang
+            Periode(LocalDate.of(2024, 1, 16), (LocalDate.of(2024, 12, 1))) to {
+                assertThat(it.utfall).isEqualTo(Utfall.OPPFYLT)
+                assertThat(it.manuellVurdering).isEqualTo(true)
+            },
+
+            // Automatisk oppfylt i fravær av soningsopphold
+            Periode(LocalDate.of(2024, 12, 2), Tid.MAKS) to {
+                assertThat(it.utfall).isEqualTo(Utfall.OPPFYLT)
+                assertThat(it.manuellVurdering).isEqualTo(false)
+            }
         )
-        assertEquals(Prosent.`100_PROSENT`, resultat.segmenter().elementAt(0).verdi.arbeidsgradering().gradering)
-        assertEquals(null, resultat.segmenter().elementAt(0).verdi.avslagsårsak())
-
-        //Formue under forvaltning og soner i fengsel
-        assertEquals(
-            Periode(LocalDate.of(2024, 1, 6), (LocalDate.of(2024, 1, 15))),
-            resultat.segmenter().elementAt(1).periode
-        )
-        assertEquals(UnderveisÅrsak.SONER_STRAFF, resultat.segmenter().elementAt(1).verdi.avslagsårsak())
-
-        // Arbeider utenfor anstalten og soner i ved frigang
-        assertEquals(
-            Periode(LocalDate.of(2024, 1, 16), (LocalDate.of(2024, 12, 1))),
-            resultat.segmenter().elementAt(2).periode
-        )
-        assertEquals(Prosent.`100_PROSENT`, resultat.segmenter().elementAt(2).verdi.arbeidsgradering().gradering)
-
     }
 }
