8c8c649b48124b72409b2780305bb1a849deb5f4
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-01-07 13:51:49 +0100

    Fjern siste rest av student i beregning (#1850)

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/delvurdering/beregning/år/Inntektsbehov.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/delvurdering/beregning/år/Inntektsbehov.kt
@@ -6,8 +6,6 @@ import no.nav.aap.behandlingsflyt.faktagrunnlag.register.inntekt.InntektPerÅr
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.uføre.Uføre
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.uføre.tilTidslinje
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.beregning.BeregningGrunnlag
-import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.beregning.BeregningstidspunktVurdering
-import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.student.StudentVurdering
 import no.nav.aap.komponenter.verdityper.Beløp
 import no.nav.aap.komponenter.verdityper.GUnit
 import no.nav.aap.komponenter.verdityper.Prosent
@@ -165,9 +163,15 @@ class Inntektsbehov(private val beregningInput: BeregningInput) {
             return datoerForInnhenting.flatMap(::treÅrForutFor).toSortedSet()
         }
 
+        /**
+         * Returnerer en mengde av de tre foregående årene fra nedsettelsesdatoen og
+         * dato for ytterligere nedsatt arbeidsevne.
+         * 
+         * Dersom nedsettelsesdato ikke er satt, returneres en tom mengde.
+         */
         fun utledAlleRelevanteÅr(beregningGrunnlag: BeregningGrunnlag?): Set<Year> {
-            val nedsettelsesDato = beregningGrunnlag?.tidspunktVurdering?.nedsattArbeidsevneEllerStudieevneDato
-            requireNotNull(nedsettelsesDato) { "Nedsettelsesdato kan ikke være null ved utledning av relevante år." }
+            val nedsettelsesDato =
+                beregningGrunnlag?.tidspunktVurdering?.nedsattArbeidsevneEllerStudieevneDato ?: return emptySet()
             val ytterligereNedsattArbeidsevneDato =
                 beregningGrunnlag.tidspunktVurdering.ytterligereNedsattArbeidsevneDato
 
@@ -176,18 +180,6 @@ class Inntektsbehov(private val beregningInput: BeregningInput) {
 
         fun utledRelevanteYtterligereNedsattÅr(beregningGrunnlag: BeregningGrunnlag?): Set<Year> {
             return beregningGrunnlag?.tidspunktVurdering?.ytterligereNedsattArbeidsevneDato?.let(::treÅrForutFor).orEmpty()
-        }
-
-        fun utledNedsettelsesdato(
-            beregningVurdering: BeregningstidspunktVurdering?,
-            studentVurdering: StudentVurdering?
-        ): LocalDate {
-            val nedsettelsesdatoer = setOfNotNull(
-                beregningVurdering?.nedsattArbeidsevneEllerStudieevneDato,
-                studentVurdering?.avbruttStudieDato
-            )
-
-            return nedsettelsesdatoer.min()
         }
 
         private fun treÅrForutFor(nedsettelsesdato: LocalDate): SortedSet<Year> {
