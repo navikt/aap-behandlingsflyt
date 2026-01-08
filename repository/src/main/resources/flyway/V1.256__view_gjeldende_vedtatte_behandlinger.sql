4d4f57db2b31005b914c8b94186e8be6e7066c41
charliemidtlyng <charlie.midtlyng@bekk.no>
2026-01-08 11:51:07 +0100

    Trenger ikke joine på sak for å få med sak_id. Bør ha index på status for å unngå trege spørringer. Legger til tester for å verifisere oppførsel

--- repository/src/main/resources/flyway/V1.256__view_gjeldende_vedtatte_behandlinger.sql
+++ repository/src/main/resources/flyway/V1.256__view_gjeldende_vedtatte_behandlinger.sql
@@ -1,11 +1,12 @@
 CREATE OR REPLACE VIEW gjeldende_vedtatte_behandlinger AS
 SELECT *
-FROM (SELECT s.id as                                                       sak_id,
+FROM (SELECT b.sak_id as                                                       sak_id,
              ROW_NUMBER()
              OVER (PARTITION BY b.sak_id ORDER BY v.vedtakstidspunkt DESC) rn,
              b.id as                                                       behandling_id
       FROM behandling b
-               JOIN sak s ON b.sak_id = s.id
                join vedtak v on b.id = v.behandling_id
       WHERE b.status in ('AVSLUTTET', 'IVERKSETTES')) q
 WHERE rn = 1;
+
+CREATE INDEX IDX_BEHANDLING_STATUS ON BEHANDLING (status);
