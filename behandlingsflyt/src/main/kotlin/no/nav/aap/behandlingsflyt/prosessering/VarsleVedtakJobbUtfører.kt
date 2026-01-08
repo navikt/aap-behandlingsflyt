797aa522afa5de8561ba7087cf5db2af78048f2d
VHollund <vetle.hollund@nav.no>
2026-01-08 09:51:41 +0100

    varsle endringsvedtak ved bestemte endringsparametre

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/prosessering/VarsleVedtakJobbUtfører.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/prosessering/VarsleVedtakJobbUtfører.kt
@@ -1,19 +1,33 @@
 package no.nav.aap.behandlingsflyt.prosessering
 
+import no.nav.aap.behandlingsflyt.behandling.rettighetsperiode.VurderRettighetsperiodeRepository
+import no.nav.aap.behandlingsflyt.behandling.rettighetstype.RettighetstypeVurdering
+import no.nav.aap.behandlingsflyt.behandling.tilkjentytelse.TilkjentYtelsePeriode
+import no.nav.aap.behandlingsflyt.behandling.tilkjentytelse.TilkjentYtelseRepository
 import no.nav.aap.behandlingsflyt.behandling.vedtak.VedtakRepository
 import no.nav.aap.behandlingsflyt.datadeling.sam.SamGateway
 import no.nav.aap.behandlingsflyt.datadeling.sam.SamordneVedtakRequest
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.underveis.UnderveisGrunnlag
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.underveis.UnderveisRepository
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.RettighetsType
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.rettighetsperiode.RettighetsperiodeVurdering
 import no.nav.aap.behandlingsflyt.kontrakt.behandling.TypeBehandling
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingId
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingRepository
 import no.nav.aap.behandlingsflyt.sakogbehandling.sak.SakRepository
 import no.nav.aap.komponenter.gateway.GatewayProvider
 import no.nav.aap.komponenter.repository.RepositoryProvider
+import no.nav.aap.komponenter.tidslinje.Segment
+import no.nav.aap.komponenter.tidslinje.Tidslinje
+import no.nav.aap.komponenter.type.Periode
 import no.nav.aap.motor.FlytJobbRepository
 import no.nav.aap.motor.JobbInput
 import no.nav.aap.motor.JobbUtfører
 import no.nav.aap.motor.ProvidersJobbSpesifikasjon
 import org.slf4j.LoggerFactory
+import java.time.LocalDate
+import kotlin.collections.mapNotNull
+import kotlin.collections.orEmpty
 
 class VarsleVedtakJobbUtfører(
     private val repositoryProvider: RepositoryProvider,
@@ -27,6 +41,7 @@ class VarsleVedtakJobbUtfører(
         val sakRepository: SakRepository = repositoryProvider.provide()
         val behandlingRepository: BehandlingRepository = repositoryProvider.provide()
         val vedtakRepository: VedtakRepository = repositoryProvider.provide()
+        val underveisRepository: RettighetstypeVurdering = repositoryProvider.provide()
 
         val behandlingId = input.payload<BehandlingId>()
         val behandling = behandlingRepository.hent(behandlingId)
@@ -34,6 +49,19 @@ class VarsleVedtakJobbUtfører(
         val vedtak = vedtakRepository.hent(behandling.id)
         val vedtakId =
             requireNotNull(vedtakRepository.hentId(behandling.id)) { "Fant ikke vedtak for behandlingId $behandlingId." }
+        val forrigeBehandlingId = behandling.forrigeBehandlingId
+
+        val rettighetsPeriodeRepo = repositoryProvider.provide<VurderRettighetsperiodeRepository>()
+        val forrigeRettighetsperiodeVurdering =  forrigeBehandlingId?.let{ rettighetsPeriodeRepo.hentVurdering(forrigeBehandlingId) }
+        val nåværendeRettighetsperiodeVurdering = rettighetsPeriodeRepo.hentVurdering(behandling.id)
+
+        val tilkjentRepository: TilkjentYtelseRepository = repositoryProvider.provide()
+        val forrigeTilkjentYtelse = forrigeBehandlingId?.let { tilkjentRepository.hentHvisEksisterer(it) }
+        val nåværendeTilkjentYtelse = tilkjentRepository.hentHvisEksisterer(behandling.id)
+
+        val underveisRepo: UnderveisRepository = repositoryProvider.provide()
+        val forrigeUnderveisGrunnlag = forrigeBehandlingId?.let { underveisRepo.hentHvisEksisterer(it) }
+        val nåværendeUnderveisGrunnlag = underveisRepo.hentHvisEksisterer(behandling.id)
 
 
         requireNotNull(vedtak) { "Forventer at vedtak-objekter er lagret når denne jobben kjøres." }
@@ -56,6 +84,11 @@ class VarsleVedtakJobbUtfører(
             } ?: false,
             utvidetFrist = null,
         )
+
+        val relevantEndring =
+            endringIRettighetsPeriode(forrigeRettighetsperiodeVurdering,nåværendeRettighetsperiodeVurdering)
+                    || endringITilkjentYtelseTidslinje(forrigeTilkjentYtelse,nåværendeTilkjentYtelse)
+                    || endringIRettighetstypeTidslinje(forrigeUnderveisGrunnlag, nåværendeUnderveisGrunnlag!!)
 
         // For nå: kun varsle ved førstegangsbehandlinger.
         // På sikt skal vi varsle hver gang det skjer en "betydelig" endring i ytelsen. F.eks rettighetstype, stans,
@@ -80,5 +113,29 @@ class VarsleVedtakJobbUtfører(
         override val beskrivelse = "Varsler om endring nytt eller endring i vedtak til SAM"
         override val navn = "VarsleVedtakSam"
         override val type = "flyt.Varsler"
+    }
+
+    fun endringIRettighetsPeriode(prev: RettighetsperiodeVurdering?, curr:RettighetsperiodeVurdering?): Boolean{
+        // blir dette riktig? vanskelig å sammenligne rettighetsperiode da den ligger på saken, som ikke kan sjekkes historisk med "forrigeBehandlingId"
+        return prev==null || prev!=curr
+    }
+
+    fun endringITilkjentYtelseTidslinje(
+        forrigeTilkjentYtelse: List<TilkjentYtelsePeriode>?,
+        nåværendeTilkjentYtelse: List<TilkjentYtelsePeriode>?
+    ): Boolean {
+        return forrigeTilkjentYtelse!=nåværendeTilkjentYtelse
+    }
+
+    fun underveisTilRettighetsTypeTidslinje(underveis: UnderveisGrunnlag?): Tidslinje<RettighetsType> {
+        return underveis?.perioder.orEmpty()
+            .mapNotNull { if (it.rettighetsType != null) Segment(it.periode, it.rettighetsType) else null }
+            .let(::Tidslinje).begrensetTil(Periode(LocalDate.MIN, LocalDate.now()))
+    }
+
+    fun endringIRettighetstypeTidslinje(forrigeUnderveisGrunnlag: UnderveisGrunnlag?, nåværendeUnderveisGrunnlag: UnderveisGrunnlag): Boolean {
+        // Implementer logikk for å sammenligne tidslinjer og avgjøre om det har vært en endring i rettighetstype
+
+        return forrigeUnderveisGrunnlag!=null && underveisTilRettighetsTypeTidslinje(forrigeUnderveisGrunnlag)!=underveisTilRettighetsTypeTidslinje(nåværendeUnderveisGrunnlag)
     }
 }
