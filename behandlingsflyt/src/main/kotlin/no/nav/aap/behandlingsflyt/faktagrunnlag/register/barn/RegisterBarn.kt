6dca9c1825cd004eafc2a0618fe18c30f4ec2a2d
thao1 <thao.than.nguyen@nav.no>
2026-01-09 14:42:12 +0100

    Filtrer bort migrerte barn-dubletter fra folkeregister
    - Legg til filtrerBortMigrerteBarn() funksjon i RegisterBarn.kt
    - Fjerner barn med "migrert fra dsf" i navnet når barn med ident og samme fødselsdato finnes
    - Bruker filtreringen i BarnetilleggService for tidslinje-beregning
    - Bruker filtreringen i BarnetilleggGrunnlagApi for API-respons
    - Legger til tester i RegisterBarnTest.kt
    
    Dette forhindrer at migrerte barn vises som dubletter i frontend og påvirker beregninger når det eksisterer et "ekte" barn med fødselsnummer.

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/register/barn/RegisterBarn.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/register/barn/RegisterBarn.kt
@@ -1,3 +1,14 @@
 package no.nav.aap.behandlingsflyt.faktagrunnlag.register.barn
 
 data class RegisterBarn(val id: Long, val barn: List<Barn>)
+
+fun List<Barn>.filtrerBortMigrerteBarn(): List<Barn> {
+    return this.filterNot { barn ->
+        barn.navn?.startsWith("migrert fra dsf") == true &&
+                this.any { annetBarn ->
+                    annetBarn != barn &&
+                            annetBarn.fødselsdato == barn.fødselsdato &&
+                            annetBarn.ident.hentIdent() != null
+                }
+    }
+}
