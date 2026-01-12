c6bb691bb2375db5cff8a76f19db7e497f87eaae
Henrik Gundersen <henrikgun@gmail.com>
2026-01-12 12:07:49 +0100

    Fjerner migreringskode for arbeidsevne (#1874)

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/saksbehandler/arbeidsevne/ArbeidsevneRepository.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/saksbehandler/arbeidsevne/ArbeidsevneRepository.kt
@@ -8,6 +8,5 @@ interface ArbeidsevneRepository : Repository {
     fun hentHvisEksisterer(behandlingId: BehandlingId): ArbeidsevneGrunnlag?
     fun hentAlleVurderinger(sakId: SakId, behandlingId: BehandlingId): Set<ArbeidsevneVurdering>
     fun lagre(behandlingId: BehandlingId, vurderinger: List<ArbeidsevneVurdering>)
-    fun migrerArbeidsevne()
     override fun kopier(fraBehandling: BehandlingId, tilBehandling: BehandlingId)
 }
