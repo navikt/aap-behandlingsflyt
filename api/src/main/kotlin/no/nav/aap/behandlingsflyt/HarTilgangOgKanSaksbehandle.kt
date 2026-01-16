36a611bb1bed0c26a5d4a9e0b83aaa19c78762a7
Martin Aasen Fromreide <martin.aasen.fromreide@nav.no>
2026-01-16 09:43:09 +0100

    Revert "Update api/src/main/kotlin/no/nav/aap/behandlingsflyt/HarTilgangOgKanSaksbehandle.kt"
    This reverts commit 3f04f0ae50c4ac344386346e8ea3936b0b501baf.

--- api/src/main/kotlin/no/nav/aap/behandlingsflyt/HarTilgangOgKanSaksbehandle.kt
+++ api/src/main/kotlin/no/nav/aap/behandlingsflyt/HarTilgangOgKanSaksbehandle.kt
@@ -5,7 +5,8 @@ import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon
 import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Status
 
 fun harTilgangOgKanSaksbehandle(harTilgang: Boolean, avklaringsbehovene: Avklaringsbehovene): Boolean {
-    val erFørAvsluttetKvalitetssikring = avklaringsbehovene.hentBehovForDefinisjon(Definisjon.KVALITETSSIKRING)
+    val erFørAvsluttetKvalitetssikring = avklaringsbehovene.alle()
+        .filter { it.definisjon == Definisjon.KVALITETSSIKRING }
         .let { behov ->
             behov.isEmpty() || behov.singleOrNull()?.status() != Status.AVSLUTTET
         }
