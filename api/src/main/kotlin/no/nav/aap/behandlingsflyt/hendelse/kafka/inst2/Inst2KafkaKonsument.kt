058d2de2d1356be8dc0c70bd894745b19e0b1daf
Hein Haraldsen <hein.haraldsen@bekk.no>
2026-01-16 08:11:59 +0100

    Fjerner unødig mapping på instopphold

--- api/src/main/kotlin/no/nav/aap/behandlingsflyt/hendelse/kafka/inst2/Inst2KafkaKonsument.kt
+++ api/src/main/kotlin/no/nav/aap/behandlingsflyt/hendelse/kafka/inst2/Inst2KafkaKonsument.kt
@@ -3,12 +3,11 @@ package no.nav.aap.behandlingsflyt.hendelse.kafka.inst2
 import no.nav.aap.behandlingsflyt.hendelse.kafka.KafkaConsumerConfig
 import no.nav.aap.behandlingsflyt.hendelse.kafka.KafkaKonsument
 import no.nav.aap.behandlingsflyt.hendelse.mottak.MottattHendelseService
-import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.Inst2HendelseKafkaMelding
-import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.InstitusjonsOppholdHendelse
+import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.InstitusjonsOppholdHendelseKafkaMelding
+import no.nav.aap.behandlingsflyt.kontrakt.sak.Saksnummer
 import no.nav.aap.komponenter.config.requiredConfigForKey
 import no.nav.aap.komponenter.dbconnect.transaction
 import no.nav.aap.komponenter.gateway.GatewayProvider
-import no.nav.aap.komponenter.httpklient.httpclient.ClientConfig
 import no.nav.aap.komponenter.json.DefaultJsonMapper
 import no.nav.aap.komponenter.repository.RepositoryRegistry
 import org.apache.kafka.clients.consumer.ConsumerRecord
@@ -22,13 +21,13 @@ val INSTITUSJONSOPPHOLD_EVENT_TOPIC: String =
     requiredConfigForKey("integrasjon.institusjonsopphold.event.topic")
 
 class Inst2KafkaKonsument(
-    config: KafkaConsumerConfig<InstitusjonsOppholdHendelse, String>,
+    config: KafkaConsumerConfig<String, InstitusjonsOppholdHendelseKafkaMelding>,
     pollTimeout: Duration = 10.seconds,
     closeTimeout: Duration = 30.seconds,
     private val dataSource: DataSource,
     private val repositoryRegistry: RepositoryRegistry,
     private val gatewayProvider: GatewayProvider,
-) : KafkaKonsument<InstitusjonsOppholdHendelse, String>(
+) : KafkaKonsument<String, InstitusjonsOppholdHendelseKafkaMelding>(
     topic = INSTITUSJONSOPPHOLD_EVENT_TOPIC,
     config = config,
     pollTimeout = pollTimeout,
@@ -38,29 +37,19 @@ class Inst2KafkaKonsument(
     private val log = LoggerFactory.getLogger(javaClass)
     private val secureLogger = LoggerFactory.getLogger("team-logs")
 
-    override fun håndter(meldinger: ConsumerRecords<InstitusjonsOppholdHendelse, String>) {
+    override fun håndter(meldinger: ConsumerRecords<String, InstitusjonsOppholdHendelseKafkaMelding>) {
         meldinger.forEach(::håndter)
     }
 
-    fun håndter(melding: ConsumerRecord<InstitusjonsOppholdHendelse, String>) {
+    fun håndter(melding: ConsumerRecord<String, InstitusjonsOppholdHendelseKafkaMelding>) {
         log.info(
             "Behandler institusjonsopphold-record med id: ${melding.key()}, partition ${melding.partition()}, offset: ${melding.offset()}, topic: ${topic}"
         )
-        melding.topic()
         val meldingKey = "${melding.partition()}-${melding.offset()}"
         håndter(meldingKey, melding.value())
     }
 
-    fun håndter(meldingKey: String, meldingVerdi: String) {
-        val institusjonsoppholdHendelse = try {
-            DefaultJsonMapper.fromJson<Inst2HendelseKafkaMelding>(meldingVerdi)
-        } catch (exception: Exception) {
-            secureLogger.error(
-                "Kunne ikke parse melding fra institusjonsopphold: $meldingKey med verdi: $meldingVerdi",
-                exception
-            )
-            throw exception
-        }
+    fun håndter(meldingKey: String, meldingVerdi: InstitusjonsOppholdHendelseKafkaMelding) {
         val saksnummer =
             finnSaksNummer()
         log.info("Mottatt institusjonsoppholdhendelse for saksnummer: $saksnummer")
@@ -70,8 +59,9 @@ class Inst2KafkaKonsument(
                 MottattHendelseService(repositoryProvider)
 
             //TODO: Finn saksnummer først
-            /*hendelseService.registrerMottattHendelse(dto = institusjonsoppholdHendelse.tilInnsending(meldingKey,
-                Saksnummer(saksnummer))
+          /*  hendelseService.registrerMottattHendelse(dto = meldingVerdi.tilInnsending(meldingKey,
+                Saksnummer(saksnummer)
+            )
             )*/
         }
 
