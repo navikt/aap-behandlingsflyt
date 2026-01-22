0b4336f0a5a0f02d80a48f73fe1f51ab658cab15
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-01-22 12:33:31 +0100

    Fiks student-bug der avklaringsbehov for sykdom blir løftet i gammel løsning * Vi må ta hensyn til tom liste når vi sjekker om det ikke finnes en oppfylt periode

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/VurderSykdomSteg.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/VurderSykdomSteg.kt
@@ -6,6 +6,7 @@ import no.nav.aap.behandlingsflyt.behandling.vilkår.TidligereVurderinger
 import no.nav.aap.behandlingsflyt.behandling.vilkår.TidligereVurderingerImpl
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.VilkårsresultatRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.student.StudentRepository
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.student.harPeriodeSomIkkeErOppfylt
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.student.vilkårIkkeOppfylt
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.sykdom.SykdomRepository
 import no.nav.aap.behandlingsflyt.flyt.steg.BehandlingSteg
@@ -123,7 +124,7 @@ class VurderSykdomSteg(
                 val studentGrunnlag = studentRepository.hentHvisEksisterer(kontekst.behandlingId)
 
                 tidligereVurderinger.muligMedRettTilAAP(kontekst, type()) &&
-                        studentGrunnlag.vilkårIkkeOppfylt() &&
+                        studentGrunnlag.harPeriodeSomIkkeErOppfylt() &&
                         kontekst.vurderingsbehovRelevanteForSteg.isNotEmpty()
             }
             VurderingType.UTVID_VEDTAKSLENGDE,
