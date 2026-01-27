5ddea8410e8dcaca40a16c30a82e9ca3017fb928
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-01-27 13:22:02 +0100

    Legg tilbake student som spesiell innvilgelsesårsak

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/delvurdering/vilkårsresultat/Innvilgelsesårsak.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/delvurdering/vilkårsresultat/Innvilgelsesårsak.kt
@@ -4,4 +4,5 @@ enum class Innvilgelsesårsak(val kode: String, val hjemmel: String) {
     YRKESSKADE_ÅRSAKSSAMMENHENG("11-5_11-22", "§ 11-5 jamfør § 11-22 1. ledd"),
     SYKEPENGEERSTATNING("11-5_11-13", "§ 11-13"),
     VURDERES_FOR_UFØRETRYGD("11-5_11-18", "§ 11-18"),
+    STUDENT("11-5_11-14", "§ 11-14"), // Kun for bakoverkompatibilitet
 }
