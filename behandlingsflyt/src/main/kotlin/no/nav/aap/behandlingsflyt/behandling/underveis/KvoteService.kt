ffd7e89612be53b4684eb14e0880c5ace211fc44
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2026-01-28 14:04:23 +0100

    Sett kvote for sykepengeerstatning til 131 hverdager (#2035)
    Var implementert som 130 dager. Regelspesifiseringen har
    nå avklart at det skal være 131 dager.
    
    Vi vil ikke sende brev om endringen.
    
    Det er ingen saker i prod per i dag som er opphørt
    på grunn av at kvoten er brukt opp.

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/underveis/KvoteService.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/underveis/KvoteService.kt
@@ -1,16 +1,20 @@
 package no.nav.aap.behandlingsflyt.behandling.underveis
 
 class KvoteService {
-
-    private val ANTALL_ARBEIDSDAGER_I_ÅRET = 260
-
     fun beregn(): Kvoter {
-        // TODO ta hensyn til når du har rett på hvilken kvote (Kvoter-objektet burde ha en tidslinje et sted)
-        // Dette burde skje ved å hente en tidslinje av rettighetstyper
         return Kvoter.create(
             /* Så lenge Arena har 784 må vi ha samme som dem, i stede for ANTALL_ARBEIDSDAGER_I_ÅRET * 3. */
             ordinærkvote = 784,
-            sykepengeerstatningkvote = ANTALL_ARBEIDSDAGER_I_ÅRET / 2
+
+            /* Fra regelspesifiseringen:
+             *
+             *  Perioden på inntil 6 måneder er en kvote som består av 131 dager.
+             *  Begrunnelsen for at 6 mnd = 131 dager er at tre år etter § 11-12
+             *  er (261 dager ganger 3) + 1 = 784 dager. Dersom man deler 784 på 6
+             *  for å få 6 måneder tilsvarer dette 130,67 dager, som rundes opp til
+             *  131.
+             */
+            sykepengeerstatningkvote = 131,
         )
     }
 }
