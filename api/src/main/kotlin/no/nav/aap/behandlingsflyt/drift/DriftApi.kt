f79e710777b53a628cbf61a5a5e06da3d685e2d5
Nicolas N. <nicolas.nordhagen@nav.no>
2026-01-28 14:19:32 +0100

    Utvide DriftApi med funksjon for å hente sak med behandlinger

--- api/src/main/kotlin/no/nav/aap/behandlingsflyt/drift/DriftApi.kt
+++ api/src/main/kotlin/no/nav/aap/behandlingsflyt/drift/DriftApi.kt
@@ -9,21 +9,31 @@ import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovRepo
 import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon
 import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Status
 import no.nav.aap.behandlingsflyt.kontrakt.behandling.BehandlingReferanse
+import no.nav.aap.behandlingsflyt.kontrakt.sak.Saksnummer
 import no.nav.aap.behandlingsflyt.kontrakt.steg.StegType
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.Behandling
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingRepository
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.VurderingsbehovMedPeriode
-import no.nav.aap.behandlingsflyt.sakogbehandling.sak.flate.BehandlinginfoDTO
+import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.ÅrsakTilOpprettelse
+import no.nav.aap.behandlingsflyt.sakogbehandling.flyt.Vurderingsbehov
+import no.nav.aap.behandlingsflyt.sakogbehandling.sak.SakRepository
+import no.nav.aap.behandlingsflyt.sakogbehandling.sak.flate.SaksnummerParameter
 import no.nav.aap.komponenter.dbconnect.transaction
 import no.nav.aap.komponenter.gateway.GatewayProvider
+import no.nav.aap.komponenter.httpklient.exception.VerdiIkkeFunnetException
 import no.nav.aap.komponenter.json.DefaultJsonMapper
 import no.nav.aap.komponenter.repository.RepositoryRegistry
+import no.nav.aap.komponenter.type.Periode
 import no.nav.aap.tilgang.AuthorizationParamPathConfig
 import no.nav.aap.tilgang.BehandlingPathParam
 import no.nav.aap.tilgang.Operasjon
+import no.nav.aap.tilgang.SakPathParam
 import no.nav.aap.tilgang.authorizedPost
 import java.time.LocalDateTime
+import java.util.*
 import javax.sql.DataSource
+import no.nav.aap.behandlingsflyt.kontrakt.behandling.Status as BehandlingStatus
+import no.nav.aap.behandlingsflyt.kontrakt.sak.Status as SakStatus
 
 /**
  * API for å utføre manuelle operasjoner med forsøk på å rette opp i låste saker av varierende grunn.
@@ -57,63 +67,91 @@ fun NormalOpenAPIRoute.driftApi(
             }
         }
 
-        route("/behandling/{referanse}/info") {
-            authorizedPost<BehandlingReferanse, BehandlingDriftsinfoDTO, Unit>(
+        route("/sak/{saksnummer}/info") {
+            authorizedPost<SaksnummerParameter, SakDriftsinfoDTO, Unit>(
                 AuthorizationParamPathConfig(
-                    behandlingPathParam = BehandlingPathParam("referanse"),
+                    sakPathParam = SakPathParam("saksnummer"),
                     operasjon = Operasjon.DRIFTE
-                )
+                ),
             ) { params, _ ->
-                val behandlingDriftsinfo = dataSource.transaction { connection ->
+                val sakDriftsinfoDTO = dataSource.transaction { connection ->
                     val repositoryProvider = repositoryRegistry.provider(connection)
+
+                    val sakRepository = repositoryProvider.provide<SakRepository>()
                     val behandlingRepository = repositoryProvider.provide<BehandlingRepository>()
+                    val avklaringsbehovRepository = repositoryProvider.provide<AvklaringsbehovRepository>()
 
-                    val behandling = behandlingRepository.hent(BehandlingReferanse(params.referanse))
+                    val sak = sakRepository.hentHvisFinnes(Saksnummer(params.saksnummer))
+                        ?: throw VerdiIkkeFunnetException("Sak med saksnummer ${params.saksnummer} finnes ikke")
 
-                    val avklaringsbehovene = repositoryProvider.provide<AvklaringsbehovRepository>()
-                        .hentAvklaringsbehovene(behandling.id)
-                        .alle()
-                        .flatMap { avklaringsbehov ->
-                            avklaringsbehov.historikk.map { endring ->
-                                ForenkletAvklaringsbehov(
-                                    definisjon = avklaringsbehov.definisjon,
-                                    status = endring.status,
-                                    tidsstempel = endring.tidsstempel,
-                                    endretAv = endring.endretAv
-                                )
-                            }
-                        }.sortedByDescending { it.tidsstempel }
+                    val behandlinger = behandlingRepository.hentAlleFor(sak.id)
+                        .map { behandling ->
+                            val avklaringsbehovene = avklaringsbehovRepository
+                                .hentAvklaringsbehovene(behandling.id)
+                                .alle()
+                                .flatMap { avklaringsbehov ->
+                                    avklaringsbehov.historikk.map { endring ->
+                                        ForenkletAvklaringsbehov(
+                                            definisjon = avklaringsbehov.definisjon,
+                                            status = endring.status,
+                                            tidsstempel = endring.tidsstempel,
+                                            endretAv = endring.endretAv
+                                        )
+                                    }
+                                }.sortedBy { it.tidsstempel }
 
-                    BehandlingDriftsinfoDTO(
-                        behandling = behandling.tilDTO(),
-                        avklaringsbehov = avklaringsbehovene,
+                            BehandlingDriftsinfo.fra(behandling, avklaringsbehovene)
+                        }
+
+                    SakDriftsinfoDTO(
+                        saksnummer = sak.saksnummer.toString(),
+                        status = sak.status(),
+                        rettighetsperiode = sak.rettighetsperiode,
+                        opprettetTidspunkt = sak.opprettetTidspunkt,
+                        behandlinger = behandlinger,
                     )
                 }
 
-                require(!Regex("""\d{11}""").containsMatchIn(DefaultJsonMapper.toJson(behandlingDriftsinfo))) {
-                    "Behandlingen inneholder (potensielt) sensitive personopplysninger!"
+                require(!Regex("""\d{11}""").containsMatchIn(DefaultJsonMapper.toJson(sakDriftsinfoDTO))) {
+                    "DTO-en inneholder (potensielt) sensitive personopplysninger!"
                 }
 
-                respond(behandlingDriftsinfo)
+                respond(sakDriftsinfoDTO)
             }
         }
     }
 }
 
-private data class BehandlingDriftsinfoDTO(
-    val behandling: BehandlinginfoDTO,
-    val avklaringsbehov: List<ForenkletAvklaringsbehov>,
+private data class SakDriftsinfoDTO(
+    val saksnummer: String,
+    val status: SakStatus,
+    val rettighetsperiode: Periode,
+    val opprettetTidspunkt: LocalDateTime = LocalDateTime.now(),
+    val behandlinger: List<BehandlingDriftsinfo>,
 )
 
-private fun Behandling.tilDTO() = BehandlinginfoDTO(
-    referanse = this.referanse.referanse,
-    type = this.typeBehandling().identifikator(),
-    status = this.status(),
-    vurderingsbehov = this.vurderingsbehov().map(VurderingsbehovMedPeriode::type),
-    årsakTilOpprettelse = this.årsakTilOpprettelse,
-    opprettet = this.opprettetTidspunkt,
-    eksternSaksbehandlingsløsningUrl = null,
-)
+private data class BehandlingDriftsinfo(
+    val referanse: UUID,
+    val type: String,
+    val status: BehandlingStatus,
+    val vurderingsbehov: List<Vurderingsbehov>,
+    val årsakTilOpprettelse: ÅrsakTilOpprettelse?,
+    val opprettet: LocalDateTime,
+    val avklaringsbehov: List<ForenkletAvklaringsbehov>,
+) {
+    companion object {
+        fun fra(behandling: Behandling, avklaringsbehovene: List<ForenkletAvklaringsbehov>) =
+            BehandlingDriftsinfo(
+                referanse = behandling.referanse.referanse,
+                type = behandling.typeBehandling().identifikator(),
+                status = behandling.status(),
+                vurderingsbehov = behandling.vurderingsbehov().map(VurderingsbehovMedPeriode::type),
+                årsakTilOpprettelse = behandling.årsakTilOpprettelse,
+                opprettet = behandling.opprettetTidspunkt,
+                avklaringsbehov = avklaringsbehovene,
+            )
+    }
+}
 
 private data class ForenkletAvklaringsbehov(
     val definisjon: Definisjon,
