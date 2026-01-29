408d786a22499794a054a2e2aa88f0d1adca6dcf
Hein Haraldsen <hein.haraldsen@bekk.no>
2026-01-29 11:21:58 +0100

    Fjerner ubrukt import2

--- api/src/main/kotlin/no/nav/aap/behandlingsflyt/hendelse/kafka/person/PdlHendelseKafkaKonsument.kt
+++ api/src/main/kotlin/no/nav/aap/behandlingsflyt/hendelse/kafka/person/PdlHendelseKafkaKonsument.kt
@@ -1,6 +1,5 @@
 package no.nav.aap.behandlingsflyt.hendelse.kafka.person
 
-import no.nav.aap.behandlingsflyt.behandling.Resultat
 import no.nav.aap.behandlingsflyt.behandling.søknad.TrukketSøknadService
 import no.nav.aap.behandlingsflyt.faktagrunnlag.SakOgBehandlingService
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.underveis.UnderveisRepository
