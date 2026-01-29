98eab020d195a06c9eff1edf0fbefa1bad13428b
Fredrik Meyer <fredrik.meyer@nav.no>
2026-01-29 09:17:40 +0100

    Endre a til å og fjerne ubrukte argumenter

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/register/yrkesskade/YrkesskadeRepository.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/register/yrkesskade/YrkesskadeRepository.kt
@@ -3,7 +3,7 @@ package no.nav.aap.behandlingsflyt.faktagrunnlag.register.yrkesskade
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingId
 import no.nav.aap.lookup.repository.Repository
 
-interface YrkesskadeRepository : Repository{
+interface YrkesskadeRepository : Repository {
     fun hentHvisEksisterer(behandlingId: BehandlingId): YrkesskadeGrunnlag?
     fun lagre(behandlingId: BehandlingId, yrkesskader: Yrkesskader?)
     override fun kopier(fraBehandling: BehandlingId, tilBehandling: BehandlingId)
