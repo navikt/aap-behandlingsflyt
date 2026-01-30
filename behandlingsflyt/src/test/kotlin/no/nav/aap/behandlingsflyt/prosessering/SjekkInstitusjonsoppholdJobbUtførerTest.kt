ba41edafaff64701b6544ff02f70d332f608a9b7
Hein Haraldsen <hein.haraldsen@bekk.no>
2026-01-30 13:35:23 +0100

    Sjekker om instopphold ikke har avslag eller vurderinger fra før

--- behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/prosessering/SjekkInstitusjonsoppholdJobbUtførerTest.kt
+++ behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/prosessering/SjekkInstitusjonsoppholdJobbUtførerTest.kt
@@ -6,7 +6,15 @@ import io.mockk.junit5.MockKExtension
 import io.mockk.just
 import io.mockk.mockk
 import io.mockk.verify
+
 import no.nav.aap.behandlingsflyt.faktagrunnlag.SakOgBehandlingService
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.underveis.ArbeidsGradering
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.underveis.UnderveisGrunnlag
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.underveis.UnderveisRepository
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.underveis.Underveisperiode
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.underveis.UnderveisÅrsak
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.RettighetsType
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Utfall
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.institusjonsopphold.Institusjonsopphold
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.institusjonsopphold.InstitusjonsoppholdGrunnlag
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.institusjonsopphold.InstitusjonsoppholdRepository
@@ -24,19 +32,26 @@ import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingMedVedtak
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingRepository
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.StegTilstand
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.VurderingsbehovMedPeriode
+import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.VurderingsbehovOgÅrsak
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.ÅrsakTilOpprettelse
 import no.nav.aap.behandlingsflyt.sakogbehandling.flyt.StegStatus
+import no.nav.aap.behandlingsflyt.sakogbehandling.flyt.Vurderingsbehov
 import no.nav.aap.behandlingsflyt.sakogbehandling.sak.Person
 import no.nav.aap.behandlingsflyt.sakogbehandling.sak.PersonId
 import no.nav.aap.behandlingsflyt.sakogbehandling.sak.Sak
 import no.nav.aap.behandlingsflyt.sakogbehandling.sak.SakId
 import no.nav.aap.behandlingsflyt.sakogbehandling.sak.SakRepository
 import no.nav.aap.behandlingsflyt.sakogbehandling.sak.SakService
+import no.nav.aap.behandlingsflyt.test.januar
 import no.nav.aap.behandlingsflyt.unleash.BehandlingsflytFeature
 import no.nav.aap.behandlingsflyt.unleash.UnleashGateway
 import no.nav.aap.komponenter.type.Periode
+import no.nav.aap.komponenter.verdityper.Dagsatser
+import no.nav.aap.komponenter.verdityper.Prosent
+import no.nav.aap.komponenter.verdityper.TimerArbeid
 import no.nav.aap.motor.JobbInput
 import org.junit.jupiter.api.extension.ExtendWith
+import java.math.BigDecimal
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.*
@@ -74,13 +89,14 @@ class `SjekkInstitusjonsoppholdJobbUtførerTest` {
     private fun mockAvhengigheterForInstitusjonsoppholdJobbUtfører(
         årsakerPåTidligereBehandling: List<VurderingsbehovMedPeriode> = emptyList(),
         hentInstitusjonsoppholdReturn: InstitusjonsoppholdGrunnlag
-    ): Pair<SjekkInstitusjonsOppholdJobbUtfører, SakOgBehandlingService>  {
+    ): Pair<SjekkInstitusjonsOppholdJobbUtfører, SakOgBehandlingService> {
         val sakServiceMock = mockk<SakService>()
         val sakOgBehandlingServiceMock = mockk<SakOgBehandlingService>()
         val sakRepositoryMock = mockk<SakRepository>()
         val behandlingRepositoryMock = mockk<BehandlingRepository>()
         val prosesserBehandlingServiceMock = mockk<ProsesserBehandlingService>()
         val institusjonsoppholdRepositoryMock = mockk<InstitusjonsoppholdRepository>()
+        val underveisgrunnlagRepositoryMock = mockk<UnderveisRepository>()
         val unleashGateway = mockk<UnleashGateway> {
             every { isEnabled(BehandlingsflytFeature.InstitusjonsoppholdJobb) } returns true
         }
@@ -117,6 +133,23 @@ class `SjekkInstitusjonsoppholdJobbUtførerTest` {
             opprettetTidspunkt = LocalDateTime.now(),
             versjon = 0L,
         )
+
+        every {
+            behandlingRepositoryMock.hentVurderingsbehovOgÅrsaker(any())
+        } returns listOf(
+            VurderingsbehovOgÅrsak(
+                vurderingsbehov = listOf(
+                    VurderingsbehovMedPeriode(
+                        type = Vurderingsbehov.VURDER_RETTIGHETSPERIODE,
+                        periode = null,
+                    )
+                ),
+                årsak = ÅrsakTilOpprettelse.ENDRING_I_REGISTERDATA,
+                opprettet = LocalDateTime.now(),
+                beskrivelse = "Institusjonsopphold registrert"
+            )
+        )
+
 
         val fakeBehandling = Behandling(
             id = BehandlingId(456L),
@@ -156,7 +189,24 @@ class `SjekkInstitusjonsoppholdJobbUtførerTest` {
 
         every { sakOgBehandlingServiceMock.finnSisteYtelsesbehandlingFor(sakId) } returns fakeBehandling
 
-        every { sakOgBehandlingServiceMock.finnEllerOpprettOrdinærBehandling(any<SakId>(), any()) } returns fakeOpprettetBehandling
+        every {
+            sakOgBehandlingServiceMock.finnEllerOpprettOrdinærBehandling(
+                any<SakId>(),
+                any()
+            )
+        } returns fakeOpprettetBehandling
+
+        every { underveisgrunnlagRepositoryMock.hent(any()) } returns UnderveisGrunnlag(
+            id = 100L,
+            perioder = listOf(
+                underveisperiode(
+                    periode = Periode(16 januar 2026, 31 januar 2026),
+                    rettighetsType = RettighetsType.BISTANDSBEHOV,
+                    avslagsÅrsak = UnderveisÅrsak.IKKE_GRUNNLEGGENDE_RETT,
+                    utfall = Utfall.OPPFYLT,
+                )
+            )
+        )
 
         every {
             prosesserBehandlingServiceMock.triggProsesserBehandling(
@@ -214,6 +264,8 @@ class `SjekkInstitusjonsoppholdJobbUtførerTest` {
             sakRepository = sakRepositoryMock,
             institusjonsOppholdRepository = institusjonsoppholdRepositoryMock,
             sakOgBehandlingService = sakOgBehandlingServiceMock,
+            behandlingRepository = behandlingRepositoryMock,
+            underveisgrunnlagRepository = underveisgrunnlagRepositoryMock,
             unleashGateway = unleashGateway,
         ) to sakOgBehandlingServiceMock
     }
@@ -247,4 +299,32 @@ class `SjekkInstitusjonsoppholdJobbUtførerTest` {
             ).map { it.tilInstitusjonSegment() })
     )
 
+    private fun underveisperiode(
+        periode: Periode,
+        rettighetsType: RettighetsType,
+        avslagsÅrsak: UnderveisÅrsak? = null,
+        utfall: Utfall
+    ): Underveisperiode {
+        return Underveisperiode(
+            periode = periode,
+            meldePeriode = periode,
+            utfall = utfall,
+            rettighetsType = rettighetsType,
+            avslagsårsak = avslagsÅrsak,
+            grenseverdi = Prosent.`100_PROSENT`,
+            institusjonsoppholdReduksjon = Prosent.`0_PROSENT`,
+            arbeidsgradering = ArbeidsGradering(
+                totaltAntallTimer = TimerArbeid(BigDecimal(0)),
+                andelArbeid = Prosent.`0_PROSENT`,
+                fastsattArbeidsevne = Prosent.`100_PROSENT`,
+                gradering = Prosent.`100_PROSENT`,
+                opplysningerMottatt = null,
+            ),
+            trekk = Dagsatser(0),
+            brukerAvKvoter = emptySet(),
+            meldepliktStatus = null,
+            meldepliktGradering = Prosent.`0_PROSENT`,
+        )
+
+    }
 }
