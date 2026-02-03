19575a608c7e01dbd223923d4336c3c28ce23d21
Martin Aasen Fromreide <martin.aasen.fromreide@nav.no>
2026-02-03 16:12:18 +0100

    Fjern ubrukte imports

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/KvalitetssikringsSteg.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/KvalitetssikringsSteg.kt
@@ -18,8 +18,6 @@ import no.nav.aap.behandlingsflyt.kontrakt.steg.StegType
 import no.nav.aap.behandlingsflyt.sakogbehandling.flyt.FlytKontekstMedPerioder
 import no.nav.aap.komponenter.gateway.GatewayProvider
 import no.nav.aap.lookup.repository.RepositoryProvider
-import kotlin.compareTo
-import kotlin.text.get
 
 class KvalitetssikringsSteg(
     private val avklaringsbehovRepository: AvklaringsbehovRepository,
