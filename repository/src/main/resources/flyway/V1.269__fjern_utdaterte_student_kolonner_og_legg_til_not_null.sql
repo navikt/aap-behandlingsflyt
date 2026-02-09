5a8a6be82eeee5b7583c6088967593432ee86cfe
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-02-09 07:55:39 +0100

    Riktig kolonne

--- repository/src/main/resources/flyway/V1.269__fjern_utdaterte_student_kolonner_og_legg_til_not_null.sql
+++ repository/src/main/resources/flyway/V1.269__fjern_utdaterte_student_kolonner_og_legg_til_not_null.sql
@@ -3,4 +3,4 @@ alter table student_grunnlag drop column student_id;
 alter table student_vurdering
     alter column fom set not null,
     alter column vurdert_i_behandling set not null,
-    alter column student_vurderinger_ids set not null;
+    alter column student_vurderinger_id set not null;
