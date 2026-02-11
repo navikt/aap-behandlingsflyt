aed8c38a8156062df266705c988db59bf2fd6205
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-02-11 10:16:01 +0100

    Bruk persistert rettighetstidslinje * Dette gjør at rett = false der kvote er oppbrukt

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/underveis/regler/AapEtterRegel.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/underveis/regler/AapEtterRegel.kt
@@ -9,7 +9,8 @@ class AapEtterRegel : UnderveisRegel {
     override fun vurder(input: UnderveisInput, resultat: Tidslinje<Vurdering>): Tidslinje<Vurdering> {
         if (input.forenkletKvoteFeature) {
             if (input.rettighetstypeGrunnlag != null) {
-                val rettighetstypeTidslinje = input.vilkårsresultat.rettighetstypeTidslinje()
+                val rettighetstypeTidslinje =
+                    input.rettighetstypeGrunnlag.rettighetstypeTidslinje
                 val ordinærKvote = input.vilkårsresultat.finnVilkår(Vilkårtype.ORDINÆR_KVOTE).tidslinje()
                 val speKvote = input.vilkårsresultat.finnVilkår(Vilkårtype.SYKEPENGEERSTATNING_KVOTE).tidslinje()
                 val varighetTidslinje = ordinærKvote.outerJoin(speKvote) { ordinær, spe ->
@@ -18,13 +19,13 @@ class AapEtterRegel : UnderveisRegel {
                         ordinær?.utfall == Utfall.OPPFYLT -> Oppfylt(brukerAvKvoter = setOf(Kvote.ORDINÆR))
                         spe?.utfall == Utfall.OPPFYLT -> Oppfylt(brukerAvKvoter = setOf(Kvote.SYKEPENGEERSTATNING))
                         ordinær?.utfall == Utfall.IKKE_OPPFYLT -> Avslag(
-                            brukerAvKvoter = emptySet(), avslagsårsaker = setOf(
+                            brukerAvKvoter = setOf(Kvote.ORDINÆR), avslagsårsaker = setOf(
                                 VarighetVurdering.Avslagsårsak.ORDINÆRKVOTE_BRUKT_OPP
                             )
                         )
 
                         spe?.utfall == Utfall.IKKE_OPPFYLT -> Avslag(
-                            brukerAvKvoter = emptySet(), avslagsårsaker = setOf(
+                            brukerAvKvoter = setOf(Kvote.SYKEPENGEERSTATNING), avslagsårsaker = setOf(
                                 VarighetVurdering.Avslagsårsak.SYKEPENGEERSTATNINGKVOTE_BRUKT_OPP
                             )
                         )
--- behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/behandling/underveis/UnderveisServiceTest.kt
+++ behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/behandling/underveis/UnderveisServiceTest.kt
@@ -1,17 +1,31 @@
 package no.nav.aap.behandlingsflyt.behandling.underveis
 
+import no.nav.aap.behandlingsflyt.behandling.rettighetstype.KvoteOk
+import no.nav.aap.behandlingsflyt.behandling.rettighetstype.vurderRettighetstypeOgKvoter
+import no.nav.aap.behandlingsflyt.behandling.underveis.regler.Hverdager
 import no.nav.aap.behandlingsflyt.behandling.underveis.regler.tomUnderveisInput
+import no.nav.aap.behandlingsflyt.behandling.vilkår.kvote.OrdinærKvoteFaktagrunnlag
+import no.nav.aap.behandlingsflyt.behandling.vilkår.kvote.OrdinærKvoteVilkår
+import no.nav.aap.behandlingsflyt.behandling.vilkår.kvote.SykepengeerstatningKvoteFaktagrunnlag
+import no.nav.aap.behandlingsflyt.behandling.vilkår.kvote.SykepengeerstatningKvoteVilkår
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.rettighetstype.RettighetstypeGrunnlag
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.RettighetsType
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Utfall
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Vilkår
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Vilkårsperiode
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Vilkårsresultat
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Vilkårtype
+import no.nav.aap.behandlingsflyt.help.assertTidslinje
+import no.nav.aap.behandlingsflyt.help.assertTidslinjeEquals
+import no.nav.aap.behandlingsflyt.help.genererVilkårsresultat
 import no.nav.aap.behandlingsflyt.integrasjon.createGatewayProvider
 import no.nav.aap.behandlingsflyt.repository.postgresRepositoryRegistry
 import no.nav.aap.behandlingsflyt.test.AlleAvskruddUnleash
 import no.nav.aap.behandlingsflyt.test.MockDataSource
+import no.nav.aap.behandlingsflyt.test.januar
 import no.nav.aap.komponenter.dbconnect.transaction
 import no.nav.aap.komponenter.type.Periode
+import no.nav.aap.komponenter.verdityper.Tid
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
@@ -32,77 +46,283 @@ class UnderveisServiceTest {
             )
             val søknadsdato = LocalDate.now().minusDays(29)
             val periode = Periode(søknadsdato, søknadsdato.plusYears(3))
-            val aldersVilkåret =
-                Vilkår(
-                    Vilkårtype.ALDERSVILKÅRET, setOf(
+            val vilkårsresultat = genererVilkårsresultat(
+                periode,
+                studentVilkår = Vilkår(
+                    Vilkårtype.STUDENT,
+                    setOf(
                         Vilkårsperiode(
                             periode,
                             Utfall.OPPFYLT,
                             false,
                             null,
-                            faktagrunnlag = null
+                            faktagrunnlag = null,
+                            avslagsårsak = null
                         )
                     )
-                )
-            val lovvalgVilkåret =
-                Vilkår(
-                    Vilkårtype.LOVVALG, setOf(
+                ),
+                bistandVilkåret = Vilkår(
+                    Vilkårtype.BISTANDSVILKÅRET,
+                    setOf(
                         Vilkårsperiode(
                             periode,
                             Utfall.OPPFYLT,
                             false,
                             null,
-                            faktagrunnlag = null
+                            faktagrunnlag = null,
+                            avslagsårsak = null
                         )
                     )
-                )
-            val sykdomsVilkåret =
-                Vilkår(
-                    Vilkårtype.SYKDOMSVILKÅRET, setOf(
+                ),
+                sykepengeerstatningVilkåret = Vilkår(
+                    Vilkårtype.SYKEPENGEERSTATNING, setOf(
+
                         Vilkårsperiode(
                             periode,
                             Utfall.OPPFYLT,
                             false,
                             null,
-                            faktagrunnlag = null
+                            faktagrunnlag = null,
+                            avslagsårsak = null
                         )
                     )
-                )
-            val medlemskapVilkåret =
-                Vilkår(
-                    Vilkårtype.MEDLEMSKAP, setOf(
-                        Vilkårsperiode(
-                            periode,
-                            Utfall.OPPFYLT,
-                            false,
-                            null,
-                            faktagrunnlag = null
-                        )
-                    )
-                )
-            val bistandVilkåret =
-                Vilkår(
-                    Vilkårtype.BISTANDSVILKÅRET, setOf(
-                        Vilkårsperiode(
-                            periode,
-                            Utfall.OPPFYLT,
-                            false,
-                            null,
-                            faktagrunnlag = null
-                        )
-                    )
-                )
-            val relevanteVilkår = listOf(aldersVilkåret, lovvalgVilkåret, bistandVilkåret, medlemskapVilkåret, sykdomsVilkåret)
+                ),
+            )
             val input = tomUnderveisInput(
                 rettighetsperiode = periode,
-                vilkårsresultat = Vilkårsresultat(null, relevanteVilkår),
+                vilkårsresultat = vilkårsresultat,
                 opptrappingPerioder = listOf(Periode(søknadsdato.plusYears(2), søknadsdato.plusYears(3))),
-                kvoter = kvoter
+                kvoter = kvoter,
+                forenkletKvoteFeature = true
             )
 
             val vurderingTidslinje = underveisService.vurderRegler(input)
 
             assertThat(vurderingTidslinje.segmenter()).isNotEmpty()
+        }
+    }
+
+    @Test
+    fun `Persistert rettighetstype med kvotevilkår skal gi samme svar som utledet bortsett fra rett der det er avslag på kvote`() {
+        dataSource.transaction { connection ->
+            val underveisService = UnderveisService(
+                postgresRepositoryRegistry.provider(connection),
+                createGatewayProvider {
+                    register<AlleAvskruddUnleash>()
+                }
+            )
+            val søknadsdato = 1 januar 2024
+
+            val studentOppfylt = Periode(søknadsdato, 7 januar 2024) // Bruker 5 dager av ordinær kvote
+            val speOppfylt =
+                Periode(8 januar 2024, 14 januar 2024) // Bruker 5 dager av sykepengeerstatning-kvote
+            val periodeBistandOppfylt = Periode(15 januar 2024, Tid.MAKS) // Bruker gjenværende dager av ordinær kvote
+
+
+            val rettighetsperiode = Periode(søknadsdato, Tid.MAKS)
+            val vilkårsresultat = genererVilkårsresultat(
+                rettighetsperiode,
+                studentVilkår = Vilkår(
+                    Vilkårtype.STUDENT,
+                    setOf(
+                        Vilkårsperiode(
+                            studentOppfylt,
+                            Utfall.OPPFYLT,
+                            false,
+                            null,
+                            faktagrunnlag = null,
+                            avslagsårsak = null
+                        )
+                    )
+                ),
+                bistandVilkåret = Vilkår(
+                    Vilkårtype.BISTANDSVILKÅRET,
+                    setOf(
+                        Vilkårsperiode(
+                            periodeBistandOppfylt,
+                            Utfall.OPPFYLT,
+                            false,
+                            null,
+                            faktagrunnlag = null,
+                            avslagsårsak = null
+                        )
+                    )
+                ),
+                sykepengeerstatningVilkåret = Vilkår(
+                    Vilkårtype.SYKEPENGEERSTATNING, setOf(
+
+                        Vilkårsperiode(
+                            speOppfylt,
+                            Utfall.OPPFYLT,
+                            false,
+                            null,
+                            faktagrunnlag = null,
+                            avslagsårsak = null
+                        )
+                    )
+                ),
+            )
+
+            val fiktiveKvoter = Kvoter(
+                ordinærkvote = Hverdager(10),
+                sykepengeerstatningkvote = Hverdager(5)
+            )
+
+            // Gjøres i RettighetstypeSteg
+            val vilkårsresultat2 = Vilkårsresultat(vilkår = vilkårsresultat.alle())
+            val kvotevurdering = vurderRettighetstypeOgKvoter(vilkårsresultat2, fiktiveKvoter)
+            val rettighetstypeTidslinje =
+                kvotevurdering
+                    .filter { it.verdi is KvoteOk }
+                    .mapNotNull { it.rettighetsType }.komprimer()
+            OrdinærKvoteVilkår(vilkårsresultat2).vurder(OrdinærKvoteFaktagrunnlag(kvotevurdering, fiktiveKvoter))
+            SykepengeerstatningKvoteVilkår(vilkårsresultat2).vurder(
+                SykepengeerstatningKvoteFaktagrunnlag(
+                    kvotevurdering,
+                    fiktiveKvoter
+                )
+            )
+            
+            // Genrer bare underveisperioder ett år frem i tid
+            val underveisPeriode = Periode(rettighetsperiode.fom, rettighetsperiode.fom.plusYears(1))
+
+            val input1 = tomUnderveisInput(
+                rettighetsperiode = underveisPeriode,
+                vilkårsresultat = vilkårsresultat,
+                opptrappingPerioder = listOf(Periode(søknadsdato.plusYears(2), søknadsdato.plusYears(3))),
+                kvoter = fiktiveKvoter,
+                forenkletKvoteFeature = true,
+                rettighetstypeGrunnlag = null
+            )
+
+            val input2 = tomUnderveisInput(
+                rettighetsperiode = underveisPeriode,
+                vilkårsresultat = vilkårsresultat2,
+                opptrappingPerioder = listOf(Periode(søknadsdato.plusYears(2), søknadsdato.plusYears(3))),
+                kvoter = fiktiveKvoter,
+                forenkletKvoteFeature = true,
+                rettighetstypeGrunnlag = RettighetstypeGrunnlag(rettighetstypeTidslinje)
+            )
+
+            val vurderingTidslinje1 = underveisService.vurderRegler(input1)
+            val vurderingTidslinje2 = underveisService.vurderRegler(input2)
+
+            assertThat(vurderingTidslinje1.segmenter()).isNotEmpty()
+            assertTidslinjeEquals(vurderingTidslinje1.map{it.fårAapEtter}, vurderingTidslinje2.map{it.fårAapEtter})
+        }
+    }
+
+    @Test
+    fun `oppfylt kvotevilkår bruker av kvoten hvis hverdag, ikke oppfylt er avslag på kvote, og ikke relevant bruker ikke av kvoten`() {
+        dataSource.transaction { connection ->
+            val underveisService = UnderveisService(
+                postgresRepositoryRegistry.provider(connection),
+                createGatewayProvider {
+                    register<AlleAvskruddUnleash>()
+                }
+            )
+            val søknadsdato = 1 januar 2024
+
+            val studentOppfylt = Periode(søknadsdato, 7 januar 2024) // Bruker 5 dager av ordinær kvote
+            val speOppfylt =
+                Periode(8 januar 2024, 14 januar 2024) // Bruker 5 dager av sykepengeerstatning-kvote
+            val periodeBistandOppfylt = Periode(15 januar 2024, Tid.MAKS) // Bruker gjenværende dager av ordinær kvote
+
+
+            val periode = Periode(søknadsdato, Tid.MAKS)
+            val vilkårsresultat = genererVilkårsresultat(
+                periode,
+                studentVilkår = Vilkår(
+                    Vilkårtype.STUDENT,
+                    setOf(
+                        Vilkårsperiode(
+                            studentOppfylt,
+                            Utfall.OPPFYLT,
+                            false,
+                            null,
+                            faktagrunnlag = null,
+                            avslagsårsak = null
+                        )
+                    )
+                ),
+                bistandVilkåret = Vilkår(
+                    Vilkårtype.BISTANDSVILKÅRET,
+                    setOf(
+                        Vilkårsperiode(
+                            periodeBistandOppfylt,
+                            Utfall.OPPFYLT,
+                            false,
+                            null,
+                            faktagrunnlag = null,
+                            avslagsårsak = null
+                        )
+                    )
+                ),
+                sykepengeerstatningVilkåret = Vilkår(
+                    Vilkårtype.SYKEPENGEERSTATNING, setOf(
+
+                        Vilkårsperiode(
+                            speOppfylt,
+                            Utfall.OPPFYLT,
+                            false,
+                            null,
+                            faktagrunnlag = null,
+                            avslagsårsak = null
+                        )
+                    )
+                ),
+            )
+
+            val fiktiveKvoter = Kvoter(
+                ordinærkvote = Hverdager(10),
+                sykepengeerstatningkvote = Hverdager(5)
+            )
+
+            // Gjøres i RettighetstypeSteg
+            val kvotevurdering = vurderRettighetstypeOgKvoter(vilkårsresultat, fiktiveKvoter)
+            val rettighetstypeTidslinje =
+                kvotevurdering
+                    .filter { it.verdi is KvoteOk }
+                    .mapNotNull { it.rettighetsType }.komprimer()
+            OrdinærKvoteVilkår(vilkårsresultat).vurder(OrdinærKvoteFaktagrunnlag(kvotevurdering, fiktiveKvoter))
+            SykepengeerstatningKvoteVilkår(vilkårsresultat).vurder(
+                SykepengeerstatningKvoteFaktagrunnlag(
+                    kvotevurdering,
+                    fiktiveKvoter
+                )
+            )
+
+            val underveisPeriode = Periode(periode.fom, periode.fom.plusYears(1))
+            
+            val input = tomUnderveisInput(
+                rettighetsperiode = underveisPeriode,
+                vilkårsresultat = vilkårsresultat,
+                kvoter = fiktiveKvoter,
+                forenkletKvoteFeature = true,
+                rettighetstypeGrunnlag = RettighetstypeGrunnlag(rettighetstypeTidslinje)
+            )
+
+            val vurderingTidslinje = underveisService.vurderRegler(input)
+
+
+            val forventetPeriodeOppfyltOrdinær = Periode(
+                periodeBistandOppfylt.fom,
+                21 januar 2024 // Siste dag med kvote
+            )
+
+            val forventetPeriodeOrdinærKvoteBruktOpp = Periode(
+                22 januar 2024,
+                underveisPeriode.tom
+            )
+
+
+            assertTidslinje(
+                vurderingTidslinje,
+                studentOppfylt to { assertThat(it.fårAapEtter).isEqualTo(RettighetsType.STUDENT) },
+                speOppfylt to { assertThat(it.fårAapEtter).isEqualTo(RettighetsType.SYKEPENGEERSTATNING) },
+                forventetPeriodeOppfyltOrdinær to { assertThat(it.fårAapEtter).isEqualTo(RettighetsType.BISTANDSBEHOV) },
+                forventetPeriodeOrdinærKvoteBruktOpp to { assertThat(it.fårAapEtter).isEqualTo(null) },
+            )
         }
     }
 }
