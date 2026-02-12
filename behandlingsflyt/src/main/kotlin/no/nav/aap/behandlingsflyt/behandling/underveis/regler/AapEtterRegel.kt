f7c00aa6154645b25296d29e1934fcdad6810608
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-02-12 09:26:48 +0100

    Litt mer lesbart

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/underveis/regler/AapEtterRegel.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/underveis/regler/AapEtterRegel.kt
@@ -9,50 +9,52 @@ class AapEtterRegel : UnderveisRegel {
     override fun vurder(input: UnderveisInput, resultat: Tidslinje<Vurdering>): Tidslinje<Vurdering> {
         if (input.forenkletKvoteFeature) {
             if (input.rettighetstypeGrunnlag != null) {
-                val rettighetstypeTidslinje =
-                    input.rettighetstypeGrunnlag.rettighetstypeTidslinje
-                val ordinærKvote = input.vilkårsresultat.finnVilkår(Vilkårtype.ORDINÆR_KVOTE).tidslinje()
-                val speKvote = input.vilkårsresultat.finnVilkår(Vilkårtype.SYKEPENGEERSTATNING_KVOTE).tidslinje()
-                val varighetTidslinje = ordinærKvote.outerJoin(speKvote) { ordinær, spe ->
-
-                    when {
-                        ordinær?.utfall == Utfall.OPPFYLT -> Oppfylt(brukerAvKvoter = setOf(Kvote.ORDINÆR))
-                        spe?.utfall == Utfall.OPPFYLT -> Oppfylt(brukerAvKvoter = setOf(Kvote.SYKEPENGEERSTATNING))
-                        ordinær?.utfall == Utfall.IKKE_OPPFYLT -> Avslag(
-                            brukerAvKvoter = setOf(Kvote.ORDINÆR), avslagsårsaker = setOf(
-                                VarighetVurdering.Avslagsårsak.ORDINÆRKVOTE_BRUKT_OPP
-                            )
-                        )
-
-                        spe?.utfall == Utfall.IKKE_OPPFYLT -> Avslag(
-                            brukerAvKvoter = setOf(Kvote.SYKEPENGEERSTATNING), avslagsårsaker = setOf(
-                                VarighetVurdering.Avslagsårsak.SYKEPENGEERSTATNINGKVOTE_BRUKT_OPP
-                            )
-                        )
-
-                        else -> Oppfylt(brukerAvKvoter = emptySet())
-                    }
-
-                }
+                val rettighetstypeTidslinje = input.rettighetstypeGrunnlag.rettighetstypeTidslinje
+                val varighetTidslinje = byggVarighetstidslinjeFraKvotevilkår(input)
 
                 return resultat
                     .leggTilVurderinger(rettighetstypeTidslinje, Vurdering::leggTilRettighetstype)
                     .leggTilVurderinger(varighetTidslinje, Vurdering::leggTilVarighetVurdering)
 
+            } else {
+                val kvoteOgRettighetstype = vurderRettighetstypeOgKvoter(input.vilkårsresultat, input.kvoter)
+                return resultat
+                    .leggTilVurderinger(
+                        kvoteOgRettighetstype.mapNotNull { it.rettighetsType },
+                        Vurdering::leggTilRettighetstype
+                    )
+                    .leggTilVurderinger(kvoteOgRettighetstype, Vurdering::leggTilVarighetVurdering)
             }
-
-
-            val kvoteOgRettighetstype = vurderRettighetstypeOgKvoter(input.vilkårsresultat, input.kvoter)
-
-            return resultat
-                .leggTilVurderinger(
-                    kvoteOgRettighetstype.mapNotNull { it.rettighetsType },
-                    Vurdering::leggTilRettighetstype
-                )
-                .leggTilVurderinger(kvoteOgRettighetstype, Vurdering::leggTilVarighetVurdering)
         } else {
             val rettighetstypeTidslinje = input.vilkårsresultat.rettighetstypeTidslinje()
             return resultat.leggTilVurderinger(rettighetstypeTidslinje, Vurdering::leggTilRettighetstype)
+        }
+    }
+
+    private fun byggVarighetstidslinjeFraKvotevilkår(input: UnderveisInput): Tidslinje<VarighetVurdering> {
+        val ordinærKvote = input.vilkårsresultat.finnVilkår(Vilkårtype.ORDINÆR_KVOTE).tidslinje()
+        val speKvote = input.vilkårsresultat.finnVilkår(Vilkårtype.SYKEPENGEERSTATNING_KVOTE).tidslinje()
+
+        return ordinærKvote.outerJoin(speKvote) { ordinær, spe ->
+
+            when {
+                ordinær?.utfall == Utfall.OPPFYLT -> Oppfylt(brukerAvKvoter = setOf(Kvote.ORDINÆR))
+                spe?.utfall == Utfall.OPPFYLT -> Oppfylt(brukerAvKvoter = setOf(Kvote.SYKEPENGEERSTATNING))
+                ordinær?.utfall == Utfall.IKKE_OPPFYLT -> Avslag(
+                    brukerAvKvoter = setOf(Kvote.ORDINÆR), avslagsårsaker = setOf(
+                        VarighetVurdering.Avslagsårsak.ORDINÆRKVOTE_BRUKT_OPP
+                    )
+                )
+
+                spe?.utfall == Utfall.IKKE_OPPFYLT -> Avslag(
+                    brukerAvKvoter = setOf(Kvote.SYKEPENGEERSTATNING), avslagsårsaker = setOf(
+                        VarighetVurdering.Avslagsårsak.SYKEPENGEERSTATNINGKVOTE_BRUKT_OPP
+                    )
+                )
+
+                else -> Oppfylt(brukerAvKvoter = emptySet())
+            }
+
         }
     }
 }
