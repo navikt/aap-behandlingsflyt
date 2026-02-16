0677b74469df3a73a3010581f49faebee7ab1662
Matias Vinjevoll <matias.vinjevoll@nav.no>
2026-02-16 12:27:27 +0100

    Utled signaturer med enhet fra oppgave Bak feature-toggle.

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/brev/SignaturService.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/brev/SignaturService.kt
@@ -1,7 +1,9 @@
 package no.nav.aap.behandlingsflyt.behandling.brev
 
+import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.Avklaringsbehov
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovRepository
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.Avklaringsbehovene
+import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.Endring
 import no.nav.aap.behandlingsflyt.behandling.brev.bestilling.Brevbestilling
 import no.nav.aap.behandlingsflyt.behandling.brev.bestilling.Status
 import no.nav.aap.behandlingsflyt.hendelse.oppgavestyring.OppgavestyringGateway
@@ -13,6 +15,7 @@ import no.nav.aap.brev.kontrakt.SignaturGrunnlag
 import no.nav.aap.komponenter.gateway.GatewayProvider
 import no.nav.aap.komponenter.verdityper.Bruker
 import no.nav.aap.lookup.repository.RepositoryProvider
+import no.nav.aap.oppgave.enhet.OppgaveEnhetDto
 import no.nav.aap.tilgang.Rolle
 import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Status as AvklaringsbehovStatus
 import no.nav.aap.brev.kontrakt.Rolle as SignaturRolle
@@ -33,9 +36,18 @@ class SignaturService(
         avklaringsbehovRepository = repositoryProvider.provide()
     )
 
+    data class SignaturGrunnlagV2(
+        val navIdent: String,
+        val enhet: String?,
+        val rolle: SignaturRolle?
+    ) // TODO brev-kontrakt
+
     fun finnSignaturGrunnlag(brevbestilling: Brevbestilling, bruker: Bruker): List<SignaturGrunnlag> {
+        require(brevbestilling.status == Status.FORHÅNDSVISNING_KLAR) {
+            "Kan ikke utlede signaturer på brev i status ${brevbestilling.status}"
+        }
         return if (unleashGateway.isEnabled(BehandlingsflytFeature.SignaturEnhetFraOppgave)) {
-            finnSignaturGrunnlagV2(brevbestilling, bruker).map {
+            finnSignaturGrunnlagV2(brevbestilling, bruker).map {// TODO oppdater etter endring i brev-kontrakt
                 SignaturGrunnlag(it.navIdent, it.rolle)
             }
         } else {
@@ -43,15 +55,7 @@ class SignaturService(
         }
     }
 
-    fun finnSignaturGrunnlagV2(brevbestilling: Brevbestilling, bruker: Bruker): List<SignaturGrunnlag> {
-        TODO("Not yet implemented")
-    }
-
     fun finnSignaturGrunnlagV1(brevbestilling: Brevbestilling, bruker: Bruker): List<SignaturGrunnlag> {
-        require(brevbestilling.status == Status.FORHÅNDSVISNING_KLAR) {
-            "Kan ikke utlede signaturer på brev i status ${brevbestilling.status}"
-        }
-
         return if (brevbestilling.typeBrev.erAutomatiskBrev()) {
             emptyList()
         } else if (brevbestilling.typeBrev.erVedtak()) {
@@ -83,6 +87,16 @@ class SignaturService(
         }
     }
 
+    fun finnSignaturGrunnlagV2(brevbestilling: Brevbestilling, innloggetBruker: Bruker): List<SignaturGrunnlagV2> {
+        return if (brevbestilling.typeBrev.erAutomatiskBrev()) {
+            emptyList()
+        } else if (brevbestilling.typeBrev.erVedtak()) {
+            utledSignaturerForVedtak(brevbestilling, innloggetBruker)
+        } else {
+            listOf(utledSignaturMedInnloggetBruker(brevbestilling, innloggetBruker))
+        }
+    }
+
     private val rolleTilAvklaringsbehov: Map<Rolle, List<Definisjon>> = buildMap {
         put(Rolle.SAKSBEHANDLER_OPPFOLGING, definisjonerSomLøsesAv(Rolle.SAKSBEHANDLER_OPPFOLGING))
         put(Rolle.SAKSBEHANDLER_NASJONAL, definisjonerSomLøsesAv(Rolle.SAKSBEHANDLER_NASJONAL))
@@ -111,6 +125,107 @@ class SignaturService(
         }.maxOrNull()?.let {
             SignaturGrunnlag(it.endretAv, mapRolle(rolle))
         }
+    }
+
+    private fun utledSignaturerForVedtak(
+        brevbestilling: Brevbestilling,
+        innloggetBruker: Bruker
+    ): List<SignaturGrunnlagV2> {
+        val behandling = behandlingRepository.hent(brevbestilling.behandlingId)
+        val oppgaveEnhetListe = oppgavestyringGateway.hentOppgaveEnhet(behandling.referanse).oppgaver
+        val avklaringsbehovene = avklaringsbehovRepository.hentAvklaringsbehovene(brevbestilling.behandlingId)
+
+        return listOfNotNull(
+            utledSignaturV2(Rolle.BESLUTTER, avklaringsbehovene, oppgaveEnhetListe, innloggetBruker),
+            utledSignaturV2(Rolle.SAKSBEHANDLER_NASJONAL, avklaringsbehovene, oppgaveEnhetListe, innloggetBruker),
+            utledSignaturV2(Rolle.KVALITETSSIKRER, avklaringsbehovene, oppgaveEnhetListe, innloggetBruker),
+            utledSignaturV2(Rolle.SAKSBEHANDLER_OPPFOLGING, avklaringsbehovene, oppgaveEnhetListe, innloggetBruker),
+        ).distinctBy { it.navIdent }
+    }
+
+    private fun utledSignaturMedInnloggetBruker(
+        brevbestilling: Brevbestilling,
+        innloggetBruker: Bruker
+    ): SignaturGrunnlagV2 {
+        val behandling = behandlingRepository.hent(brevbestilling.behandlingId)
+        val avklaringsbehovene = avklaringsbehovRepository.hentAvklaringsbehovene(brevbestilling.behandlingId)
+        val avklaringsbehov = avklaringsbehovene.åpne().sistEndret()
+        val enhet = if (avklaringsbehov != null) {
+            val oppgaveEnhetListe = oppgavestyringGateway.hentOppgaveEnhet(behandling.referanse).oppgaver
+            enhetForDefinisjon(avklaringsbehov.definisjon, oppgaveEnhetListe)
+        } else {
+            null
+        }
+        return SignaturGrunnlagV2(innloggetBruker.ident, enhet, null)
+    }
+
+    private fun utledSignaturV2(
+        rolle: Rolle,
+        avklaringsbehovene: Avklaringsbehovene,
+        oppgaveEnhetListe: List<OppgaveEnhetDto>,
+        innloggetBruker: Bruker
+
+    ): SignaturGrunnlagV2? {
+        val definisjoner = rolleTilAvklaringsbehov.getValue(rolle)
+        val avklaringsbehovForRolle = avklaringsbehovene.hentBehovForDefinisjon(definisjoner)
+        val definisjonTilNavIdent = sisteDefinisjonLøstAvPerson(avklaringsbehovForRolle)
+        // hvis ingen avklaringsbehov er løst av rolle tidligere antar vi eventuelt åpent avklaringsbehov skal løses
+        // av innlogget bruker
+            ?: åpenDefinisjonMedInnloggetBruker(avklaringsbehovForRolle, innloggetBruker)
+            ?: return null
+
+        val enhet = enhetForDefinisjon(definisjonTilNavIdent.definisjon, oppgaveEnhetListe)
+
+        return SignaturGrunnlagV2(
+            navIdent = definisjonTilNavIdent.navIdent,
+            enhet = enhet,
+            rolle = mapRolle(rolle)
+        )
+    }
+
+    private data class DefinisjonTilNavIdent(val definisjon: Definisjon, val navIdent: String)
+
+    private fun sisteDefinisjonLøstAvPerson(
+        avklaringsbehov: List<Avklaringsbehov>,
+    ): DefinisjonTilNavIdent? {
+        return avklaringsbehov.mapNotNull { avklaringsbehov ->
+            val endring = sisteEndringAvPerson(avklaringsbehov) ?: return@mapNotNull null
+            endring.tidsstempel to DefinisjonTilNavIdent(
+                definisjon = avklaringsbehov.definisjon,
+                navIdent = endring.endretAv,
+            )
+        }.maxByOrNull { it.first }?.second
+    }
+
+    private fun åpenDefinisjonMedInnloggetBruker(
+        avklaringsbehov: List<Avklaringsbehov>,
+        innloggetBruker: Bruker
+    ): DefinisjonTilNavIdent? {
+        return avklaringsbehov.filter { it.erÅpent() }
+            .sistEndret()
+            ?.let { avklaringsbehov ->
+                DefinisjonTilNavIdent(
+                    definisjon = avklaringsbehov.definisjon,
+                    navIdent = innloggetBruker.ident,
+                )
+            }
+    }
+
+    private fun List<Avklaringsbehov>.sistEndret(): Avklaringsbehov? {
+        return this.maxByOrNull { it.sistEndret() }
+    }
+
+    private fun sisteEndringAvPerson(avklaringsbehov: Avklaringsbehov): Endring? {
+        return avklaringsbehov.historikk
+            .filter { it.endretAv.erNavIdent() && it.status == AvklaringsbehovStatus.AVSLUTTET }
+            .maxOrNull()
+    }
+
+    private fun enhetForDefinisjon(
+        definisjon: Definisjon,
+        oppgaveEnhetListe: List<OppgaveEnhetDto>,
+    ): String? {
+        return oppgaveEnhetListe.find { it.avklaringsbehovKode == definisjon.kode.name }?.enhet
     }
 
     private fun mapRolle(rolle: Rolle): SignaturRolle? {
