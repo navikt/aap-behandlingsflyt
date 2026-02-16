b15cee20afa615e6f1822536aa7546f995e08c8a
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-02-16 07:55:24 +0100

    Periodisert løsning for student

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løser/AvklarStudentLøser.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løser/AvklarStudentLøser.kt
@@ -3,21 +3,17 @@ package no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løser
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovKontekst
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løsning.AvklarStudentEnkelLøsning
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.student.StudentRepository
-import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.student.StudentVurdering
 import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon
-import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingRepository
 import no.nav.aap.behandlingsflyt.sakogbehandling.sak.SakRepository
 import no.nav.aap.lookup.repository.RepositoryProvider
 import kotlin.collections.orEmpty
 
 class AvklarStudentLøser(
-    private val behandlingRepository: BehandlingRepository,
     private val studentRepository: StudentRepository,
     private val sakRepository: SakRepository
 ) : AvklaringsbehovsLøser<AvklarStudentEnkelLøsning> {
 
     constructor(repositoryProvider: RepositoryProvider) : this(
-        behandlingRepository = repositoryProvider.provide(),
         studentRepository = repositoryProvider.provide(),
         sakRepository = repositoryProvider.provide()
     )
@@ -26,34 +22,34 @@ class AvklarStudentLøser(
         kontekst: AvklaringsbehovKontekst,
         løsning: AvklarStudentEnkelLøsning
     ): LøsningsResultat {
-        val løsninger = løsning.løsningerForPerioder ?: listOf(løsning.studentvurdering)
-        
-        if (løsninger.size != 1) {
-            throw IllegalArgumentException("Støtter kun én vurdering per løsning")
-        }
-        
         val sak = sakRepository.hent(kontekst.kontekst.sakId)
+
+        val nyeVurderinger = løsning.løsningerForPerioder?.map {
+            it.tilStudentVurdering(
+                kontekst.bruker,
+                kontekst.behandlingId(),
+            )
+        }?.toSet() ?: setOf(
+            løsning.studentvurdering.tilStudentVurdering(
+                kontekst.bruker,
+                kontekst.behandlingId(),
+                sak.rettighetsperiode.fom
+            )
+        )
+
 
         val forrigeBehandlingId = kontekst.kontekst.forrigeBehandlingId
 
         val forrigeVedtatteGrunnlag = forrigeBehandlingId
             ?.let { studentRepository.hentHvisEksisterer(it) }
-        
+
         val vedtatteVurderinger = forrigeVedtatteGrunnlag?.vurderinger.orEmpty()
 
-        val nyeVurderinger = løsninger.map {
-            it.tilStudentVurdering(
-                kontekst.bruker,
-                kontekst.behandlingId(),
-                sak.rettighetsperiode.fom
-            )
-        }.toSet()
-        
         val nyePlussVedtatte = nyeVurderinger + vedtatteVurderinger
-        
+
         studentRepository.lagre(
             behandlingId = kontekst.behandlingId(),
-            vurderinger = nyeVurderinger // TODO: Lagre nye + vedtatte
+            vurderinger = nyePlussVedtatte
         )
 
         return LøsningsResultat(
--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/saksbehandler/student/StudentVurdering.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/saksbehandler/student/StudentVurdering.kt
@@ -1,5 +1,6 @@
 package no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.student
 
+import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løsning.LøsningForPeriode
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingId
 import no.nav.aap.komponenter.verdityper.Bruker
 import java.time.LocalDate
@@ -25,6 +26,49 @@ data class StudentVurdering(
                 avbruttPgaSykdomEllerSkade == true &&
                 harBehovForBehandling == true &&
                 avbruddMerEnn6Måneder == true
+    }
+}
+
+data class PeriodisertStudentDto(
+    override val fom: LocalDate,
+    override val tom: LocalDate? = null,
+    override val begrunnelse: String,
+    val harAvbruttStudie: Boolean,
+    val godkjentStudieAvLånekassen: Boolean?,
+    val avbruttPgaSykdomEllerSkade: Boolean?,
+    val harBehovForBehandling: Boolean?,
+    val avbruttStudieDato: LocalDate?,
+    val avbruddMerEnn6Måneder: Boolean?,
+) : LøsningForPeriode {
+    fun tilStudentVurdering(bruker: Bruker, vurdertIBehandling: BehandlingId): StudentVurdering {
+        return StudentVurdering(
+            fom = fom,
+            tom = tom,
+            begrunnelse = begrunnelse,
+            harAvbruttStudie = harAvbruttStudie,
+            godkjentStudieAvLånekassen = godkjentStudieAvLånekassen,
+            avbruttPgaSykdomEllerSkade = avbruttPgaSykdomEllerSkade,
+            harBehovForBehandling = harBehovForBehandling,
+            avbruttStudieDato = avbruttStudieDato,
+            avbruddMerEnn6Måneder = avbruddMerEnn6Måneder,
+            vurdertAv = bruker.ident,
+            vurdertTidspunkt = LocalDateTime.now(),
+            vurdertIBehandling = vurdertIBehandling
+        )
+    }
+
+    fun tilGammelDto(): StudentVurderingDTO {
+        return StudentVurderingDTO(
+            fom = fom,
+            tom = tom,
+            begrunnelse = begrunnelse,
+            harAvbruttStudie = harAvbruttStudie,
+            godkjentStudieAvLånekassen = godkjentStudieAvLånekassen,
+            avbruttPgaSykdomEllerSkade = avbruttPgaSykdomEllerSkade,
+            harBehovForBehandling = harBehovForBehandling,
+            avbruttStudieDato = avbruttStudieDato,
+            avbruddMerEnn6Måneder = avbruddMerEnn6Måneder
+        )
     }
 }
 
