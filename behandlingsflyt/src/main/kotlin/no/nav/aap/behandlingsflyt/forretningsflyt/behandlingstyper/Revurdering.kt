c0ba9b1d9201932ac1dc4b9e4d45225999b07691
HenrikBuggeHalvorsen <henrik.bugge.halvorsen@nav.no>
2026-02-18 11:43:26 +0100

    Støtte for "etablering av egen virksomhet" 11-15 (#2105)
    Nytt vilkårskort,  "etablering av egen virksomhet" 11-15

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/behandlingstyper/Revurdering.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/behandlingstyper/Revurdering.kt
@@ -30,6 +30,7 @@ import no.nav.aap.behandlingsflyt.forretningsflyt.steg.BarnetilleggSteg
 import no.nav.aap.behandlingsflyt.forretningsflyt.steg.BeregnTilkjentYtelseSteg
 import no.nav.aap.behandlingsflyt.forretningsflyt.steg.BeregningAvklarFaktaSteg
 import no.nav.aap.behandlingsflyt.forretningsflyt.steg.Effektuer11_7Steg
+import no.nav.aap.behandlingsflyt.forretningsflyt.steg.EtableringEgenVirksomhetSteg
 import no.nav.aap.behandlingsflyt.forretningsflyt.steg.FastsettArbeidsevneSteg
 import no.nav.aap.behandlingsflyt.forretningsflyt.steg.FastsettGrunnlagSteg
 import no.nav.aap.behandlingsflyt.forretningsflyt.steg.FastsettMeldeperiodeSteg
@@ -156,6 +157,13 @@ object Revurdering : BehandlingType {
                     Vurderingsbehov.DØDSFALL_BRUKER,
                     Vurderingsbehov.REVURDER_LOVVALG,
                     Vurderingsbehov.LOVVALG_OG_MEDLEMSKAP,
+                )
+            )
+            .medSteg(
+                steg = EtableringEgenVirksomhetSteg, vurderingsbehovRelevanteForSteg = listOf(
+                    Vurderingsbehov.MOTTATT_SØKNAD,
+                    Vurderingsbehov.HELHETLIG_VURDERING,
+                    Vurderingsbehov.ETABLERING_EGEN_VIRKSOMHET
                 )
             )
             .medSteg(
