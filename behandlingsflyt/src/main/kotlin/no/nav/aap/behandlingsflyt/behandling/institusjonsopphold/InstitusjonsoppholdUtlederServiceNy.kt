baba4ccaf66c8158dd2397efe01bd3d97eca3d03
Hein Haraldsen <hein.haraldsen@bekk.no>
2026-02-18 13:27:15 +0100

    Endrer tomånedersregel til å være på hele oppholdet, ikke bare enkeltperioden (#2181)
    * Fjerner tomånedersregel på instopphold
    
    * Sjekker på oppholdets startdato fremfor periodens startdato

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/institusjonsopphold/InstitusjonsoppholdUtlederServiceNy.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/institusjonsopphold/InstitusjonsoppholdUtlederServiceNy.kt
@@ -352,12 +352,13 @@ class InstitusjonsoppholdUtlederServiceNy(
             oppholdUtenBarnetillegg.segmenter()
                 .filter { segment -> segment.verdi }
                 .filter { segment ->
-                    harOppholdSomVarerMerEnnFireMånederOgErMinstToMånederInnIOppholdet(segment)
+                    harOppholdSomVarerMerEnnFireMånederOgErMinstToMånederInnIOppholdet(segment, oppholdUtenBarnetillegg.minDato())
                 })
     }
 
     private fun harOppholdSomVarerMerEnnFireMånederOgErMinstToMånederInnIOppholdet(
-        segment: Segment<Boolean>
+        segment: Segment<Boolean>,
+        oppholdStartDato: LocalDate
     ): Boolean {
         val fom = segment.fom().withDayOfMonth(1).plusMonths(1)
 
@@ -366,7 +367,7 @@ class InstitusjonsoppholdUtlederServiceNy(
         }
         val førsteDagMedMuligReduksjon = fom.plusMonths(3)
         val justertPeriode = Periode(fom, segment.tom())
-        return justertPeriode.inneholder(førsteDagMedMuligReduksjon) && (fom.plusMonths(2) <= LocalDate.now())
+        return justertPeriode.inneholder(førsteDagMedMuligReduksjon) && (oppholdStartDato.plusMonths(2) <= LocalDate.now())
     }
 
     private fun <T> opprettTidslinje(segmenter: List<Segment<T>>): Tidslinje<Boolean> {
