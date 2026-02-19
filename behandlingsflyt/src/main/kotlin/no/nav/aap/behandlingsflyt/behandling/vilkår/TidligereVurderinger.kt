8c02650e64554727ab0da565a7234dd30660ed0b
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-02-19 09:24:42 +0100

    Legg til sjekker for overgang uføre og overgang arbeid, og forenkle SPE-sjekk

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/vilkår/TidligereVurderinger.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/vilkår/TidligereVurderinger.kt
@@ -118,7 +118,41 @@ class TidligereVurderingerImpl(
             },
 
             Sjekk(StegType.AVKLAR_SYKDOM) { _, kontekst, _ ->
-                sykdomssjekk(kontekst)
+                val periode = kontekst.rettighetsperiode
+                val sykdomstidslinje = sykdomRepository.hentHvisEksisterer(kontekst.behandlingId)
+                    ?.somSykdomsvurderingstidslinje().orEmpty()
+                val studenttidslinje =
+                    studentRepository.hentHvisEksisterer(kontekst.behandlingId)?.somStudenttidslinje(periode.tom)
+                        .orEmpty()
+
+                sykdomstidslinje.outerJoin(studenttidslinje) { segmentPeriode, sykdomsvurdering, studentVurdering ->
+                    if (studentVurdering != null && studentVurdering.erOppfylt()) return@outerJoin TidligereVurderinger.PotensieltOppfylt(
+                        RettighetsType.STUDENT
+                    )
+
+                    val erIkkeFørsteSykdomsvurdering =
+                        !Sykdomsvurdering.erFørsteVurdering(kontekst.rettighetsperiode.fom, segmentPeriode)
+
+                    val harTidligereInnvilgetSykdomsvurdering by lazy {
+                        sykdomstidslinje
+                            .begrensetTil(Periode(Tid.MIN, segmentPeriode.fom.minusDays(1)))
+                            .segmenter()
+                            .any { it.verdi.erOppfyltForYrkesskadeSettBortIfraÅrsakssammenhengOgVissVarighet() }
+                    }
+
+                    val sykdomDefinitivtAvslag =
+                        sykdomsvurdering?.erOppfyltOrdinærSettBortIfraVissVarighet() == false
+                                && !sykdomsvurdering.erOppfyltForYrkesskadeSettBortIfraÅrsakssammenhengOgVissVarighet()
+
+                    val potensieltOvergangArbeid = erIkkeFørsteSykdomsvurdering && harTidligereInnvilgetSykdomsvurdering
+
+                    if (sykdomDefinitivtAvslag && !potensieltOvergangArbeid) {
+                        return@outerJoin TidligereVurderinger.UunngåeligAvslag
+
+                    }
+
+                    return@outerJoin TidligereVurderinger.PotensieltOppfylt(null)
+                }
             },
 
             Sjekk(StegType.VURDER_BISTANDSBEHOV) { _, kontekst, _ ->
@@ -149,77 +183,39 @@ class TidligereVurderingerImpl(
                 }
             },
 
-            Sjekk(StegType.VURDER_SYKEPENGEERSTATNING) { vilkårsresultat, kontekst, _ ->
-
-                val sykdomVurdering = sykdomRepository.hentHvisEksisterer(kontekst.behandlingId)
-                val sykdomstidslinje = sykdomVurdering?.somSykdomsvurderingstidslinje().orEmpty()
-                val yrkesskaderTidslinje =
-                    sykdomVurdering?.yrkesskadevurdringTidslinje(kontekst.rettighetsperiode).orEmpty()
-                val bistandTidslinje =
-                    bistandRepository.hentHvisEksisterer(kontekst.behandlingId)?.somBistandsvurderingstidslinje()
-                        .orEmpty()
-                val overgangUføre1118 = vilkårsresultat.tidslinjeFor(Vilkårtype.OVERGANGUFØREVILKÅRET)
-                val overgangArbeid1117 = vilkårsresultat.tidslinjeFor(Vilkårtype.OVERGANGARBEIDVILKÅRET)
-                val sykdomErstating1113 = vilkårsresultat.tidslinjeFor(Vilkårtype.SYKEPENGEERSTATNING)
-
-                Tidslinje.map6(
-                    sykdomstidslinje,
-                    yrkesskaderTidslinje,
-                    bistandTidslinje,
-                    overgangUføre1118,
-                    overgangArbeid1117,
-                    sykdomErstating1113
-                ) { sykdomVurdering115Segment,
-                    yrkesskaderVudering1122Segment,
-                    bistandsVurdering116Segment,
-                    overgangUføre1118VilkårsSegment,
-                    overgangArbeid1117VilkårSegment,
-                    sykdomErstating1113VilkårSegment ->
-
-                    val sykdomOppfylt = (sykdomVurdering115Segment?.harSkadeSykdomEllerLyte == true
-                            && sykdomVurdering115Segment.erArbeidsevnenNedsatt == true
-                            && (sykdomVurdering115Segment.erNedsettelseIArbeidsevneMerEnnHalvparten == true
-                            || sykdomVurdering115Segment.erNedsettelseIArbeidsevneMerEnnYrkesskadeGrense == true)
-                            && sykdomVurdering115Segment.erSkadeSykdomEllerLyteVesentligdel == true
-                            && (sykdomVurdering115Segment.erNedsettelseIArbeidsevneAvEnVissVarighet == true
-                            || sykdomVurdering115Segment.erNedsettelseIArbeidsevneAvEnVissVarighet == null))
-
-                    val bistandOppfylt = (bistandsVurdering116Segment?.erBehovForAktivBehandling == true
-                            || bistandsVurdering116Segment?.erBehovForArbeidsrettetTiltak == true) ||
-                            (bistandsVurdering116Segment?.erBehovForAnnenOppfølging == true)
-
-                    val førerTilAvslag = when {
-                        // ja 115, nei 116, nei 1118, nei/ikke vudert 1117, nei 1113
-                        sykdomOppfylt
-                                && !bistandOppfylt
-                                && overgangUføre1118VilkårsSegment?.utfall == IKKE_OPPFYLT
-                                && overgangArbeid1117VilkårSegment?.utfall != Utfall.OPPFYLT
-                                && sykdomErstating1113VilkårSegment?.utfall == IKKE_OPPFYLT -> true
-
-                        //nei,vis varigghet
-                        sykdomVurdering115Segment?.harSkadeSykdomEllerLyte == true
-                                && sykdomVurdering115Segment.erArbeidsevnenNedsatt == true
-                                && sykdomVurdering115Segment.erNedsettelseIArbeidsevneMerEnnHalvparten == true
-                                && sykdomVurdering115Segment.erSkadeSykdomEllerLyteVesentligdel == true
-                                && sykdomVurdering115Segment.erNedsettelseIArbeidsevneAvEnVissVarighet == false
-                                && sykdomErstating1113VilkårSegment?.utfall == IKKE_OPPFYLT -> true
-
-                        //YS 11-22 veien til avslag
-                        sykdomOppfylt && sykdomVurdering115Segment.erNedsettelseIArbeidsevneMerEnnHalvparten == false
-                                && sykdomVurdering115Segment.erNedsettelseIArbeidsevneMerEnnYrkesskadeGrense == true
-                                && bistandOppfylt && yrkesskaderTidslinje.filter { it.verdi.erÅrsakssammenheng }
-                            .isEmpty()
-                            -> true
-                        // nei 115, ikke vurdert/ikke oppfylt1117, nei 1113
-                        !sykdomOppfylt
-                                && sykdomErstating1113VilkårSegment?.utfall == IKKE_OPPFYLT
-                                && overgangArbeid1117VilkårSegment?.utfall != Utfall.OPPFYLT -> true
-
-                        else -> false
-                    }
-                    if (førerTilAvslag) TidligereVurderinger.UunngåeligAvslag else TidligereVurderinger.PotensieltOppfylt(
-                        RettighetsType.SYKEPENGEERSTATNING
+            Sjekk(StegType.OVERGANG_UFORE) { vilkårsresultat, _, _ ->
+                vilkårsresultat.tidslinjeFor(Vilkårtype.OVERGANGUFØREVILKÅRET).map {
+                    TidligereVurderinger.PotensieltOppfylt(
+                        when {
+                            it.utfall == Utfall.OPPFYLT -> RettighetsType.VURDERES_FOR_UFØRETRYGD
+                            else -> null
+                        }
                     )
+                }
+            },
+
+            Sjekk(StegType.OVERGANG_ARBEID) { vilkårsresultat, _, _ ->
+                vilkårsresultat.tidslinjeFor(Vilkårtype.OVERGANGARBEIDVILKÅRET).map {
+                    TidligereVurderinger.PotensieltOppfylt(
+                        when {
+                            it.utfall == Utfall.OPPFYLT -> RettighetsType.ARBEIDSSØKER
+                            else -> null
+                        }
+                    )
+                }
+            },
+
+            Sjekk(StegType.VURDER_SYKEPENGEERSTATNING) { vilkårsresultat, _, tidligereVurderinger ->
+                vilkårsresultat.tidslinjeFor(Vilkårtype.SYKEPENGEERSTATNING).leftJoin(tidligereVurderinger) { speVilkår, akkumulertUtfall ->
+                    when {
+                        speVilkår.utfall == Utfall.OPPFYLT -> TidligereVurderinger.PotensieltOppfylt(
+                            RettighetsType.SYKEPENGEERSTATNING
+                        )
+                        // Siste mulige rettighetstype
+                        akkumulertUtfall is TidligereVurderinger.PotensieltOppfylt && akkumulertUtfall.rettighetstype == null -> TidligereVurderinger.UunngåeligAvslag
+
+                        else -> TidligereVurderinger.PotensieltOppfylt(null)
+                    }
                 }
             },
 
@@ -386,43 +382,4 @@ class TidligereVurderingerImpl(
             }
     }
 
-    private fun sykdomssjekk(kontekst: FlytKontekstMedPerioder): Tidslinje<TidligereVurderinger.Behandlingsutfall> {
-        /* TODO: Tror ikke dette er riktig. Sykdomsvilkåret er ikke satt når
-               *   man er i steget VURDER_BiSTANDSBEHOV. */
-        val periode = kontekst.rettighetsperiode
-        val sykdomstidslinje = sykdomRepository.hentHvisEksisterer(kontekst.behandlingId)
-            ?.somSykdomsvurderingstidslinje().orEmpty()
-        val studenttidslinje =
-            studentRepository.hentHvisEksisterer(kontekst.behandlingId)?.somStudenttidslinje(periode.tom)
-                .orEmpty()
-
-        return sykdomstidslinje.outerJoin(studenttidslinje) { segmentPeriode, sykdomsvurdering, studentVurdering ->
-            if (studentVurdering != null && studentVurdering.erOppfylt()) return@outerJoin TidligereVurderinger.PotensieltOppfylt(
-                RettighetsType.STUDENT
-            )
-
-            val erIkkeFørsteSykdomsvurdering =
-                !Sykdomsvurdering.erFørsteVurdering(kontekst.rettighetsperiode.fom, segmentPeriode)
-
-            val harTidligereInnvilgetSykdomsvurdering by lazy {
-                sykdomstidslinje
-                    .begrensetTil(Periode(Tid.MIN, segmentPeriode.fom.minusDays(1)))
-                    .segmenter()
-                    .any { it.verdi.erOppfyltForYrkesskadeSettBortIfraÅrsakssammenhengOgVissVarighet() }
-            }
-
-            val sykdomDefinitivtAvslag =
-                sykdomsvurdering?.erOppfyltOrdinærSettBortIfraVissVarighet() == false
-                        && !sykdomsvurdering.erOppfyltForYrkesskadeSettBortIfraÅrsakssammenhengOgVissVarighet()
-
-            val potensieltOvergangArbeid = erIkkeFørsteSykdomsvurdering && harTidligereInnvilgetSykdomsvurdering
-
-            if (sykdomDefinitivtAvslag && !potensieltOvergangArbeid) {
-                return@outerJoin TidligereVurderinger.UunngåeligAvslag
-
-            }
-
-            return@outerJoin TidligereVurderinger.PotensieltOppfylt(null)
-        }
-    }
 }
