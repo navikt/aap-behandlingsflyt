7c27d2b3cbf28c55116faaa75fcf00a060e4de1b
VHollund <vetle.hollund@nav.no>
2026-02-19 12:26:53 +0100

    prettyfy sql

--- repository/src/main/resources/flyway/V1.272__stans_opphor_grunnlag.sql
+++ repository/src/main/resources/flyway/V1.272__stans_opphor_grunnlag.sql
@@ -1,29 +1,29 @@
 create table stans_opphor_vurderinger
 (
-    id bigserial primary key,
+    id            bigserial primary key,
     opprettet_tid timestamp(3) not null
 );
 
 create table stans_opphor_grunnlag
 (
-    id bigserial primary key,
-    behandling_id bigint not null references behandling(id),
-    vurderinger_id bigint not null references stans_opphor_vurderinger(id),
-    opprettet_tid timestamp(3) not null,
-    aktiv boolean not null
+    id             bigserial primary key,
+    behandling_id  bigint       not null references behandling (id),
+    vurderinger_id bigint       not null references stans_opphor_vurderinger (id),
+    opprettet_tid  timestamp(3) not null,
+    aktiv          boolean      not null
 );
 
 create table stans_opphor_vurdering
 (
-    id bigserial primary key,
-    vurderinger_id bigint not null references stans_opphor_vurderinger(id),
-    opprettet_tid timestamp(3) not null,
+    id                   bigserial primary key,
+    vurderinger_id       bigint       not null references stans_opphor_vurderinger (id),
+    opprettet_tid        timestamp(3) not null,
 
-    vedtaksstatus text not null,
-    fom date not null,
-    vurdert_i_behandling bigint not null references behandling(id),
+    vedtaksstatus        text         not null,
+    fom                  date         not null,
+    vurdert_i_behandling bigint       not null references behandling (id),
 
     -- Kun for 'GJELDENDE'
-    vedtakstype text null check (vedtaksstatus <> 'GJELDENDE' or vedtakstype in ('STANS', 'OPPHØR')),
-    avslagsaarsaker text[] null check (vedtaksstatus <> 'GJELDENDE' or avslagsaarsaker is not null)
+    vedtakstype          text         null check (vedtaksstatus <> 'GJELDENDE' or vedtakstype in ('STANS', 'OPPHØR')),
+    avslagsaarsaker      text[]       null check (vedtaksstatus <> 'GJELDENDE' or avslagsaarsaker is not null)
 );
