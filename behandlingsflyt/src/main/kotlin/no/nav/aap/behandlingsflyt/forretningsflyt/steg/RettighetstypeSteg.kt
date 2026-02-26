0e3095a38663b71fc118c87d545fb2e223cbc611
VHollund <vetle.hollund@nav.no>
2026-02-26 17:25:07 +0100

    Lagre stans opphør i steg (#2244)
    * lagrer ned stans og opphør på førstegangsbehandling eller hvis tidligere grunnlag eksisterer
    
    * lagt til inMemoryStansOpphørRepo
    
    * Registrer StansOpphørRepositoryImpl i registry
    
    * Legg til feature toggle i FakeUnleash
    
    ---------
    
    Co-authored-by: Peter Brottveit Bock <peter.brottveit.bock@nav.no>

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/RettighetstypeSteg.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/RettighetstypeSteg.kt
@@ -1,6 +1,7 @@
 package no.nav.aap.behandlingsflyt.forretningsflyt.steg
 
 import no.nav.aap.behandlingsflyt.behandling.rettighetstype.KvoteOk
+import no.nav.aap.behandlingsflyt.behandling.rettighetstype.utledStansEllerOpphør
 import no.nav.aap.behandlingsflyt.behandling.rettighetstype.vurderRettighetstypeOgKvoter
 import no.nav.aap.behandlingsflyt.behandling.underveis.KvoteService
 import no.nav.aap.behandlingsflyt.behandling.vilkår.kvote.OrdinærKvoteFaktagrunnlag
@@ -9,6 +10,8 @@ import no.nav.aap.behandlingsflyt.behandling.vilkår.kvote.SykepengeerstatningKv
 import no.nav.aap.behandlingsflyt.behandling.vilkår.kvote.SykepengeerstatningKvoteVilkår
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.rettighetstype.RettighetstypeFaktagrunnlag
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.rettighetstype.RettighetstypeRepository
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.stansopphør.StansOpphørGrunnlag
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.stansopphør.StansOpphørRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.ApplikasjonsVersjon
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.VilkårsresultatRepository
 import no.nav.aap.behandlingsflyt.flyt.steg.BehandlingSteg
@@ -26,11 +29,15 @@ import no.nav.aap.lookup.repository.RepositoryProvider
 class RettighetstypeSteg(
     val rettighetstypeRepository: RettighetstypeRepository,
     val vilkårsresultatRepository: VilkårsresultatRepository,
+    val stansOpphørRepository: StansOpphørRepository,
+    val unleashGateway: UnleashGateway,
     val kvoteService: KvoteService,
 ) : BehandlingSteg {
     constructor(repositoryProvider: RepositoryProvider, gatewayProvider: GatewayProvider) : this(
         rettighetstypeRepository = repositoryProvider.provide(),
         vilkårsresultatRepository = repositoryProvider.provide(),
+        stansOpphørRepository = repositoryProvider.provide(),
+        unleashGateway = gatewayProvider.provide(),
         kvoteService = KvoteService(),
     )
 
@@ -72,6 +79,7 @@ class RettighetstypeSteg(
             VurderingType.FØRSTEGANGSBEHANDLING,
             VurderingType.REVURDERING ->
                 lagreStansOgOpphør(kontekst)
+
             else -> {
             }
         }
@@ -80,6 +88,30 @@ class RettighetstypeSteg(
     }
 
     fun lagreStansOgOpphør(kontekst: FlytKontekstMedPerioder) {
+
+        if (unleashGateway.isDisabled(BehandlingsflytFeature.LagreStansOgOpphor)) {
+            return
+        }
+
+        val forrigeGrunnlag = kontekst.forrigeBehandlingId?.let { stansOpphørRepository.hentHvisEksisterer(it) }
+            ?: when (kontekst.vurderingType) {
+                VurderingType.FØRSTEGANGSBEHANDLING -> StansOpphørGrunnlag(emptySet())
+                VurderingType.REVURDERING -> return
+                else -> return
+            }
+
+
+        val stansOpphørGrunnlag = forrigeGrunnlag.utledNyttGrunnlag(
+            utledStansEllerOpphør(
+                vilkårsresultatRepository.hent(kontekst.behandlingId),
+                kvoteService.beregn(),
+                kontekst.rettighetsperiode
+            ),
+                kontekst.behandlingId
+        )
+
+        stansOpphørRepository.lagre(kontekst.behandlingId, stansOpphørGrunnlag)
+
 
     }
 
--- lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/test/FakeUnleash.kt
+++ lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/test/FakeUnleash.kt
@@ -50,6 +50,7 @@ object LokalUnleash : FakeUnleashBase(
         BehandlingsflytFeature.VisIkkeRelevantPeriode to true,
         BehandlingsflytFeature.NyTidligereVurderinger to true,
         BehandlingsflytFeature.MigrerInstitusjonsopphold to true,
+        BehandlingsflytFeature.LagreStansOgOpphor to true,
         )
 )
 
