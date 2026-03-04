f80f5f4657b69ccbdf3e18bffd9b3b3b49863834
Fredrik Meyer <fredrik.meyer@nav.no>
2026-03-04 08:32:28 +0100

    Unngå delete

--- repository/src/main/resources/flyway/V1.276__fjern_periode_vurderingsbehov.sql
+++ repository/src/main/resources/flyway/V1.276__fjern_periode_vurderingsbehov.sql
@@ -2,14 +2,3 @@ DROP index uidx_vurderingsbehov;
 
 ALTER TABLE vurderingsbehov
     drop column periode;
-
-delete
-from vurderingsbehov
-where id in (select id
-             from (select *,
-                          row_number()
-                          over (partition by behandling_id, aarsak order by id desc) as rn
-                   from vurderingsbehov) as vurderingsbehov_rn
-             where rn > 1);
-
-CREATE UNIQUE INDEX UIDX_AARSAK_TIL_BEHANDLING ON vurderingsbehov (BEHANDLING_ID, AARSAK);
