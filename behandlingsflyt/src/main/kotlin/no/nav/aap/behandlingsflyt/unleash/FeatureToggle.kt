18601b3ded0599aa148a029dc580094f92681ffb
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-03-09 09:28:39 +0100

    Periodisk meldekortjobb er ikke lenger nødvendig (#2287)
    * Det sjekkes for ubehandlede meldekort etter iverksettelse av førstegangsbehandlingen

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/unleash/FeatureToggle.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/unleash/FeatureToggle.kt
@@ -14,7 +14,6 @@ enum class BehandlingsflytFeature : FeatureToggle {
     ForlengelseIManuellBehandling,
     InstitusjonsoppholdJobb,
     TrekkSoeknadOpprettetFraLegeerklaering,
-    UbehandledeMeldekortJobb,
     VirksomhetsEtablering,
     KvalitetssikringVed2213,
     tilbakekrevingsOppgaverTilOppgave,
