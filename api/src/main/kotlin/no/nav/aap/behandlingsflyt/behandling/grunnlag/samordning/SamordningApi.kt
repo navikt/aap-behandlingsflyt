29f6596b4652ab580c53b31b3fe621c2d08af0fa
Lillegård <Arild.Lillegard@Lillegards-MacBook-Pro.local>
2026-03-09 10:42:37 +0100

    1850: Liten refaktorering
    Flyttet flere dto-klasser ut i egen fil for Samordning.

--- api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/grunnlag/samordning/SamordningApi.kt
+++ api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/grunnlag/samordning/SamordningApi.kt
@@ -3,19 +3,11 @@ package no.nav.aap.behandlingsflyt.behandling.grunnlag.samordning
 import com.papsign.ktor.openapigen.route.path.normal.NormalOpenAPIRoute
 import com.papsign.ktor.openapigen.route.response.respond
 import com.papsign.ktor.openapigen.route.route
-import no.nav.aap.behandlingsflyt.behandling.andrestatligeytelser.DagpengerPeriodeDto
 import no.nav.aap.behandlingsflyt.behandling.ansattinfo.AnsattInfoService
-import no.nav.aap.behandlingsflyt.behandling.samordning.EndringStatus
 import no.nav.aap.behandlingsflyt.behandling.samordning.SamordningPeriodeSammenligner
-import no.nav.aap.behandlingsflyt.behandling.samordning.Ytelse
-import no.nav.aap.behandlingsflyt.behandling.vurdering.VurdertAvResponse
-import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.andrestatligeytelservurdering.AndreStatligeYtelser
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.andrestatligeytelservurdering.SamordningAndreStatligeYtelserRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.arbeidsgiver.SamordningArbeidsgiverRepository
-import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.tjenestepensjon.TjenestePensjonForhold
-import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.tjenestepensjon.TjenestePensjonOrdning
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.tjenestepensjon.TjenestePensjonRepository
-import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.tjenestepensjon.YtelseTypeCode
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.uførevurdering.SamordningUføreRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.ytelsevurdering.SamordningVurderingRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.ytelsevurdering.SamordningYtelseRepository
@@ -23,7 +15,6 @@ import no.nav.aap.behandlingsflyt.faktagrunnlag.register.dagpenger.DagpengerRepo
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.uføre.UførePeriodeSammenligner
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.uføre.UføreRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.samordning.refusjonskrav.TjenestepensjonRefusjonsKravVurderingRepository
-import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.samordning.refusjonskrav.TjenestepensjonRefusjonskravVurdering
 import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon
 import no.nav.aap.behandlingsflyt.kontrakt.behandling.BehandlingReferanse
 import no.nav.aap.behandlingsflyt.kontrakt.behandling.TypeBehandling
@@ -38,115 +29,7 @@ import no.nav.aap.komponenter.repository.RepositoryRegistry
 import no.nav.aap.komponenter.type.Periode
 import no.nav.aap.tilgang.BehandlingPathParam
 import no.nav.aap.tilgang.getGrunnlag
-import java.time.LocalDate
 import javax.sql.DataSource
-
-/**
- * @param ytelser Hvilke ytelser det er funnet på denne personen.
- * @param vurdering Manuelle vurderinger gjort av saksbehandler for gitte ytelser.
- */
-data class SamordningYtelseVurderingGrunnlagDTO(
-    val harTilgangTilÅSaksbehandle: Boolean,
-    val ytelser: List<SamordningYtelseDTO>,
-    val vurdering: SamordningYtelseVurderingDTO?,
-    val historiskeVurderinger: List<SamordningYtelseVurderingDTO>,
-    val tpYtelser: List<TjenestePensjonForhold>?,
-)
-
-data class SamordningYtelseVurderingDTO(
-    val begrunnelse: String?,
-    val vurderinger: List<SamordningVurderingDTO>,
-    val vurdertAv: VurdertAvResponse?
-)
-
-data class SamordningYtelseDTO(
-    val ytelseType: Ytelse,
-    val periode: Periode,
-    val gradering: Int?,
-    val kronesum: Int?,
-    val kilde: String,
-    val saksRef: String?,
-    val endringStatus: EndringStatus
-)
-
-data class SamordningVurderingDTO(
-    val ytelseType: Ytelse,
-    val periode: Periode,
-    val gradering: Int?,
-    val kronesum: Int?,
-    val manuell: Boolean?
-)
-
-data class SamordningUføreVurderingGrunnlagDTO(
-    val harTilgangTilÅSaksbehandle: Boolean,
-    val vurdering: SamordningUføreVurderingDTO?,
-    val grunnlag: List<SamordningUføreGrunnlagDTO>
-)
-
-/**
- * @param kilde Alltid lik PESYS.
- */
-data class SamordningUføreGrunnlagDTO(
-    val virkningstidspunkt: LocalDate,
-    val uføregrad: Int,
-    val endringStatus: EndringStatus
-) {
-    val kilde = "PESYS"
-}
-
-data class SamordningUføreVurderingDTO(
-    val begrunnelse: String,
-    val vurderingPerioder: List<SamordningUføreVurderingPeriodeDTO>,
-    val vurdertAv: VurdertAvResponse
-)
-
-data class SamordningUføreVurderingPeriodeDTO(
-    val virkningstidspunkt: LocalDate,
-    val uføregradTilSamordning: Int
-)
-
-data class SamordningAndreStatligeYtelserGrunnlagDTO(
-    val harTilgangTilÅSaksbehandle: Boolean,
-    val vurdering: SamordningAndreStatligeYtelserVurderingDTO?,
-    val historiskeVurderinger: List<SamordningAndreStatligeYtelserVurderingDTO>? = emptyList(),
-    val dagpengerPerioder: List<DagpengerPeriodeDto>? = emptyList(),
-)
-
-data class SamordningAndreStatligeYtelserVurderingDTO(
-    val begrunnelse: String,
-    val vurderingPerioder: List<SamordningAndreStatligeYtelserVurderingPeriodeDTO>,
-    val vurdertAv: VurdertAvResponse?
-)
-
-data class SamordningAndreStatligeYtelserVurderingPeriodeDTO(
-    val periode: Periode,
-    val ytelse: AndreStatligeYtelser,
-)
-
-data class SamordningArbeidsgiverGrunnlagDTO(
-    val harTilgangTilÅSaksbehandle: Boolean,
-    val vurdering: SamordningArbeidsgiverVurderingDTO?,
-    val historiskeVurderinger: List<SamordningArbeidsgiverVurderingDTO>? = emptyList(),
-)
-
-data class SamordningArbeidsgiverVurderingDTO(
-    val begrunnelse: String,
-    val perioder: List<Periode>,
-    val vurdertAv: VurdertAvResponse?
-)
-
-data class TjenestepensjonGrunnlagDTO(
-    val harTilgangTilÅSaksbehandle: Boolean,
-    val tjenestepensjonYtelser: List<TjenestepensjonYtelseDTO>,
-    val tjenestepensjonRefusjonskravVurdering: TjenestepensjonRefusjonskravVurdering? = null
-)
-
-data class TjenestepensjonYtelseDTO(
-    val ytelseIverksattFom: LocalDate,
-    val ytelseIverksattTom: LocalDate?,
-    val ytelse: YtelseTypeCode,
-    val ordning: TjenestePensjonOrdning
-)
 
 fun NormalOpenAPIRoute.samordningGrunnlag(
     dataSource: DataSource,
@@ -189,7 +72,7 @@ fun NormalOpenAPIRoute.samordningGrunnlag(
                 )
             }
         }
-        
+
         route("/{referanse}/grunnlag/samordning/tjenestepensjon") {
             getGrunnlag<BehandlingReferanse, TjenestepensjonGrunnlagDTO>(
                 relevanteIdenterResolver = relevanteIdenterForBehandlingResolver(repositoryRegistry, dataSource),
