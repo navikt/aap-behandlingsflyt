72304beddec2f9a090a57615239bdf87a8614de0
HenrikBuggeHalvorsen <henrik.bugge.halvorsen@nav.no>
2026-03-16 11:24:19 +0100

    Kjører ekstra fritaksjobber kun ved tilfeller hvor det er helligdagsunntak (#2344)

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/prosessering/OpprettJobbForFritakMeldepliktJobbUtfører.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/prosessering/OpprettJobbForFritakMeldepliktJobbUtfører.kt
@@ -1,9 +1,8 @@
 package no.nav.aap.behandlingsflyt.prosessering
 
-import no.nav.aap.behandlingsflyt.behandling.underveis.regler.erHelligdagsUnntakPeriode
+import no.nav.aap.behandlingsflyt.behandling.underveis.regler.erHelligdagsUnntak
 import no.nav.aap.behandlingsflyt.sakogbehandling.sak.SakRepository
 import no.nav.aap.komponenter.gateway.GatewayProvider
-import no.nav.aap.komponenter.type.Periode
 import no.nav.aap.lookup.repository.RepositoryProvider
 import no.nav.aap.motor.FlytJobbRepository
 import no.nav.aap.motor.JobbInput
@@ -14,7 +13,6 @@ import org.slf4j.LoggerFactory
 import java.time.Clock
 import java.time.DayOfWeek
 import java.time.LocalDate
-import java.time.temporal.TemporalAdjusters
 
 class OpprettJobbForFritakMeldepliktJobbUtfører(
     private val flytJobbRepository: FlytJobbRepository,
@@ -25,14 +23,13 @@ class OpprettJobbForFritakMeldepliktJobbUtfører(
     private val log = LoggerFactory.getLogger(javaClass)
 
     override fun utfør(input: JobbInput) {
-        // Skal kun kjøres mandager dersom det er et helligdagstilfelle
+        // Skal kun kjøre ekstra dersom det er et helligdagstilfelle
         val idag = LocalDate.now(clock)
-        val mandagInneværendeUke = idag.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY))
-        val søndagInneværendeUke = idag.with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY))
 
-        val helligdagsPeriode = erHelligdagsUnntakPeriode(Periode(mandagInneværendeUke, søndagInneværendeUke))
+        val helligdagsUnntak = erHelligdagsUnntak(idag)
+        val dagerMedEkstraKjøring = setOf(DayOfWeek.MONDAY, DayOfWeek.TUESDAY)
 
-        if (idag.dayOfWeek == DayOfWeek.MONDAY && !helligdagsPeriode) {
+        if (idag.dayOfWeek in (dagerMedEkstraKjøring) && !helligdagsUnntak) {
             return
         }
 
@@ -62,6 +59,6 @@ class OpprettJobbForFritakMeldepliktJobbUtfører(
          * Kjøres på både mandag og onsdag for å fange opp tidlig utbetaling i helligdager. På sikt bør vi få en litt
          * mindre sløsende løsning.
          */
-        override val cron = CronExpression.createWithoutSeconds("10 2 * * 1,3")
+        override val cron = CronExpression.createWithoutSeconds("10 2 * * 1,2,3")
     }
 }
