381fdc3d711b37a4f24d16b1f7ca0f4ed932706b
Charlie Midtlyng <charlie.midtlyng@nav.no>
2026-03-23 13:45:37 +0100

    Skal hente ut opplysninger om uføre-søknad ved uthenting av overgang … (#2375)

--- api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/beregning/grunnlag/sykdom/overgangufore/OvergangUføreGrunnlagResponse.kt
+++ api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/beregning/grunnlag/sykdom/overgangufore/OvergangUføreGrunnlagResponse.kt
@@ -3,7 +3,9 @@ package no.nav.aap.behandlingsflyt.behandling.beregning.grunnlag.sykdom.overgang
 import no.nav.aap.behandlingsflyt.PeriodiserteVurderingerDto
 import no.nav.aap.behandlingsflyt.behandling.beregning.grunnlag.sykdom.sykdom.SykdomsvurderingResponse
 import no.nav.aap.behandlingsflyt.behandling.vurdering.VurdertAvResponse
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.sykdom.flate.InnhentetSykdomsOpplysninger
 import no.nav.aap.komponenter.type.Periode
+import java.time.LocalDate
 
 data class OvergangUføreGrunnlagResponse(
     override val harTilgangTilÅSaksbehandle: Boolean,
@@ -20,4 +22,10 @@ data class OvergangUføreGrunnlagResponse(
     override val behøverVurderinger: List<Periode>,
     val perioderSomIkkeErTilstrekkeligVurdert: List<Periode>,
     val kvalitetssikretAv: VurdertAvResponse?,
+    val uføreSøknadOpplysninger: UføreSøknadOpplysninger? = null,
 ): PeriodiserteVurderingerDto<OvergangUføreVurderingResponse>
+
+
+data class UføreSøknadOpplysninger(
+    val soknadsdato: LocalDate
+)
