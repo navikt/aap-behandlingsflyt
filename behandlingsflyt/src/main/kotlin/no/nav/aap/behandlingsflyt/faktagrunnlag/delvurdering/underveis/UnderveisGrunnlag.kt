273e473b744fdb6cf1fd33aea37f61d136c0393a
Tor Idland <tor.idland@nav.no>
2026-03-23 13:23:53 +0100

    fjernet rettighetsperiodeservice, og funksjoner i underveisgrunnlag som kun er brukt i rettighetApi

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/delvurdering/underveis/UnderveisGrunnlag.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/delvurdering/underveis/UnderveisGrunnlag.kt
@@ -1,15 +1,9 @@
 package no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.underveis
 
-import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Utfall
-import no.nav.aap.behandlingsflyt.behandling.underveis.KvoteService
-import no.nav.aap.behandlingsflyt.behandling.underveis.RettighetsperiodeService
-import no.nav.aap.behandlingsflyt.behandling.underveis.regler.Hverdager
-import no.nav.aap.behandlingsflyt.behandling.underveis.regler.Hverdager.Companion.antallHverdager
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.RettighetsType
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Utfall
 import no.nav.aap.komponenter.tidslinje.Tidslinje
 import no.nav.aap.komponenter.tidslinje.somTidslinje
-import no.nav.aap.komponenter.type.Periode
-import no.nav.aap.komponenter.verdityper.Tid
 import java.time.LocalDate
 
 data class UnderveisGrunnlag(
@@ -28,62 +22,5 @@ data class UnderveisGrunnlag(
 
     fun utledStartdatoForRettighet(rettighetsType: RettighetsType): LocalDate? {
         return utledInnfriddePerioderForRettighet(rettighetsType).firstOrNull()?.periode?.fom
-    }
-
-    fun utledMaksdatoForRettighet(type: RettighetsType, gjeldendeDato: LocalDate): LocalDate? {
-        val gjenværendeKvote = utledKvoterForRettighetstype(type, gjeldendeDato).gjenværendeKvote
-
-        if (gjenværendeKvote == 0) {
-            return utledInnfriddePerioderForRettighet(type).last().periode.tom
-        }
-        return Hverdager(gjenværendeKvote).fraOgMed(gjeldendeDato)
-    }
-
-    fun utledTotalKvote(rettighetstype: RettighetsType, startdatoForRettighet: LocalDate?): Int? {
-        if (arrayOf(RettighetsType.BISTANDSBEHOV, RettighetsType.SYKEPENGEERSTATNING).contains(rettighetstype)) {
-            return KvoteService().beregn().hentKvoteForRettighetstype(rettighetstype)?.asInt
-        }
-
-        val sluttdatoForPerioderettighet =
-            RettighetsperiodeService().utledMaksdatoForRettighet(rettighetstype, startdatoForRettighet)
-
-        return if (startdatoForRettighet != null && sluttdatoForPerioderettighet != null) {
-            Periode(startdatoForRettighet, sluttdatoForPerioderettighet).antallHverdager().asInt
-        } else {
-            null
-        }
-    }
-
-    fun utledKvoterForRettighetstype(rettighetsType: RettighetsType, gjeldendeDato: LocalDate): RettighetKvoter {
-        val perioderForRettighet = utledInnfriddePerioderForRettighet(rettighetsType)
-        val totalKvote = utledTotalKvote(rettighetsType, perioderForRettighet.firstOrNull()?.periode?.fom)
-        val periodeKvoter = perioderForRettighet.map {
-            val bruktKvote = perioderForRettighet
-                .somTidslinje { it.periode }.begrensetTil(Periode(Tid.MIN, it.periode.tom)).segmenter()
-                .sumOf { it.periode.antallHverdager().asInt }
-
-            PeriodeKvote(
-                periode = it.periode,
-                bruktKvote = bruktKvote,
-                gjenværendeKvote = totalKvote?.minus(bruktKvote)
-            )
-        }
-        val senesteInnfriddePeriode = periodeKvoter.lastOrNull { it.periode.fom <= gjeldendeDato }
-        val periodeSluttdato = senesteInnfriddePeriode?.periode?.tom
-        val ubruktKvoteIPeriode =
-            if (periodeSluttdato != null && periodeSluttdato.isAfter(gjeldendeDato)) {
-                Periode(
-                    gjeldendeDato.plusDays(1),
-                    periodeSluttdato
-                ).antallHverdager().asInt
-            } else 0
-        val gjenværendeKvote = senesteInnfriddePeriode?.gjenværendeKvote?.plus(ubruktKvoteIPeriode) ?: totalKvote ?: 0
-
-        return RettighetKvoter(
-            totalKvote = totalKvote,
-            bruktKvote = totalKvote?.minus(gjenværendeKvote) ?: 0,
-            gjenværendeKvote = gjenværendeKvote,
-            periodeKvoter = periodeKvoter
-        )
     }
 }
