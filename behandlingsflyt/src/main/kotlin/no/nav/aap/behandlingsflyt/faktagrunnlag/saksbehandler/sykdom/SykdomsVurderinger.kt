bfeb00bf3f1fbb0f3b27aaf465705672b7b9f325
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-03-26 07:30:08 +0100

    Ny kolonne på sykdomsvurdering: er_nedsettelse_minst_halvparten * Erstatter er_nedsettelse_mer_enn_halvparten + er_nedsettelse_av_en_viss_varighet

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/saksbehandler/sykdom/SykdomsVurderinger.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/saksbehandler/sykdom/SykdomsVurderinger.kt
@@ -147,6 +147,11 @@ data class YrkesskadeSak(
     val manuellYrkesskadeDato: LocalDate?,
 )
 
+enum class ErNedsettelseMinstHalvpartenValg {
+    JA,
+    NEI,
+    FORBIGÅENDE_PROBLEMER
+}
 
 
 
---
+++ repository/src/main/resources/flyway/V1.286__ny_kolonne_er_nedsettelse_minst_halvparten.sql
@@ -0,0 +1,2 @@
+alter table sykdom_vurdering
+    add column er_nedsettelse_minst_halvparten text default null;
