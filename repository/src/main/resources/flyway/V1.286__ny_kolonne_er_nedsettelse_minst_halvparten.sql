7255420db2ebbf67c270e490e584502b3b384b2b
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-03-26 14:47:12 +0100

    Skal også spørre om forbigående problemer ved yrkesskade

--- repository/src/main/resources/flyway/V1.286__ny_kolonne_er_nedsettelse_minst_halvparten.sql
+++ repository/src/main/resources/flyway/V1.286__ny_kolonne_er_nedsettelse_minst_halvparten.sql
@@ -1,2 +1,3 @@
 alter table sykdom_vurdering
-    add column er_nedsettelse_minst_halvparten text default null;
+    add column er_nedsettelse_minst_halvparten text default null,
+    add column er_nedsettelse_mer_enn_yrkesskadegrense text default null;
