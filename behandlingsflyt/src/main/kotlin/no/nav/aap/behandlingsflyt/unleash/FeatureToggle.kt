25cfa10a8a90e030b75cff1cb440f07527fd95e8
SelmaBergstrand <78426277+SelmaBergstrand@users.noreply.github.com>
2026-03-27 08:10:23 +0100

    Fjerner påskrudd featuretoggle for refusjonskrav i revurdering (#2401)

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/unleash/FeatureToggle.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/unleash/FeatureToggle.kt
@@ -20,7 +20,6 @@ enum class BehandlingsflytFeature : FeatureToggle {
     LagreStansOgOpphor,
     MigrerStansOgOpphor,
     SamordningBarnepensjon,
-    RefusjonkravIRevurdering,
     SignaturEnhetFraOppgave,
     VedtakslengdeAvklaringsbehov,
     hentDagpengerPerioder,
