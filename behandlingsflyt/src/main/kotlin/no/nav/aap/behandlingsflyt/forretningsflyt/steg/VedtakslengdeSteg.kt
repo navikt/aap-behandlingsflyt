1a611b341449f69ec40f0cb61744de35d06d04fb
Fredrik Meyer <fredrik.meyer@nav.no>
2026-04-09 10:26:58 +0200

    Fjern fil som ikke skulle commites

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/VedtakslengdeSteg.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/VedtakslengdeSteg.kt
@@ -57,7 +57,7 @@ class VedtakslengdeSteg(
                                     Vurderingsbehov.VEDTAKSLENGDE_MANUELT in kontekst.vurderingsbehovRelevanteForSteg
                             }
                         },
-                        erTilstrekkeligVurdert = { true }, // <-- vil ikke dette lukke behovet igjen med en gang?
+                        erTilstrekkeligVurdert = { true },
                         tilbakestillGrunnlag = { },
                         kontekst = kontekst
                     )
