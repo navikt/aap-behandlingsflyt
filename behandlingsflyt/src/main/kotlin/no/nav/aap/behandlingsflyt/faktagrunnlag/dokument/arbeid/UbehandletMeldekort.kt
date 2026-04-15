14c3688661178f6bb1703915c6bd7598fc17530c
Henrik Gundersen <henrik.gundersen@nav.no>
2026-04-15 14:46:38 +0200

    Håndterer begge varianter

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/dokument/arbeid/UbehandletMeldekort.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/dokument/arbeid/UbehandletMeldekort.kt
@@ -1,6 +1,7 @@
 package no.nav.aap.behandlingsflyt.faktagrunnlag.dokument.arbeid
 
 import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.Meldekort
+import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.MeldekortFraSaksbehandlerV0
 import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.MeldekortV0
 import no.nav.aap.komponenter.type.Periode
 import no.nav.aap.komponenter.verdityper.TimerArbeid
@@ -21,20 +22,22 @@ data class UbehandletMeldekort(
             mottattTidspunkt: LocalDateTime,
             digitalisertAvPostmottak: Boolean?
         ): UbehandletMeldekort {
-            return when (meldekort) {
-                is MeldekortV0 -> UbehandletMeldekort(
-                    journalpostId = journalpostId,
-                    timerArbeidPerPeriode = meldekort.timerArbeidPerPeriode.map {
-                        ArbeidIPeriode(
-                            periode = Periode(it.fraOgMedDato, it.tilOgMedDato),
-                            timerArbeid = TimerArbeid(it.timerArbeid.toBigDecimal())
-                        )
-                    }.toSet(),
-                    mottattTidspunkt = mottattTidspunkt,
-                    harDuArbeidet = meldekort.harDuArbeidet,
-                    digitalisertAvPostmottak = digitalisertAvPostmottak
-                )
+            val (harDuArbeidet, timerArbeidPerPeriode) = when (meldekort) {
+                is MeldekortV0 -> meldekort.harDuArbeidet to meldekort.timerArbeidPerPeriode
+                is MeldekortFraSaksbehandlerV0 -> meldekort.harDuArbeidet to meldekort.timerArbeidPerPeriode
             }
+            return UbehandletMeldekort(
+                journalpostId = journalpostId,
+                timerArbeidPerPeriode = timerArbeidPerPeriode.map {
+                    ArbeidIPeriode(
+                        periode = Periode(it.fraOgMedDato, it.tilOgMedDato),
+                        timerArbeid = TimerArbeid(it.timerArbeid.toBigDecimal())
+                    )
+                }.toSet(),
+                mottattTidspunkt = mottattTidspunkt,
+                harDuArbeidet = harDuArbeidet,
+                digitalisertAvPostmottak = digitalisertAvPostmottak
+            )
         }
     }
 }
