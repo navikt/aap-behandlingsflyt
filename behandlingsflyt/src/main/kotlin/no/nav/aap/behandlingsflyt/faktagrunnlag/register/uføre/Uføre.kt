f6e19d35d3fb9dc21dddc592b2333a9538fca73c
SelmaBergstrand <selma.kristine.bergstrand@nav.no>
2026-04-28 14:22:58 +0200

    lenker til slack-tråd

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/register/uføre/Uføre.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/register/uføre/Uføre.kt
@@ -15,7 +15,6 @@ data class Uføre(
 )
 
 private fun skalSetteSluttdatoPåSegmentPgaReellStans(uføre1: Uføre, uføre2: Uføre): Boolean {
-    // TODO Hvis fom for neste segment ikke finnes kan vi ikke vite om det er stans eller ikke
     if (uføre1.uføregradTom == null || uføre2.uføregradFom == null) return false
     return uføre1.uføregradTom.isBefore(uføre2.uføregradFom.minusDays(1))
 }
@@ -33,6 +32,13 @@ private fun Collection<Uføre>.utledRiktigSluttdatoForSegment(uføre: Uføre): L
     }
 }
 
+
+/**
+ * Utleder en tidslinje av uføregrad basert på en liste av [Uføre]-perioder.
+ * Tidslinjen lages i utgangspunktet fra virkningstidspunkt til virkningstidspunkt
+ * Unntaket er når uføregradTom for en gitt periode _ikke_ er dagen før uføregradFom i neste periode. Da har det vært en reell stans i utbetaling av uføre mellom disse datoene.
+ * Se slack-tråd for mer detaljer: https://nav-it.slack.com/archives/C06NKNY1399/p1776925834329269
+ **/
 fun Collection<Uføre>.tilTidslinje(): Tidslinje<Prosent> {
     return this
         .sortedBy { it.virkningstidspunkt }
