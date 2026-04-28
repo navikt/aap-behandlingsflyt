7eb4adfb927af8ea34eaff74ac44f2d3a9b6618d
Henrik Gundersen <henrik.gundersen@nav.no>
2026-04-28 21:41:23 +0200

    Hent meldekort inkluderer begrunnelse og oppdatertAv  (#2536)
    * Legg til begrunnelse og opprettetAv i meldekort-API
    
    Utvider MeldekortDto med begrunnelse og oppdatertAv-felter, hentet fra
     strukturert dokument (MottattDokumentRepository). Legger til testdata
     i MeldekortApiTest med InMemoryMottattDokumentRepository og verifiserer
     at begrunnelse og oppdatertAv returneres korrekt fra API-et.
    
    * Optimaliser henting av mottatte dokumenter i meldekort-API
    
     Erstatter N separate kall til mottattDokumentRepository.hent() med ett
     samlet kall via hentDokumenterAvType().

--- api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/meldekort/MeldekortApi.kt
+++ api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/meldekort/MeldekortApi.kt
@@ -8,6 +8,7 @@ import no.nav.aap.behandlingsflyt.Tags
 import no.nav.aap.behandlingsflyt.behandling.ansattinfo.AnsattInfoService
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.underveis.UnderveisGrunnlag
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.underveis.UnderveisRepository
+import no.nav.aap.behandlingsflyt.faktagrunnlag.dokument.MottattDokumentRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.dokument.arbeid.Meldekort
 import no.nav.aap.behandlingsflyt.faktagrunnlag.dokument.arbeid.MeldekortRepository
 import no.nav.aap.behandlingsflyt.hendelse.mottak.MottattHendelseService
@@ -63,6 +64,7 @@ fun NormalOpenAPIRoute.meldekortApi(
                 val meldekortRepository = repositoryProvider.provide<MeldekortRepository>()
                 val underveisRepository = repositoryProvider.provide<UnderveisRepository>()
                 val sakRepository = repositoryProvider.provide<SakRepository>()
+                val mottattDokumentRepository = repositoryProvider.provide<MottattDokumentRepository>()
                 val behandlingService = BehandlingService(repositoryProvider, gatewayProvider)
 
                 val sak = sakRepository.hent(Saksnummer(req.saksnummer))
@@ -72,14 +74,28 @@ fun NormalOpenAPIRoute.meldekortApi(
                     val underveisGrunnlag = underveisRepository.hentHvisEksisterer(behandling.id) ?: return@let null
                     val meldeperioder = hentAktuelleMeldeperioder(underveisGrunnlag, clock)
                     val meldekortene = meldekortRepository.hentHvisEksisterer(behandling.id)?.meldekort().orEmpty()
+                    val mottatteDokumenter = mottattDokumentRepository
+                        .hentDokumenterAvType(behandling.id, InnsendingType.MELDEKORT)
+                        .associateBy { it.referanse }
 
                     meldeperioder.map { meldeperiode ->
                         val meldekort = nyesteMeldekortForMeldeperiode(meldekortene, meldeperiode)
 
-                        MeldeperiodeMedMeldekortDto(
-                            meldeperiode = meldeperiode,
-                            meldekort = meldekort?.toDto()
-                        )
+                        if (meldekort != null) {
+                            val innsendingReferanse = InnsendingReferanse(meldekort.journalpostId)
+                            val meldekortData = mottatteDokumenter[innsendingReferanse]
+                                ?.strukturerteData<MeldekortV0>()?.data
+
+                            MeldeperiodeMedMeldekortDto(
+                                meldeperiode = meldeperiode,
+                                meldekort = meldekort.toDto(meldekortData?.begrunnelse, meldekortData?.opprettetAv)
+                            )
+                        } else {
+                            MeldeperiodeMedMeldekortDto(
+                                meldeperiode = meldeperiode,
+                                meldekort = null
+                            )
+                        }
                     }
                 }?.let { MeldeperioderMedMeldekortResponse(it.toSet()) }
             }
