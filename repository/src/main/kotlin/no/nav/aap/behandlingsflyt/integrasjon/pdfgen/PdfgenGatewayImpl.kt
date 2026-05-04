4e391ba4e9de0cba5faf4ecdf47c3ac843414b85
Thomas Rognes <thomas.sebastian.rognes@nav.no>
2026-05-04 13:57:39 +0200

    legg til scope for pdf gen

--- repository/src/main/kotlin/no/nav/aap/behandlingsflyt/integrasjon/pdfgen/PdfgenGatewayImpl.kt
+++ repository/src/main/kotlin/no/nav/aap/behandlingsflyt/integrasjon/pdfgen/PdfgenGatewayImpl.kt
@@ -14,10 +14,12 @@ import java.net.URI
 
 class PdfgenGatewayImpl : PdfgenGateway {
     private val baseUri = URI.create(requiredConfigForKey("integrasjon.pdfgen.url"))
+    private val config = ClientConfig(scope = requiredConfigForKey("integrasjon.pdfgen.scope"))
+
     private val client = RestClient.withDefaultResponseHandler(
-        config = ClientConfig(),
+        config = config,
         tokenProvider = NoTokenTokenProvider(),
-        prometheus = prometheus
+        prometheus = prometheus,
     )
 
     override fun genererMeldekortPdf(request: MeldekortPdfRequest): ByteArray {
