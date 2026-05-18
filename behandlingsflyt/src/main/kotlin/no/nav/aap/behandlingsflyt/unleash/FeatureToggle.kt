4863897e7a4fc8ba3d097db9fb8208a85a52c04d
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2026-05-18 14:02:00 +0200

    Migrer MANGLENDE_DOKUMENTAJON

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/unleash/FeatureToggle.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/unleash/FeatureToggle.kt
@@ -24,6 +24,7 @@ enum class BehandlingsflytFeature : FeatureToggle {
     GrunnbeloepInformasjonskrav,
     GJustering2026,
     BackfillYrkesskadeNyeFelter,
+    MigrerManglendeDokumentasjon,
     ;
 
     override fun key(): String = name
