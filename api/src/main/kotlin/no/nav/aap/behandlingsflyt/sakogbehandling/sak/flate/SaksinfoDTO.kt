9fbc45befbe1f0baa58ddb70699dd2d091908814
Nicolas N. <nicolas.nordhagen@nav.no>
2026-05-21 10:29:20 +0200

    returnere tilstand på sak og deprekere resultat

--- api/src/main/kotlin/no/nav/aap/behandlingsflyt/sakogbehandling/sak/flate/SaksinfoDTO.kt
+++ api/src/main/kotlin/no/nav/aap/behandlingsflyt/sakogbehandling/sak/flate/SaksinfoDTO.kt
@@ -1,5 +1,6 @@
 package no.nav.aap.behandlingsflyt.sakogbehandling.sak.flate
 
+import no.nav.aap.behandlingsflyt.behandling.Resultat
 import no.nav.aap.behandlingsflyt.kontrakt.statistikk.ResultatKode
 import no.nav.aap.komponenter.type.Periode
 import java.time.LocalDateTime
@@ -9,5 +10,20 @@ data class SaksinfoDTO(
     val opprettetTidspunkt: LocalDateTime,
     val periode: Periode,
     val ident: String,
+    val tilstand: SakTilstandDto? = null,
+    @Deprecated("Bruk tilstand i stedet for å utlede sakens tilstand")
     val resultat: ResultatKode? = null
 )
+
+enum class SakTilstandDto {
+    ÅPEN,
+    TRUKKET,
+    ;
+
+    companion object {
+        fun fra(resultat: Resultat?) = when (resultat) {
+            Resultat.TRUKKET -> TRUKKET
+            else -> ÅPEN
+        }
+    }
+}
