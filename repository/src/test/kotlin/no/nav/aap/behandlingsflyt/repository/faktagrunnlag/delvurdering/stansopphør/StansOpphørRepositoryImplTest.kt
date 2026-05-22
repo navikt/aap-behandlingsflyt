1052ab7881841a94274d573478a37c2f50ac6f1c
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2026-05-22 16:38:15 +0200

    Støtte for å lese og lagre stans og opphør V2

--- repository/src/test/kotlin/no/nav/aap/behandlingsflyt/repository/faktagrunnlag/delvurdering/stansopphør/StansOpphørRepositoryImplTest.kt
+++ repository/src/test/kotlin/no/nav/aap/behandlingsflyt/repository/faktagrunnlag/delvurdering/stansopphør/StansOpphørRepositoryImplTest.kt
@@ -1,10 +1,13 @@
 package no.nav.aap.behandlingsflyt.repository.faktagrunnlag.delvurdering.stansopphør
 
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.stansopphør.GjeldendeStansEllerOpphør
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.stansopphør.IkkeStansOpphørVurdering
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.stansopphør.OpphevetStansEllerOpphør
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.stansopphør.Opphør
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.stansopphør.OpphørVurdering
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.stansopphør.Stans
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.stansopphør.StansOpphørGrunnlag
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.stansopphør.StansVurdering
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Avslagsårsak
 import no.nav.aap.behandlingsflyt.help.finnEllerOpprettBehandling
 import no.nav.aap.behandlingsflyt.help.sak
@@ -44,6 +47,18 @@ class StansOpphørRepositoryImplTest {
     }
 
     @Test
+    fun `kan lese og lagre nye felter som null`() {
+        dataSource.transaction { connection ->
+            val behandlingId = finnEllerOpprettBehandling(connection, sak(connection)).id
+            val repo = StansOpphørRepositoryImpl(connection)
+            repo.lagre(behandlingId, StansOpphørGrunnlag())
+            val lagretGrunnlag = repo.hentHvisEksisterer(behandlingId)!!
+            assertThat(lagretGrunnlag.stansOpphørV2).isNull()
+            assertThat(lagretGrunnlag.stansOpphørVurderingerV2).isNull()
+        }
+    }
+
+    @Test
     fun `får tilbake det man skriver`() {
         dataSource.transaction { connection ->
             val s1 = sak(connection)
@@ -60,7 +75,11 @@ class StansOpphørRepositoryImplTest {
                         vurdertIBehandling = b1.id,
                         vurdering = Stans(setOf(Avslagsårsak.BRUDD_PÅ_AKTIVITETSPLIKT_STANS))
                     )
-                )
+                ),
+                stansOpphørV2 = mapOf(
+                    1 januar 2020 to Stans(setOf(Avslagsårsak.BRUDD_PÅ_AKTIVITETSPLIKT_STANS))
+                ),
+                stansOpphørVurderingerV2 = setOf(),
             )
 
             val grunnlag2 = StansOpphørGrunnlag(
@@ -71,20 +90,23 @@ class StansOpphørRepositoryImplTest {
                         vurdertIBehandling = b2.id,
                         vurdering = Opphør(setOf(Avslagsårsak.BRUDD_PÅ_AKTIVITETSPLIKT_OPPHØR))
                     )
-                )
+                ),
+                stansOpphørV2 = mapOf(
+                    2 januar 2020 to Opphør(setOf(Avslagsårsak.BRUDD_PÅ_AKTIVITETSPLIKT_OPPHØR))
+                ),
+                stansOpphørVurderingerV2 = null,
             )
 
-                repo.lagre(b1.id, grunnlag1)
-                repo.lagre(b2.id, grunnlag2)
+            repo.lagre(b1.id, grunnlag1)
+            repo.lagre(b2.id, grunnlag2)
 
-                assertThat(repo.hentHvisEksisterer(b1.id)).isEqualTo(grunnlag1)
-                assertThat(repo.hentHvisEksisterer(b2.id)).isEqualTo(grunnlag2)
-
+            assertThat(repo.hentHvisEksisterer(b1.id)).isEqualTo(grunnlag1)
+            assertThat(repo.hentHvisEksisterer(b2.id)).isEqualTo(grunnlag2)
         }
     }
 
     @Test
-    fun `Overskriver med ny vurdering ved lagring`(){
+    fun `Overskriver med ny vurdering ved lagring`() {
         dataSource.transaction { connection ->
             val s1 = sak(connection)
             val b1 = finnEllerOpprettBehandling(connection, s1)
@@ -98,7 +120,11 @@ class StansOpphørRepositoryImplTest {
                         vurdertIBehandling = b1.id,
                         vurdering = Opphør(setOf(Avslagsårsak.BRUDD_PÅ_AKTIVITETSPLIKT_OPPHØR))
                     )
-                )
+                ),
+                stansOpphørV2 = mapOf(
+                    1 januar 2020 to Opphør(setOf(Avslagsårsak.BRUDD_PÅ_AKTIVITETSPLIKT_OPPHØR))
+                ),
+                stansOpphørVurderingerV2 = null,
             )
 
             repo.lagre(b1.id, gjeldendeOpphør)
@@ -112,7 +138,21 @@ class StansOpphørRepositoryImplTest {
                         opprettet = Instant.now().truncatedTo(ChronoUnit.MILLIS),
                         vurdertIBehandling = b1.id,
                     )
-                )
+                ),
+                stansOpphørV2 = mapOf(),
+                stansOpphørVurderingerV2 = setOf(
+                    StansVurdering(
+                        fom = 2 januar 2020,
+                        vurdertIBehandling = b1.id,
+                        vurdertTidspunkt = Instant.now().truncatedTo(ChronoUnit.MILLIS),
+                        årsaker = setOf(Avslagsårsak.BRUDD_PÅ_AKTIVITETSPLIKT_STANS),
+                    ),
+                    IkkeStansOpphørVurdering(
+                        fom = 2 januar 2020,
+                        vurdertIBehandling = b1.id,
+                        vurdertTidspunkt = Instant.now().plusSeconds(5000).truncatedTo(ChronoUnit.MILLIS),
+                    )
+                ),
             )
 
             repo.lagre(b1.id, opphevet)
@@ -127,7 +167,9 @@ class StansOpphørRepositoryImplTest {
                         vurdertIBehandling = b1.id,
                         vurdering = Stans(setOf(Avslagsårsak.BRUDD_PÅ_AKTIVITETSPLIKT_STANS))
                     )
-                )
+                ),
+                stansOpphørV2 = null,
+                stansOpphørVurderingerV2 = null,
             )
 
             repo.lagre(b1.id, gjeldendeStans)
@@ -135,19 +177,19 @@ class StansOpphørRepositoryImplTest {
             assertThat(repo.hentHvisEksisterer(b1.id)).isEqualTo(gjeldendeStans)
 
             val sammensatt = StansOpphørGrunnlag(
-                gjeldendeStans.stansOgOpphør+opphevet.stansOgOpphør+ gjeldendeOpphør.stansOgOpphør
+                stansOgOpphør = gjeldendeStans.stansOgOpphør + opphevet.stansOgOpphør + gjeldendeOpphør.stansOgOpphør,
+                stansOpphørV2 = null,
+                stansOpphørVurderingerV2 = null,
             )
 
             repo.lagre(b1.id, sammensatt)
 
             assertThat(repo.hentHvisEksisterer(b1.id)).isEqualTo(sammensatt)
-
-
         }
     }
 
     @Test
-    fun `kan slette aktive og ikke aktive grunnlag for behandling`(){
+    fun `kan slette aktive og ikke aktive grunnlag for behandling`() {
         dataSource.transaction { connection ->
             val s1 = sak(connection)
             val s2 = sak(connection)
@@ -155,26 +197,49 @@ class StansOpphørRepositoryImplTest {
             val b2 = finnEllerOpprettBehandling(connection, s2)
             val repo = StansOpphørRepositoryImpl(connection)
 
+            val vurdertTidspunkt = Instant.now().truncatedTo(ChronoUnit.MILLIS)
             val gjeldendeOpphør = StansOpphørGrunnlag(
-                setOf(
+                stansOgOpphør = setOf(
                     GjeldendeStansEllerOpphør(
                         fom = 1 januar 2020,
-                        opprettet = Instant.now().truncatedTo(ChronoUnit.MILLIS).minusSeconds(5000),
+                        opprettet = vurdertTidspunkt.minusSeconds(5000),
                         vurdertIBehandling = b1.id,
                         vurdering = Opphør(setOf(Avslagsårsak.BRUDD_PÅ_AKTIVITETSPLIKT_OPPHØR))
                     )
-                )
+                ),
+                stansOpphørV2 = mapOf(
+                    1 januar 2020 to Opphør(setOf(Avslagsårsak.BRUDD_PÅ_AKTIVITETSPLIKT_OPPHØR))
+                ),
+                stansOpphørVurderingerV2 = setOf(
+                    StansVurdering(
+                        fom = 1 januar 2020,
+                        årsaker = setOf(Avslagsårsak.BRUDD_PÅ_AKTIVITETSPLIKT_OPPHØR),
+                        vurdertIBehandling = b1.id,
+                        vurdertTidspunkt = vurdertTidspunkt.minusSeconds(5000),
+                    )
+                ),
             )
 
             val gjeldendeOpphørNy = StansOpphørGrunnlag(
-                setOf(
+                stansOgOpphør = setOf(
                     GjeldendeStansEllerOpphør(
                         fom = 1 januar 2021,
-                        opprettet = Instant.now().truncatedTo(ChronoUnit.MILLIS),
+                        opprettet = vurdertTidspunkt,
                         vurdertIBehandling = b1.id,
                         vurdering = Opphør(setOf(Avslagsårsak.BRUDD_PÅ_AKTIVITETSPLIKT_OPPHØR))
                     )
-                )
+                ),
+                stansOpphørV2 = mapOf(
+                    1 januar 2021 to Opphør(setOf(Avslagsårsak.BRUDD_PÅ_AKTIVITETSPLIKT_OPPHØR))
+                ),
+                stansOpphørVurderingerV2 = setOf(
+                    OpphørVurdering(
+                        fom = 1 januar 2021,
+                        årsaker = setOf(Avslagsårsak.BRUDD_PÅ_AKTIVITETSPLIKT_OPPHØR),
+                        vurdertIBehandling = b1.id,
+                        vurdertTidspunkt = vurdertTidspunkt,
+                    )
+                ),
             )
 
             repo.lagre(b1.id, gjeldendeOpphør)
