062d6b7fa0e326f6829cf52d967cad58378f4801
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2026-05-28 13:54:53 +0200

    Ikke opprett behandlinger for fastsatt periode som er passert
    Det er per 28. mai 2026 seks aktive utbetalingsplaner som er laget
    med utgangspunkt i 'FREMTIDIG_OPPFYLT', og ingen nye behandlinger
    vil lage utbetalignsplaner basert på 'FREMTIDIG_OPPFYLT'.
    
    Av de seks aktive utbetalingsplanene, så er det ingen av dem som
    har planlagt utbetaling for periodene som er vurdert som
    'FREMTIDIG_OPPFYLT'. Det skal derfor ikke være noen konsekvens
    grunnet 'FREMTIDIG_OPPFYLT' hvis utbetalingsplanen blir prosessert
    på nytt. Sannsynligheten er også lav, siden vi ikke har noen mekansime
    som kjører gamle utbetalingsplaner.

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/prosessering/OpprettBehandlingFastsattPeriodePassertJobbUtfører.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/prosessering/OpprettBehandlingFastsattPeriodePassertJobbUtfører.kt
@@ -1,101 +1,26 @@
 package no.nav.aap.behandlingsflyt.prosessering
 
-import no.nav.aap.behandlingsflyt.behandling.underveis.regler.MeldepliktStatus
-import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingService
-import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.underveis.UnderveisRepository
-import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.VurderingsbehovOgÅrsak
-import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.VurderingsbehovMedPeriode
-import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.ÅrsakTilOpprettelse
-import no.nav.aap.behandlingsflyt.sakogbehandling.flyt.Vurderingsbehov
-import no.nav.aap.behandlingsflyt.sakogbehandling.sak.SakId
-import no.nav.aap.behandlingsflyt.sakogbehandling.sak.SakService
 import no.nav.aap.komponenter.gateway.GatewayProvider
 import no.nav.aap.lookup.repository.RepositoryProvider
 import no.nav.aap.motor.JobbInput
 import no.nav.aap.motor.JobbUtfører
 import no.nav.aap.motor.ProvidersJobbSpesifikasjon
-import org.slf4j.LoggerFactory
-import java.time.LocalDate
-import java.time.LocalDateTime
 
-class OpprettBehandlingFastsattPeriodePassertJobbUtfører(
-    private val sakService: SakService,
-    private val underveisRepository: UnderveisRepository,
-    private val behandlingService: BehandlingService,
-    private val prosesserBehandlingService: ProsesserBehandlingService,
-) : JobbUtfører {
-
-    private val log = LoggerFactory.getLogger(javaClass)
-
+/** Vi antar ikke lenger at meldeplikt kan være oppfylt i fremtiden. Denne jobben
+ *   endret "FREMTIDIG_OPPFYLT" til ikke oppfylt når fristen var passert, for å forhindre
+ *   at det lå planlagte utbetalinger når meldeplikten ikke var oppfylt.
+ **/
+object OpprettBehandlingFastsattPeriodePassertJobbUtfører : JobbUtfører, ProvidersJobbSpesifikasjon {
     override fun utfør(input: JobbInput) {
-        val sak = sakService.hent(SakId(input.sakId()))
-
-        if (!sak.rettighetsperiode.inneholder(LocalDate.now())) {
-            return
-        }
-
-        val sisteBehandling = behandlingService.finnSisteYtelsesbehandlingFor(sak.id) ?: return
-        val sistVedtatteBehandling = behandlingService.finnBehandlingMedSisteFattedeVedtak(sak.id) ?: return
-
-        if (sisteBehandling.status().erÅpen() && Vurderingsbehov.FASTSATT_PERIODE_PASSERT in sisteBehandling.vurderingsbehov()
-                .map { it.type }
-        ) {
-            log.info("Det finnes allerede en kjørende jobb av årsak FASTSATT_PERIODE_PASSERT. Avbryter.")
-            return
-        }
-
-        val underveisperioder = underveisRepository.hentHvisEksisterer(sistVedtatteBehandling.id)
-            ?.perioder
-
-        if (underveisperioder == null) {
-            log.info("Fant ikke underveisperioder for behandling med id ${sistVedtatteBehandling.id}. Avbryter.")
-            return
-        }
-
-        val førsteAntatteMeldeperiode = underveisperioder
-            .filter { it.meldepliktStatus == MeldepliktStatus.FREMTIDIG_OPPFYLT }
-            .minByOrNull { it.meldePeriode }
-
-        if (førsteAntatteMeldeperiode == null) {
-            log.info("Fant ikke førsteAntatteMeldeperiode. Avbryter.")
-            return
-        }
-
-        val tidspunktForKjøringVedManglendeMeldekort =
-            førsteAntatteMeldeperiode.meldePeriode.fom.plusDays(8).atStartOfDay().plusHours(2)
-
-        if (LocalDateTime.now() < tidspunktForKjøringVedManglendeMeldekort) {
-            log.info("Jobben ble kjørt før tidspunktForKjøringVedManglendeMeldekort. Avbryter.")
-            return
-        }
-
-        val fastsattPeriodePassertBehandling = behandlingService.finnEllerOpprettBehandling(
-            sakId = sak.id,
-            vurderingsbehovOgÅrsak = VurderingsbehovOgÅrsak(
-                årsak = ÅrsakTilOpprettelse.FASTSATT_PERIODE_PASSERT,
-                vurderingsbehov = listOf(VurderingsbehovMedPeriode(Vurderingsbehov.FASTSATT_PERIODE_PASSERT))
-            )
-        )
-
-        prosesserBehandlingService.triggProsesserBehandling(fastsattPeriodePassertBehandling)
     }
 
-    companion object : ProvidersJobbSpesifikasjon {
-        override fun konstruer(repositoryProvider: RepositoryProvider, gatewayProvider: GatewayProvider): JobbUtfører {
-            return OpprettBehandlingFastsattPeriodePassertJobbUtfører(
-                sakService = SakService(repositoryProvider, gatewayProvider),
-                underveisRepository = repositoryProvider.provide(),
-                behandlingService = BehandlingService(repositoryProvider, gatewayProvider),
-                prosesserBehandlingService = ProsesserBehandlingService(repositoryProvider, gatewayProvider),
-            )
-        }
+    override fun konstruer(repositoryProvider: RepositoryProvider, gatewayProvider: GatewayProvider): JobbUtfører =
+        OpprettBehandlingFastsattPeriodePassertJobbUtfører
 
-        override val type = "batch.OpprettBehandlingFastsattPeriodePassert"
-        override val navn = "Opprett behandling fordi fastsatt dag er passert"
-        override val beskrivelse =
-            """
+    override val type = "batch.OpprettBehandlingFastsattPeriodePassert"
+    override val navn = "Opprett behandling fordi fastsatt dag er passert"
+    override val beskrivelse = """
                 Starter ny behandling hvis siste behandlig har antatt at meldeplikten er oppfylt, men
                 fastsatt dag er passert, og meldekort nå mangler.
             """.trimIndent()
-    }
 }
--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/prosessering/OpprettJobbForFastsattPeriodeJobbUtfører.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/prosessering/OpprettJobbForFastsattPeriodeJobbUtfører.kt
@@ -1,85 +1,23 @@
 package no.nav.aap.behandlingsflyt.prosessering
 
-import no.nav.aap.behandlingsflyt.sakogbehandling.sak.SakRepository
 import no.nav.aap.komponenter.gateway.GatewayProvider
 import no.nav.aap.lookup.repository.RepositoryProvider
-import no.nav.aap.motor.FlytJobbRepository
 import no.nav.aap.motor.JobbInput
 import no.nav.aap.motor.JobbUtfører
 import no.nav.aap.motor.ProvidersJobbSpesifikasjon
-import no.nav.aap.motor.cron.CronExpression
 
-/** Denne jobben kan snart fjernes.
- * Formålet med jobben, var å stanse planlagte utbetalinger (`FREMTIDIG_OPPFYLT`).
- * Men vi har sluttet å planlegge utbetalinger under antagelse om at meldeplikten vil
- * være oppfylt.
- *
- * PR 19. mai finnes det i underkant av 300 gjeldende behandlinger med `FREMTIDIG_OPPFYLT`
- * i en underveisperiode:
- *
- * | fom | count |
- * | :--- | :--- |
- * | 2026-05-18 | 267 |
- *| 2026-05-25 | 5 |
- *| 2026-06-15 | 1 |
- *| 2026-06-22 | 1 |
- *| 2026-09-14 | 1 |
- *| 2026-12-07 | 1 |
- *| 2027-01-18 | 1 |
- *
- * ```postgres
- * with tidligste_antatte_dato as (select min(lower(underveis_periode.periode)) as fom
- *              from gjeldende_vedtatte_behandlinger
- *                       join underveis_grunnlag
- *                            on underveis_grunnlag.behandling_id = gjeldende_vedtatte_behandlinger.behandling_id
- *                       join underveis_perioder on underveis_grunnlag.perioder_id = underveis_perioder.id
- *                       join underveis_periode on underveis_perioder.id = underveis_periode.perioder_id
- *              where underveis_grunnlag.aktiv
- *                and underveis_periode.utfall = 'OPPFYLT'
- *                and underveis_periode.rettighetstype is not null
- *                and underveis_periode.meldeplikt_status = 'FREMTIDIG_OPPFYLT'
- *              group by gjeldende_vedtatte_behandlinger.behandling_id
- *              )
- *
- * select fom, count(*) from tidligste_antatte_dato group by fom
- * ```
- *
- * Det er natt til tirsdagen 8 dager etter fom-datoen over at denne jobben kjører
- * og vil fjerne `FREMTIDIG_OPPFYLT`.
- *
- * Må undersøke om det er trygt å fjerne denne jobben etter at de 267 behandlingene
- * er prosessert 26. mai 2026.
- */
-class OpprettJobbForFastsattPeriodeJobbUtfører(
-    private val flytJobbRepository: FlytJobbRepository,
-    private val sakRepository: SakRepository,
-) : JobbUtfører {
-
+/** Vi antar ikke lenger at meldeplikt kan være oppfylt i fremtiden. Denne jobben
+ *   endret "FREMTIDIG_OPPFYLT" til ikke oppfylt når fristen var passert, for å forhindre
+ *   at det lå planlagte utbetalinger når meldeplikten ikke var oppfylt.
+ **/
+object OpprettJobbForFastsattPeriodeJobbUtfører : JobbUtfører, ProvidersJobbSpesifikasjon {
     override fun utfør(input: JobbInput) {
-        sakRepository
-            .finnAlleSakIder()
-            .forEach {
-                flytJobbRepository.leggTil(JobbInput(OpprettBehandlingFastsattPeriodePassertJobbUtfører).forSak(it.toLong()))
-            }
     }
 
+    override fun konstruer(repositoryProvider: RepositoryProvider, gatewayProvider: GatewayProvider) =
+        OpprettJobbForFastsattPeriodeJobbUtfører
 
-    companion object : ProvidersJobbSpesifikasjon {
-        override fun konstruer(repositoryProvider: RepositoryProvider, gatewayProvider: GatewayProvider): JobbUtfører {
-            return OpprettJobbForFastsattPeriodeJobbUtfører(
-                flytJobbRepository = repositoryProvider.provide(),
-                sakRepository = repositoryProvider.provide(),
-            )
-        }
-
-        override val type = "batch.OpprettJobbForFastsattPeriode"
-
-        override val navn = "Start jobb for å sjekke behov for revurdering pga manglende meldekort"
-
-        override val beskrivelse = """
-            Start jobb for å sjekke om fastsatt dager er passert.
-            """.trimIndent()
-
-        override val cron = CronExpression.createWithoutSeconds("10 2 * * 2")
-    }
+    override val type = "batch.OpprettJobbForFastsattPeriode"
+    override val navn = "Start jobb for å sjekke behov for revurdering pga manglende meldekort"
+    override val beskrivelse = """Start jobb for å sjekke om fastsatt dager er passert."""
 }
