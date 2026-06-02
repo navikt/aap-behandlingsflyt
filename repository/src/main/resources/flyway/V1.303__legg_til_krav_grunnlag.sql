3c0d023fa0e96eec3f698c2572a472b501cbcc97
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-06-02 08:52:55 +0200

    Tweak indekser

--- repository/src/main/resources/flyway/V1.303__legg_til_krav_grunnlag.sql
+++ repository/src/main/resources/flyway/V1.303__legg_til_krav_grunnlag.sql
@@ -6,24 +6,21 @@ CREATE TABLE krav_vurderinger
 
 CREATE TABLE krav_vurdering
 (
-    id                         BIGSERIAL                              NOT NULL PRIMARY KEY,
-    krav_vurderinger_id        BIGINT                                 NOT NULL REFERENCES krav_vurderinger (id),
-    journalpost_id             TEXT                                   NOT NULL,
-    vurdert_av                 TEXT                                   NOT NULL,
-    vurdert_tidspunkt          TIMESTAMP(3)                           NOT NULL,
-    krav_type                  TEXT                                   NOT NULL,
-    soknadsdato               DATE                                   NULL,
-    soknadsdato_aarsak         TEXT                                   NULL,
-    mulig_rett_fra             DATE                                   NULL,
-    mulig_rett_fra_aarsak      TEXT                                   NULL,
-    begrunnelse                TEXT                                   NOT NULL,
-    kravdato                   DATE                                   NULL,
-    vurdert_i_behandling       BIGINT                                 NOT NULL REFERENCES behandling (id),
-    opprettet_tid              TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP NOT NULL
+    id                    BIGSERIAL                              NOT NULL PRIMARY KEY,
+    krav_vurderinger_id   BIGINT                                 NOT NULL REFERENCES krav_vurderinger (id),
+    vurdert_i_behandling  BIGINT                                 NOT NULL REFERENCES behandling (id),
+    journalpost_id        TEXT                                   NOT NULL,
+    vurdert_av            TEXT                                   NOT NULL,
+    vurdert_tidspunkt     TIMESTAMP(3)                           NOT NULL,
+    begrunnelse           TEXT                                   NOT NULL,
+    opprettet_tid         TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    krav_type             TEXT                                   NOT NULL,
+    kravdato              DATE                                   NULL,
+    soknadsdato           DATE                                   NULL,
+    soknadsdato_aarsak    TEXT                                   NULL,
+    mulig_rett_fra        DATE                                   NULL,
+    mulig_rett_fra_aarsak TEXT                                   NULL
 );
-
-CREATE INDEX idx_krav_vurdering_vurderinger_id ON krav_vurdering (krav_vurderinger_id);
-CREATE INDEX idx_krav_vurdering_vurdert_i_behandling ON krav_vurdering (vurdert_i_behandling);
 
 CREATE TABLE krav_grunnlag
 (
@@ -34,5 +31,9 @@ CREATE TABLE krav_grunnlag
     opprettet_tid       TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
+CREATE INDEX idx_krav_vurdering_vurderinger_id ON krav_vurdering (krav_vurderinger_id);
 CREATE INDEX idx_krav_grunnlag_behandling_id ON krav_grunnlag (behandling_id);
-CREATE INDEX idx_krav_grunnlag_behandling_aktiv ON krav_grunnlag (behandling_id, aktiv);
+
+CREATE UNIQUE INDEX idx_krav_grunnlag_behandling_aktiv
+    on krav_grunnlag (behandling_id)
+    where (aktiv = true);
