4540ee0247dc8c7b20fa5c647fcfad427d3c2fff
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-06-05 07:14:40 +0200

    Fjern flyway-migrering - kjøres separat etter merge

--- repository/src/main/resources/flyway/V1.305__fjern_default_formkrav.sql
+++
@@ -1,5 +0,0 @@
--- Fjerner DEFAULT CURRENT_TIMESTAMP fra formkrav-tabeller.
-ALTER TABLE formkrav_vurdering
-    ALTER COLUMN opprettet_tid DROP DEFAULT;
-ALTER TABLE formkrav_grunnlag
-    ALTER COLUMN opprettet_tid DROP DEFAULT;
