7e68013c42c1634beaffe1ef5c59f774e912fa5b
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-06-05 07:14:41 +0200

    Fjern flyway-migrering - kjøres separat etter merge

--- repository/src/main/resources/flyway/V1.305__fjern_default_bistand.sql
+++
@@ -1,6 +0,0 @@
--- Fjerner DEFAULT CURRENT_TIMESTAMP fra bistand-tabeller.
-ALTER TABLE bistand_vurderinger
-    ALTER COLUMN opprettet_tid SET NOT NULL,
-    ALTER COLUMN opprettet_tid DROP DEFAULT;
-ALTER TABLE bistand_grunnlag
-    ALTER COLUMN opprettet_tid DROP DEFAULT;
