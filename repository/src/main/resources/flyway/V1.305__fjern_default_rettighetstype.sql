3b27e6391586454c5fb443f255aae2b8a7361095
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-06-05 07:14:41 +0200

    Fjern flyway-migrering - kjøres separat etter merge

--- repository/src/main/resources/flyway/V1.305__fjern_default_rettighetstype.sql
+++
@@ -1,4 +0,0 @@
--- Fjerner DEFAULT CURRENT_TIMESTAMP fra rettighetstype_grunnlag.
-ALTER TABLE rettighetstype_grunnlag
-    ALTER COLUMN opprettet_tid TYPE TIMESTAMP(3),
-    ALTER COLUMN opprettet_tid DROP DEFAULT;
