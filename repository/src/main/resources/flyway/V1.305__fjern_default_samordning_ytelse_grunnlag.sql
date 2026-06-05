4fcff8ef7967b6c3f59232c0966addaa376e3d3d
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-06-05 07:14:42 +0200

    Fjern flyway-migrering - kjøres separat etter merge

--- repository/src/main/resources/flyway/V1.305__fjern_default_samordning_ytelse_grunnlag.sql
+++
@@ -1,5 +0,0 @@
--- Fjerner DEFAULT CURRENT_TIMESTAMP fra samordning_ytelse_grunnlag.
-ALTER TABLE samordning_ytelse_grunnlag
-    ALTER COLUMN opprettet TYPE TIMESTAMP(3),
-    ALTER COLUMN opprettet SET NOT NULL,
-    ALTER COLUMN opprettet DROP DEFAULT;
