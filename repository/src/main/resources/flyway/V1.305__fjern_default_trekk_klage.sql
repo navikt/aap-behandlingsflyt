313be6696e90eb8bd5c1a8542025ad2b402b1477
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-06-05 07:14:40 +0200

    Fjern flyway-migrering - kjøres separat etter merge

--- repository/src/main/resources/flyway/V1.305__fjern_default_trekk_klage.sql
+++
@@ -1,5 +0,0 @@
--- Fjerner DEFAULT CURRENT_TIMESTAMP fra trekk_klage-tabeller.
-ALTER TABLE trekk_klage_vurdering
-    ALTER COLUMN opprettet_tid DROP DEFAULT;
-ALTER TABLE trekk_klage_grunnlag
-    ALTER COLUMN opprettet_tid DROP DEFAULT;
