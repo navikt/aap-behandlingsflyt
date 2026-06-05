1f881219a290a9fa50f4ed51e71773b7cce96846
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-06-05 07:14:40 +0200

    Fjern flyway-migrering - kjøres separat etter merge

--- repository/src/main/resources/flyway/V1.305__fjern_default_svar_fra_andreinstans.sql
+++
@@ -1,5 +0,0 @@
--- Fjerner DEFAULT CURRENT_TIMESTAMP fra svar_fra_andreinstans-tabeller.
-ALTER TABLE svar_fra_andreinstans_vurdering
-    ALTER COLUMN opprettet_tid DROP DEFAULT;
-ALTER TABLE svar_fra_andreinstans_grunnlag
-    ALTER COLUMN opprettet_tid DROP DEFAULT;
