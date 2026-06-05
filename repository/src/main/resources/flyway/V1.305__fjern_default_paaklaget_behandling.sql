1330418598f5efa7ea294677a367819c7108b0ac
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-06-05 07:14:40 +0200

    Fjern flyway-migrering - kjøres separat etter merge

--- repository/src/main/resources/flyway/V1.305__fjern_default_paaklaget_behandling.sql
+++
@@ -1,5 +0,0 @@
--- Fjerner DEFAULT CURRENT_TIMESTAMP fra paaklaget_behandling-tabeller.
-ALTER TABLE paaklaget_behandling_vurdering
-    ALTER COLUMN opprettet_tid DROP DEFAULT;
-ALTER TABLE paaklaget_behandling_grunnlag
-    ALTER COLUMN opprettet_tid DROP DEFAULT;
