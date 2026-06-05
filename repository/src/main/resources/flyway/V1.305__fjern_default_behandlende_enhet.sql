d5e6777c3f3283027e1687488e802f33191b9316
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-06-05 07:14:40 +0200

    Fjern flyway-migrering - kjøres separat etter merge

--- repository/src/main/resources/flyway/V1.305__fjern_default_behandlende_enhet.sql
+++
@@ -1,5 +0,0 @@
--- Fjerner DEFAULT CURRENT_TIMESTAMP fra behandlende_enhet_grunnlag.
--- Tidspunktet settes eksplisitt i kode.
-ALTER TABLE behandlende_enhet_grunnlag
-    ALTER COLUMN opprettet_tid DROP DEFAULT;
-
