92d64847659eb16c2511a9464f4e26dcb6d06883
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-06-05 07:14:39 +0200

    Fjern flyway-migrering - kjøres separat etter merge

--- repository/src/main/resources/flyway/V1.305__fjern_default_fullmektig.sql
+++
@@ -1,7 +0,0 @@
--- Fjerner DEFAULT CURRENT_TIMESTAMP fra fullmektig-tabeller.
--- Tidspunktet settes eksplisitt i kode.
-ALTER TABLE fullmektig_vurdering
-    ALTER COLUMN opprettet_tid DROP DEFAULT;
-ALTER TABLE fullmektig_grunnlag
-    ALTER COLUMN opprettet_tid DROP DEFAULT;
-
