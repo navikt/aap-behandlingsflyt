795bb90b4789cd5fbb4ffeca9a801ccca95ba6a2
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-06-05 07:14:41 +0200

    Fjern flyway-migrering - kjøres separat etter merge

--- repository/src/main/resources/flyway/V1.305__fjern_default_aktivitetsplikt_11_9.sql
+++
@@ -1,5 +0,0 @@
--- Fjerner DEFAULT CURRENT_TIMESTAMP fra aktivitetsplikt_11_9-tabeller.
-ALTER TABLE aktivitetsplikt_11_9_vurderinger
-    ALTER COLUMN opprettet_tid DROP DEFAULT;
-ALTER TABLE aktivitetsplikt_11_9_grunnlag
-    ALTER COLUMN opprettet_tid DROP DEFAULT;
