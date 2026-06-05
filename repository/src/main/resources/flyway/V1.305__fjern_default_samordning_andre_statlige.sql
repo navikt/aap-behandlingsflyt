aec95e84c98e19706e931d870c4d449092a2c975
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-06-05 07:14:40 +0200

    Fjern flyway-migrering - kjøres separat etter merge

--- repository/src/main/resources/flyway/V1.305__fjern_default_samordning_andre_statlige.sql
+++
@@ -1,3 +0,0 @@
--- Fjerner DEFAULT CURRENT_TIMESTAMP fra samordning_andre_statlige_ytelser_vurdering.
-ALTER TABLE samordning_andre_statlige_ytelser_vurdering
-    ALTER COLUMN opprettet_tid DROP DEFAULT;
