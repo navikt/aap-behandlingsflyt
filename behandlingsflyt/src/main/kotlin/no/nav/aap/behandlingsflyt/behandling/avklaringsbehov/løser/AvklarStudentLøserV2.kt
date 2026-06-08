c1789311a59da9ebbcc9aa2c509873b988b70741
SelmaBergstrand <selma.kristine.bergstrand@nav.no>
2026-06-08 16:41:12 +0200

    Må bruke oppdaterPeriodisertAvklaringsbehov i nytt studentsteg + validering av perioder i løser

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løser/AvklarStudentLøserV2.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løser/AvklarStudentLøserV2.kt
@@ -2,16 +2,26 @@ package no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løser
 
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovKontekst
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løsning.AvklarStudentLøsningV2
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.student.StudentGrunnlag
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.student.StudentRepository
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.student.StudentVurdering
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.sykdom.SykdomRepository
 import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon
+import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingId
+import no.nav.aap.behandlingsflyt.utils.toHumanReadable
+import no.nav.aap.komponenter.httpklient.exception.UgyldigForespørselException
+import no.nav.aap.komponenter.tidslinje.Tidslinje
+import no.nav.aap.komponenter.tidslinje.orEmpty
 import no.nav.aap.lookup.repository.RepositoryProvider
 
 class AvklarStudentLøserV2(
     private val studentRepository: StudentRepository,
+    private val sykdomRepository: SykdomRepository,
 ) : AvklaringsbehovsLøser<AvklarStudentLøsningV2> {
 
     constructor(repositoryProvider: RepositoryProvider) : this(
         studentRepository = repositoryProvider.provide(),
+        sykdomRepository = repositoryProvider.provide(),
     )
 
     override fun løs(
@@ -37,6 +47,11 @@ class AvklarStudentLøserV2(
         val vedtatteVurderinger = forrigeVedtatteGrunnlag?.vurderinger.orEmpty()
 
         val nyePlussVedtatte = nyeVurderinger + vedtatteVurderinger
+        val nyTidslinje = StudentGrunnlag(
+            vurderinger = nyePlussVedtatte,
+            oppgittStudent = null
+        ).somStudenttidslinje()
+        valider(kontekst.behandlingId(), nyTidslinje)
 
         studentRepository.lagre(
             behandlingId = kontekst.behandlingId(),
@@ -46,6 +61,23 @@ class AvklarStudentLøserV2(
         return LøsningsResultat(
             begrunnelse = nyeVurderinger.joinToString("\n") { it.begrunnelse }
         )
+    }
+
+    private fun valider(behandlingId: BehandlingId, studentTidslinje: Tidslinje<StudentVurdering>) {
+        val sykdomTidslinje = sykdomRepository.hentHvisEksisterer(behandlingId)
+            ?.somSykdomsvurderingstidslinje()
+            .orEmpty()
+
+        val inkonsistentePerioder =
+            Tidslinje.map2(studentTidslinje, sykdomTidslinje) { studentVurdering, sykdomsvurdering ->
+                sykdomsvurdering?.potensieltOppfyltStudent() != true && studentVurdering?.erOppfylt() == true
+            }.perioder().toSet()
+
+        if (inkonsistentePerioder.isNotEmpty()) {
+            throw UgyldigForespørselException(
+                "Vurderingene for ${inkonsistentePerioder.toHumanReadable()} stemmer ikke med periodene i § 11-5." // todo bedre feilmelding
+            )
+        }
     }
 
     override fun forBehov(): Definisjon {
--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/VurderStudentStegV2.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/VurderStudentStegV2.kt
@@ -8,7 +8,10 @@ import no.nav.aap.behandlingsflyt.behandling.vilkår.student.StudentVilkår
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.VilkårsresultatRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Vilkårtype
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.student.StudentRepository
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.student.StudentVurdering
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.sykdom.SykdomRepository
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.sykdom.Sykdomsvurdering
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.sykdom.somSykdomsvurderingTidslinje
 import no.nav.aap.behandlingsflyt.flyt.steg.BehandlingSteg
 import no.nav.aap.behandlingsflyt.flyt.steg.FlytSteg
 import no.nav.aap.behandlingsflyt.flyt.steg.Fullført
@@ -21,6 +24,10 @@ import no.nav.aap.behandlingsflyt.sakogbehandling.flyt.Vurderingsbehov
 import no.nav.aap.behandlingsflyt.unleash.BehandlingsflytFeature
 import no.nav.aap.behandlingsflyt.unleash.UnleashGateway
 import no.nav.aap.komponenter.gateway.GatewayProvider
+import no.nav.aap.komponenter.tidslinje.Tidslinje
+import no.nav.aap.komponenter.tidslinje.orEmpty
+import no.nav.aap.komponenter.tidslinje.tidslinjeOf
+import no.nav.aap.komponenter.type.Periode
 import no.nav.aap.lookup.repository.RepositoryProvider
 
 class VurderStudentStegV2 private constructor(
@@ -41,50 +48,18 @@ class VurderStudentStegV2 private constructor(
     )
 
     override fun utfør(kontekst: FlytKontekstMedPerioder): StegResultat {
-        val studentGrunnlag = studentRepository.hentHvisEksisterer(kontekst.behandlingId)
-        val skalVurdereStudent =
-            sykdomRepository.hent(kontekst.behandlingId).sykdomsvurderinger.last().potensieltOppfyltStudent()
-
-        avklaringsbehovService.oppdaterAvklaringsbehov(
+        avklaringsbehovService.oppdaterAvklaringsbehovForPeriodisertYtelsesvilkårTilstrekkeligVurdert(
             definisjon = Definisjon.AVKLAR_STUDENT_V2,
-            vedtakBehøverVurdering = {
-                when (kontekst.vurderingType) {
-                    VurderingType.FØRSTEGANGSBEHANDLING -> {
-                        unleashGateway.isEnabled(BehandlingsflytFeature.StudentV2) && skalVurdereStudent && tidligereVurderinger.muligMedRettTilAAP(
-                            kontekst,
-                            type()
-                        )
-                    }
-
-                    VurderingType.REVURDERING -> {
-                        unleashGateway.isEnabled(BehandlingsflytFeature.StudentV2) && skalVurdereStudent && tidligereVurderinger.muligMedRettTilAAP(
-                            kontekst,
-                            type()
-                        ) &&
-                                Vurderingsbehov.REVURDER_STUDENT in kontekst.vurderingsbehovRelevanteForSteg
-                    }
-
-                    VurderingType.UTVID_VEDTAKSLENGDE,
-                    VurderingType.MIGRER_RETTIGHETSPERIODE,
-                    VurderingType.MELDEKORT,
-                    VurderingType.AUTOMATISK_BREV,
-                    VurderingType.EFFEKTUER_AKTIVITETSPLIKT,
-                    VurderingType.EFFEKTUER_AKTIVITETSPLIKT_11_9,
-                    VurderingType.G_REGULERING,
-                    VurderingType.IKKE_RELEVANT ->
-                        false
-                }
-            },
-            erTilstrekkeligVurdert = {
-                !studentGrunnlag?.vurderinger.isNullOrEmpty()
-            },
+            tvingerAvklaringsbehov = setOf(Vurderingsbehov.REVURDER_STUDENT), // TODO funker dette?
+            nårVurderingErRelevant = ::nårVurderingErRelevant,
+            kontekst = kontekst,
             tilbakestillGrunnlag = {
                 val vedtatteVurderinger = kontekst.forrigeBehandlingId
                     ?.let { studentRepository.hentHvisEksisterer(it) }
                     ?.vurderinger
                 studentRepository.lagre(kontekst.behandlingId, vedtatteVurderinger)
             },
-            kontekst
+            perioderSomIkkeErTilstrekkeligVurdert = { perioderSomIkkeErTilstrekkeligVurdert(kontekst) },
         )
 
         when (kontekst.vurderingType) {
@@ -105,6 +80,64 @@ class VurderStudentStegV2 private constructor(
 
         return Fullført
 
+    }
+
+    private fun nårVurderingErRelevant(kontekst: FlytKontekstMedPerioder): Tidslinje<Boolean> {
+        val utfall = tidligereVurderinger.behandlingsutfall(kontekst, OvergangUføreSteg.type())
+        val sykdomsvurderinger =
+            sykdomRepository.hentHvisEksisterer(kontekst.behandlingId)?.somSykdomsvurderingstidslinje().orEmpty()
+
+        return Tidslinje.map2(
+            utfall,
+            sykdomsvurderinger
+        ) { _, utfall, sykdomsvurdering ->
+            when (utfall) {
+                TidligereVurderinger.IkkeBehandlingsgrunnlag, TidligereVurderinger.UunngåeligAvslag -> false
+                is TidligereVurderinger.PotensieltOppfylt -> sykdomsvurdering?.potensieltOppfyltStudent() == true && unleashGateway.isEnabled(
+                    BehandlingsflytFeature.StudentV2
+                )
+
+                else -> false
+            }
+        }
+    }
+
+    /**
+     * Det må finnes en vurdering for alle perioder der sykdom er vurdert til potensielt oppfylt student,
+     * og det kan ikke finnes en vurdering som er oppfylt for perioder der sykdomsvurdering ikke er potensielt oppfylt student
+     */
+    private fun perioderSomIkkeErTilstrekkeligVurdert(kontekst: FlytKontekstMedPerioder): Set<Periode> {
+        val studentTidslinje =
+            studentRepository.hentHvisEksisterer(kontekst.behandlingId)?.somStudenttidslinje() ?: tidslinjeOf()
+        val sykdomTidslinje =
+            sykdomRepository.hent(kontekst.behandlingId).sykdomsvurderinger.somSykdomsvurderingTidslinje()
+
+        val nårPåkrevdVurderingMangler =
+            nårVurderingErRelevant(kontekst).leftJoin(studentTidslinje) { erRelevant, studentVurdering ->
+                erRelevant && studentVurdering == null
+            }
+
+        val nårVurderingErKonsistentMedSykdom = nårVurderingErKonsistentMedSykdom(
+            studentTidslinje,
+            sykdomTidslinje
+        )
+
+        return Tidslinje.map2(
+            nårPåkrevdVurderingMangler,
+            nårVurderingErKonsistentMedSykdom
+        ) { vurderingMangler, erKonsistent ->
+            vurderingMangler == true || erKonsistent == false
+        }.komprimer().filter { erUtilstrekkelig -> erUtilstrekkelig.verdi }.perioder().toSet()
+
+    }
+
+    private fun nårVurderingErKonsistentMedSykdom(
+        studentTidslinje: Tidslinje<StudentVurdering>,
+        sykdomstidslinje: Tidslinje<Sykdomsvurdering>
+    ): Tidslinje<Boolean> {
+        return Tidslinje.map2(studentTidslinje, sykdomstidslinje) { studentVurdering, sykdomsvurdering ->
+            !(sykdomsvurdering?.potensieltOppfyltStudent() != true && studentVurdering?.erOppfylt() == true)
+        }
     }
 
     private fun vurderStudentvilkår(kontekst: FlytKontekstMedPerioder) {
