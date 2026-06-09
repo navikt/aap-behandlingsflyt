775b536c0c67cc11ce7c8bda444bc4c6889a30cc
VHollund <vetle.hollund@nav.no>
2026-06-09 13:08:22 +0200

    lagt til ny VurderingsType til ikke relevant i studentV2 steg

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/VurderStudentStegV2.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/VurderStudentStegV2.kt
@@ -73,6 +73,7 @@ class VurderStudentStegV2 private constructor(
             VurderingType.EFFEKTUER_AKTIVITETSPLIKT,
             VurderingType.EFFEKTUER_AKTIVITETSPLIKT_11_9,
             VurderingType.G_REGULERING,
+            VurderingType.OVERGANG_UFORE_STANS,
             VurderingType.IKKE_RELEVANT -> {
                 /* noop */
             }
