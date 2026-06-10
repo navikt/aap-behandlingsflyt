4d1952573412910d2723a0314ff4539334c92b29
SelmaBergstrand <selma.kristine.bergstrand@nav.no>
2026-06-10 14:10:50 +0200

    litt vanskeligere å gjøre feil i DynamiskStegGruppeVisningService

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/flyt/flate/visning/DynamiskStegGruppeVisningService.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/flyt/flate/visning/DynamiskStegGruppeVisningService.kt
@@ -4,23 +4,30 @@ import no.nav.aap.behandlingsflyt.kontrakt.steg.StegGruppe
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingId
 import no.nav.aap.komponenter.gateway.GatewayProvider
 import no.nav.aap.komponenter.repository.RepositoryProvider
+import kotlin.reflect.KClass
 
-class DynamiskStegGruppeVisningService(private val repositoryProvider: RepositoryProvider, private val gatewayProvider: GatewayProvider) {
+class DynamiskStegGruppeVisningService(
+    repositoryProvider: RepositoryProvider,
+    gatewayProvider: GatewayProvider
+) {
 
     private val utledere = mutableMapOf<StegGruppe, StegGruppeVisningUtleder>()
 
+    private val tilgjengeligeArgs: Map<KClass<*>, Any> = mapOf(
+        RepositoryProvider::class to repositoryProvider,
+        GatewayProvider::class to gatewayProvider
+    )
+
     init {
         StegGruppeVisningUtleder::class.sealedSubclasses.forEach { utleder ->
-            val visningUtleder = utleder.constructors
-                .find {
-                    val params = it.parameters
-                    params.size == 2
-                            && params[0].type.classifier == RepositoryProvider::class
-                            && params[1].type.classifier == GatewayProvider::class
-                }?.call(repositoryProvider, gatewayProvider)
-                ?: utleder.constructors
-                    .find { it.parameters.singleOrNull()?.type?.classifier == RepositoryProvider::class }!!
-                    .call(repositoryProvider)
+            val constructor = utleder.constructors
+                .find { c -> c.parameters.all { it.type.classifier in tilgjengeligeArgs } }
+                ?: error("Ingen passende konstruktør funnet for ${utleder.simpleName}")
+
+            val args = constructor.parameters.associateWith {
+                tilgjengeligeArgs[it.type.classifier] ?: error("Parameter må finnes i tilgjengeligeArgs")
+            }
+            val visningUtleder = constructor.callBy(args)
             utledere[visningUtleder.gruppe()] = visningUtleder
         }
     }
