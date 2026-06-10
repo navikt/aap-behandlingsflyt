fcd6118904fa0dae8b25938740c70dadc0131171
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2026-06-10 10:37:51 +0200

    Update behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/utils/FarligMutering.kt
    Co-authored-by: Fredrik Meyer <fredrik.meyer@nav.no>

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/utils/FarligMutering.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/utils/FarligMutering.kt
@@ -3,7 +3,7 @@ package no.nav.aap.behandlingsflyt.utils
 @RequiresOptIn(
     message = """
         Denne metoden muterer et database-objekt. Det er i utgangspunktet kun én service
-        som skal kalle på denne metoden, og services skal da som samtidig muterer domeneobjektet.
+        som skal kalle på denne metoden, og services skal da som samtidig mutere domeneobjektet.
         
         Det kan gi riktig resultat å mutere direkte i et API-endepunkt, men er det verdt risikoen?
         For en annen utvikler flytter kanskje koden uta fra API-et og inn i en metode i en service, og
