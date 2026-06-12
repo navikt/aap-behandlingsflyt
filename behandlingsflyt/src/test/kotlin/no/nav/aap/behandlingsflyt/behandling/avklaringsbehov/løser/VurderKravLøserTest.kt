5f0406cba75ddf01629d196f35416b5eaca6045c
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-06-12 10:15:45 +0200

    Bruk inmemoryrepository i stedet for mockk

--- behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løser/VurderKravLøserTest.kt
+++ behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løser/VurderKravLøserTest.kt
@@ -1,20 +1,9 @@
 package no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løser
 
-import io.mockk.checkUnnecessaryStub
-import io.mockk.clearMocks
-import io.mockk.every
-import io.mockk.just
-import io.mockk.mockk
-import io.mockk.runs
-import io.mockk.slot
-import io.mockk.verify
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovKontekst
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løsning.VurderKravLøsning
 import no.nav.aap.behandlingsflyt.faktagrunnlag.dokument.MottattDokument
-import no.nav.aap.behandlingsflyt.faktagrunnlag.dokument.MottattDokumentRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.krav.GjenopptakKravLøsningDto
-import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.krav.KravRepository
-import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.krav.KravVurdering
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.krav.MuligRettFra
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.krav.MuligRettFraÅrsak
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.krav.NyttKrav
@@ -27,13 +16,14 @@ import no.nav.aap.behandlingsflyt.kontrakt.hendelse.InnsendingType
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingId
 import no.nav.aap.behandlingsflyt.sakogbehandling.flyt.FlytKontekst
 import no.nav.aap.behandlingsflyt.sakogbehandling.sak.SakId
+import no.nav.aap.behandlingsflyt.test.inmemoryrepo.InMemoryKravRepository
+import no.nav.aap.behandlingsflyt.test.inmemoryrepo.InMemoryMottattDokumentRepository
 import no.nav.aap.behandlingsflyt.test.januar
 import no.nav.aap.komponenter.httpklient.exception.UgyldigForespørselException
 import no.nav.aap.komponenter.verdityper.Bruker
 import no.nav.aap.verdityper.dokument.JournalpostId
 import no.nav.aap.verdityper.dokument.Kanal
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import java.time.LocalDateTime
@@ -41,23 +31,13 @@ import kotlin.random.Random
 
 class VurderKravLøserTest {
 
-    private val mottattDokumentRepository = mockk<MottattDokumentRepository>()
-    private val kravRepository = mockk<KravRepository>()
-
-    private val løser = VurderKravLøser(kravRepository, mottattDokumentRepository)
-
-    @AfterEach
-    fun tearDown() {
-        checkUnnecessaryStub(mottattDokumentRepository, kravRepository)
-        clearMocks(mottattDokumentRepository, kravRepository)
-    }
+    private val løser = VurderKravLøser(InMemoryKravRepository, InMemoryMottattDokumentRepository)
 
     @Test
     fun `skal feile hvis søknadsdato er ulik dato for mottatt søknad`() {
         val behandlingId = BehandlingId(Random.nextLong())
-        val søknad = mottattDokument(journalpostId = "1112223", mottattTidspunkt = (15 januar 2026).atStartOfDay())
-        every { mottattDokumentRepository.hentDokumenterAvType(behandlingId, InnsendingType.SØKNAD) } returns setOf(
-            søknad
+        InMemoryMottattDokumentRepository.lagre(
+            mottattDokument(behandlingId, journalpostId = "1112223", mottattTidspunkt = (15 januar 2026).atStartOfDay())
         )
 
         val løsning = VurderKravLøsning(
@@ -79,9 +59,8 @@ class VurderKravLøserTest {
     @Test
     fun `skal feile hvis mulig rett fra dato er etter søknadsdato`() {
         val behandlingId = BehandlingId(Random.nextLong())
-        val søknad = mottattDokument(journalpostId = "1112223", mottattTidspunkt = (15 januar 2026).atStartOfDay())
-        every { mottattDokumentRepository.hentDokumenterAvType(behandlingId, InnsendingType.SØKNAD) } returns setOf(
-            søknad
+        InMemoryMottattDokumentRepository.lagre(
+            mottattDokument(behandlingId, journalpostId = "1112223", mottattTidspunkt = (15 januar 2026).atStartOfDay())
         )
 
         val løsning = VurderKravLøsning(
@@ -106,9 +85,8 @@ class VurderKravLøserTest {
     @Test
     fun `skal feile for kravtyper som ikke er implementert`() {
         val behandlingId = BehandlingId(Random.nextLong())
-        val søknad = mottattDokument(journalpostId = "1112223")
-        every { mottattDokumentRepository.hentDokumenterAvType(behandlingId, InnsendingType.SØKNAD) } returns setOf(
-            søknad
+        InMemoryMottattDokumentRepository.lagre(
+            mottattDokument(behandlingId, journalpostId = "1112223")
         )
 
         val løsning = VurderKravLøsning(
@@ -130,13 +108,9 @@ class VurderKravLøserTest {
     @Test
     fun `skal mappe nytt krav`() {
         val behandlingId = BehandlingId(Random.nextLong())
-        val søknad = mottattDokument(journalpostId = "1112223", mottattTidspunkt = (15 januar 2026).atStartOfDay())
-        every { mottattDokumentRepository.hentDokumenterAvType(behandlingId, InnsendingType.SØKNAD) } returns setOf(
-            søknad
+        InMemoryMottattDokumentRepository.lagre(
+            mottattDokument(behandlingId, journalpostId = "1112223", mottattTidspunkt = (15 januar 2026).atStartOfDay())
         )
-
-        val lagreSlot = slot<Set<KravVurdering>>()
-        every { kravRepository.lagre(behandlingId, capture(lagreSlot)) } just runs
 
         val løsning = VurderKravLøsning(
             krav_vurderinger = setOf(
@@ -151,27 +125,27 @@ class VurderKravLøserTest {
 
         val resultat = løser.løs(kontekst(behandlingId), løsning)
 
-        verify { kravRepository.lagre(behandlingId, any()) }
-        val lagretVurdering = lagreSlot.captured.single() as NyttKrav
+        val lagretVurdering = InMemoryKravRepository.hentHvisEksisterer(behandlingId)!!.vurderinger.single() as NyttKrav
         assertThat(lagretVurdering.journalpostId).isEqualTo(JournalpostId("1112223"))
         assertThat(lagretVurdering.søknadsdato.dato).isEqualTo(15 januar 2026)
         assertThat(lagretVurdering.begrunnelse).isEqualTo("Gyldig krav")
         assertThat(resultat.begrunnelse).isEqualTo("Fullført")
     }
 
-    private fun kontekst(behandlingId: BehandlingId = BehandlingId(Random.nextLong())): AvklaringsbehovKontekst =
+    private fun kontekst(behandlingId: BehandlingId): AvklaringsbehovKontekst =
         AvklaringsbehovKontekst(
             Bruker("Z123456"),
             FlytKontekst(SakId(Random.nextLong()), behandlingId, null, TypeBehandling.Førstegangsbehandling)
         )
 
     private fun mottattDokument(
+        behandlingId: BehandlingId,
         journalpostId: String = "1112223",
         mottattTidspunkt: LocalDateTime = LocalDateTime.now(),
     ) = MottattDokument(
         referanse = InnsendingReferanse(JournalpostId(journalpostId)),
         sakId = SakId(Random.nextLong()),
-        behandlingId = null,
+        behandlingId = behandlingId,
         mottattTidspunkt = mottattTidspunkt,
         type = InnsendingType.SØKNAD,
         kanal = Kanal.DIGITAL,
