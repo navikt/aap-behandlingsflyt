37d8e3d6692bc28daca30d1d3487b46a6fae6d66
Thomas Rognes <thomas.sebastian.rognes@nav.no>
2026-06-16 11:07:42 +0200

    legger til meldeDato i pdf

--- api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/meldekort/MeldekortApi.kt
+++ api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/meldekort/MeldekortApi.kt
@@ -201,7 +201,8 @@ fun NormalOpenAPIRoute.meldekortApi(
                         meldekort = meldekort,
                         oppdatertAv = bruker,
                         enhet = enhet,
-                        tidspunkt = tidspunkt
+                        tidspunkt = tidspunkt,
+                        meldeDato = meldedato,
                     )
 
                     val innsending = tilInnsending(sak, journalpostId, mottattTidspunkt, meldekort)
