916d121463afa25c0ec751f508bcd2148f8d6201
Charlie Midtlyng <charlie.midtlyng@nav.no>
2026-06-16 14:51:19 +0200

    Skal skille mellom nye vurderinger, vedtatte vurderinger og søknader uten vurdering (#2881)

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/saksbehandler/krav/KravGrunnlag.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/saksbehandler/krav/KravGrunnlag.kt
@@ -1,14 +1,11 @@
 package no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.krav
 
-import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingId
-
 data class KravGrunnlag(
     val vurderinger: Set<KravVurdering>,
 ) {
-    fun gjeldendeVedtatteVurderinger(behandlingId: BehandlingId): Set<KravVurdering> {
+    fun gjeldendeVurderinger(): Set<KravVurdering> {
         return vurderinger
-            .filter { it.vurdertIBehandling != behandlingId }
-            .groupBy { it.journalpostId } // TODO: Bytt denne ut med referanse
+            .groupBy { it.referanse }
             .values
             .map { kravForReferanse -> kravForReferanse.maxBy { it.opprettet } }
             .toSet()
