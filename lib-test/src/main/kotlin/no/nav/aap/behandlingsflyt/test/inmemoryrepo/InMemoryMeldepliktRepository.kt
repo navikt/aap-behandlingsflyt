d43deaeff821acb21f5c0d6f07c0b9ab32d5b901
Hein Haraldsen <hein.haraldsen@nav.no>
2026-06-16 10:40:29 +0200

    Renamer test

--- lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/test/inmemoryrepo/InMemoryMeldepliktRepository.kt
+++ lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/test/inmemoryrepo/InMemoryMeldepliktRepository.kt
@@ -4,27 +4,24 @@ import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.meldeplikt.Fritaks
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.meldeplikt.MeldepliktGrunnlag
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.meldeplikt.MeldepliktRepository
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingId
-
 object InMemoryMeldepliktRepository : MeldepliktRepository {
-    override fun hentHvisEksisterer(behandlingId: BehandlingId): MeldepliktGrunnlag? {
-        TODO("Not yet implemented")
-    }
+    private val tomtMeldepliktGrunnlag = mutableMapOf<BehandlingId, MeldepliktGrunnlag>()
+
+    override fun hentHvisEksisterer(behandlingId: BehandlingId): MeldepliktGrunnlag? =
+        tomtMeldepliktGrunnlag[behandlingId]
 
     override fun lagre(
         behandlingId: BehandlingId,
         vurderinger: List<Fritaksvurdering>
     ) {
-        TODO("Not yet implemented")
+        tomtMeldepliktGrunnlag[behandlingId] = MeldepliktGrunnlag(vurderinger)
     }
 
-    override fun kopier(
-        fraBehandling: BehandlingId,
-        tilBehandling: BehandlingId
-    ) {
-        TODO("Not yet implemented")
+    override fun kopier(fraBehandling: BehandlingId, tilBehandling: BehandlingId) {
+        tomtMeldepliktGrunnlag[fraBehandling]?.let { tomtMeldepliktGrunnlag[tilBehandling] = it }
     }
 
     override fun slett(behandlingId: BehandlingId) {
-        TODO("Not yet implemented")
+        tomtMeldepliktGrunnlag.remove(behandlingId)
     }
 }
