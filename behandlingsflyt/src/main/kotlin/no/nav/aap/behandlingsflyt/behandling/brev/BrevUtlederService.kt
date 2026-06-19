560d100fc22c9ae93f69cfb8f412738f8915d0de
Hein Haraldsen <hein.haraldsen@nav.no>
2026-06-19 09:59:50 +0200

    Legger til faktagrunnlag for fritakmeldeplikt

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/brev/BrevUtlederService.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/brev/BrevUtlederService.kt
@@ -41,6 +41,9 @@ import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.beregning.Beregnin
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.beregning.BeregningstidspunktVurdering
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.yrkesskade.YrkesskadeRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.barn.VurderingAvForeldreAnsvar
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.meldeplikt.Fritaksvurdering
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.meldeplikt.MeldepliktGrunnlag
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.meldeplikt.MeldepliktRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.overgangufore.OvergangUføreRepository
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.overgangufore.UføreSøknadVedtakResultat
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.samordning.barnepensjon.BarnepensjonRepository
@@ -103,6 +106,7 @@ class BrevUtlederService(
     private val sykdomRepository: SykdomRepository,
     private val yrkesskadeRepository: YrkesskadeRepository,
     private val barnRepository: BarnRepository,
+    private val meldepliktRepository: MeldepliktRepository
 ) {
     constructor(repositoryProvider: RepositoryProvider, gatewayProvider: GatewayProvider) : this(
         behandlingRepository = repositoryProvider.provide(),
@@ -129,6 +133,7 @@ class BrevUtlederService(
         sykdomRepository = repositoryProvider.provide(),
         yrkesskadeRepository = repositoryProvider.provide(),
         barnRepository = repositoryProvider.provide(),
+        meldepliktRepository = repositoryProvider.provide(),
         avbrytAktivitetspliktbehandlingService = AvbrytAktivitetspliktbehandlingService(repositoryProvider)
     )
 
@@ -394,6 +399,9 @@ class BrevUtlederService(
         } else null
 
         val foreldreAnsvarVurderinger = hentBarnVurderingPerioder(behandling.id)
+
+        val meldepliktGrunnlag = hentMeldepliktGrunnlag(behandling.id)
+
         return Innvilgelse(
             virkningstidspunkt = vedtak.virkningstidspunkt,
             sisteDagMedYtelse = underveisGrunnlag.sisteDagMedYtelse(),
@@ -404,6 +412,7 @@ class BrevUtlederService(
             yrkesskadeBeregning = yrkesskadeBeregning,
             yrkesSkadeISøknadIkkeIRegister = yrkesSkadeISøknadIkkeIRegister,
             foreldreansvarVurderinger = foreldreAnsvarVurderinger,
+            meldepliktGrunnlag = meldepliktGrunnlag
         )
     }
 
@@ -798,5 +807,22 @@ class BrevUtlederService(
                 )
             }
         } ?: emptyList()
+    }
+
+    private fun hentMeldepliktGrunnlag(behandlingId: BehandlingId): MeldepliktGrunnlag {
+        return meldepliktRepository.hentHvisEksisterer(behandlingId)?.let { grunnlag ->
+            MeldepliktGrunnlag(
+                vurderinger = grunnlag.vurderinger.map { vurdering ->
+                    Fritaksvurdering(
+                        harFritak = vurdering.harFritak,
+                        fraDato = vurdering.fraDato,
+                        tilDato = vurdering.tilDato,
+                        begrunnelse = vurdering.begrunnelse,
+                        vurdertAv = vurdering.vurdertAv,
+                        opprettetTid = vurdering.opprettetTid,
+                        vurdertIBehandling = vurdering.vurdertIBehandling,
+                    )
+            })
+        } ?: MeldepliktGrunnlag ( emptyList())
     }
 }
