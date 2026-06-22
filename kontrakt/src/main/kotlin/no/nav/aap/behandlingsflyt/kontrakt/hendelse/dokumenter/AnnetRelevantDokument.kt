7fdd9f1d3f533c76eba33321bfcfb2956fc72d75
Thomas Rognes <thomas.sebastian.rognes@nav.no>
2026-06-22 12:55:49 +0200

    AAP-2030 Legg til kategori i AnnetRelevantDokument i stedet

--- kontrakt/src/main/kotlin/no/nav/aap/behandlingsflyt/kontrakt/hendelse/dokumenter/AnnetRelevantDokument.kt
+++ kontrakt/src/main/kotlin/no/nav/aap/behandlingsflyt/kontrakt/hendelse/dokumenter/AnnetRelevantDokument.kt
@@ -6,6 +6,28 @@ import no.nav.aap.behandlingsflyt.kontrakt.statistikk.Vurderingsbehov
 public sealed interface AnnetRelevantDokument : Melding {
     public val årsakerTilBehandling: List<Vurderingsbehov>
     public val begrunnelse: String?
+    public val kategori: AnnetRelevantDokumentKategori?
+}
+
+public enum class AnnetRelevantDokumentKategori {
+    ARBEIDSUTPROVING,
+    BARNETILLEGG,
+    ETABLERING,
+    INSTITUSJONSOPPHOLD,
+    FENGSEL_VARETEKT,
+    KLAGE,
+    LAERLING,
+    MEDLEMSKAP,
+    PARTSINNSYN,
+    REFUSJONSKRAV,
+    SLUTTAVTALE,
+    STUDENTBESTEMMELSEN,
+    YRKESSKADE,
+    KARAKTERUTSKRIFTER_OG_CV,
+    HELSEOPPLYSNINGER,
+    ETTERSENDELSE_TIL_KLAGE,
+    ETTERSENDELSE_TIL_FEILUTBETALING,
+    TILTAKSRAPPORT
 }
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -13,10 +35,12 @@ public data class AnnetRelevantDokumentV0(
     public override val årsakerTilBehandling: List<Vurderingsbehov>,
 ) : AnnetRelevantDokument {
     public override val begrunnelse: String? = null
+    public override val kategori: AnnetRelevantDokumentKategori? = null
 }
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public data class AnnetRelevantDokumentV1(
     public override val årsakerTilBehandling: List<Vurderingsbehov>,
-    public override val begrunnelse: String
+    public override val begrunnelse: String,
+    public override val kategori: AnnetRelevantDokumentKategori,
 ) : AnnetRelevantDokument
