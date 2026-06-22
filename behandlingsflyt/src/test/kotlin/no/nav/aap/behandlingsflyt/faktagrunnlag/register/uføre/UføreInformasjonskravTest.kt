ca05bbf7d222dcfb9a4afa82bc2b7874c7e0784e
Fredrik Meyer <fredrik.meyer@nav.no>
2026-06-22 08:10:58 +0200

    FAGSYSTEM-434367 Bruk fødselsdato som fradato for uføregateway (#2873)
    * FAGSYSTEM-434367 Bruk fødselsdato som fradato for uføregateway
    
    Dette fordi fra-dato-feltet begrenser for mye.
    
    * Fiks kompileringsfeil
    
    * Ikke utled til tre år før fødselsdato
    
    * Hent fra fødselsdato
    
    * Ikke mockk

--- behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/register/uføre/UføreInformasjonskravTest.kt
+++ behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/register/uføre/UføreInformasjonskravTest.kt
@@ -1,67 +1,53 @@
 package no.nav.aap.behandlingsflyt.faktagrunnlag.register.uføre
 
-import no.nav.aap.behandlingsflyt.behandling.vilkår.TidligereVurderinger
-import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.beregning.BeregningVurderingRepository
 import no.nav.aap.behandlingsflyt.help.flytKontekstMedPerioder
-import no.nav.aap.behandlingsflyt.kontrakt.sak.Saksnummer
+import no.nav.aap.behandlingsflyt.help.opprettInMemorySakOgBehandling
 import no.nav.aap.behandlingsflyt.kontrakt.steg.StegType
-import no.nav.aap.behandlingsflyt.sakogbehandling.Ident
-import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingId
-import no.nav.aap.behandlingsflyt.sakogbehandling.flyt.VurderingType
 import no.nav.aap.behandlingsflyt.sakogbehandling.sak.Person
-import no.nav.aap.behandlingsflyt.sakogbehandling.sak.PersonId
-import no.nav.aap.behandlingsflyt.sakogbehandling.sak.Sak
 import no.nav.aap.behandlingsflyt.sakogbehandling.sak.SakService
-import no.nav.aap.komponenter.type.Periode
+import no.nav.aap.behandlingsflyt.test.FakeTidligereVurderinger
+import no.nav.aap.behandlingsflyt.test.inmemoryrepo.InMemoryBehandlingRepository
+import no.nav.aap.behandlingsflyt.test.inmemoryrepo.InMemoryBeregningVurderingRepository
+import no.nav.aap.behandlingsflyt.test.inmemoryrepo.InMemoryPersonopplysningRepository
+import no.nav.aap.behandlingsflyt.test.inmemoryrepo.InMemorySakRepository
+import no.nav.aap.behandlingsflyt.test.inmemoryrepo.InMemoryUføreRepository
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import io.mockk.every
-import io.mockk.mockk
-import no.nav.aap.behandlingsflyt.sakogbehandling.sak.SakId
 import java.time.LocalDate
-import java.util.UUID
 
 class UføreInformasjonskravTest {
 
-    private val sakService = mockk<SakService>()
-    private val uføreRepository = mockk<UføreRepository>(relaxed = true)
-    private val beregningVurderingRepository = mockk<BeregningVurderingRepository>(relaxed = true)
-    private val uføreRegisterGateway = mockk<UføreRegisterGateway>(relaxed = true)
-    private val tidligereVurderinger = mockk<TidligereVurderinger>(relaxed = true)
+    private val sakService = SakService(InMemorySakRepository, InMemoryBehandlingRepository)
+    private val uføreRepository = InMemoryUføreRepository
+    private val beregningVurderingRepository = InMemoryBeregningVurderingRepository
+    private val uføreRegisterGateway = object : UføreRegisterGateway {
+        override fun innhentMedHistorikk(
+            person: Person,
+            fraDato: LocalDate
+        ): Set<Uføre> = TODO()
+
+        override fun hentÅpenUføreSøknad(person: Person): UføreSøknad = TODO()
+    }
+    private val tidligereVurderinger = FakeTidligereVurderinger()
 
     private val informasjonskrav = UføreInformasjonskrav(
         sakService = sakService,
         uføreRepository = uføreRepository,
         beregningVurderingRepository = beregningVurderingRepository,
+        personopplysningRepository = InMemoryPersonopplysningRepository,
         uføreRegisterGateway = uføreRegisterGateway,
         tidligereVurderinger = tidligereVurderinger,
     )
 
     @Test
     fun `skal være relevant for overgang uføre stans`() {
-        val behandlingId = BehandlingId(1)
+        val (_, behandling) = opprettInMemorySakOgBehandling()
         val kontekst = flytKontekstMedPerioder {
-            this.behandlingId = behandlingId
-            this.vurderingType = VurderingType.OVERGANG_UFORE_STANS
-            this.rettighetsperiode = Periode(LocalDate.now(), LocalDate.now().plusYears(1))
+            this.behandling = behandling
         }
-        every { sakService.hentSakFor(behandlingId) } returns sak()
 
         val resultat = informasjonskrav.erRelevant(kontekst, StegType.OVERGANG_UFORE, null)
 
         assertThat(resultat).isTrue()
-    }
-
-    private fun sak(): Sak {
-        return Sak(
-            id = SakId(1),
-            saksnummer = Saksnummer("1"),
-            person = Person(
-                id = PersonId(1),
-                referanse = UUID.randomUUID(),
-                identer = listOf(Ident("12345678901"))
-            ),
-            rettighetsperiode = Periode(LocalDate.now(), LocalDate.now().plusYears(1)),
-        )
     }
 }
