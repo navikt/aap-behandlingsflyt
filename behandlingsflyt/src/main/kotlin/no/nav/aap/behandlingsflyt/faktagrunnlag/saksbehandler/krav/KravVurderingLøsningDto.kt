ff6cac9b78272996a7660653ffbc7cad2a987608
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-06-22 15:15:46 +0200

    Trenger typeinfo for å kunne deserialisere kravløsning

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/saksbehandler/krav/KravVurderingLøsningDto.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/saksbehandler/krav/KravVurderingLøsningDto.kt
@@ -1,13 +1,21 @@
 package no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.krav
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.fasterxml.jackson.annotation.JsonTypeName
 import no.nav.aap.verdityper.dokument.JournalpostId
 import java.time.LocalDate
 import java.util.UUID
 
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.PROPERTY,
+    property = "kravType",
+)
 sealed class KravVurderingLøsningDto(
-    val kravType: KravType
+    val kravType: KravType,
 )
 
+@JsonTypeName(value = "NYTT_KRAV_AAP")
 data class NyttKravLøsningDto(
     val referanse: UUID? = null,
     val journalpostId: JournalpostId,
@@ -23,6 +31,7 @@ data class NyttKravLøsningDto(
     }
 }
 
+@JsonTypeName(value = "GJENOPPTAK")
 data class GjenopptakKravLøsningDto(
     val referanse: UUID? = null,
     val journalpostId: JournalpostId,
@@ -38,18 +47,21 @@ data class GjenopptakKravLøsningDto(
     }
 }
 
+@JsonTypeName(value = "TRUKKET_SØKNAD")
 data class TrukketSøknadKravLøsningDto(
     val referanse: UUID? = null,
     val journalpostId: JournalpostId,
     val begrunnelse: String,
 ) : KravVurderingLøsningDto(KravType.TRUKKET_SØKNAD)
 
+@JsonTypeName(value = "KLAGE")
 data class KlageKravLøsningDto(
     val referanse: UUID? = null,
     val journalpostId: JournalpostId,
     val begrunnelse: String,
 ) : KravVurderingLøsningDto(KravType.KLAGE)
 
+@JsonTypeName(value = "TILLEGGSOPPLYSNING")
 data class TilleggsopplysningKravLøsningDto(
     val referanse: UUID? = null,
     val journalpostId: JournalpostId,
