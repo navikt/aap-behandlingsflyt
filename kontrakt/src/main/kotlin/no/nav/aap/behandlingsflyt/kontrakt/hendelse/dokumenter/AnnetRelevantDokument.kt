ba8c66c7ddaa49ccb446d26fc03bcbe95c5738a5
Thomas Rognes <thomas.sebastian.rognes@nav.no>
2026-06-24 14:41:32 +0200

    AAP-2030 underKategori -> underkategori

--- kontrakt/src/main/kotlin/no/nav/aap/behandlingsflyt/kontrakt/hendelse/dokumenter/AnnetRelevantDokument.kt
+++ kontrakt/src/main/kotlin/no/nav/aap/behandlingsflyt/kontrakt/hendelse/dokumenter/AnnetRelevantDokument.kt
@@ -6,10 +6,10 @@ import no.nav.aap.behandlingsflyt.kontrakt.statistikk.Vurderingsbehov
 public sealed interface AnnetRelevantDokument : Melding {
     public val årsakerTilBehandling: List<Vurderingsbehov>
     public val begrunnelse: String?
-    public val underKategori: AnnetRelevantDokumentUnderKategori?
+    public val underkategori: AnnetRelevantDokumentUnderkategori?
 }
 
-public enum class AnnetRelevantDokumentUnderKategori {
+public enum class AnnetRelevantDokumentUnderkategori {
     ARBEIDSUTPROVING,
     BARNETILLEGG,
     ETABLERING,
@@ -35,13 +35,13 @@ public data class AnnetRelevantDokumentV0(
     public override val årsakerTilBehandling: List<Vurderingsbehov>,
 ) : AnnetRelevantDokument {
     public override val begrunnelse: String? = null
-    public override val underKategori: AnnetRelevantDokumentUnderKategori? = null
+    public override val underkategori: AnnetRelevantDokumentUnderkategori? = null
 }
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public data class AnnetRelevantDokumentV1(
     public override val årsakerTilBehandling: List<Vurderingsbehov>,
     public override val begrunnelse: String,
-    public override val underKategori: AnnetRelevantDokumentUnderKategori? = null,
+    public override val underkategori: AnnetRelevantDokumentUnderkategori? = null,
 ) : AnnetRelevantDokument
 
