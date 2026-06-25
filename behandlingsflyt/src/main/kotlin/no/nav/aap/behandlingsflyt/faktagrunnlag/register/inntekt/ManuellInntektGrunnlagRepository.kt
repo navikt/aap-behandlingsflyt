3017f5ed7bea14c9e197d8a524766e856a70e203
Fredrik Meyer <fredrik.meyer@nav.no>
2026-06-25 11:00:46 +0200

    AAP-2194 Slett ubrukte responsobjekter

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/register/inntekt/ManuellInntektGrunnlagRepository.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/register/inntekt/ManuellInntektGrunnlagRepository.kt
@@ -10,5 +10,4 @@ interface ManuellInntektGrunnlagRepository : Repository {
     fun lagre(behandlingId: BehandlingId, manuellVurderinger: Set<ManuellInntektVurdering>)
     override fun kopier(fraBehandling: BehandlingId, tilBehandling: BehandlingId)
     fun hentHvisEksisterer(behandlingId: BehandlingId): ManuellInntektGrunnlag?
-    fun hentHistoriskeVurderinger(sakId: SakId, behandlingId: BehandlingId): Set<Set<ManuellInntektVurdering>>
 }
