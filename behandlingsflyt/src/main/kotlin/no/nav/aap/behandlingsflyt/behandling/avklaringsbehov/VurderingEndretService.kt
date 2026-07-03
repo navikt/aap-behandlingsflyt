6f711c3d1ecb0d54c287aa5584dbe1cdd67395d4
SelmaBergstrand <selma.kristine.bergstrand@nav.no>
2026-07-03 14:03:32 +0200

    AAP-2192 utled endret siden sist for sykdom

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/VurderingEndretService.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/VurderingEndretService.kt
@@ -1,5 +1,7 @@
 package no.nav.aap.behandlingsflyt.behandling.avklaringsbehov
 
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.sykdom.SykdomRepository
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.sykdom.erFunksjoneltLik
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.sykdomsvurderingbrev.SykdomsvurderingForBrevRepository
 import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingId
@@ -7,10 +9,12 @@ import no.nav.aap.lookup.repository.RepositoryProvider
 import java.time.LocalDateTime
 
 class VurderingEndretService(
-    private val sykdomsvurderingForBrevRepository: SykdomsvurderingForBrevRepository
+    private val sykdomsvurderingForBrevRepository: SykdomsvurderingForBrevRepository,
+    private val sykdomRepository: SykdomRepository
 ) {
     constructor(repositoryProvider: RepositoryProvider) : this(
-        sykdomsvurderingForBrevRepository = repositoryProvider.provide()
+        sykdomsvurderingForBrevRepository = repositoryProvider.provide(),
+        sykdomRepository = repositoryProvider.provide()
     )
 
     fun endretSidenTidspunkt(
@@ -31,6 +35,15 @@ class VurderingEndretService(
                     // avklaringsbehov aldri vurdert
                     return null
                 }
+                !aktivVurderingNå.erFunksjoneltLik(aktivPåTidspunkt)
+            }
+
+            Definisjon.AVKLAR_SYKDOM -> {
+                val aktivPåTidspunkt = requireNotNull(sykdomRepository.hentSykdomsvurderingerPåTidspunkt(behandlingId, tidspunkt)) {
+                    "Fant ingen sykdomsvurdering på tidspunkt $tidspunkt for behandling $behandlingId"
+                }
+                val aktivVurderingNå = sykdomRepository.hent(behandlingId).sykdomsvurderinger
+
                 !aktivVurderingNå.erFunksjoneltLik(aktivPåTidspunkt)
             }
 
