84942e16a310b861197e9d5329ef0dfe83c65cf1
SelmaBergstrand <selma.kristine.bergstrand@nav.no>
2026-07-14 15:21:07 +0200

    AAP-2192 endret siden sist på bistand + skriver om VurderingEndretService

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/VurderingEndretService.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/VurderingEndretService.kt
@@ -1,7 +1,12 @@
 package no.nav.aap.behandlingsflyt.behandling.avklaringsbehov
 
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.bistand.BistandRepository
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.bistand.Bistandsvurdering
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.bistand.erFunksjoneltLik
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.sykdom.SykdomRepository
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.sykdom.Sykdomsvurdering
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.sykdom.erFunksjoneltLik
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.sykdomsvurderingbrev.SykdomsvurderingForBrev
 import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.sykdomsvurderingbrev.SykdomsvurderingForBrevRepository
 import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingId
@@ -10,11 +15,35 @@ import java.time.LocalDateTime
 
 class VurderingEndretService(
     private val sykdomsvurderingForBrevRepository: SykdomsvurderingForBrevRepository,
-    private val sykdomRepository: SykdomRepository
+    private val sykdomRepository: SykdomRepository,
+    private val bistandRepository: BistandRepository
 ) {
     constructor(repositoryProvider: RepositoryProvider) : this(
         sykdomsvurderingForBrevRepository = repositoryProvider.provide(),
-        sykdomRepository = repositoryProvider.provide()
+        sykdomRepository = repositoryProvider.provide(),
+        bistandRepository = repositoryProvider.provide()
+    )
+
+    private val sjekker: Map<Definisjon, EndretSjekk<*>> = mapOf(
+        // TODO: tilsvarende for alle avklaringsbehov som skal kvalitetssikres
+        Definisjon.AVKLAR_SYKDOM to EndretSjekk(
+            hentPåTidspunkt = sykdomRepository::hentSykdomsvurderingerPåTidspunkt,
+            hentNåværende = { sykdomRepository.hentHvisEksisterer(it)?.sykdomsvurderinger },
+            erLik = List<Sykdomsvurdering>::erFunksjoneltLik,
+            beskrivelse = "sykdomsvurdering"
+        ),
+        Definisjon.AVKLAR_BISTANDSBEHOV to EndretSjekk(
+            hentPåTidspunkt = bistandRepository::hentBistandsvurderingPåTidspunkt,
+            hentNåværende = { bistandRepository.hentHvisEksisterer(it)?.vurderinger },
+            erLik = List<Bistandsvurdering>::erFunksjoneltLik,
+            beskrivelse = "bistandsvurdering"
+        ),
+        Definisjon.SKRIV_SYKDOMSVURDERING_BREV to EndretSjekk(
+            hentPåTidspunkt = sykdomsvurderingForBrevRepository::hentAktivPåTidspunkt,
+            hentNåværende = sykdomsvurderingForBrevRepository::hent,
+            erLik = SykdomsvurderingForBrev::erFunksjoneltLik,
+            beskrivelse = "sykdomsvurderingForBrev"
+        )
     )
 
     fun endretSidenTidspunkt(
@@ -22,32 +51,28 @@ class VurderingEndretService(
         avklaringsbehov: Avklaringsbehov,
         tidspunkt: LocalDateTime
     ): Boolean? {
-        // TODO: tilsvarende for alle avklaringsbehov som skal kvalitetssikres
-        return when (avklaringsbehov.definisjon) {
-            Definisjon.SKRIV_SYKDOMSVURDERING_BREV -> {
-                val aktivPåTidspunkt = sykdomsvurderingForBrevRepository.hentAktivPåTidspunkt(behandlingId, tidspunkt)
-                val aktivVurderingNå = sykdomsvurderingForBrevRepository.hent(behandlingId)
-
-                if ((aktivPåTidspunkt == null && aktivVurderingNå != null) || (aktivPåTidspunkt != null && aktivVurderingNå == null)) {
-                    return true
-                }
-                if (aktivPåTidspunkt == null || aktivVurderingNå == null) {
-                    // avklaringsbehov aldri vurdert
-                    return null
-                }
-                !aktivVurderingNå.erFunksjoneltLik(aktivPåTidspunkt)
-            }
-
-            Definisjon.AVKLAR_SYKDOM -> {
-                val aktivPåTidspunkt = requireNotNull(sykdomRepository.hentSykdomsvurderingerPåTidspunkt(behandlingId, tidspunkt)) {
-                    "Fant ingen sykdomsvurdering på tidspunkt $tidspunkt for behandling $behandlingId"
-                }
-                val aktivVurderingNå = sykdomRepository.hent(behandlingId).sykdomsvurderinger
-
-                !aktivVurderingNå.erFunksjoneltLik(aktivPåTidspunkt)
-            }
-
-            else -> null
-        }
+        val sjekk = sjekker[avklaringsbehov.definisjon] ?: return null
+        return sjekk.harEndring(behandlingId, tidspunkt)
     }
 }
+
+private class EndretSjekk<T>(
+    val hentPåTidspunkt: (BehandlingId, LocalDateTime) -> T?,
+    val hentNåværende: (BehandlingId) -> T?,
+    val erLik: (T, T) -> Boolean,
+    val beskrivelse: String
+)
+
+private fun <T> EndretSjekk<T>.harEndring(
+    behandlingId: BehandlingId,
+    tidspunkt: LocalDateTime
+): Boolean {
+    val aktivPåTidspunkt = requireNotNull(hentPåTidspunkt(behandlingId, tidspunkt)) {
+        "Fant ingen $beskrivelse på tidspunkt $tidspunkt for behandling $behandlingId"
+    }
+    val aktivVurderingNå = requireNotNull(hentNåværende(behandlingId)) {
+        "Fant ingen aktiv $beskrivelse for behandling $behandlingId"
+    }
+    return !erLik(aktivVurderingNå, aktivPåTidspunkt)
+}
+
