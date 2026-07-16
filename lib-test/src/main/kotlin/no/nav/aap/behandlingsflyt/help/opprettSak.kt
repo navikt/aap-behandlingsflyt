fcbceb02ee624810df26e82723e50efa7291078f
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2026-07-16 17:35:44 +0200

    AAP-2194 Støtte for "ekte" frivillige avklaringsbehov i avklaringsbehovservice (#3085)
    Et "ekte" frivillig avklaringsbehov er et avklaringsbehov som "alltid" er frivillig.
    Det er en liten caveat der, i form av at en saksbehandler kan opprette en behandling
    med vurderingsbehov som kun skal stoppe ved det frivillige avklaringsbehovet.
    
    Eksempler på dette er i hvert fall vurderingen for fritak fra meldeplikt og
    arbeidsevne-vurderingen.
    
    Eksempler på "uekte" frivilige avklaringsbehov, er f.eks. lovvalg, som noen 
    ganger er automatisert, men andre ganger påkrevd. Legger ikke til støtte
    for å håndtere dette i denne commiten.

--- lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/help/opprettSak.kt
+++ lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/help/opprettSak.kt
@@ -1,20 +1,29 @@
 package no.nav.aap.behandlingsflyt.help
 
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Utfall
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Vilkårsvurdering
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Vilkårtype
+import no.nav.aap.behandlingsflyt.forretningsflyt.behandlingstyper.Førstegangsbehandling
 import no.nav.aap.behandlingsflyt.repository.sak.PersonRepositoryImpl
 import no.nav.aap.behandlingsflyt.repository.sak.SakRepositoryImpl
 import no.nav.aap.behandlingsflyt.sakogbehandling.Ident
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.Behandling
+import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.StegTilstand
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.VurderingsbehovMedPeriode
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.VurderingsbehovOgÅrsak
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.ÅrsakTilOpprettelse
+import no.nav.aap.behandlingsflyt.sakogbehandling.flyt.StegStatus
 import no.nav.aap.behandlingsflyt.sakogbehandling.flyt.Vurderingsbehov
 import no.nav.aap.behandlingsflyt.sakogbehandling.sak.PersonOgSakService
 import no.nav.aap.behandlingsflyt.sakogbehandling.sak.Sak
 import no.nav.aap.behandlingsflyt.test.FakeApiInternGateway
+import no.nav.aap.behandlingsflyt.test.inmemoryrepo.InMemoryBehandlingRepository
 import no.nav.aap.behandlingsflyt.test.inmemoryrepo.InMemoryPersonRepository
 import no.nav.aap.behandlingsflyt.test.inmemoryrepo.InMemorySakRepository
+import no.nav.aap.behandlingsflyt.test.inmemoryrepo.InMemoryVilkårsresultatRepository
 import no.nav.aap.behandlingsflyt.test.inmemoryservice.InMemoryBehandlingService
 import no.nav.aap.komponenter.dbconnect.DBConnection
+import no.nav.aap.komponenter.tidslinje.tidslinjeOf
 import java.time.LocalDate
 
 fun opprettSak(connection: DBConnection, søknadsdato: LocalDate): Sak {
@@ -41,14 +50,63 @@ fun opprettInMemorySak(søknadsdato: LocalDate = LocalDate.now(), ident: Ident =
 
 fun opprettInMemorySakOgBehandling(
     søknadsdato: LocalDate = LocalDate.now(),
-    vurderingsbehov: List<VurderingsbehovMedPeriode> = listOf(VurderingsbehovMedPeriode(Vurderingsbehov.MOTTATT_SØKNAD)),
+    vurderingsbehov: List<Vurderingsbehov> = listOf(Vurderingsbehov.MOTTATT_SØKNAD),
     årsakTilOpprettelse: ÅrsakTilOpprettelse = ÅrsakTilOpprettelse.SØKNAD,
     ident: Ident = ident(),
 ): Pair<Sak, Behandling> {
+    val vurderingsbehovMedPeriode = vurderingsbehov.map { VurderingsbehovMedPeriode(it) }
     val sak = opprettInMemorySak(søknadsdato, ident)
     val behandling = InMemoryBehandlingService.finnEllerOpprettOrdinærBehandling(
         sak.id,
-        VurderingsbehovOgÅrsak(vurderingsbehov, årsakTilOpprettelse)
+        VurderingsbehovOgÅrsak(vurderingsbehovMedPeriode, årsakTilOpprettelse)
     )
     return sak to behandling
+}
+
+fun opprettInMemorySakOgRevurdering(
+    søknadsdato: LocalDate = LocalDate.now(),
+    vurderingsbehov: List<Vurderingsbehov> = listOf(Vurderingsbehov.MOTTATT_SØKNAD),
+    årsakTilOpprettelse: ÅrsakTilOpprettelse = ÅrsakTilOpprettelse.SØKNAD,
+    ident: Ident = ident(),
+): Triple<Sak, Behandling, Behandling> {
+    val (sak, førstegangsbehandling) = opprettInMemorySakOgBehandling(
+        søknadsdato = søknadsdato,
+        ident = ident,
+    )
+
+    InMemoryBehandlingRepository.leggTilNyttAktivtSteg(
+        førstegangsbehandling.id, StegTilstand(
+            stegStatus = StegStatus.AVSLUTTER,
+            stegType = Førstegangsbehandling.flyt().stegene().last(),
+            aktiv = true,
+        )
+    )
+
+    InMemoryBehandlingService.lukkBehandling(førstegangsbehandling.id)
+
+    /* Legg på en vurdering av aldersvilkåret like lang som rettighetsperioden, siden vi har
+     * noe kode som utleder rettighetsperiode for gamle behandlinger ved å se på aldersvilkåret.
+     */
+    val vilkårsresultat = InMemoryVilkårsresultatRepository.hent(førstegangsbehandling.id)
+    vilkårsresultat.leggTilHvisIkkeEksisterer(Vilkårtype.ALDERSVILKÅRET)
+        .leggTilVurderinger(
+            tidslinjeOf(
+                sak.rettighetsperiode to Vilkårsvurdering(
+                    utfall = Utfall.IKKE_VURDERT,
+                    manuellVurdering = true,
+                    begrunnelse = "automatisk vurdering for å simulere rettighetsperiode",
+                    faktagrunnlag = null,
+                )
+            )
+        )
+    InMemoryVilkårsresultatRepository.lagre(førstegangsbehandling.id, vilkårsresultat)
+
+    val vurderingsbehovMedPeriode = vurderingsbehov.map { VurderingsbehovMedPeriode(it) }
+    val revurdering = InMemoryBehandlingService.finnEllerOpprettOrdinærBehandling(
+        sak.id,
+        VurderingsbehovOgÅrsak(vurderingsbehovMedPeriode, årsakTilOpprettelse)
+    )
+
+    check(førstegangsbehandling.id != revurdering.id)
+    return Triple(sak, InMemoryBehandlingRepository.hent(førstegangsbehandling.id), revurdering)
 }
