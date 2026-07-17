ff2eefd46fd6d0cc28941b2343f377e7e7567d0e
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-07-17 13:22:33 +0200

    Utlede relevant kravtype

---
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/krav/KravService.kt
@@ -0,0 +1,71 @@
+package no.nav.aap.behandlingsflyt.behandling.krav
+
+import no.nav.aap.behandlingsflyt.behandling.StansOpphørService
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.stansopphør.GjeldendeStansEllerOpphør
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.stansopphør.Opphør
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.stansopphør.Stans
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.krav.KravRepository
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.krav.RelevantKrav
+import no.nav.aap.behandlingsflyt.sakogbehandling.flyt.FlytKontekst
+import no.nav.aap.komponenter.gateway.GatewayProvider
+import no.nav.aap.komponenter.tidslinje.Tidslinje
+import no.nav.aap.komponenter.tidslinje.orEmpty
+import no.nav.aap.lookup.repository.RepositoryProvider
+
+class KravService(
+    private val kravRepository: KravRepository,
+    private val stansOpphørService: StansOpphørService
+) {
+    constructor(repositoryProvider: RepositoryProvider, gatewayProvider: GatewayProvider) : this(
+        kravRepository = repositoryProvider.provide(),
+        stansOpphørService = StansOpphørService(repositoryProvider, gatewayProvider)
+    )
+
+    fun kravtypeTidslinje(kontekst: FlytKontekst): Tidslinje<RelevantKravType> {
+        val kravTidslinje = kravRepository.hentHvisEksisterer(kontekst.behandlingId)?.kravtidslinje().orEmpty()
+        val vedtatteStansOpphør =
+            kontekst.forrigeBehandlingId?.let { stansOpphørService.vedtattStansOpphør(it) } ?: emptyList()
+        val gjeldendeStønadsperiodeVurderinger = emptySet<Boolean>()
+
+        return kravTidslinje.map { krav ->
+            utledKravtype(
+                krav,
+                erEksisterendeStønadsperiode(krav, gjeldendeStønadsperiodeVurderinger),
+                vedtatteStansOpphør
+            )
+        }
+    }
+
+    private fun utledKravtype(
+        krav: RelevantKrav,
+        erEksisterendeStønadsperiode: Boolean,
+        vedtattStansOpphør: List<GjeldendeStansEllerOpphør>
+    ): RelevantKravType {
+        if (!erEksisterendeStønadsperiode
+            || vedtattStansOpphør.isEmpty() // Må vi støtte gjeninntreden hvis vi ikke har en vedtatt behandling, eller kan vi anta nytt krav?
+        ) {
+            return RelevantKravType.NYTT_KRAV
+        } else {
+            val stansEllerOpphør = vedtattStansOpphør.lastOrNull { it.fom < krav.muligRettFra }
+            return when (stansEllerOpphør?.vurdering) {
+                null -> throw IllegalStateException("Forventet å finne stans/opphør-årsak ved inntredelse i eksisterende stønadsperiode")
+                is Stans -> RelevantKravType.GJENOPPTAK_ETTER_STANS
+                is Opphør -> RelevantKravType.GJENINNTREDEN_ETTER_OPPHØR
+            }
+        }
+    }
+
+    private fun erEksisterendeStønadsperiode(
+        krav: RelevantKrav,
+        gjeldendeStønadsperiodeVurderinger: Set<Boolean>
+    ): Boolean {
+        // TODO: Sjekk § 12-vurdering for krav
+        return false
+    }
+}
+
+enum class RelevantKravType {
+    GJENOPPTAK_ETTER_STANS,
+    GJENINNTREDEN_ETTER_OPPHØR,
+    NYTT_KRAV
+}
