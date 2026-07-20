7e4b880e966ae6922a3da764bffe6c206137e64b
SelmaBergstrand <selma.kristine.bergstrand@nav.no>
2026-07-20 16:57:53 +0200

    AAP-2194 Fjern featuretoggle oppfølgingsoppgave

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/oppfølgingsbehandling/StartOppfølgingsBehandlingSteg.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/oppfølgingsbehandling/StartOppfølgingsBehandlingSteg.kt
@@ -1,70 +1,25 @@
 package no.nav.aap.behandlingsflyt.forretningsflyt.steg.oppfølgingsbehandling
 
-import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovRepository
-import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løser.ÅrsakTilSettPåVent
-import no.nav.aap.behandlingsflyt.faktagrunnlag.dokument.MottaDokumentService
-import no.nav.aap.behandlingsflyt.faktagrunnlag.dokument.MottattDokumentRepository
 import no.nav.aap.behandlingsflyt.flyt.steg.BehandlingSteg
-import no.nav.aap.behandlingsflyt.flyt.steg.FantVentebehov
 import no.nav.aap.behandlingsflyt.flyt.steg.FlytSteg
 import no.nav.aap.behandlingsflyt.flyt.steg.Fullført
 import no.nav.aap.behandlingsflyt.flyt.steg.StegResultat
-import no.nav.aap.behandlingsflyt.flyt.steg.Ventebehov
-import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon
-import no.nav.aap.behandlingsflyt.kontrakt.behandling.TypeBehandling
 import no.nav.aap.behandlingsflyt.kontrakt.steg.StegType
 import no.nav.aap.behandlingsflyt.sakogbehandling.flyt.FlytKontekstMedPerioder
-import no.nav.aap.behandlingsflyt.unleash.BehandlingsflytFeature
-import no.nav.aap.behandlingsflyt.unleash.UnleashGateway
 import no.nav.aap.komponenter.gateway.GatewayProvider
 import no.nav.aap.lookup.repository.RepositoryProvider
-import org.slf4j.LoggerFactory
 
-class StartOppfølgingsBehandlingSteg(
-    private val mottattDokumentRepository: MottattDokumentRepository,
-    val avklaringsbehovRepository: AvklaringsbehovRepository,
-    val unleashGateway: UnleashGateway
-) : BehandlingSteg {
-    private val log = LoggerFactory.getLogger(javaClass)
-
+class StartOppfølgingsBehandlingSteg : BehandlingSteg {
     override fun utfør(kontekst: FlytKontekstMedPerioder): StegResultat {
-        if (unleashGateway.isEnabled(BehandlingsflytFeature.OppfoelgingsoppgaveSynligMedEnGang)) {
-            return Fullført
-        }
-
-        if (kontekst.behandlingType != TypeBehandling.OppfølgingsBehandling) {
-            log.info("Dette steget skal bare kalles for behandlingstype ${TypeBehandling.OppfølgingsBehandling}")
-            return Fullført
-        }
-
-        val hentAvklaringsbehovene =
-            avklaringsbehovRepository.hentAvklaringsbehovene(kontekst.behandlingId)
-
-        if (hentAvklaringsbehovene.erVurdertTidligereIBehandlingen(Definisjon.VENT_PÅ_OPPFØLGING)) {
-            return Fullført
-        }
-
-        val oppfølgingsDokument = MottaDokumentService(mottattDokumentRepository).hentOppfølgingsBehandlingDokument(kontekst.behandlingId)
-        requireNotNull(oppfølgingsDokument)
-        
-
-        return FantVentebehov(
-            Ventebehov(
-                definisjon = Definisjon.VENT_PÅ_OPPFØLGING,
-                grunn = ÅrsakTilSettPåVent.VENTER_PÅ_OPPLYSNINGER,
-                frist = oppfølgingsDokument.datoForOppfølging
-            )
-        )
-
+        return Fullført
     }
 
     companion object : FlytSteg {
-        override fun konstruer(repositoryProvider: RepositoryProvider, gatewayProvider: GatewayProvider): BehandlingSteg {
-            return StartOppfølgingsBehandlingSteg(
-                mottattDokumentRepository = repositoryProvider.provide(),
-                avklaringsbehovRepository = repositoryProvider.provide(),
-                unleashGateway = gatewayProvider.provide()
-            )
+        override fun konstruer(
+            repositoryProvider: RepositoryProvider,
+            gatewayProvider: GatewayProvider
+        ): BehandlingSteg {
+            return StartOppfølgingsBehandlingSteg()
         }
 
         override fun type(): StegType {
