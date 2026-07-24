2ab89668308f65a9d138bef51c93a962c2d6d35c
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-07-24 15:09:09 +0200

    AAP-2194: Slett utdaterte kolonner i sykdomsvurdering * Har blitt migrert

---
+++ repository/src/main/resources/flyway/V1.327__sykdom_vurdering_fjern_ubrukte_kolonner.sql
@@ -0,0 +1,6 @@
+alter table sykdom_vurdering
+    drop column er_nedsettelse_minst_halvparten,
+    drop column er_nedsettelse_mer_enn_yrkesskadegrense,
+    drop column er_nedsettelse_av_en_viss_varighet,
+    drop column er_arbeidsevne_nedsatt;
+
