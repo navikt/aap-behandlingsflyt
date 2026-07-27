c9a0e26837c3962fb8ff8e68ba2b636105ec6f1a
Fredrik Meyer <fredrik.meyer@nav.no>
2026-07-27 15:33:25 +0200

    AAP-2194 Gjør kall til avklaringsbehov i ett db-kall (#3156)

--- repository/src/test/kotlin/no/nav/aap/behandlingsflyt/repository/avklaringsbehov/AvklaringsbehovRepositoryTest.kt
+++ repository/src/test/kotlin/no/nav/aap/behandlingsflyt/repository/avklaringsbehov/AvklaringsbehovRepositoryTest.kt
@@ -5,6 +5,7 @@ import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.Avklaringsbehovene
 import no.nav.aap.behandlingsflyt.help.finnEllerOpprettBehandling
 import no.nav.aap.behandlingsflyt.help.sak
 import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon
+import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Status
 import no.nav.aap.behandlingsflyt.kontrakt.steg.StegType
 import no.nav.aap.komponenter.dbconnect.transaction
 import no.nav.aap.komponenter.dbtest.TestDataSource
@@ -59,6 +60,73 @@ class AvklaringsbehovRepositoryTest {
 
             val avklaringsbehovEtterLøst = repository.hentAvklaringsbehovene(behandling.id)
             assertThat(avklaringsbehovEtterLøst.alle()[0].erAvsluttet()).isTrue()
+        }
+    }
+
+    @Test
+    fun `hentAlleAvklaringsbehovForSak returnerer behov for hver behandling`() {
+        dataSource.transaction { connection ->
+            val sak1 = sak(connection)
+            val sak2 = sak(connection)
+            val behandling1 = finnEllerOpprettBehandling(connection, sak1)
+            val behandling2 = finnEllerOpprettBehandling(connection, sak2)
+            val repository = AvklaringsbehovRepositoryImpl(connection)
+
+            val avklaringsbehovene = Avklaringsbehovene(repository, behandling1.id)
+            avklaringsbehovene.leggTil(
+                Definisjon.AVKLAR_SYKDOM,
+                StegType.AVKLAR_SYKDOM,
+                begrunnelse = "Første behov",
+                bruker = SYSTEMBRUKER,
+                perioderVedtaketBehøverVurdering = null,
+                perioderSomIkkeErTilstrekkeligVurdert = null
+            )
+
+            val resultat = repository.hentAlleAvklaringsbehovForSak(listOf(behandling1.id, behandling2.id))
+            val resultatPerBehandling = resultat.associateBy { it.behandlingId }
+
+            assertThat(resultat).hasSize(2)
+            assertThat(resultatPerBehandling.keys).containsExactlyInAnyOrder(behandling1.id, behandling2.id)
+            assertThat(resultatPerBehandling.getValue(behandling1.id).avklaringsbehov)
+                .extracting("definisjon")
+                .contains(Definisjon.AVKLAR_SYKDOM)
+        }
+    }
+
+    @Test
+    fun `opprett bruker samme avklaringsbehov ved samme definisjon`() {
+        dataSource.transaction { connection ->
+            val sak = sak(connection)
+            val behandling = finnEllerOpprettBehandling(connection, sak)
+            val repository = AvklaringsbehovRepositoryImpl(connection)
+
+            repository.opprett(
+                behandlingId = behandling.id,
+                definisjon = Definisjon.AVKLAR_SYKDOM,
+                funnetISteg = StegType.AVKLAR_SYKDOM,
+                frist = null,
+                begrunnelse = "Første",
+                grunn = null,
+                endretAv = SYSTEMBRUKER,
+                perioderSomIkkeErTilstrekkeligVurdert = null,
+                perioderVedtaketBehøverVurdering = null
+            )
+            repository.opprett(
+                behandlingId = behandling.id,
+                definisjon = Definisjon.AVKLAR_SYKDOM,
+                funnetISteg = StegType.AVKLAR_SYKDOM,
+                frist = null,
+                begrunnelse = "Andre",
+                grunn = null,
+                endretAv = SYSTEMBRUKER,
+                perioderSomIkkeErTilstrekkeligVurdert = null,
+                perioderVedtaketBehøverVurdering = null
+            )
+
+            val behov = repository.hent(behandling.id)
+            assertThat(behov).hasSize(1)
+            assertThat(behov.first().historikk).hasSize(2)
+            assertThat(behov.first().historikk.map { it.status }).containsOnly(Status.OPPRETTET)
         }
     }
 }
