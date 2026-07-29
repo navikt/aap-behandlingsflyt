dbd2bbde6ae52af653f41e2af414b4ae13da731d
Fredrik Meyer <fredrik.meyer@nav.no>
2026-07-29 12:49:24 +0200

    Spesifiser type

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/AsyncExecutors.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/AsyncExecutors.kt
@@ -1,5 +1,6 @@
 package no.nav.aap.behandlingsflyt.faktagrunnlag
 
+import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
-val informasjonskravExecutor = Executors.newVirtualThreadPerTaskExecutor()
+val informasjonskravExecutor: ExecutorService = Executors.newVirtualThreadPerTaskExecutor()
