e9e37aceaa84f0b1a4a77c2bcec77018571937c1
Fredrik Meyer <fredrik.meyer@nav.no>
2026-07-30 15:26:58 +0200

    AAP-2210 Hvis avslag pga full ytelse fredag og mandag, fyll inn avslag også i helgen

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avslag11_27/Avslag11_27Repository.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avslag11_27/Avslag11_27Repository.kt
@@ -3,6 +3,7 @@ package no.nav.aap.behandlingsflyt.behandling.avslag11_27
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingId
 import no.nav.aap.lookup.repository.Repository
 
+@Suppress("ClassName")
 interface Avslag11_27Repository : Repository {
     fun lagre(behandlingId: BehandlingId, vurderinger: Set<Avslag11_27Vurdering>)
     fun hentHvisEksisterer(behandlingId: BehandlingId): Avslag11_27Grunnlag?
