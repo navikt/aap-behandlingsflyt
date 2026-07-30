80d1f99d2df8a3e3fe7d90f7c809fa737fea8132
Fredrik Meyer <fredrik.meyer@nav.no>
2026-07-30 11:45:57 +0200

    AAP-2194 Forenkle samordningservice, dra ting ut som extension functions (#3183)
    * AAP-2194 Forenkle samordningservice, dra ting ut som extension functions
    
    * Update behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/behandling/vilkår/samordning/annenfullytelse/SamordningAnnenFullYtelseVilkårTest.kt
    
    Co-authored-by: Peter Brottveit Bock <peter.brottveit.bock@nav.no>
    
    ---------
    
    Co-authored-by: Peter Brottveit Bock <peter.brottveit.bock@nav.no>

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/delvurdering/samordning/SamordningGrunnlag.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/delvurdering/samordning/SamordningGrunnlag.kt
@@ -1,15 +1,84 @@
 package no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning
 
+import no.nav.aap.behandlingsflyt.behandling.samordning.AvklaringsType
+import no.nav.aap.behandlingsflyt.behandling.samordning.SamordningGradering
+import no.nav.aap.behandlingsflyt.behandling.samordning.Ytelse
+import no.nav.aap.behandlingsflyt.behandling.samordning.YtelseGradering
 import no.nav.aap.behandlingsflyt.faktagrunnlag.Faktagrunnlag
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.ytelsevurdering.SamordningVurderingGrunnlag
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.ytelsevurdering.SamordningYtelseGrunnlag
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.samordning.ytelsevurdering.SamordningYtelsePeriode
+import no.nav.aap.komponenter.tidslinje.JoinStyle
+import no.nav.aap.komponenter.tidslinje.Segment
+import no.nav.aap.komponenter.tidslinje.StandardSammenslåere
+import no.nav.aap.komponenter.tidslinje.StandardSammenslåere.slåSammenTilListe
+import no.nav.aap.komponenter.tidslinje.Tidslinje
+import no.nav.aap.komponenter.tidslinje.orEmpty
 import no.nav.aap.komponenter.type.Periode
 import no.nav.aap.komponenter.verdityper.Prosent
+import kotlin.collections.associate
+import kotlin.collections.orEmpty
+import kotlin.math.min
 
 data class SamordningYtelseVurderingGrunnlag(
     val ytelseGrunnlag: SamordningYtelseGrunnlag?,
     val vurderingGrunnlag: SamordningVurderingGrunnlag?
-) : Faktagrunnlag
+) : Faktagrunnlag {
+
+    fun perioderSomIkkeHarBlittVurdert(
+    ): Tidslinje<List<Ytelse>> {
+        val hentedeYtelserByManuelleYtelser = ytelseGrunnlag?.tidslinjeMedSamordningYtelser().orEmpty()
+
+        val perioderSomIkkeHarBlittVurdert =
+            hentedeYtelserByManuelleYtelser.kombiner(
+                vurderingGrunnlag?.vurderingTidslinje().orEmpty(),
+                StandardSammenslåere.minus()
+            )
+
+        return perioderSomIkkeHarBlittVurdert
+    }
+
+    fun vurder(): Tidslinje<SamordningGradering> {
+        /**
+         * Henter kun automatiske ytelser fra register - disse skal ikke ha overlappende perioder
+         * Pr nå har vi ingen typer som er satt opp til å vurderes automatisk
+         */
+        val hentedeYtelserFraRegisterForAutomatiskVurdering =
+            ytelseGrunnlag?.ytelser.orEmpty().filter { it.ytelseType.type == AvklaringsType.AUTOMATISK }.map { ytelse ->
+                Tidslinje(ytelse.ytelsePerioder.map { Segment(it.periode, Pair(ytelse.ytelseType, it)) })
+            }.fold(Tidslinje.empty<List<Pair<Ytelse, SamordningYtelsePeriode>>>()) { acc, curr ->
+                acc.kombiner(curr, slåSammenTilListe())
+            }
+
+        // Slå sammen med vurderinger og regn ut graderinger
+
+        val samordningTidslinje =
+            hentedeYtelserFraRegisterForAutomatiskVurdering.kombiner(
+                vurderingGrunnlag?.vurderingTidslinje().orEmpty(),
+                JoinStyle.OUTER_JOIN { periode, venstre, høyre ->
+                    // Manuelt vurderte perioder er allerede validert
+                    val manueltVurderteGraderinger =
+                        høyre?.verdi.orEmpty().associate { it.first to it.second }
+                            .mapValues { it.value.gradering!! }
+                            .filterKeys { it.type == AvklaringsType.MANUELL }
+
+                    val registerVurderinger = venstre?.verdi.orEmpty().associate { it.first to it.second.gradering!! }
+                        .filterKeys { it.type == AvklaringsType.AUTOMATISK }
+
+                    val alleSammen = manueltVurderteGraderinger.plus(registerVurderinger)
+                    val gradering =
+                        min(alleSammen.values.sumOf { it.prosentverdi() }, 100)
+                    Segment(
+                        periode, SamordningGradering(
+                            gradering = Prosent(gradering),
+                            ytelsesGraderinger = alleSammen.entries.map { YtelseGradering(it.key, it.value) }
+                        )
+                    )
+                })
+
+        return samordningTidslinje
+    }
+}
 
 /**
  * Grunnlag fra samordningssteget som brukes i følgende steg.
