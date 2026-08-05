137473b76761d32e54dff70da037621351cdf980
HenrikBuggeHalvorsen <henrik.bugge.halvorsen@nav.no>
2026-08-05 15:15:31 +0200

    feil import

--- lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/test/FakePdfGeneratorGateway.kt
+++ lib-test/src/main/kotlin/no/nav/aap/behandlingsflyt/test/FakePdfGeneratorGateway.kt
@@ -1,11 +1,13 @@
 package no.nav.aap.behandlingsflyt.test
 
-import no.nav.aap.behandlingsflyt.behandling.meldekort.MeldekortPdfRequest
-import no.nav.aap.behandlingsflyt.integrasjon.pdfgen.PdfGeneratorGateway
+import no.nav.aap.behandlingsflyt.behandling.vilkår.innsikt.Dokument
+import no.nav.aap.behandlingsflyt.behandling.vilkår.innsikt.PdfGeneratorGateway
 import no.nav.aap.komponenter.gateway.Factory
 
 class FakePdfGeneratorGateway : PdfGeneratorGateway {
-    override fun genererVurderingerOppsummeringDokument(request: MeldekortPdfRequest): ByteArray = ByteArray(0)
+    override fun genererVurderingerOppsummeringDokument(request: Dokument): ByteArray {
+        TODO("Not yet implemented")
+    }
 
     companion object : Factory<PdfGeneratorGateway> {
         override fun konstruer(): PdfGeneratorGateway = FakePdfGeneratorGateway()
