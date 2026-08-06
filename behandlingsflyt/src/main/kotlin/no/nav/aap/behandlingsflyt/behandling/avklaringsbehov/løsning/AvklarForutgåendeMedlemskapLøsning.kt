62c2afe216705985006388bac8ea2638696e5822
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-08-06 16:35:44 +0200

    medlemskap

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løsning/AvklarForutgåendeMedlemskapLøsning.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løsning/AvklarForutgåendeMedlemskapLøsning.kt
@@ -6,15 +6,18 @@ import com.fasterxml.jackson.annotation.JsonTypeName
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovKontekst
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løser.AvklarForutgåendeMedlemskapLøser
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løser.LøsningsResultat
+import no.nav.aap.behandlingsflyt.faktagrunnlag.lovvalgmedlemskap.ManuellVurderingForForutgåendeMedlemskap
 import no.nav.aap.behandlingsflyt.faktagrunnlag.lovvalgmedlemskap.PeriodisertManuellVurderingForForutgåendeMedlemskapDto
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.medlemskap.MedlemskapArbeidInntektForutgåendeRepository
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.PeriodisertVurdering
 import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.AVKLAR_FORUTGÅENDE_MEDLEMSKAP_KODE
 import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.AvklaringsbehovKode
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingId
 import no.nav.aap.komponenter.gateway.GatewayProvider
 import no.nav.aap.komponenter.tidslinje.Tidslinje
+import no.nav.aap.komponenter.verdityper.Bruker
 import no.nav.aap.lookup.repository.RepositoryProvider
-
+import kotlin.collections.orEmpty
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonTypeName(value = AVKLAR_FORUTGÅENDE_MEDLEMSKAP_KODE)
@@ -25,7 +28,7 @@ class AvklarPeriodisertForutgåendeMedlemskapLøsning(
         defaultValue = AVKLAR_FORUTGÅENDE_MEDLEMSKAP_KODE
     ) val behovstype: AvklaringsbehovKode = AvklaringsbehovKode.`5020`,
     override val løsningerForPerioder: List<PeriodisertManuellVurderingForForutgåendeMedlemskapDto>
-) : PeriodisertAvklaringsbehovLøsning<PeriodisertManuellVurderingForForutgåendeMedlemskapDto> {
+) : PeriodisertAvklaringsbehovLøsning<PeriodisertManuellVurderingForForutgåendeMedlemskapDto>, LøsningMedPeriodiserteVurderinger {
     override fun løs(repositoryProvider: RepositoryProvider, kontekst: AvklaringsbehovKontekst, gatewayProvider: GatewayProvider): LøsningsResultat {
         return AvklarForutgåendeMedlemskapLøser(repositoryProvider).løs(kontekst, this)
     }
@@ -36,5 +39,17 @@ class AvklarPeriodisertForutgåendeMedlemskapLøsning(
     ): Tidslinje<*> {
         val repository = repositoryProvider.provide<MedlemskapArbeidInntektForutgåendeRepository>()
         return repository.hentHvisEksisterer(behandlingId)?.gjeldendeVurderinger() ?: Tidslinje<Unit>()
+    }
+
+    override fun hentVurderinger(
+        behandlingId: BehandlingId,
+        repositoryProvider: RepositoryProvider
+    ): List<PeriodisertVurdering> {
+        val repository = repositoryProvider.provide<MedlemskapArbeidInntektForutgåendeRepository>()
+        return repository.hentHvisEksisterer(behandlingId)?.vurderinger.orEmpty()
+    }
+
+    override fun somVurderinger(bruker: Bruker, behandlingId: BehandlingId): List<ManuellVurderingForForutgåendeMedlemskap> {
+        return løsningerForPerioder.map { it.toManuellVurderingForForutgåendeMedlemskap(overstyrt = false, bruker, behandlingId) }
     }
 }
--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løsning/AvklarOverstyrtForutgåendeMedlemskapLøsning.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løsning/AvklarOverstyrtForutgåendeMedlemskapLøsning.kt
@@ -6,13 +6,16 @@ import com.fasterxml.jackson.annotation.JsonTypeName
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.AvklaringsbehovKontekst
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løser.AvklarOverstyrtForutgåendeMedlemskapLøser
 import no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.løser.LøsningsResultat
+import no.nav.aap.behandlingsflyt.faktagrunnlag.lovvalgmedlemskap.ManuellVurderingForForutgåendeMedlemskap
 import no.nav.aap.behandlingsflyt.faktagrunnlag.lovvalgmedlemskap.PeriodisertManuellVurderingForForutgåendeMedlemskapDto
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.medlemskap.MedlemskapArbeidInntektForutgåendeRepository
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.PeriodisertVurdering
 import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.AvklaringsbehovKode
 import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.MANUELL_OVERSTYRING_MEDLEMSKAP
 import no.nav.aap.behandlingsflyt.sakogbehandling.behandling.BehandlingId
 import no.nav.aap.komponenter.gateway.GatewayProvider
 import no.nav.aap.komponenter.tidslinje.Tidslinje
+import no.nav.aap.komponenter.verdityper.Bruker
 import no.nav.aap.lookup.repository.RepositoryProvider
 
 
@@ -25,7 +28,7 @@ class AvklarPeriodisertOverstyrtForutgåendeMedlemskapLøsning(
         defaultValue = MANUELL_OVERSTYRING_MEDLEMSKAP
     ) val behovstype: AvklaringsbehovKode = AvklaringsbehovKode.`5022`,
     override val løsningerForPerioder: List<PeriodisertManuellVurderingForForutgåendeMedlemskapDto>
-) : PeriodisertAvklaringsbehovLøsning<PeriodisertManuellVurderingForForutgåendeMedlemskapDto> {
+) : PeriodisertAvklaringsbehovLøsning<PeriodisertManuellVurderingForForutgåendeMedlemskapDto>, LøsningMedPeriodiserteVurderinger {
     override fun løs(repositoryProvider: RepositoryProvider, kontekst: AvklaringsbehovKontekst, gatewayProvider: GatewayProvider): LøsningsResultat {
         return AvklarOverstyrtForutgåendeMedlemskapLøser(repositoryProvider).løs(kontekst, this)
     }
@@ -36,5 +39,17 @@ class AvklarPeriodisertOverstyrtForutgåendeMedlemskapLøsning(
     ): Tidslinje<*> {
         val repository = repositoryProvider.provide<MedlemskapArbeidInntektForutgåendeRepository>()
         return repository.hentHvisEksisterer(behandlingId)?.gjeldendeVurderinger() ?: Tidslinje<Unit>()
+    }
+
+    override fun hentVurderinger(
+        behandlingId: BehandlingId,
+        repositoryProvider: RepositoryProvider
+    ): List<PeriodisertVurdering> {
+        val repository = repositoryProvider.provide<MedlemskapArbeidInntektForutgåendeRepository>()
+        return repository.hentHvisEksisterer(behandlingId)?.vurderinger.orEmpty()
+    }
+
+    override fun somVurderinger(bruker: Bruker, behandlingId: BehandlingId): List<ManuellVurderingForForutgåendeMedlemskap> {
+        return løsningerForPerioder.map { it.toManuellVurderingForForutgåendeMedlemskap(overstyrt = true, bruker, behandlingId) }
     }
 }
