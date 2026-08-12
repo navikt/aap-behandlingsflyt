c72a6efac5b9a431e5052ac8111fa23259a1619e
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-08-12 07:41:12 +0200

    fiks

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løser/FritakFraMeldepliktLøser.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/avklaringsbehov/løser/FritakFraMeldepliktLøser.kt
@@ -27,9 +27,10 @@ class FritakFraMeldepliktLøser(
         val behandling = behandlingRepository.hent(kontekst.kontekst.behandlingId)
 
         val nyeVurderinger =
-            løsning.løsningerForPerioder.map { toFritaksvurdering(it, kontekst) }
+            løsning.løsningerForPerioder.map { it.tilFritaksvurdering(kontekst.bruker, kontekst.behandlingId()) }
 
-        val vedtatteVurderinger = behandling.forrigeBehandlingId?.let { meldepliktRepository.hentHvisEksisterer(it) }?.vurderinger.orEmpty()
+        val vedtatteVurderinger =
+            behandling.forrigeBehandlingId?.let { meldepliktRepository.hentHvisEksisterer(it) }?.vurderinger.orEmpty()
 
         meldepliktRepository.lagre(
             behandlingId = behandling.id,
