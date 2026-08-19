3ba477b7708adeda91a8b15d10d92ae2c5c6bebf
FatimaYari <61829146+FatimaYari@users.noreply.github.com>
2026-08-19 10:24:58 +0200

    Update kontrakt/src/main/kotlin/no/nav/aap/behandlingsflyt/kontrakt/hendelse/UførevedtakV0Dto.kt
    Co-authored-by: Fredrik Meyer <fredrik.meyer@nav.no>

--- kontrakt/src/main/kotlin/no/nav/aap/behandlingsflyt/kontrakt/hendelse/UførevedtakV0Dto.kt
+++ kontrakt/src/main/kotlin/no/nav/aap/behandlingsflyt/kontrakt/hendelse/UførevedtakV0Dto.kt
@@ -3,7 +3,7 @@ package no.nav.aap.behandlingsflyt.kontrakt.hendelse
 import no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.UførevedtakResultat
 import java.time.LocalDate
 
-public data class UførevedtakV0Dto (
+public data class UførevedtakDto (
     val resultat: UførevedtakResultat,
     val virkningsdato: LocalDate
 )
