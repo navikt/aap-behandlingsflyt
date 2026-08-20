c2b3a0414f06113ff2817796436f050654752fa3
FatimaYari <61829146+FatimaYari@users.noreply.github.com>
2026-08-20 15:19:23 +0200

    [AAP-1150]: Legg til årsak til trekk av søknad - BE (#3243)
    * AAP-1150: legg til årsak
    
    * rename sql file
    
    * [AAP-1150]: endre enum navn
    
    * AAP-1150: legg til årsak i test
    
    * AAP-1150: endre sql navn
    
    * AP-1150: bakover kompatibel
    
    * AAP-1150: endre til setenum

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/søknad/TrukketSøknadVurdering.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/søknad/TrukketSøknadVurdering.kt
@@ -1,5 +1,6 @@
 package no.nav.aap.behandlingsflyt.behandling.søknad
 
+import no.nav.aap.behandlingsflyt.behandling.søknad.flate.AarsakTilTrekkSoknadDto
 import no.nav.aap.komponenter.verdityper.Bruker
 import no.nav.aap.verdityper.dokument.JournalpostId
 import java.time.Instant
@@ -10,4 +11,23 @@ data class TrukketSøknadVurdering(
     val skalTrekkes: Boolean,
     val vurdertAv: Bruker,
     val vurdert: Instant,
+    val aarsak: AarsakTilTrekkSoknad?,
 )
+
+enum class AarsakTilTrekkSoknad {
+    BRUKER_SOKTE_FOR_TIDLIG,
+    BRUKER_SOKTE_FEIL_YTELSE,
+    BRUKER_ONSKER_IKKE_SOKE_LENGER,
+    ANNET,
+}
+
+fun AarsakTilTrekkSoknad?.tilDto(): AarsakTilTrekkSoknadDto? =
+    when (this) {
+        AarsakTilTrekkSoknad.BRUKER_SOKTE_FOR_TIDLIG -> AarsakTilTrekkSoknadDto.BRUKER_SOKTE_FOR_TIDLIG
+        AarsakTilTrekkSoknad.BRUKER_SOKTE_FEIL_YTELSE -> AarsakTilTrekkSoknadDto.BRUKER_SOKTE_FEIL_YTELSE
+        AarsakTilTrekkSoknad.BRUKER_ONSKER_IKKE_SOKE_LENGER -> AarsakTilTrekkSoknadDto.BRUKER_ONSKER_IKKE_SOKE_LENGER
+        AarsakTilTrekkSoknad.ANNET -> AarsakTilTrekkSoknadDto.ANNET
+        null -> null
+    }
+
+
---
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/søknad/flate/AarsakTilTrekkSoknadDto.kt
@@ -0,0 +1,8 @@
+package no.nav.aap.behandlingsflyt.behandling.søknad.flate
+
+enum class AarsakTilTrekkSoknadDto {
+    BRUKER_SOKTE_FOR_TIDLIG,
+    BRUKER_SOKTE_FEIL_YTELSE,
+    BRUKER_ONSKER_IKKE_SOKE_LENGER,
+    ANNET
+}
