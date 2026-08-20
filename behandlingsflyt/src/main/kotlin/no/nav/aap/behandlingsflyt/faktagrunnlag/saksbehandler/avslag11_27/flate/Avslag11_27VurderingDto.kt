d75114a6bb1f0cb3b409a94486c980e717d0b85b
Thao Thanh Nguyen <43989424+thao1@users.noreply.github.com>
2026-08-20 15:25:12 +0200

    AAP-2142: Reverte sykepengegrunnlag felt og lagt til har_sykepengegrunnlag_over_2g på nytt. (#3313)

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/saksbehandler/avslag11_27/flate/Avslag11_27VurderingDto.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/faktagrunnlag/saksbehandler/avslag11_27/flate/Avslag11_27VurderingDto.kt
@@ -1,8 +1,6 @@
 package no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.avslag11_27.flate
 
 import no.nav.aap.behandlingsflyt.behandling.samordning.Ytelse
-import no.nav.aap.komponenter.verdityper.Beløp
-import java.math.BigDecimal
 import java.time.LocalDate
 
 data class Avslag11_27VurderingDto(
@@ -12,7 +10,7 @@ data class Avslag11_27VurderingDto(
     val brukersYtelse: Ytelse? = null,
     val brukersYtelseTom: LocalDate? = null,
     /*  Kun for sykepenger */
-    val sykepengegrunnlag: Beløp? = null,
+    val harSykepengegrunnlagOver2G: Boolean? = null,
     val harArbeidsgiverSykepengerUtbetaling: Boolean? = null,
     val skalAvslås1127: Boolean? = null,
 )
