0a024844af5afbf4985f287e8c1cca417820acd2
Fredrik Meyer <fredrik.meyer@nav.no>
2026-08-25 09:44:37 +0200

    AAP-BAU Bruk ny vilkårservice for Aktivitetsplikt11_7Vi (#3336)

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/vilkår/aktivitetsplikt/Aktivitetspliktvilkåret.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/vilkår/aktivitetsplikt/Aktivitetspliktvilkåret.kt
@@ -1,15 +1,15 @@
 package no.nav.aap.behandlingsflyt.behandling.vilkår.aktivitetsplikt
 
-import no.nav.aap.behandlingsflyt.behandling.vilkår.Vilkårsvurderer
 import no.nav.aap.behandlingsflyt.faktagrunnlag.Faktagrunnlag
 import no.nav.aap.behandlingsflyt.faktagrunnlag.aktivitetsplikt.Aktivitetsplikt11_7Grunnlag
 import no.nav.aap.behandlingsflyt.faktagrunnlag.aktivitetsplikt.Utfall.OPPHØR
 import no.nav.aap.behandlingsflyt.faktagrunnlag.aktivitetsplikt.Utfall.STANS
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Avslagsårsak
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Utfall
-import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Vilkårsresultat
+import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Vilkårsvurderer
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Vilkårsvurdering
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Vilkårtype
+import no.nav.aap.komponenter.tidslinje.Tidslinje
 import no.nav.aap.komponenter.tidslinje.tidslinjeOf
 import no.nav.aap.komponenter.type.Periode
 import no.nav.aap.komponenter.verdityper.Tid
@@ -20,23 +20,23 @@ data class AktivitetspliktvilkåretGrunnlag(
     val vurderFra: LocalDate,
 ) : Faktagrunnlag
 
-class Aktivitetspliktvilkåret(vilkårsresultat: Vilkårsresultat) : Vilkårsvurderer<AktivitetspliktvilkåretGrunnlag> {
-    private val vilkåret = vilkårsresultat.leggTilHvisIkkeEksisterer(Vilkårtype.AKTIVITETSPLIKT)
+object Aktivitetspliktvilkåret : Vilkårsvurderer<AktivitetspliktvilkåretGrunnlag> {
+    override val vilkårtype = Vilkårtype.AKTIVITETSPLIKT
 
-    override fun vurder(grunnlag: AktivitetspliktvilkåretGrunnlag) {
-        val rettighetsperiode = Periode(grunnlag.vurderFra, Tid.MAKS)
+    override fun vurder(faktagrunnlag: AktivitetspliktvilkåretGrunnlag): Tidslinje<Vilkårsvurdering> {
+        val rettighetsperiode = Periode(faktagrunnlag.vurderFra, Tid.MAKS)
 
         val utgangspunktOppfylt = tidslinjeOf(
             rettighetsperiode to Vilkårsvurdering(
                 utfall = Utfall.OPPFYLT,
                 manuellVurdering = false,
                 begrunnelse = null,
-                faktagrunnlag = grunnlag,
+                faktagrunnlag = faktagrunnlag,
             )
         )
 
         val saksbehandlersVurderinger =
-            grunnlag.aktivitetsplikt117grunnlag
+            faktagrunnlag.aktivitetsplikt117grunnlag
                 .tidslinje()
                 .map {
                     Vilkårsvurdering(
@@ -51,12 +51,10 @@ class Aktivitetspliktvilkåret(vilkårsresultat: Vilkårsresultat) : Vilkårsvur
                             OPPHØR -> Avslagsårsak.BRUDD_PÅ_AKTIVITETSPLIKT_OPPHØR
                             null -> null
                         },
-                        faktagrunnlag = grunnlag,
+                        faktagrunnlag = faktagrunnlag,
                     )
                 }
 
-        vilkåret.leggTilVurderinger(
-            utgangspunktOppfylt.mergePrioriterHøyre(saksbehandlersVurderinger)
-        )
+        return utgangspunktOppfylt.mergePrioriterHøyre(saksbehandlersVurderinger)
     }
 }
