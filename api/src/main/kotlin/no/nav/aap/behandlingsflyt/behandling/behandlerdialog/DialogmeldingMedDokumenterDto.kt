46d45ea7bf443872a2d31914ee0c946ad3d4d243
Arild Lillegård <arild.lillegard@nav.no>
2026-08-31 15:54:53 +0200

    AAP-2317: Fikset noen PR-tilbakemeldinger
    Bumpet også versjonsnummeret på pakke med typer fra dokumentinnhenting
    
    # Conflicts:
    #	gradle/libs.versions.toml

--- api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/behandlerdialog/DialogmeldingMedDokumenterDto.kt
+++ api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/behandlerdialog/DialogmeldingMedDokumenterDto.kt
@@ -1,6 +1,6 @@
 package no.nav.aap.behandlingsflyt.behandling.behandlerdialog
 
-data class DialogmeldingMedDokumenter(
+data class DialogmeldingMedDokumenterDto(
     val dialogmelding: FellesDialogmeldingDto,
     val dokumentIdListe: List<BegrensetDokumentInfoDto>,
 )
--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/dialogmelding/HentDialogmeldingerForSakParams.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/dialogmelding/HentDialogmeldingerForSakParams.kt
@@ -1,7 +1,3 @@
 package no.nav.aap.behandlingsflyt.behandling.dialogmelding
 
-import com.papsign.ktor.openapigen.annotations.parameters.PathParam
-
-data class HentDialogmeldingerForSakParams(
-    @param:PathParam(description = "Saksnummer") val saksnummer: String,
-)
+data class HentDialogmeldingerForSakParams(val saksnummer: String)
