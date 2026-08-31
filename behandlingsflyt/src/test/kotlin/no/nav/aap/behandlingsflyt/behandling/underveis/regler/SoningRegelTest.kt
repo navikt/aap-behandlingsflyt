64034dc35e4270ea6879035418f0bac837afdf80
Fredrik Meyer <fredrik.meyer@nav.no>
2026-08-31 07:56:37 +0200

    AAP-BAU Flytt et par vilkår over på nytt stateless format

--- behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/behandling/underveis/regler/SoningRegelTest.kt
+++ behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/behandling/underveis/regler/SoningRegelTest.kt
@@ -6,28 +6,27 @@ import no.nav.aap.behandlingsflyt.behandling.vilkår.straffegjennomføring.Straf
 import no.nav.aap.behandlingsflyt.behandling.vilkår.straffegjennomføring.StraffegjennomføringVilkår
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Avslagsårsak
 import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Utfall
-import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Vilkårsresultat
-import no.nav.aap.behandlingsflyt.faktagrunnlag.delvurdering.vilkårsresultat.Vilkårtype
-import no.nav.aap.behandlingsflyt.faktagrunnlag.register.institusjonsopphold.Helseoppholdvurderinger
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.institusjonsopphold.Institusjon
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.institusjonsopphold.Institusjonstype
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.institusjonsopphold.Oppholdstype
-import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.institusjon.Soningsvurdering
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.institusjonsopphold.Soningsvurderinger
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.institusjon.Soningsvurdering
 import no.nav.aap.behandlingsflyt.help.assertTidslinje
 import no.nav.aap.behandlingsflyt.repository.behandling.BehandlingRepositoryImpl
 import no.nav.aap.behandlingsflyt.repository.faktagrunnlag.barnetillegg.BarnetilleggRepositoryImpl
 import no.nav.aap.behandlingsflyt.repository.faktagrunnlag.register.institusjonsopphold.InstitusjonsoppholdRepositoryImpl
 import no.nav.aap.behandlingsflyt.repository.sak.SakRepositoryImpl
 import no.nav.aap.behandlingsflyt.test.MockConnection
+import no.nav.aap.behandlingsflyt.test.desember
+import no.nav.aap.behandlingsflyt.test.februar
+import no.nav.aap.behandlingsflyt.test.januar
 import no.nav.aap.komponenter.tidslinje.Segment
 import no.nav.aap.komponenter.type.Periode
+import no.nav.aap.komponenter.verdityper.Bruker
 import no.nav.aap.komponenter.verdityper.Tid
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import java.time.LocalDate
 import java.time.LocalDateTime
-import no.nav.aap.komponenter.verdityper.Bruker
 
 class SoningRegelTest {
 
@@ -41,11 +40,11 @@ class SoningRegelTest {
 
     @Test
     fun vurder() {
-        val periode = Periode(LocalDate.of(2024, 1, 1), LocalDate.of(2024, 12, 1))
+        val periode = Periode(1 januar 2024, 1 desember 2024)
         val utlederInput = InstitusjonsoppholdInput(
             institusjonsOpphold = listOf(
                 Segment(
-                    Periode(LocalDate.of(2024, 1, 6), (LocalDate.of(2024, 2, 15))),
+                    Periode(6 januar 2024, 15 februar 2024),
                     Institusjon(
                         Institusjonstype.FO,
                         Oppholdstype.S,
@@ -59,22 +58,22 @@ class SoningRegelTest {
                     Soningsvurdering(
                         skalOpphøre = true,
                         begrunnelse = "Formue under forvaring",
-                        fraDato = LocalDate.of(2024, 1, 6)
+                        fraDato = 6 januar 2024
                     ),
                     Soningsvurdering(
                         skalOpphøre = true,
                         begrunnelse = "Soner i fengsel",
-                        fraDato = LocalDate.of(2024, 1, 11)
+                        fraDato = 11 januar 2024
                     ),
                     Soningsvurdering(
                         skalOpphøre = false,
                         begrunnelse = "Jobber utenfor anstalten",
-                        fraDato = LocalDate.of(2024, 1, 16)
+                        fraDato = 16 januar 2024
                     ),
                     Soningsvurdering(
                         skalOpphøre = false,
                         begrunnelse = "Fotlenke",
-                        fraDato = LocalDate.of(2024, 2, 6)
+                        fraDato = 6 februar 2024
                     ),
                 ),
                 vurdertAv = Bruker("ident"),
@@ -87,39 +86,35 @@ class SoningRegelTest {
 
         val delresultat = utlederService.utledBehov(utlederInput)
 
-        val vilkårsresultat = Vilkårsresultat()
-        StraffegjennomføringVilkår(vilkårsresultat).vurder(
-            StraffegjennomføringGrunnlag(
-                institusjonsopphold = MapInstitusjonoppholdTilRegel.map(delresultat),
-                vurderFra = periode.fom,
-            )
-        )
-        val straffegjennomføringVilkårsvurderinger =
-            vilkårsresultat.finnVilkår(Vilkårtype.STRAFFEGJENNOMFØRING).tidslinje()
+        val straffegjennomføringVilkårsvurderinger = StraffegjennomføringVilkår.vurder(StraffegjennomføringGrunnlag(
+            institusjonsopphold = MapInstitusjonoppholdTilRegel.map(delresultat),
+            vurderFra = periode.fom,
+        ))
+
         assertTidslinje(
             straffegjennomføringVilkårsvurderinger,
 
             //Soner ikke
-            Periode(LocalDate.of(2024, 1, 1), (LocalDate.of(2024, 1, 5))) to {
+            Periode(1 januar 2024, 5 januar 2024) to {
                 assertThat(it.utfall).isEqualTo(Utfall.OPPFYLT)
                 assertThat(it.manuellVurdering).isEqualTo(false)
             },
 
             //Formue under forvaltning og soner i fengsel
-            Periode(LocalDate.of(2024, 1, 6), (LocalDate.of(2024, 1, 15))) to {
+            Periode(6 januar 2024, 15 januar 2024) to {
                 assertThat(it.utfall).isEqualTo(Utfall.IKKE_OPPFYLT)
                 assertThat(it.avslagsårsak).isEqualTo(Avslagsårsak.IKKE_RETT_UNDER_STRAFFEGJENNOMFØRING)
                 assertThat(it.manuellVurdering).isEqualTo(true)
             },
 
             // Arbeider utenfor anstalten og soner i ved frigang
-            Periode(LocalDate.of(2024, 1, 16), (LocalDate.of(2024, 12, 1))) to {
+            Periode(16 januar 2024, 1 desember 2024) to {
                 assertThat(it.utfall).isEqualTo(Utfall.OPPFYLT)
                 assertThat(it.manuellVurdering).isEqualTo(true)
             },
 
             // Automatisk oppfylt i fravær av soningsopphold
-            Periode(LocalDate.of(2024, 12, 2), Tid.MAKS) to {
+            Periode(2 desember 2024, Tid.MAKS) to {
                 assertThat(it.utfall).isEqualTo(Utfall.OPPFYLT)
                 assertThat(it.manuellVurdering).isEqualTo(false)
             }
