e65e5a9004cae6d57b6b67319b82660de09116d6
Arild Lillegård <arild.lillegard@nav.no>
2026-09-04 17:07:00 +0200

    AAP-2317: Bruker flere typer fra kontrakt istedet
    Bytter ut flere dupliserte typer med typer hentet fra kontrakt/
    dokumentinnhenting.

--- api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/behandlerdialog/MeldingMedDokumenterDto.kt
+++ api/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/behandlerdialog/MeldingMedDokumenterDto.kt
@@ -1,6 +1,6 @@
 package no.nav.aap.behandlingsflyt.behandling.behandlerdialog
 
 data class MeldingMedDokumenterDto(
-    val dialogmelding: MeldingDto,
+    val melding: MeldingDto,
     val dokumentIdListe: List<DokumentInfoDto>,
 )
