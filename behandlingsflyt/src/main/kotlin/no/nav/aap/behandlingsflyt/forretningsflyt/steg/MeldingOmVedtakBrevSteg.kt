e5b9ba32ccfe8b58bae0649ba1b2dbe45d669c63
Øivind Stensrud <93574268+steoiv@users.noreply.github.com>
2026-09-08 07:24:35 +0200

    Bytter contains med in
    Co-authored-by: Peter Brottveit Bock <peter.brottveit.bock@nav.no>

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/MeldingOmVedtakBrevSteg.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/forretningsflyt/steg/MeldingOmVedtakBrevSteg.kt
@@ -189,7 +189,7 @@ class MeldingOmVedtakBrevSteg(
     private fun brukBrevbyggerTogglePåBrevtypeOgBrukerident(typeBrev: TypeBrev, bruker: Bruker): Boolean {
         val brukereSomSkalBrukeBrevbygger = listOf("B101286", "B171715", "L168492", "S128047", "S132438", "H136900", "J158692", "R152079")
         val brevtyperSomSkalBrukeBrevbygger = listOf(TypeBrev.VEDTAK_INNVILGELSE)
-        return brevtyperSomSkalBrukeBrevbygger.contains(typeBrev) && brukereSomSkalBrukeBrevbygger.contains(bruker.ident)
+        return typeBrev in brevtyperSomSkalBrukeBrevbygger && bruker.ident in brukereSomSkalBrukeBrevbygger
     }
 
     companion object : FlytSteg {
