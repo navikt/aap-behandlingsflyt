25143c96eef7c0309ff850219fa996a218fca72b
Henrik Gundersen <henrik.gundersen@nav.no>
2026-09-22 14:13:10 +0200

    Fikset test

--- behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/behandling/vilkår/bistand/BistandsvilkåretTest.kt
+++ behandlingsflyt/src/test/kotlin/no/nav/aap/behandlingsflyt/behandling/vilkår/bistand/BistandsvilkåretTest.kt
@@ -32,6 +32,7 @@ import no.nav.aap.behandlingsflyt.sakogbehandling.flyt.VurderingType
 import no.nav.aap.behandlingsflyt.sakogbehandling.flyt.Vurderingsbehov
 import no.nav.aap.behandlingsflyt.sakogbehandling.sak.Sak
 import no.nav.aap.behandlingsflyt.test.AlleAvskruddUnleash
+import no.nav.aap.behandlingsflyt.test.FakeArenaOppslagGateway
 import no.nav.aap.behandlingsflyt.test.januar
 import no.nav.aap.komponenter.dbconnect.DBConnection
 import no.nav.aap.komponenter.dbconnect.transaction
@@ -64,6 +65,7 @@ class BistandsvilkåretTest {
 
     private val gatewayProvider = createGatewayProvider {
         register<AlleAvskruddUnleash>()
+        register<FakeArenaOppslagGateway>()
     }
 
     @Test
