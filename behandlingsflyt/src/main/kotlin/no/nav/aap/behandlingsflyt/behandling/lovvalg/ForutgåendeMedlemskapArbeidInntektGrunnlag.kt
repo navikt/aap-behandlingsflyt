2234096b9bb65c3d9f3f9e0684922733ac4d3a83
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2026-09-22 16:14:16 +0200

    AAP-BAU Bruk felles metode for å lage medlemskap-tidslinje (#3521)

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/lovvalg/ForutgåendeMedlemskapArbeidInntektGrunnlag.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/lovvalg/ForutgåendeMedlemskapArbeidInntektGrunnlag.kt
@@ -5,11 +5,8 @@ import no.nav.aap.behandlingsflyt.faktagrunnlag.lovvalgmedlemskap.ManuellVurderi
 import no.nav.aap.behandlingsflyt.faktagrunnlag.lovvalgmedlemskap.utenlandsopphold.UtenlandsOppholdData
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.medlemskap.MedlemskapUnntakGrunnlag
 import no.nav.aap.behandlingsflyt.faktagrunnlag.register.personopplysninger.PersonopplysningMedHistorikkGrunnlag
+import no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.gjeldendeVurderinger
 import no.nav.aap.komponenter.tidslinje.Tidslinje
-import no.nav.aap.komponenter.tidslinje.somTidslinje
-import no.nav.aap.komponenter.type.Periode
-import no.nav.aap.komponenter.verdityper.Tid
-import java.time.LocalDate
 
 data class ForutgåendeMedlemskapGrunnlag(
     val medlemskapArbeidInntektGrunnlag: ForutgåendeMedlemskapArbeidInntektGrunnlag?,
@@ -23,13 +20,7 @@ data class ForutgåendeMedlemskapArbeidInntektGrunnlag(
     val arbeiderINorgeGrunnlag: List<ArbeidINorgeGrunnlag>,
     val vurderinger: List<ManuellVurderingForForutgåendeMedlemskap>,
 ) {
-    fun gjeldendeVurderinger(maksDato: LocalDate = Tid.MAKS): Tidslinje<ManuellVurderingForForutgåendeMedlemskap> {
-        return vurderinger.tilTidslinje(maksDato)
+    fun gjeldendeVurderinger(): Tidslinje<ManuellVurderingForForutgåendeMedlemskap> {
+        return vurderinger.gjeldendeVurderinger()
     }
 }
-
-fun List<ManuellVurderingForForutgåendeMedlemskap>.tilTidslinje(maksDato: LocalDate = Tid.MAKS): Tidslinje<ManuellVurderingForForutgåendeMedlemskap> =
-    sortedBy { it.vurdertTidspunkt }
-        .somTidslinje { Periode(it.fom, it.tom ?: Tid.MAKS) }
-        .komprimer()
-        .begrensetTil(Periode(Tid.MIN, maksDato))
