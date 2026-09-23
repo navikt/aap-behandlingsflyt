1943a69d25e63cce58a236da375b8f14c5622268
Fredrik Meyer <fredrik.meyer@nav.no>
2026-09-23 08:32:41 +0200

    Etter review, forenkle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,8 +56,19 @@ tasks.register<JavaExec>("runTestAppIntegrated") {
     environment("NAIS_DATABASE_BEHANDLINGSFLYT_BEHANDLINGSFLYT_USERNAME", "postgres")
     environment("NAIS_DATABASE_BEHANDLINGSFLYT_BEHANDLINGSFLYT_PASSWORD", "")
     environment("INTEGRASJON_OPPGAVESTYRING_URL", "http://localhost:8084")
-    environment("INTEGRAJSON_POSTMOTTAK_AZP","c62cff74-505a-4858-ac15-16061c2e8290")
     environment("INTEGRASJON_BREV_URL", "http://localhost:8082")
+}
+
+tasks.register<JavaExec>("runTestAppMotOppgaveOgPostmottak") {
+    group = "application"
+    description = "Kjør TestApp mot Oppgave og Postmottak. Forventer at db kjører på port 5438, og oppgave-app på port 8084."
+    classpath = sourceSets.test.get().runtimeClasspath
+    mainClass.set("no.nav.aap.behandlingsflyt.TestAppKt")
+    environment("NAIS_CLUSTER_NAME", "LOCAL")
+    environment("NAIS_DATABASE_BEHANDLINGSFLYT_BEHANDLINGSFLYT_JDBC_URL", "jdbc:postgresql://localhost:5438/postgres")
+    environment("NAIS_DATABASE_BEHANDLINGSFLYT_BEHANDLINGSFLYT_USERNAME", "postgres")
+    environment("NAIS_DATABASE_BEHANDLINGSFLYT_BEHANDLINGSFLYT_PASSWORD", "")
+    environment("INTEGRASJON_OPPGAVESTYRING_URL", "http://localhost:8084")
 }
 
 tasks.register<JavaExec>("genererOpenApiJson") {
