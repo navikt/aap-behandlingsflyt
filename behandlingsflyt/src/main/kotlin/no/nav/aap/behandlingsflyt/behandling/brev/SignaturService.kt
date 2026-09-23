3df191ed153f3f818ad48b04bc5e0417caf9520a
Hein Haraldsen <hein.haraldsen@nav.no>
2026-09-23 16:11:22 +0200

    AAP-BAU Legger til LoggerFactory i SignaturService

--- behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/brev/SignaturService.kt
+++ behandlingsflyt/src/main/kotlin/no/nav/aap/behandlingsflyt/behandling/brev/SignaturService.kt
@@ -17,6 +17,7 @@ import no.nav.aap.komponenter.gateway.GatewayProvider
 import no.nav.aap.komponenter.verdityper.Bruker
 import no.nav.aap.lookup.repository.RepositoryProvider
 import no.nav.aap.tilgang.Rolle
+import org.slf4j.LoggerFactory
 import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Status as AvklaringsbehovStatus
 import no.nav.aap.brev.kontrakt.Rolle as SignaturRolle
 
